681459042fb2a9d72727479608a4b0e893eda1fb
Lenik <xjl@99jsj.com>
2010-07-03 10:04:39 +0000

    more aliases

--- deployment/root.d/eci-secca/etc/profile.d/lenik.sh
+++ deployment/root.d/eci-secca/etc/profile.d/lenik.sh
@@ -1,15 +1,40 @@
 #!/bin/bash
 
-alias d='ls -FANl --color=auto'
+alias d='ls -FNl --color=auto'
 alias dt='d -t | tail'
-alias ps='COLUMNS=1000 ps fx'
+alias dd='d -d'
+
+alias cd..='cd ..'
+alias cd...='cd ../..'
+alias cd....='cd ../../..'
+
+function cdd() {
+    cd "$1"
+    shift
+    if [ $# = 0 ]; then
+        d
+    else
+        for a in "$@"; do
+            d "$a"
+        done
+    fi
+}
+alias ccd=cdd
 
 function hello() {
 	echo Hello, this is Lenik.
 }
 
 export EDITOR=vi
-export PATH=$PATH:~/bin:~/sbin
+export PATH=$PATH:/lam/kala/bin:/lam/kala/sbin:~/bin:~/sbin
 
+export LAPIOTA=/lam/kala
+export LAM_KALA=/lam/kala
 
+alias ps='COLUMNS=1000 ps fx'
 alias up='svn update --ignore-externals'
+
+alias du1='du --max-depth=1'
+alias du2='du --max-depth=2'
+
+alias sci='scilab -nwni'
