7e66eeb250329cfcd75d7d14148f6a3f7b145091
Lenik <xjl@99jsj.com>
2010-09-09 02:30:21 +0000

    install glassfish by -unix.sh with answer file.

--- deployment/node/sdu/sdu-opt-glassfish3/post-install.sh
+++ deployment/node/sdu/sdu-opt-glassfish3/post-install.sh
@@ -1,24 +1,61 @@
-#!/bin/sh
+#!/bin/bash
+
+srcfile=`readlink -f $0`
+srcdir="${srcfile%/*}"
 
 ver=3.0.1
 instdir=/opt/java/glassfish-$ver
 
 cd /opt/java
 
-if [ ! -d $instdir ]; then
+if [ ! -d $instdir/glassfish/bin ]; then
 
-    if ! file=`wgetc http://download.java.net/glassfish/$ver/release/glassfish-$ver.zip`; then
-        echo "Failed to download glassfish $ver"
-        exit 1
-    fi
+    mode=sh
 
-    if ! unzip "$file"; then
-        echo "Unzip failed. "
-        exit 1
-    fi
+    case "$mode" in
+    sh)
+        if ! file_sh=`wgetc http://download.java.net/glassfish/$ver/release/glassfish-$ver-unix.sh`; then
+            echo "Failed to download self-extract archive"
+            exit 1
+        fi
+        chmod a+x "$file_sh"
 
-    mv glassfishv3 glassfish-$ver
+        # -a <answerfile>
+        # -l <Log Directory>
+        # -q Logging level set to WARNING
+        # -v Logging level set to FINEST
+        # -s run this application silent mode
+        # -j <javahome>
+        # -n <savefile>
+        answer_file=$srcdir/glassfishv3.ans
+        if [ ! -f $answer_file ]; then
+            echo "answer-file $answer_file doesn't exist. "
+            exit 1
+        fi
+        if ! "$file_sh" -s -a "$answer_file"; then
+            echo "Failed to install glassfish"
+            exit 1
+        fi
+        ;;
+
+    zip)
+        if ! file=`wgetc http://download.java.net/glassfish/$ver/release/glassfish-$ver.zip`; then
+            echo "Failed to download zip archive"
+            exit 1
+        fi
+
+        if ! unzip "$file"; then
+            echo "Unzip failed. "
+            exit 1
+        fi
+
+        mv glassfishv3 glassfish-$ver
+        ;;
+    esac
 
 fi
 
 ln -snf glassfish-$ver glassfish
+
+# install/upgrade the pkg utility
+# glassfish/bin/pkg
