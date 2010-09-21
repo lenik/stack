cc4dc9fea974a310c21501c653fb9c46c599df7b
Lenik <xjl@99jsj.com>
2010-09-21 00:57:11 +0000

    remove post-remove.

--- deployment/node/sdu/sdu-opt-glassfish3/pre-remove.sh
+++ deployment/node/sdu/sdu-opt-glassfish3/pre-remove.sh
@@ -2,3 +2,16 @@
 
 instdir=/opt/java/glassfish-3.0.1
 
+if [ -d $instdir ]; then
+
+    echo "Remove $instdir (exclude domains/*)"
+        # Don't remove domains:
+        # rm -fr $instdir
+        find $instdir -depth -path '*/domains/*' -prune -o -delete
+
+    echo "Remove symlink /opt/java/glassfish"
+    rm -f  /opt/java/glassfish
+
+fi
+
+exit 0
