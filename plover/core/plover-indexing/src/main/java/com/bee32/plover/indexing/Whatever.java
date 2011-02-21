a72d73c588666df8359c414cf73ca65bd029b42a
Lenik <xjl@99jsj.com>
2011-02-21 17:37:25 +0800

    Think about Indexing...

--- plover/core/plover-indexing/src/main/java/com/bee32/plover/indexing/Whatever.java
+++ plover/core/plover-indexing/src/main/java/com/bee32/plover/indexing/Whatever.java
@@ -1,5 +1,8 @@
 package com.bee32.plover.indexing;
 
+import org.hibernate.criterion.DetachedCriteria;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 
 class ModelRequest {
 }
@@ -24,12 +27,38 @@ interface IHtmlDisplay
         extends IDisplay {
 }
 
-class UserApp {
-    void listToVerifyActivities() {
+class UserApp
+        extends HibernateDaoSupport {
+
+    private int userId;
+
+    void listToVerifyActivities_OneMan() {
         // Activity is-a Indexable
         // ActivityIndexInfo
 
-
-        Activity
+        DetachedCriteria criteria = DetachedCriteria.forClass(Activity.class);
+        criteria.add(Restrictions.like("post", "man=" + userId));
+        criteria.getExecutableCriteria(getSession()).list();
     }
+
+}
+
+class Activity {
+
+    void verify(String oneMan) {
+        setState(VERIFIED, oneMan, message);
+    }
+
+}
+
+// Nothing related to the message...
+class VInfo {
+
+    boolean state;
+
+//    (state 1)
+//    (step jerry_tom 8432847 Some_messages_left_for_checking)
+//    (step Some_one_Else 8432847 \LMore\R_messages)
+//     The most recent month.
+
 }
