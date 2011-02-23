4903d0f521b71b454a6340eb3dff200f3f650a2c
Lenik <xjl@99jsj.com>
2011-02-23 23:23:43 +0800

    Rewrite SEMOids.

---
+++ sem/base/sem-module/src/main/java/com/bee32/sem/SEMOids.java
@@ -0,0 +1,41 @@
+package com.bee32.sem;
+
+public interface SEMOids {
+
+    int[] ROOT = { 3, 15 };
+
+    int base = 1;
+    int process = 2;
+    int resource = 3;
+    int social = 4;
+    int support = 5;
+
+    int baseFrame = 1;
+    int baseModule = 2;
+    int baseDesktop = 3;
+    int baseUser = 4;
+    int basePeople = 5;
+    int baseFile = 6;
+    int baseMail = 7;
+    int baseEvent = 8;
+    int baseBank = 9;
+
+    int processProcess = 1;
+    int processTechproc = 2;
+    int processWorkflow = 3;
+
+    int resourceStore = 1;
+    int resourceThing = 2;
+    int resourceAsset = 3;
+    int resourceContract = 4;
+
+    int socialStaff = 1;
+    int socialCustomer = 2;
+    int socialSupplier = 3;
+    int socialPartner = 4;
+
+    int supportCalendar = 1;
+    int supportTrack = 2;
+    int supportKb = 3;
+
+}
