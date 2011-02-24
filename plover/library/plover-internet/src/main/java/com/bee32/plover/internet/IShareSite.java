088f302418f3c0f9db98f028e37721f65bdea853
Lenik <xjl@99jsj.com>
2011-02-24 08:40:39 +0800

    Extension point for test.

--- plover/library/plover-internet/src/main/java/com/bee32/plover/internet/IShareSite.java
+++ plover/library/plover-internet/src/main/java/com/bee32/plover/internet/IShareSite.java
@@ -3,9 +3,10 @@ package com.bee32.plover.internet;
 import java.net.URL;
 
 import com.bee32.plover.arch.IComponent;
+import com.bee32.plover.arch.extension.IExtensionPoint;
 
 public interface IShareSite
-        extends IComponent {
+        extends IComponent, IExtensionPoint {
 
     URL getLogoURL();
 
