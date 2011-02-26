be68d8e292003f64d49186afa51ef82fa3082241
Lenik <xjl@99jsj.com>
2011-02-26 12:38:11 +0800

    Prepare for initial m2 deployment.

--- plover/core/plover-indexing/src/main/java/com/bee32/plover/indexing/Whatever.java
+++ plover/core/plover-indexing/src/main/java/com/bee32/plover/indexing/Whatever.java
@@ -1,9 +1,5 @@
 package com.bee32.plover.indexing;
 
-import org.hibernate.criterion.DetachedCriteria;
-import org.hibernate.criterion.Restrictions;
-import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
-
 class ModelRequest {
 }
 
@@ -25,40 +21,4 @@ interface IDisplay {
 
 interface IHtmlDisplay
         extends IDisplay {
-}
-
-class UserApp
-        extends HibernateDaoSupport {
-
-    private int userId;
-
-    void listToVerifyActivities_OneMan() {
-        // Activity is-a Indexable
-        // ActivityIndexInfo
-
-        DetachedCriteria criteria = DetachedCriteria.forClass(Activity.class);
-        criteria.add(Restrictions.like("post", "man=" + userId));
-        criteria.getExecutableCriteria(getSession()).list();
-    }
-
-}
-
-class Activity {
-
-    void verify(String oneMan) {
-        setState(VERIFIED, oneMan, message);
-    }
-
-}
-
-// Nothing related to the message...
-class VInfo {
-
-    boolean state;
-
-    // (state 1)
-    // (step jerry_tom 8432847 Some_messages_left_for_checking)
-    // (step Some_one_Else 8432847 \LMore\R_messages)
-    // The most recent month.
-
 }
