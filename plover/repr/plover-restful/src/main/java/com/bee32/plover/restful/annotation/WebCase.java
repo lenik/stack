92359796ec799de51ae5c0c035e4a6416fa918c9
Lenik <xjl@99jsj.com>
2011-03-07 04:16:41 +0800

    Add WebCase/Page for reference.

---
+++ plover/repr/plover-restful/src/main/java/com/bee32/plover/restful/annotation/WebCase.java
@@ -0,0 +1,5 @@
+package com.bee32.plover.restful.annotation;
+
+public interface WebCase {
+
+}
---
+++ plover/repr/plover-restful/src/main/java/com/bee32/plover/restful/annotation/WebPage.java
@@ -0,0 +1,7 @@
+package com.bee32.plover.restful.annotation;
+
+public @interface WebPage {
+
+    Class<? extends WebCase> value();
+
+}
