a91033fc32d413d75b4f4a99e92c5c15f4546fec
Lenik <xjl@99jsj.com>
2011-03-07 04:16:02 +0800

    Add getScalar/Array() to support servlet-request wrapping.

--- plover/core/plover-arch/src/main/java/com/bee32/plover/arch/operation/IOperationContext.java
+++ plover/core/plover-arch/src/main/java/com/bee32/plover/arch/operation/IOperationContext.java
@@ -4,15 +4,30 @@ import java.util.Map;
 
 import com.bee32.plover.inject.IContainer;
 
+/**
+ * The operation context is designed to be reusable.
+ */
 public interface IOperationContext
         extends IContainer, Map<String, Object> {
 
     String getPath();
 
-    Object get(int index);
+    void setPath(String path);
 
     IResultOutput getResultOutput();
 
+    void setResultOutput(IResultOutput resultOutput);
+
+    Object get(int index);
+
     // Logger getLogger();
+
+    Object getReturnValue();
+
+    void setReturnValue(Object returnValue);
+
+    <T> T getScalar(String key);
+
+    <T> T[] getArray(String key);
 
 }
