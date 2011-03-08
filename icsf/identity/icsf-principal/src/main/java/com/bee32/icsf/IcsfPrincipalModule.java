902e8b30b1c21daa191ea062e8692de08e17b516
Lenik <xjl@99jsj.com>
2011-03-08 21:52:17 +0800

    Export principal *Dao(s).

--- icsf/identity/icsf-principal/src/main/java/com/bee32/icsf/IcsfPrincipalModule.java
+++ icsf/identity/icsf-principal/src/main/java/com/bee32/icsf/IcsfPrincipalModule.java
@@ -1,23 +1,39 @@
 package com.bee32.icsf;
 
-import com.bee32.icsf.principal.GroupBean;
-import com.bee32.icsf.principal.RoleBean;
-import com.bee32.icsf.principal.UserBean;
-import com.bee32.icsf.principal.realm.RealmBean;
+import javax.inject.Inject;
+
+import org.springframework.context.annotation.Configuration;
+
+import com.bee32.icsf.principal.dao.GroupDao;
+import com.bee32.icsf.principal.dao.RealmDao;
+import com.bee32.icsf.principal.dao.RoleDao;
+import com.bee32.icsf.principal.dao.UserDao;
 import com.bee32.plover.orm.util.ERModule;
 import com.bee32.plover.pub.oid.Oid;
 
-// XXX Fix the oid.
+@Configuration
 @Oid({ 3, 7, IcsfOids.principal })
 public class IcsfPrincipalModule
         extends ERModule {
 
+    @Inject
+    private UserDao userDao;
+
+    @Inject
+    private GroupDao groupDao;
+
+    @Inject
+    private RoleDao roleDao;
+
+    @Inject
+    private RealmDao realmDao;
+
     @Override
     protected void preamble() {
-        exportEntity(UserBean.class, Long.class);
-        exportEntity(GroupBean.class, Integer.class);
-        exportEntity(RoleBean.class, Integer.class);
-        exportEntity(RealmBean.class, Integer.class);
+        export(userDao);
+        export(groupDao);
+        export(roleDao);
+        export(realmDao);
     }
 
 }
