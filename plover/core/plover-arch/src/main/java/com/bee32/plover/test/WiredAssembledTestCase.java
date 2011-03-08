0ca22669f4caacb85d66a774e70d9e4362413ad6
Lenik <xjl@99jsj.com>
2011-03-09 02:32:19 +0800

    Add WTC/AWTC.createBean() programatic factory support.

--- plover/core/plover-arch/src/main/java/com/bee32/plover/test/WiredAssembledTestCase.java
+++ plover/core/plover-arch/src/main/java/com/bee32/plover/test/WiredAssembledTestCase.java
@@ -1,10 +1,39 @@
 package com.bee32.plover.test;
 
+import javax.inject.Inject;
+
 import org.junit.runner.RunWith;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-public class WiredAssembledTestCase
-        extends AssembledTestCase {
+@ContextConfiguration("component-scan.xml")
+public abstract class WiredAssembledTestCase
+        extends AssembledTestCase
+        implements InitializingBean {
+
+    @Inject
+    private ApplicationContext context;
+
+    private AutowireCapableBeanFactory autowireCapableBeanFactory;
+
+    public void afterPropertiesSet() {
+        // context.getAutowireCapableBeanFactory();
+    }
+
+    protected <T> T createBean(Class<T> beanClass)
+            throws BeansException {
+        return autowireCapableBeanFactory.createBean(beanClass);
+    }
+
+    protected <T> T createBean(Class<T> beanClass, int autowireMode, boolean dependencyCheck)
+            throws BeansException {
+        Object bean = autowireCapableBeanFactory.createBean(beanClass, autowireMode, dependencyCheck);
+        return beanClass.cast(bean);
+    }
 
 }
--- plover/core/plover-arch/src/main/java/com/bee32/plover/test/WiredTestCase.java
+++ plover/core/plover-arch/src/main/java/com/bee32/plover/test/WiredTestCase.java
@@ -1,13 +1,38 @@
 package com.bee32.plover.test;
 
+import javax.inject.Inject;
+
 import org.junit.Assert;
 import org.junit.runner.RunWith;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("component-scan.xml")
-public class WiredTestCase
+public abstract class WiredTestCase
         extends Assert {
+
+    @Inject
+    private ApplicationContext context;
+
+    private AutowireCapableBeanFactory autowireCapableBeanFactory;
+
+    public void afterPropertiesSet() {
+        // context.getAutowireCapableBeanFactory();
+    }
+
+    protected <T> T createBean(Class<T> beanClass)
+            throws BeansException {
+        return autowireCapableBeanFactory.createBean(beanClass);
+    }
+
+    protected <T> T createBean(Class<T> beanClass, int autowireMode, boolean dependencyCheck)
+            throws BeansException {
+        Object bean = autowireCapableBeanFactory.createBean(beanClass, autowireMode, dependencyCheck);
+        return beanClass.cast(bean);
+    }
 
 }
