15301ed2c523759bf5d8233ed2956753fba3649f
Lenik <xjl@99jsj.com>
2011-03-08 21:45:18 +0800

    Add default component-scan behavior to WiredTestCase.
    Add new WiredATC too.

--- plover/core/plover-arch/src/main/java/com/bee32/plover/test/WiredTestCase.java
+++ plover/core/plover-arch/src/main/java/com/bee32/plover/test/WiredTestCase.java
@@ -2,9 +2,11 @@ package com.bee32.plover.test;
 
 import org.junit.Assert;
 import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("component-scan.xml")
 public class WiredTestCase
         extends Assert {
 
