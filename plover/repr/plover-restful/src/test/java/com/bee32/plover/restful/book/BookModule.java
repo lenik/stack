1d6bef1b0fe4e59162e60c4fbd847c443b1ba3a1
Lenik <xjl@99jsj.com>
2011-03-08 12:35:53 +0800

    Has moved preamble() to Module.

--- plover/repr/plover-restful/src/test/java/com/bee32/plover/restful/book/BookModule.java
+++ plover/repr/plover-restful/src/test/java/com/bee32/plover/restful/book/BookModule.java
@@ -1,14 +1,15 @@
 package com.bee32.plover.restful.book;
 
-import com.bee32.plover.arch.Module;
+import com.bee32.plover.orm.util.ERModule;
 import com.bee32.plover.pub.oid.Oid;
 import com.bee32.plover.pub.oid.PloverOids;
 
 @Oid({ 3, 12, PloverOids.repr, PloverOids.reprRestful, 10001 })
 public class BookModule
-        extends Module {
+        extends ERModule {
 
-    public BookModule() {
+    @Override
+    protected void preamble() {
         declare("book", SimpleBooks.store);
     }
 
