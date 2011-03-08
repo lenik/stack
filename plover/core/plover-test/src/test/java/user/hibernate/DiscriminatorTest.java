a40944f4fc03a981ce57c555dd57f3b178ffafed
Lenik <xjl@99jsj.com>
2011-03-08 12:36:25 +0800

    Test list() instances of abstract type.

--- plover/core/plover-test/src/test/java/user/hibernate/DiscriminatorTest.java
+++ plover/core/plover-test/src/test/java/user/hibernate/DiscriminatorTest.java
@@ -1,9 +1,11 @@
 package user.hibernate;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.util.List;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import com.bee32.plover.orm.dao.HibernateTemplate;
@@ -17,6 +19,18 @@ public class DiscriminatorTest
     }
 
     @Test
+    public void listAllColors() {
+        HibernateTemplate template = getHibernateTemplate();
+
+        for (Color color : ColorSystem.getPredefinedColors())
+            template.save(color);
+
+        List<Color> list = template.loadAll(Color.class);
+        assertTrue(list.contains(ColorSystem.black));
+    }
+
+    @Test
+    @Ignore
     public void listCMYKOnly() {
         HibernateTemplate template = getHibernateTemplate();
 
