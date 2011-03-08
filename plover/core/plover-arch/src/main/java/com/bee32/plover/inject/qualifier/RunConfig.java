d2fbe880f53a0534996c2955025f585a3a6bdd64
Lenik <xjl@99jsj.com>
2011-03-09 02:29:54 +0800

    Add qualifiers: RunConfig and Variant.

---
+++ plover/core/plover-arch/src/main/java/com/bee32/plover/inject/qualifier/RunConfig.java
@@ -0,0 +1,17 @@
+package com.bee32.plover.inject.qualifier;
+
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+
+import javax.inject.Qualifier;
+
+@Retention(RUNTIME)
+@Documented
+@Qualifier
+public @interface RunConfig {
+
+    String value();
+
+}
