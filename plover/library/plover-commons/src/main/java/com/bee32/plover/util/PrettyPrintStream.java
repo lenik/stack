759a94f00fa0df8746c791dda51bd2c8ff64d8e1
Lenik <xjl@99jsj.com>
2011-03-11 18:25:41 +0800

    Add PrettyPrintStream used by IFormatString.

---
+++ plover/library/plover-commons/src/main/java/com/bee32/plover/util/PrettyPrintStream.java
@@ -0,0 +1,173 @@
+package com.bee32.plover.util;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.nio.CharBuffer;
+import java.util.Locale;
+
+import javax.free.BCharOut;
+import javax.free.ITextIndention;
+import javax.free.IndentedOutImpl;
+
+public class PrettyPrintStream {
+
+    private BCharOut buf;
+    private IndentedOutImpl out;
+
+    public PrettyPrintStream() {
+        buf = new BCharOut();
+        out = new IndentedOutImpl(buf);
+    }
+
+    public int enter() {
+        return out.enter();
+    }
+
+    public boolean equals(Object obj) {
+        return out.equals(obj);
+    }
+
+    public ITextIndention getTextIndention() {
+        return out.getTextIndention();
+    }
+
+    public int leave() {
+        return out.leave();
+    }
+
+    public void print(boolean b) {
+        out.print(b);
+    }
+
+    public void print(char c) {
+        out.print(c);
+    }
+
+    public void print(char[] s) {
+        out.print(s);
+    }
+
+    public void print(double d) {
+        out.print(d);
+    }
+
+    public void print(float f) {
+        out.print(f);
+    }
+
+    public void print(int i) {
+        out.print(i);
+    }
+
+    public void print(long l) {
+        out.print(l);
+    }
+
+    public void print(Object... args) {
+        out.print(args);
+    }
+
+    public void print(Object obj) {
+        out.print(obj);
+    }
+
+    public void print(String s) {
+        out.print(s);
+    }
+
+    public void printf(Locale l, String format, Object... args) {
+        out.printf(l, format, args);
+    }
+
+    public void printf(String format, Object... args) {
+        out.printf(format, args);
+    }
+
+    public void println() {
+        out.println();
+    }
+
+    public void println(boolean x) {
+        out.println(x);
+    }
+
+    public void println(char x) {
+        out.println(x);
+    }
+
+    public void println(char[] x) {
+        out.println(x);
+    }
+
+    public void println(double x) {
+        out.println(x);
+    }
+
+    public void println(float x) {
+        out.println(x);
+    }
+
+    public void println(int x) {
+        out.println(x);
+    }
+
+    public void println(long x) {
+        out.println(x);
+    }
+
+    public void println(Object... args) {
+        out.println(args);
+    }
+
+    public void println(Object x) {
+        out.println(x);
+    }
+
+    public void println(String x) {
+        out.println(x);
+    }
+
+    public Writer toWriter() {
+        return out.toWriter();
+    }
+
+    public void write(char[] chars, int off, int len)
+            throws IOException {
+        out.write(chars, off, len);
+    }
+
+    public void write(char[] chars)
+            throws IOException {
+        out.write(chars);
+    }
+
+    public void write(CharBuffer charBuffer)
+            throws IOException {
+        out.write(charBuffer);
+    }
+
+    public void write(CharSequence chars, int off, int len)
+            throws IOException {
+        out.write(chars, off, len);
+    }
+
+    public void write(int c)
+            throws IOException {
+        out.write(c);
+    }
+
+    public void write(String string, int off, int len)
+            throws IOException {
+        out.write(string, off, len);
+    }
+
+    public void write(String s)
+            throws IOException {
+        out.write(s);
+    }
+
+    public String toString() {
+        return buf.toString();
+    }
+
+}
