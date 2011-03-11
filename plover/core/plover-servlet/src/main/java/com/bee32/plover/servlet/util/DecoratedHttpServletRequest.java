5d26ed5fb224b1316d637aaf22a90d51faa450ce
Lenik <xjl@99jsj.com>
2011-03-11 18:24:51 +0800

    Add missing @Overrride's.

--- plover/core/plover-servlet/src/main/java/com/bee32/plover/servlet/util/DecoratedHttpServletRequest.java
+++ plover/core/plover-servlet/src/main/java/com/bee32/plover/servlet/util/DecoratedHttpServletRequest.java
@@ -28,229 +28,283 @@ public class DecoratedHttpServletRequest
         super(proxy);
     }
 
+    @Override
     public Object getAttribute(String name) {
         return proxy.getAttribute(name);
     }
 
+    @Override
     public String getAuthType() {
         return proxy.getAuthType();
     }
 
+    @Override
     public Cookie[] getCookies() {
         return proxy.getCookies();
     }
 
     @SuppressWarnings("unchecked")
+    @Override
     public Enumeration<String> getAttributeNames() {
         return proxy.getAttributeNames();
     }
 
+    @Override
     public long getDateHeader(String name) {
         return proxy.getDateHeader(name);
     }
 
+    @Override
     public String getCharacterEncoding() {
         return proxy.getCharacterEncoding();
     }
 
+    @Override
     public void setCharacterEncoding(String env)
             throws UnsupportedEncodingException {
         proxy.setCharacterEncoding(env);
     }
 
+    @Override
     public String getHeader(String name) {
         return proxy.getHeader(name);
     }
 
+    @Override
     public int getContentLength() {
         return proxy.getContentLength();
     }
 
+    @Override
     public String getContentType() {
         return proxy.getContentType();
     }
 
     @SuppressWarnings("unchecked")
+    @Override
     public Enumeration<String> getHeaders(String name) {
         return proxy.getHeaders(name);
     }
 
+    @Override
     public ServletInputStream getInputStream()
             throws IOException {
         return proxy.getInputStream();
     }
 
+    @Override
     public String getParameter(String name) {
         return proxy.getParameter(name);
     }
 
     @SuppressWarnings("unchecked")
+    @Override
     public Enumeration<String> getHeaderNames() {
         return proxy.getHeaderNames();
     }
 
+    @Override
     public int getIntHeader(String name) {
         return proxy.getIntHeader(name);
     }
 
     @SuppressWarnings("unchecked")
+    @Override
     public Enumeration<String> getParameterNames() {
         return proxy.getParameterNames();
     }
 
+    @Override
     public String getMethod() {
         return proxy.getMethod();
     }
 
+    @Override
     public String[] getParameterValues(String name) {
         return proxy.getParameterValues(name);
     }
 
+    @Override
     public String getPathInfo() {
         return proxy.getPathInfo();
     }
 
     @SuppressWarnings("unchecked")
+    @Override
     public Map<String, String[]> getParameterMap() {
         return proxy.getParameterMap();
     }
 
+    @Override
     public String getProtocol() {
         return proxy.getProtocol();
     }
 
+    @Override
     public String getPathTranslated() {
         return proxy.getPathTranslated();
     }
 
+    @Override
     public String getScheme() {
         return proxy.getScheme();
     }
 
+    @Override
     public String getContextPath() {
         return proxy.getContextPath();
     }
 
+    @Override
     public String getServerName() {
         return proxy.getServerName();
     }
 
+    @Override
     public int getServerPort() {
         return proxy.getServerPort();
     }
 
+    @Override
     public BufferedReader getReader()
             throws IOException {
         return proxy.getReader();
     }
 
+    @Override
     public String getQueryString() {
         return proxy.getQueryString();
     }
 
+    @Override
     public String getRemoteUser() {
         return proxy.getRemoteUser();
     }
 
+    @Override
     public String getRemoteAddr() {
         return proxy.getRemoteAddr();
     }
 
+    @Override
     public String getRemoteHost() {
         return proxy.getRemoteHost();
     }
 
+    @Override
     public boolean isUserInRole(String role) {
         return proxy.isUserInRole(role);
     }
 
+    @Override
     public void setAttribute(String name, Object o) {
         proxy.setAttribute(name, o);
     }
 
+    @Override
     public Principal getUserPrincipal() {
         return proxy.getUserPrincipal();
     }
 
+    @Override
     public String getRequestedSessionId() {
         return proxy.getRequestedSessionId();
     }
 
+    @Override
     public void removeAttribute(String name) {
         proxy.removeAttribute(name);
     }
 
+    @Override
     public String getRequestURI() {
         return proxy.getRequestURI();
     }
 
+    @Override
     public Locale getLocale() {
         return proxy.getLocale();
     }
 
     @SuppressWarnings("unchecked")
+    @Override
     public Enumeration<Locale> getLocales() {
         return proxy.getLocales();
     }
 
-    public StringBuilder getRequestURL() {
+    @Override
+    public StringBuffer getRequestURL() {
         return proxy.getRequestURL();
     }
 
+    @Override
     public boolean isSecure() {
         return proxy.isSecure();
     }
 
+    @Override
     public RequestDispatcher getRequestDispatcher(String path) {
         return proxy.getRequestDispatcher(path);
     }
 
+    @Override
     public String getServletPath() {
         return proxy.getServletPath();
     }
 
+    @Override
     public HttpSession getSession(boolean create) {
         return proxy.getSession(create);
     }
 
     @Deprecated
+    @Override
     public String getRealPath(String path) {
         return proxy.getRealPath(path);
     }
 
+    @Override
     public int getRemotePort() {
         return proxy.getRemotePort();
     }
 
+    @Override
     public String getLocalName() {
         return proxy.getLocalName();
     }
 
+    @Override
     public HttpSession getSession() {
         return proxy.getSession();
     }
 
+    @Override
     public String getLocalAddr() {
         return proxy.getLocalAddr();
     }
 
+    @Override
     public boolean isRequestedSessionIdValid() {
         return proxy.isRequestedSessionIdValid();
     }
 
+    @Override
     public int getLocalPort() {
         return proxy.getLocalPort();
     }
 
+    @Override
     public boolean isRequestedSessionIdFromCookie() {
         return proxy.isRequestedSessionIdFromCookie();
     }
 
+    @Override
     public boolean isRequestedSessionIdFromURL() {
         return proxy.isRequestedSessionIdFromURL();
     }
 
     @Deprecated
+    @Override
     public boolean isRequestedSessionIdFromUrl() {
         return proxy.isRequestedSessionIdFromUrl();
     }
