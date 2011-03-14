394d559497c47db3e3c9b40d0dd202d2ab3997e2
Lenik <xjl@99jsj.com>
2011-03-14 08:30:36 +0800

    Add EnterpriseService for later refactor.

---
+++ plover/library/plover-orm/src/main/java/com/bee32/plover/arch/EnterpriseService.java
@@ -0,0 +1,12 @@
+package com.bee32.plover.arch;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import com.bee32.plover.inject.ComponentTemplate;
+
+@ComponentTemplate
+// @Logging
+@Transactional
+public class EnterpriseService {
+
+}
