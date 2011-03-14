57ac04992162d8c81e5160821377ba38086cd683
Lenik <xjl@99jsj.com>
2011-03-14 08:32:24 +0800

    Prepare ERModule-postprocess, to create transient samples.

---
+++ plover/library/plover-orm/src/main/java/com/bee32/plover/orm/ERModulePostProcessor.java
@@ -0,0 +1,50 @@
+package com.bee32.plover.orm;
+
+import java.util.Collection;
+
+import javax.inject.Inject;
+
+import org.hibernate.SessionFactory;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.orm.hibernate3.HibernateTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.bee32.plover.arch.IModule;
+import com.bee32.plover.arch.ModulePostProcessor;
+import com.bee32.plover.orm.entity.EntityRepository;
+import com.bee32.plover.orm.util.ERModule;
+
+@Component
+@Lazy
+@Transactional
+public class ERModulePostProcessor
+        extends ModulePostProcessor {
+
+    @Inject
+    SessionFactory sessionFactory;
+
+    @Transactional
+    @Override
+    public void afterModuleLoaded(IModule module)
+            throws Exception {
+        if (!(module instanceof ERModule))
+            return;
+
+        HibernateTemplate template = new HibernateTemplate(sessionFactory);
+
+        ERModule erModule = (ERModule) module;
+        for (Object child : erModule.getChildren()) {
+            if (child instanceof EntityRepository<?, ?>) {
+                EntityRepository<?, ?> er = (EntityRepository<?, ?>) child;
+
+                Collection<?> normalTransientSamples = er.getTransientSamples(false);
+                for (Object sample : normalTransientSamples) {
+                    template.save(sample);
+                }
+
+            }
+        }
+    }
+
+}
