dcd1a76bbd10c951cc0e83f8ea8ae60f8b31b155
Lenik <xjl@99jsj.com>
2011-03-14 16:53:42 +0800

    Add service prototype loader to support class type discover.

---
+++ plover/core/plover-arch/src/main/java/com/bee32/plover/arch/service/ServicePrototypeLoader.java
@@ -0,0 +1,61 @@
+package com.bee32.plover.arch.service;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import javax.free.IllegalUsageException;
+import javax.free.URLResource;
+
+public class ServicePrototypeLoader {
+
+    public static <T> Iterable<Class<? extends T>> load(Class<T> serviceBaseType)
+            throws IOException, ClassNotFoundException {
+        // Or using Context class loader?...
+        ClassLoader classLoader = serviceBaseType.getClassLoader();
+
+        return load(serviceBaseType, classLoader);
+    }
+
+    public static <T> Iterable<Class<? extends T>> load(Class<T> serviceBaseType, ClassLoader classLoader)
+            throws IOException, ClassNotFoundException {
+        String resourceName = "META-INF/prototypes/" + serviceBaseType.getName();
+
+        Enumeration<URL> resources = classLoader.getResources(resourceName);
+
+        Set<Class<? extends T>> serviceClasses = new LinkedHashSet<Class<? extends T>>();
+
+        while (resources.hasMoreElements()) {
+            URL url = resources.nextElement();
+
+            for (String line : new URLResource(url).forRead().listLines()) {
+                String className = line.trim();
+
+                if (className.isEmpty())
+                    continue;
+
+                if (className.startsWith("#"))
+                    continue;
+
+                /*
+                 * Optimize: Maybe another class loader different to the resource discoverer should
+                 * be used.
+                 */
+                Class<?> serviceClass = Class.forName(className, true, classLoader);
+
+                if (!serviceBaseType.isAssignableFrom(serviceClass))
+                    throw new IllegalUsageException(String.format("Invalid service class %s for %s.", //
+                            className, serviceBaseType.getName()));
+
+                @SuppressWarnings("unchecked")
+                Class<? extends T> casted = (Class<? extends T>) serviceClass;
+                serviceClasses.add(casted);
+            }
+        }
+
+        return serviceClasses;
+    }
+
+}
