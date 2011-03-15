c2b50ed3e8ccfe5d1a39bcae9ef51243378c8b00
Lenik <xjl@99jsj.com>
2011-03-15 13:25:41 +0800

    EnterpriseService now extends Qualified.

--- plover/library/plover-orm/src/main/java/com/bee32/plover/arch/EnterpriseService.java
+++ plover/library/plover-orm/src/main/java/com/bee32/plover/arch/EnterpriseService.java
@@ -3,10 +3,19 @@ package com.bee32.plover.arch;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.bee32.plover.inject.ComponentTemplate;
+import com.bee32.plover.model.qualifier.Qualified;
 
 @ComponentTemplate
-// @Logging
 @Transactional
-public class EnterpriseService {
+public class EnterpriseService
+        extends Qualified {
+
+    public EnterpriseService() {
+        super();
+    }
+
+    public EnterpriseService(String serviceName) {
+        super(serviceName);
+    }
 
 }
