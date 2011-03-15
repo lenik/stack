d8a5a47645eea61565d19e00b0cc447daf11d51a
Lenik <xjl@99jsj.com>
2011-03-15 13:23:39 +0800

    Rename ERModulePostProcessor to ERNormalSamplerLoader.

--- plover/library/plover-orm/src/main/java/com/bee32/plover/orm/ERNormalSamplesLoader.java
+++ plover/library/plover-orm/src/main/java/com/bee32/plover/orm/ERNormalSamplesLoader.java
@@ -5,6 +5,8 @@ import java.util.Collection;
 import javax.inject.Inject;
 
 import org.hibernate.SessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.orm.hibernate3.HibernateTemplate;
 import org.springframework.stereotype.Component;
@@ -18,22 +20,31 @@ import com.bee32.plover.orm.util.ERModule;
 @Component
 @Lazy
 @Transactional
-public class ERModulePostProcessor
+public class ERNormalSamplesLoader
         extends ModulePostProcessor {
+
+    static Logger logger = LoggerFactory.getLogger(ERNormalSamplesLoader.class);
 
     @Inject
     SessionFactory sessionFactory;
 
-    @Transactional
     @Override
     public void afterModuleLoaded(IModule module)
             throws Exception {
         if (!(module instanceof ERModule))
             return;
 
+        ERModule erModule = (ERModule) module;
+
+        loadSamples(erModule);
+    }
+
+    @Transactional
+    public void loadSamples(ERModule erModule) {
+        logger.info("Load ER Samples for module " + erModule.getName());
+
         HibernateTemplate template = new HibernateTemplate(sessionFactory);
 
-        ERModule erModule = (ERModule) module;
         for (Object child : erModule.getChildren()) {
             if (child instanceof EntityRepository<?, ?>) {
                 EntityRepository<?, ?> er = (EntityRepository<?, ?>) child;
