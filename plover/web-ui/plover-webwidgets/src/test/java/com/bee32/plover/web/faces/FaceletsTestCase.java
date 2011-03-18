4f91a1d6b9023a727da00c1bd36dcf9cabd275ef
Lenik <xjl@99jsj.com>
2011-03-18 22:39:04 +0800

    Prepare facelets test case, though still not work.

--- plover/web-ui/plover-webwidgets/src/test/java/com/bee32/plover/web/faces/FaceletsTestCase.java
+++ plover/web-ui/plover-webwidgets/src/test/java/com/bee32/plover/web/faces/FaceletsTestCase.java
@@ -1,17 +1,24 @@
 package com.bee32.plover.web.faces;
 
+import java.io.IOException;
+
 import javax.faces.webapp.FacesServlet;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import org.junit.Test;
 
 import com.bee32.plover.servlet.test.ServletTestCase;
-import com.bee32.plover.web.faces.CustomResourceResolver;
+import com.sun.faces.application.WebappLifecycleListener;
+import com.sun.faces.config.ConfigureListener;
 
 public class FaceletsTestCase
         extends ServletTestCase {
 
     @Override
     protected void configureServlets() {
-        stl.setAttribute("facelets.RESOURCE_RESOLVER", //
-                CustomResourceResolver.class.getName());
+// stl.setAttribute("facelets.RESOURCE_RESOLVER", //
+// CustomResourceResolver.class.getName());
 
         // - Use Documents Saved as *.xhtml
         stl.setAttribute("javax.faces.DEFAULT_SUFFIX", ".xhtml");
@@ -23,8 +30,33 @@ public class FaceletsTestCase
         stl.setAttribute("com.sun.faces.validateXml", "true");
         stl.setAttribute("com.sun.faces.verifyObjects", "true");
 
+        stl.addEventListener(new ConfigureListener());
+
+      final WebappLifecycleListener lifecycle= new WebappLifecycleListener();
+
+        stl.addEventListener(new ServletContextListener() {
+
+            @Override
+            public void contextInitialized(ServletContextEvent sce) {
+                lifecycle.contextInitialized(sce);
+            }
+
+            @Override
+            public void contextDestroyed(ServletContextEvent sce) {
+                lifecycle.contextDestroyed(sce);
+            }
+
+        });
+
+
         // Faces Servlet
         stl.addServlet(FacesServlet.class, "*.jsf");
+    }
+
+    @Test
+    public void play()
+            throws IOException {
+        browseAndWait();
     }
 
 }
