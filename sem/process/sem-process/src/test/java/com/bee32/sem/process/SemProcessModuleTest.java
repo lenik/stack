4ff279ea1964538221e8e96af3c0bdd9e930c949
Lenik <xjl@99jsj.com>
2011-03-19 09:14:28 +0800

    SemProcessModuleTest rebased on RTC.

--- sem/process/sem-process/src/test/java/com/bee32/sem/process/SemProcessModuleTest.java
+++ sem/process/sem-process/src/test/java/com/bee32/sem/process/SemProcessModuleTest.java
@@ -2,25 +2,14 @@ package com.bee32.sem.process;
 
 import org.junit.Test;
 
-import com.bee32.plover.restful.DispatchFilter;
-import com.bee32.plover.restful.test.RestfulTesterLibrary;
-import com.bee32.plover.test.AssembledTestCase;
+import com.bee32.plover.restful.test.RestfulTestCase;
 
 public class SemProcessModuleTest
-        extends AssembledTestCase {
-
-    RestfulTesterLibrary rtl;
-
-    public SemProcessModuleTest() {
-        install(rtl = new RestfulTesterLibrary());
-    }
+        extends RestfulTestCase {
 
     @Test
     public void testUser()
             throws Exception {
-
-        // addFilter(DispatchFilter.class, "/", 3);
-        rtl.addServlet(DispatchFilter.class, "/");
 
         String loc = "http://localhost:" + rtl.getPort();
         System.out.println(loc);
