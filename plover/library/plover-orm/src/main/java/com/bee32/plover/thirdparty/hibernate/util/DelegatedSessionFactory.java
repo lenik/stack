45949d244a69bcdf675fc6b7735323d1e19f5147
Lenik <xjl@99jsj.com>
2011-03-19 16:44:06 +0800

    Add @Override to DelegatedSessionFactory.

--- plover/library/plover-orm/src/main/java/com/bee32/plover/thirdparty/hibernate/util/DelegatedSessionFactory.java
+++ plover/library/plover-orm/src/main/java/com/bee32/plover/thirdparty/hibernate/util/DelegatedSessionFactory.java
@@ -18,7 +18,6 @@ import org.hibernate.metadata.ClassMetadata;
 import org.hibernate.metadata.CollectionMetadata;
 import org.hibernate.stat.Statistics;
 
-@SuppressWarnings("rawtypes")
 public abstract class DelegatedSessionFactory
         implements SessionFactory {
 
@@ -26,124 +25,150 @@ public abstract class DelegatedSessionFactory
 
     protected abstract SessionFactory getDelegate();
 
+    @Override
     public Reference getReference()
             throws NamingException {
         return getDelegate().getReference();
     }
 
+    @Override
     public Session openSession(Connection connection) {
         return getDelegate().openSession(connection);
     }
 
+    @Override
     public Session openSession(Interceptor interceptor)
             throws HibernateException {
         return getDelegate().openSession(interceptor);
     }
 
+    @Override
     public Session openSession(Connection connection, Interceptor interceptor) {
         return getDelegate().openSession(connection, interceptor);
     }
 
+    @Override
     public Session openSession()
             throws HibernateException {
         return getDelegate().openSession();
     }
 
+    @Override
     public Session getCurrentSession()
             throws HibernateException {
         return getDelegate().getCurrentSession();
     }
 
+    @Override
     public ClassMetadata getClassMetadata(Class persistentClass)
             throws HibernateException {
         return getDelegate().getClassMetadata(persistentClass);
     }
 
+    @Override
     public ClassMetadata getClassMetadata(String entityName)
             throws HibernateException {
         return getDelegate().getClassMetadata(entityName);
     }
 
+    @Override
     public CollectionMetadata getCollectionMetadata(String roleName)
             throws HibernateException {
         return getDelegate().getCollectionMetadata(roleName);
     }
 
+    @Override
     public Map getAllClassMetadata()
             throws HibernateException {
         return getDelegate().getAllClassMetadata();
     }
 
+    @Override
     public Map getAllCollectionMetadata()
             throws HibernateException {
         return getDelegate().getAllCollectionMetadata();
     }
 
+    @Override
     public Statistics getStatistics() {
         return getDelegate().getStatistics();
     }
 
+    @Override
     public void close()
             throws HibernateException {
         getDelegate().close();
     }
 
+    @Override
     public boolean isClosed() {
         return getDelegate().isClosed();
     }
 
+    @Override
     public void evict(Class persistentClass)
             throws HibernateException {
         getDelegate().evict(persistentClass);
     }
 
+    @Override
     public void evict(Class persistentClass, Serializable id)
             throws HibernateException {
         getDelegate().evict(persistentClass, id);
     }
 
+    @Override
     public void evictEntity(String entityName)
             throws HibernateException {
         getDelegate().evictEntity(entityName);
     }
 
+    @Override
     public void evictEntity(String entityName, Serializable id)
             throws HibernateException {
         getDelegate().evictEntity(entityName, id);
     }
 
+    @Override
     public void evictCollection(String roleName)
             throws HibernateException {
         getDelegate().evictCollection(roleName);
     }
 
+    @Override
     public void evictCollection(String roleName, Serializable id)
             throws HibernateException {
         getDelegate().evictCollection(roleName, id);
     }
 
+    @Override
     public void evictQueries()
             throws HibernateException {
         getDelegate().evictQueries();
     }
 
+    @Override
     public void evictQueries(String cacheRegion)
             throws HibernateException {
         getDelegate().evictQueries(cacheRegion);
     }
 
+    @Override
     public StatelessSession openStatelessSession() {
         return getDelegate().openStatelessSession();
     }
 
+    @Override
     public StatelessSession openStatelessSession(Connection connection) {
         return getDelegate().openStatelessSession(connection);
     }
 
+    @Override
     public Set getDefinedFilterNames() {
         return getDelegate().getDefinedFilterNames();
     }
 
+    @Override
     public FilterDefinition getFilterDefinition(String filterName)
             throws HibernateException {
         return getDelegate().getFilterDefinition(filterName);
