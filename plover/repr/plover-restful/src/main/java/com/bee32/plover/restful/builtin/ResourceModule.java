b8c4d2aed7eb653efe6981e2e4d8b64a6984a8ea
Lenik <xjl@99jsj.com>
2011-03-20 00:38:41 +0800

    polish

--- plover/repr/plover-restful/src/main/java/com/bee32/plover/restful/builtin/ResourceModule.java
+++ plover/repr/plover-restful/src/main/java/com/bee32/plover/restful/builtin/ResourceModule.java
@@ -9,7 +9,6 @@ import com.bee32.plover.pub.oid.Oid;
 import com.bee32.plover.pub.oid.PloverOids;
 import com.bee32.plover.restful.resource.ResourceDispatcher;
 
-
 @Oid({ 3, 12, PloverOids.repr, PloverOids.reprResource })
 public class ResourceModule
         extends DispatchModule {
