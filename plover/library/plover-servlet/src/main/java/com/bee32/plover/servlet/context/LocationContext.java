23008476d5a1fe926e01b47accc5abd5f324990e
Lenik <xjl@99jsj.com>
2011-03-23 03:16:12 +0800

    Add LocationContext.resolve(path) to create ContextLocation.

--- plover/library/plover-servlet/src/main/java/com/bee32/plover/servlet/context/LocationContext.java
+++ plover/library/plover-servlet/src/main/java/com/bee32/plover/servlet/context/LocationContext.java
@@ -1,11 +1,23 @@
 package com.bee32.plover.servlet.context;
 
-public final class LocationContext {
+import java.io.Serializable;
+
+public final class LocationContext
+        implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     private final String name;
 
     public LocationContext(String name) {
         this.name = name;
+    }
+
+    public ContextLocation resolve(String location) {
+        if (location == null)
+            throw new NullPointerException("location");
+
+        return new ContextLocation(this, location);
     }
 
     @Override
