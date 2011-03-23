fc240d0624735ea5d48d63bbc81aa68f5dbb53fd
Lenik <xjl@99jsj.com>
2011-03-23 17:14:50 +0800

    @Contribution as a composite-element.

--- sem/base/sem-frame/src/main/java/com/bee32/sem/frame/Contribution.java
+++ sem/base/sem-frame/src/main/java/com/bee32/sem/frame/Contribution.java
@@ -6,13 +6,24 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import com.bee32.plover.arch.CompositeElement;
+import com.bee32.sem.frame.menu.MenuContribution;
+
+/**
+ * The &#64;Contribution annotation is a composite element, so member fields with this annotation
+ * will got its appearance cascade loaded.
+ * <p>
+ * At the other side, &#64;Contribution annotation is processed by {@link MenuContribution}, so its
+ * target element id is used.
+ */
+@CompositeElement
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ ElementType.FIELD, ElementType.TYPE })
 public @interface Contribution {
 
     /**
-     * Target element id.
+     * Target element id or path.
      */
     String value();
 
