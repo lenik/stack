084590e3992c732904a79561c9950f7033839aee
Lenik <xjl@99jsj.com>
2011-03-24 10:09:12 +0800

    Clear FrameTest.

--- sem/base/sem-frame/src/test/java/com/bee32/sem/frame/FrameTest.java
+++ sem/base/sem-frame/src/test/java/com/bee32/sem/frame/FrameTest.java
@@ -2,18 +2,9 @@ package com.bee32.sem.frame;
 
 import javax.inject.Inject;
 
-import com.bee32.sem.frame.builtins.ContextMenu;
-import com.bee32.sem.frame.menu.IMenuEntry;
-
 public class FrameTest {
 
     @Inject
     IMainFrame frame;
-
-    public FrameTest() {
-
-        IMenuEntry menu = frame.getMainFrame().getMenu(ContextMenu.CONTEXT_NETWORK);
-//        menu.contribute("asd/foo", new MenuItem());
-    }
 
 }
