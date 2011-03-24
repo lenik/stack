a7a49f9a64919323caadd18b9a2422352f1d901c
Lenik <xjl@99jsj.com>
2011-03-24 10:06:29 +0800

    EnterpriseService are lazy.

--- plover/library/plover-orm/src/main/java/com/bee32/plover/arch/EnterpriseService.java
+++ plover/library/plover-orm/src/main/java/com/bee32/plover/arch/EnterpriseService.java
@@ -1,5 +1,6 @@
 package com.bee32.plover.arch;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.bee32.plover.inject.ComponentTemplate;
@@ -7,6 +8,7 @@ import com.bee32.plover.model.qualifier.Qualified;
 
 @ComponentTemplate
 @Transactional
+@Lazy
 public abstract class EnterpriseService
         extends Qualified {
 
