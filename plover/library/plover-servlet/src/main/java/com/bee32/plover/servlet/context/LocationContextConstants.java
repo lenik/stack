d69151664bbe5615c97812d5bc148ea3d9ee7ee0
Lenik <xjl@99jsj.com>
2011-03-25 11:41:23 +0800

    Add JAVASCRIPT, STYLE, LIB_3RDPARTY loc-contexts.

--- plover/library/plover-servlet/src/main/java/com/bee32/plover/servlet/context/LocationContextConstants.java
+++ plover/library/plover-servlet/src/main/java/com/bee32/plover/servlet/context/LocationContextConstants.java
@@ -1,11 +1,63 @@
 package com.bee32.plover.servlet.context;
 
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+
 public interface LocationContextConstants {
 
-    LocationContext REQUEST = new LocationContext("request");
-    LocationContext WEB_APP = new LocationContext("web-app");
+    LocationContext JAVASCRIPT = new LocationContext("<<JAVASCRIPT>>") {
 
-    LocationContext URL = new LocationContext(null);
-    LocationContext JAVASCRIPT = new LocationContext("javascript://");
+        private static final long serialVersionUID = 1L;
+
+        @Override
+        public String resolve(HttpServletRequest request, String location) {
+            return "javascript:" + location;
+        }
+
+    };
+
+    LocationContext REQUEST = new LocationContext("<<request>>") {
+
+        private static final long serialVersionUID = 1L;
+
+        @Override
+        public String resolve(HttpServletRequest request, String location) {
+            if (isAbsolute(location))
+                return location;
+
+            // Since the resolve is request-oriented, so let's just ignore request.
+
+            return location;
+        }
+    };
+
+    LocationContext URL = REQUEST;
+
+    LocationContext WEB_APP = new LocationContext("<<servlet-context>>") {
+
+        private static final long serialVersionUID = 1L;
+
+        @Override
+        public String resolve(HttpServletRequest request, String location) {
+            if (isAbsolute(location))
+                return location;
+
+            ServletContext servletContext = request.getSession().getServletContext();
+
+            // context-path == /* or ""
+            String contextPath = servletContext.getContextPath();
+            return contextPath + "/" + location;
+        }
+
+    };
+
+    LocationContext STYLE_ROOT = new PredefinedLocationContext("STYLE-ROOT", //
+            "http://static.secca-project.com/style");
+
+    LocationContext STYLE = new PredefinedLocationContext("STYLE", //
+            "http://static.secca-project.com/style/default");
+
+    LocationContext LIB_3RDPARTY = new PredefinedLocationContext("LIB-3RDPARTY", //
+            "http://static.secca-project.com/lib2");
 
 }
--- sem/base/sem-frame/src/test/java/com/bee32/sem/frame/menu/MenuContributionTest.java
+++ sem/base/sem-frame/src/test/java/com/bee32/sem/frame/menu/MenuContributionTest.java
@@ -8,7 +8,7 @@ public class MenuContributionTest {
     public static class MyMenu
             extends MenuContribution {
 
-        Action openFileAction = new Action("file.open", WEB_APP.resolve("file/open/htm"));
+        Action openFileAction = new Action("file.open", WEB_APP.join("file/open/htm"));
 
         @Contribution("menu.file")
         MenuItem fileOpen = new MenuItem("file.open", openFileAction);
