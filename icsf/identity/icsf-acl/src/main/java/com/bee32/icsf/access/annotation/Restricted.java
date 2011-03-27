f7eb3512a74bf7bcf2cae9b01e96034248fe6805
Lenik <xjl@99jsj.com>
2011-03-28 01:23:54 +0800

    Add RestrictedUtil.

--- icsf/identity/icsf-acl/src/main/java/com/bee32/icsf/access/annotation/Restricted.java
+++ icsf/identity/icsf-acl/src/main/java/com/bee32/icsf/access/annotation/Restricted.java
@@ -4,12 +4,23 @@ import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
+import com.bee32.icsf.access.Permission;
+
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Restricted {
 
+    /**
+     * The permission name implicit defined.
+     * <p>
+     * If not specified, or specify an empty string (""), the default name (field name or method
+     * name) is then choosed.
+     */
     String name() default "";
 
-    Class<?>[] require() default {};
+    /**
+     * Instead of define an implicit
+     */
+    Class<? extends Permission>[] require() default {};
 
 }
