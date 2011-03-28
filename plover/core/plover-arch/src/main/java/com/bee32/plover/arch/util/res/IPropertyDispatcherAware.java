6f3b2757bafdd82a9f73173668c63d246bf52ffc
Lenik <xjl@99jsj.com>
2011-03-28 17:12:57 +0800

    Re-add IPropertyDispatcherAware, though it's not used.

---
+++ plover/core/plover-arch/src/main/java/com/bee32/plover/arch/util/res/IPropertyDispatcherAware.java
@@ -0,0 +1,9 @@
+package com.bee32.plover.arch.util.res;
+
+public interface IPropertyDispatcherAware {
+
+    IPropertyDispatcher getPropertyDispatcher();
+
+    void setPropertyDispatcher(IPropertyDispatcher dispatcher);
+
+}
