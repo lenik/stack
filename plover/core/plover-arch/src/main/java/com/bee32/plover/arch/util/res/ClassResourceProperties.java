6fac34e82999539c6541694a0f472c3dd0e79bac
Lenik <xjl@99jsj.com>
2011-03-28 18:05:57 +0800

    ClassResourceProperties don't throw MRE.
    - If properties file not exists.

--- plover/core/plover-arch/src/main/java/com/bee32/plover/arch/util/res/ClassResourceProperties.java
+++ plover/core/plover-arch/src/main/java/com/bee32/plover/arch/util/res/ClassResourceProperties.java
@@ -1,6 +1,8 @@
 package com.bee32.plover.arch.util.res;
 
+import java.util.Collections;
 import java.util.Locale;
+import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 import java.util.Set;
 
@@ -9,23 +11,33 @@ public class ClassResourceProperties
 
     private ResourceBundle resourceBundle;
 
-    public ClassResourceProperties(Class<?> clazz, Locale locale) {
+    public ClassResourceProperties(Class<?> clazz, Locale locale)
+            throws MissingResourceException {
         if (clazz == null)
             throw new NullPointerException("clazz");
         if (locale == null)
             throw new NullPointerException("locale");
 
         String baseName = clazz.getName();
-        this.resourceBundle = ResourceBundle.getBundle(baseName, locale);
+
+        try {
+            this.resourceBundle = ResourceBundle.getBundle(baseName, locale);
+        } catch (MissingResourceException e) {
+            this.resourceBundle = null;
+        }
     }
 
     @Override
     public String get(String key) {
+        if (resourceBundle == null)
+            return null;
         return resourceBundle.getString(key);
     }
 
     @Override
     public Set<String> keySet() {
+        if (resourceBundle == null)
+            return Collections.emptySet();
         return resourceBundle.keySet();
     }
 
