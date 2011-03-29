e37c12dcfda139431bd34b82ab50135b050d69fa
Lenik <xjl@99jsj.com>
2011-03-29 18:23:13 +0800

    Add MainMenuTest.dump().

---
+++ sem/base/sem-frame/src/test/java/com/bee32/sem/frame/builtins/MainMenuTest.java
@@ -0,0 +1,34 @@
+package com.bee32.sem.frame.builtins;
+
+import javax.inject.Inject;
+
+import org.junit.Test;
+
+import com.bee32.plover.test.WiredTestCase;
+import com.bee32.sem.frame.menu.IMenuEntry;
+
+public class MainMenuTest
+        extends WiredTestCase {
+
+    @Inject
+    MainMenu mainMenu;
+
+    @Test
+    public void testBuiltinNLS() {
+        IMenuEntry entry = mainMenu.resolve("help/aboutFrame");
+        assertNotNull(entry);
+
+        String displayName = entry.getAppearance().getDisplayName();
+        assertEquals("About SEM-Frame", displayName);
+    }
+
+    @Override
+    public void dump() {
+        mainMenu.getName();
+    }
+
+    public static void main(String[] args) {
+        new MainMenuTest().wire().dump();
+    }
+
+}
