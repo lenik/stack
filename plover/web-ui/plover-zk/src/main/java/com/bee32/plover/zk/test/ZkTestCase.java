26fc662afb3679ccd5857e1b5f90420f33de1f6c
Lenik <xjl@99jsj.com>
2011-03-29 13:30:26 +0800

    Add ZkTestCase. However jsf/integration not work yet.

---
+++ plover/web-ui/plover-zk/src/main/java/com/bee32/plover/zk/test/ZkTestCase.java
@@ -0,0 +1,38 @@
+package com.bee32.plover.zk.test;
+
+import org.mortbay.jetty.servlet.ServletHolder;
+import org.zkoss.web.servlet.dsp.InterpreterServlet;
+import org.zkoss.zk.au.http.DHtmlUpdateServlet;
+import org.zkoss.zk.ui.http.DHtmlLayoutServlet;
+import org.zkoss.zk.ui.http.HttpSessionListener;
+
+import com.bee32.plover.web.faces.test.FaceletsTestCase;
+
+public class ZkTestCase
+        extends FaceletsTestCase {
+
+    public ZkTestCase() {
+        super();
+    }
+
+    public ZkTestCase(Class<?> altBaseClass) {
+        super(altBaseClass);
+    }
+
+    @Override
+    protected void configureBuiltinServlets() {
+        HttpSessionListener httpSessionListener = new HttpSessionListener();
+        stl.addEventListener(httpSessionListener);
+
+        ServletHolder zulServlet = stl.addServlet(DHtmlLayoutServlet.class, "*.zul");
+        zulServlet.setInitParameter("update-uri", "/zkau");
+        zulServlet.setInitParameter("log-level", "DEBUG");
+
+        stl.addServlet(DHtmlUpdateServlet.class, "/zkau/*");
+        stl.addServlet(InterpreterServlet.class, "*.dsp");
+
+        addTagLibrary("/WEB-INF/zuljsf.tld");
+        super.configureBuiltinServlets();
+    }
+
+}
---
+++ plover/web-ui/plover-zk/src/test/java/com/bee32/plover/zk/test/ZkTestCaseTest.java
@@ -0,0 +1,20 @@
+package com.bee32.plover.zk.test;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+public class ZkTestCaseTest
+        extends ZkTestCase {
+
+    @Test
+    public void test() {
+    }
+
+    public static void main(String[] args)
+            throws IOException, Exception {
+        new ZkTestCaseTest().wire().browseAndWait(//
+                "/zhello.jsf");
+    }
+
+}
