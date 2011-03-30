1e9ca0e59042e265992587c4a6527e059b0f8f42
Lenik <xjl@99jsj.com>
2011-03-30 14:12:59 +0800

    Re-enabled TestDataSource. (The context config are going to be refactored using spring class-appctx.)

--- plover/library/plover-orm/src/main/java/com/bee32/plover/orm/config/test/TestDataSource.java
+++ plover/library/plover-orm/src/main/java/com/bee32/plover/orm/config/test/TestDataSource.java
@@ -1,7 +1,14 @@
 package com.bee32.plover.orm.config.test;
 
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+import com.bee32.plover.inject.qualifier.TestPurpose;
 import com.bee32.plover.orm.config.CustomizedDataSource;
 
+@Component
+@TestPurpose
+@Lazy
 public class TestDataSource
         extends CustomizedDataSource {
 
