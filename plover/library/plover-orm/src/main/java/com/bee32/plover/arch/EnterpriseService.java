e36745f395abbcccc869b728ca68eb106fcf58e5
Lenik <xjl@99jsj.com>
2011-03-31 01:38:37 +0800

    EnterpriseService no @Transactional by default

--- plover/library/plover-orm/src/main/java/com/bee32/plover/arch/EnterpriseService.java
+++ plover/library/plover-orm/src/main/java/com/bee32/plover/arch/EnterpriseService.java
@@ -1,13 +1,12 @@
 package com.bee32.plover.arch;
 
 import org.springframework.context.annotation.Lazy;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.bee32.plover.inject.ComponentTemplate;
 import com.bee32.plover.model.qualifier.Qualified;
 
 @ComponentTemplate
-@Transactional
+// @Transactional
 @Lazy
 public abstract class EnterpriseService
         extends Qualified {
