71295faa7d1984941f5de63b3ff6125cffba05a4
jack <superjwl@gmail.com>
2011-03-30 17:13:08 +0800

    add SuperFishMenuBuilder and MenuController spring mvc controller

---
+++ sem/base/sem-frame/src/main/java/com/bee32/sem/frame/menu/SuperFishMenuBuilder.java
@@ -0,0 +1,79 @@
+package com.bee32.sem.frame.menu;
+
+import com.bee32.plover.servlet.context.ContextLocation;
+import com.bee32.plover.servlet.context.LocationContextConstants;
+import com.bee32.sem.frame.action.IAction;
+
+
+public class SuperFishMenuBuilder {
+    public static String buildMenubar(MenuBar menuBar) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("<ul id='mainMenu'>");
+
+        for(IMenuEntry menuEntry : menuBar) {
+            sb.append(buildMenu(menuEntry));
+        }
+        sb.append("</ul>");
+
+        return sb.toString();
+    }
+
+    public static String buildMenu(IMenuEntry menuEntry) {
+        StringBuilder sb = new StringBuilder();
+
+        for(IMenuEntry childEntry : menuEntry) {
+            sb.append("<li>");
+            sb.append("<a ");
+
+
+            IAction action = childEntry.getAction();
+            if (action != null) {
+                boolean isEnabled = action.isEnabled();
+
+                String onClick;
+                String href;
+
+                ContextLocation target = action.getTarget();
+                if (target.getContext() == LocationContextConstants.JAVASCRIPT) {
+                    onClick = target.getLocation();
+                    href = null;
+
+                    sb.append("href='#' onclick='");
+                    sb.append(onClick);
+                    sb.append("'");
+
+                } else {
+                    onClick = null;
+                    href = target.getLocation();
+                    sb.append("href='");
+                    sb.append(href);
+                    sb.append("'");
+                }
+            }
+            sb.append("'>");
+            sb.append(childEntry.getAppearance().getDisplayName());
+            sb.append("</a>");
+
+            if(!isLeafNode(childEntry)) {
+                sb.append("<ul>");
+                sb.append(buildMenu(childEntry));
+                sb.append("</ul>");
+            }
+
+            sb.append("</li>");
+        }
+
+        return sb.toString();
+    }
+
+
+    protected static boolean isLeafNode(IMenuEntry entry) {
+        if (entry.size() == 0)
+            return true;
+
+        if (entry.size() > 1)
+            return false;
+
+        return false; //isLeafNode(entry.iterator().next());
+    }
+}
---
+++ sem/base/sem-frame/src/main/java/com/bee32/sem/frame/web/MenuController.java
@@ -0,0 +1,41 @@
+package com.bee32.sem.frame.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
+
+import com.bee32.sem.frame.builtins.MainMenu;
+import com.bee32.sem.frame.menu.SuperFishMenuBuilder;
+
+/**
+ * Date: 11-3-30
+ * Time: 下午4:17
+ */
+@Controller
+public class MenuController extends MultiActionController {
+    @Inject
+    private MainMenu mainMenu;
+
+    @RequestMapping("/makeMenu.htm")
+    public void makeMenu(HttpServletRequest request, HttpServletResponse response) {
+
+        response.setContentType("text/html;charset=utf-8");
+        PrintWriter out = null;
+        try {
+            out = response.getWriter();
+            out.println(SuperFishMenuBuilder.buildMenubar(mainMenu));
+
+        } catch (IOException ex1) {
+            ex1.printStackTrace();
+        }finally{
+            out.close();
+        }
+    }
+}
