73c2fd3ad23eb47a34f77242a6a046c96dd3eed0
Lenik <xjl@99jsj.com>
2011-03-30 15:37:31 +0800

    Expand kate's ACL.

--- icsf/identity/icsf-acl/src/main/java/com/bee32/icsf/access/alt/R_ACLSamples.java
+++ icsf/identity/icsf-acl/src/main/java/com/bee32/icsf/access/alt/R_ACLSamples.java
@@ -11,15 +11,19 @@ import com.bee32.plover.orm.util.ImportSamples;
 public class R_ACLSamples
         extends EntitySamplesContribution {
 
-    public R_ACE kate_READ;
+    public R_ACL kate_ACL;
+    public R_ACE kate_CanRead;
+    public R_ACE kate_NoExecute;
 
     public R_ACLSamples() {
-        kate_READ = new R_ACE(kate, GeneralPermission.READ, true);
+        kate_ACL = new R_ACL();
+        kate_CanRead = kate_ACL.addAllowACE(kate, GeneralPermission.READ);
+        kate_NoExecute = kate_ACL.addDenyACE(kate, GeneralPermission.EXECUTE);
     }
 
     @Override
     protected void preamble() {
-        addNormalSample(kate_READ);
+        addNormalSample(kate_ACL);
     }
 
 }
