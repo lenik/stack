3dacf5f9919951c9169c6dc64498354ef4e2e936
Lenik <xjl@99jsj.com>
2011-03-30 14:15:00 +0800

    Moved IcsfPrincipalModule from icsf.* to icsf.principal.*.

--- icsf/identity/icsf-principal/src/main/java/com/bee32/icsf/principal/IcsfPrincipalModule.java
+++ icsf/identity/icsf-principal/src/main/java/com/bee32/icsf/principal/IcsfPrincipalModule.java
@@ -1,7 +1,8 @@
-package com.bee32.icsf;
+package com.bee32.icsf.principal;
 
 import javax.inject.Inject;
 
+import com.bee32.icsf.IcsfOids;
 import com.bee32.icsf.principal.dao.GroupDao;
 import com.bee32.icsf.principal.dao.PrincipalDao;
 import com.bee32.icsf.principal.dao.RealmDao;
@@ -10,7 +11,7 @@ import com.bee32.icsf.principal.dao.UserDao;
 import com.bee32.plover.orm.util.ERModule;
 import com.bee32.plover.pub.oid.Oid;
 
-@Oid({ 3, 7, IcsfOids.principal })
+@Oid({ 3, 7, IcsfOids.Principal })
 public class IcsfPrincipalModule
         extends ERModule {
 
