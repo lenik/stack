a74abc25460f03dbc229736b89f37f031b9b8f30
Lenik <xjl@99jsj.com>
2011-03-31 12:08:03 +0800

    verify: html tidy.

--- sem/process/sem-process/src/main/resources/com/bee32/sem/process/verify/impl/AllowListNew.v
+++ sem/process/sem-process/src/main/resources/com/bee32/sem/process/verify/impl/AllowListNew.v
@@ -1,22 +1,24 @@
 <html>
-<head>
-    <title>Allow List</title>
-</head>
-<body>
-    <h1>新建白名单审核策略</h1>
-    <hr>
+    <head>
+        <title>Allow List</title>
+    </head>
+    <body>
+        <h1>新建白名单审核策略</h1>
+        <hr />
 
-    <h2>责任人列表</h2>
+        <h2>责任人列表</h2>
 
-    <ul>
-    #foreach ($p in $it.responsibles)
-        <li>$p
-            <a href="~delete?id=$p.id">删除</a>
-        </li>
-    #end
-    </ul>
+        <ul>
+            #foreach ($p in $it.responsibles)
+            <li>
+                $p
+                <a href="~delete?id=$p.id">删除</a>
+            </li>
+            #end
+        </ul>
 
-    <hr>
-    <a href="~new">增加</a>
-</body>
+        <hr />
+        <a href="~new">保存</a>
+
+    </body>
 </html>
