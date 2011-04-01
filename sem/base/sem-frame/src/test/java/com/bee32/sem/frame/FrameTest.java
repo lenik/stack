048453aedf25386824416c2d084f49128cde3b1e
Lenik <xjl@99jsj.com>
2011-04-01 17:09:10 +0800

    Ignore not used FrameTest.

--- sem/base/sem-frame/src/test/java/com/bee32/sem/frame/FrameTest.java
+++ sem/base/sem-frame/src/test/java/com/bee32/sem/frame/FrameTest.java
@@ -2,10 +2,15 @@ package com.bee32.sem.frame;
 
 import javax.inject.Inject;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import com.bee32.plover.test.WiredTestCase;
 
+/**
+ * TODO Frameset hasn't been implemented yet.
+ */
+@Ignore
 public class FrameTest
         extends WiredTestCase {
 
