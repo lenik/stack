8300c93a031b5f1c4568710362291544bf6d4f11
Lenik <xjl@99jsj.com>
2011-04-04 10:27:35 +0800

    Can infer ServletContext from the request.

--- plover/library/plover-servlet/src/main/java/com/bee32/plover/servlet/context/ServletContainer.java
+++ plover/library/plover-servlet/src/main/java/com/bee32/plover/servlet/context/ServletContainer.java
@@ -3,10 +3,9 @@ package com.bee32.plover.servlet.context;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.bee32.plover.inject.AbstractContainer;
 
@@ -19,14 +18,21 @@ public class ServletContainer
         registerContext(ServletContext.class, servletContext);
     }
 
-    public ServletContainer(ServletContext servletContext, ServletRequest request, ServletResponse response) {
-        this(servletContext);
-        registerContext(ServletRequest.class, request);
-        registerContext(ServletResponse.class, response);
-        if (request instanceof HttpServletRequest)
-            registerContext(HttpServletRequest.class, (HttpServletRequest) request);
-        if (response instanceof HttpServletResponse)
-            registerContext(HttpServletResponse.class, (HttpServletResponse) request);
+    public ServletContainer(HttpServletRequest request, HttpServletResponse response) {
+        super(request.getRequestURI().toString());
+
+        // registerContext(ServletRequest.class, request);
+        // registerContext(ServletResponse.class, response);
+        // if (request instanceof HttpServletRequest)
+        registerContext(HttpServletRequest.class, (HttpServletRequest) request);
+        // if (response instanceof HttpServletResponse)
+        registerContext(HttpServletResponse.class, (HttpServletResponse) request);
+
+        HttpSession session = request.getSession();
+        registerContext(HttpSession.class, session);
+
+        ServletContext servletContext = session.getServletContext();
+        registerContext(ServletContext.class, servletContext);
     }
 
     @Override
