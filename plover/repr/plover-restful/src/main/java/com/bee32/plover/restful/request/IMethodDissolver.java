c13a7ddd88caf09ef1ff5a1075efb19604a91a32
Lenik <xjl@99jsj.com>
2011-04-04 16:24:53 +0800

    Simplied MethodDissolver to HttpMethodNames

--- plover/repr/plover-restful/src/main/java/com/bee32/plover/restful/request/IMethodDissolver.java
+++
@@ -1,9 +0,0 @@
-package com.bee32.plover.restful.request;
-
-import com.bee32.plover.restful.RestfulRequest;
-
-public interface IMethodDissolver {
-
-    void desolveMethod(String httpMethod, RestfulRequest model);
-
-}
