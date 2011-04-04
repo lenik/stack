a21e9e9a299053de2442f59f19d723cd7c35ae49
Lenik <xjl@99jsj.com>
2011-04-04 16:25:06 +0800

    Add MethodNames.reverseMap.

--- plover/repr/plover-restful/src/main/java/com/bee32/plover/restful/MethodNames.java
+++ plover/repr/plover-restful/src/main/java/com/bee32/plover/restful/MethodNames.java
@@ -1,5 +1,12 @@
 package com.bee32.plover.restful;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.free.UnexpectedException;
+
 public class MethodNames {
 
     public static final String CREATE = "create";
@@ -13,5 +20,33 @@ public class MethodNames {
     public static final String DELETE = "delete";
 
     public static final String ESTATE = "head";
+
+    static Map<String, String> reverseMap;
+    static {
+        reverseMap = new HashMap<String, String>();
+
+        for (Field field : MethodNames.class.getFields()) {
+            int modifiers = field.getModifiers();
+            if (!Modifier.isPublic(modifiers))
+                continue;
+
+            if (!Modifier.isStatic(modifiers))
+                continue;
+
+            String name = field.getName();
+            String value;
+            try {
+                value = (String) field.get(null);
+            } catch (Exception e) {
+                throw new UnexpectedException("read static field " + field, e);
+            }
+
+            reverseMap.put(value, name);
+        }
+    }
+
+    public static boolean isDefined(String name) {
+        return reverseMap.containsKey(name);
+    }
 
 }
