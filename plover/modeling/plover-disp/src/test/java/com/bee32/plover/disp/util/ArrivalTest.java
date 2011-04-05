3ece3a78613ae10a171bb5e9861789755f2ea2cb
Lenik <xjl@99jsj.com>
2011-04-06 02:34:31 +0800

    Add ArrivalTest to test backtrace().

---
+++ plover/modeling/plover-disp/src/test/java/com/bee32/plover/disp/util/ArrivalTest.java
@@ -0,0 +1,35 @@
+package com.bee32.plover.disp.util;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ArrivalTest
+        extends Assert {
+
+    @Test
+    public void testBacktrace() {
+        Arrival a = new Arrival("a");
+        Arrival b = new Arrival(a, "b", "foo", "bar");
+        Arrival c = new Arrival(b, "c", "cat");
+        Arrival d = new Arrival(c, null, "dog");
+        Arrival e = new Arrival(d, null, "xyz");
+
+        class FindC
+                implements ArrivalBacktraceCallback {
+
+            @Override
+            public boolean arriveBack(IArrival arrival, ReversedPathTokens consumedRpt) {
+                Object target = arrival.getTarget();
+                return "c".equals(target);
+            }
+
+        }
+        FindC findC = new FindC();
+
+        assertTrue(e.backtrace(findC));
+        assertTrue(d.backtrace(findC));
+        assertTrue(c.backtrace(findC));
+        assertFalse(b.backtrace(findC));
+    }
+
+}
