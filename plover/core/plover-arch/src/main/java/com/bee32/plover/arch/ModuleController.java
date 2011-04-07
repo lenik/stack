46b4870a5d34ee0a0ad5d30bb2da157749baf7cc
Lenik <xjl@99jsj.com>
2011-04-07 18:42:43 +0800

    Convert Module(velocity) to ModuleController.

---
+++ plover/core/plover-arch/src/main/java/com/bee32/plover/arch/ModuleController.java
@@ -0,0 +1,43 @@
+package com.bee32.plover.arch;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ModuleController {
+
+    public void index(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+
+        PrintWriter out = resp.getWriter();
+
+        Module module = (Module) req.getAttribute("target");
+
+        out.println("<html>");
+        out.println("    <head>");
+        out.println("        <title>Module " + module.getName() + "</title>");
+        out.println("    </head>");
+        out.println("    <body>");
+        out.println("        <h1>Module " + module.getName() + "</h1>");
+        out.println("        <h2>Entries</h2>");
+        out.println("        <hr />");
+        out.println("");
+
+        for (String childLoc : module.getChildNames()) {
+            Object child = module.getChild(childLoc);
+            out.println("        <div>");
+            out.println("            " + childLoc + " - " + child);
+            out.println("            <a href=\"" + childLoc + "\">contents</a>");
+            out.println("            <a href=\"" + childLoc + "/\">index</a>");
+            out.println("        </div>");
+        }
+
+        out.println("        <hr />");
+        out.println("        <a href=\"credit\">CREDIT</a>");
+        out.println("    </body>");
+        out.println("</html>");
+    }
+
+}
