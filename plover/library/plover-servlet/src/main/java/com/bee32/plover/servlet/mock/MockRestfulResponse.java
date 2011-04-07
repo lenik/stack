489e88fee1fb55bff04b1e1b1986977350bbda2a
Lenik <xjl@99jsj.com>
2011-04-07 17:56:27 +0800

    Add MockRestfulResponse.

---
+++ plover/library/plover-servlet/src/main/java/com/bee32/plover/servlet/mock/MockRestfulResponse.java
@@ -0,0 +1,45 @@
+package com.bee32.plover.servlet.mock;
+
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import com.bee32.plover.restful.IRestfulResponse;
+
+public class MockRestfulResponse
+        extends MockHttpServletResponse
+        implements IRestfulResponse {
+
+    private Object target;
+    private Throwable exception;
+    private String method;
+
+    @Override
+    public Object getTarget() {
+        return target;
+    }
+
+    @Override
+    public void setTarget(Object target) {
+        this.target = target;
+    }
+
+    @Override
+    public Throwable getException() {
+        return exception;
+    }
+
+    @Override
+    public void setException(Throwable exception) {
+        this.exception = exception;
+    }
+
+    @Override
+    public String getMethod() {
+        return method;
+    }
+
+    @Override
+    public void setMethod(String method) {
+        this.method = method;
+    }
+
+}
