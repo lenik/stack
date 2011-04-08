cc56e325699ab3258c13d2056478c2de0b539997
Lenik <xjl@99jsj.com>
2011-04-08 15:23:26 +0800

    MMController pretty format.

--- plover/repr/plover-restful/src/main/java/com/bee32/plover/restful/ModuleManagerController.java
+++ plover/repr/plover-restful/src/main/java/com/bee32/plover/restful/ModuleManagerController.java
@@ -2,6 +2,10 @@ package com.bee32.plover.restful;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 import javax.inject.Inject;
 
@@ -14,8 +18,8 @@ import com.bee32.plover.pub.oid.OidUtil;
 import com.bee32.plover.pub.oid.OidVector;
 import com.bee32.plover.restful.util.IRestfulController;
 
-@Controller
 @Lazy
+@Controller
 public class ModuleManagerController
         implements IRestfulController {
 
@@ -25,12 +29,36 @@ public class ModuleManagerController
     public void index(IRestfulRequest req, IRestfulResponse resp)
             throws IOException {
 
+        PrintWriter out = resp.getWriter();
         resp.setContentType("text/html");
 
-        PrintWriter out = resp.getWriter();
+        out.println("<html>");
+        out.println("    <head>");
+        out.println("        <title>Available Modules</title>");
+        out.println("    </head>");
+        out.println("    <body>");
+
+        out.println("        <h1>Available Modules</h1>");
+        out.println("        <strong>Loaded from: " + moduleLoader + "</strong>");
+
+        out.println("        <h2>Directory</h2>");
+        out.println("        <hr />");
+        out.println("");
         out.println("<ul>");
 
-        for (IModule module : moduleLoader.getModules()) {
+        List<IModule> sorted = new ArrayList<IModule>();
+        for (IModule module : moduleLoader.getModules())
+            sorted.add(module);
+
+        // Sort by module name.
+        Collections.sort(sorted, new Comparator<IModule>() {
+            @Override
+            public int compare(IModule o1, IModule o2) {
+                return o1.getName().compareTo(o2.getName());
+            }
+        });
+
+        for (IModule module : sorted) {
             OidVector oid = OidUtil.getOid(module.getClass());
 
             String name = module.getName();
@@ -42,6 +70,10 @@ public class ModuleManagerController
         }
 
         out.println("</ul>");
+
+        out.println("");
+        out.println("        <hr />");
+        out.println("(C) Copyright 2010-2011, BEE32.com");
     }
 
 }
