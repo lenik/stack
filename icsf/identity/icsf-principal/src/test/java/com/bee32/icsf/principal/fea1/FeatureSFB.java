2792f981160b7c21e62e3f2ee6cfa2bd6de798df
Lenik <xjl@99jsj.com>
2011-04-08 08:37:51 +0800

    Clean up FeaturePlayer related stuff.

--- icsf/identity/icsf-principal/src/test/java/com/bee32/icsf/principal/fea1/FeatureSFB.java
+++
@@ -1,8 +0,0 @@
-package com.bee32.icsf.principal.fea1;
-
-import com.bee32.plover.orm.config.test.TestPurposeSessionFactoryBean;
-
-public class FeatureSFB
-        extends TestPurposeSessionFactoryBean {
-
-}
