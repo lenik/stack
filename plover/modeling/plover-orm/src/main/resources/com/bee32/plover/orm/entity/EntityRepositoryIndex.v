b41d1f60b83a4a5df41c2a7e2ab4a07f5e1246fd
Lenik <xjl@99jsj.com>
2011-04-08 08:52:30 +0800

    ERIndex.v format entity in <pre>.

--- plover/modeling/plover-orm/src/main/resources/com/bee32/plover/orm/entity/EntityRepositoryIndex.v
+++ plover/modeling/plover-orm/src/main/resources/com/bee32/plover/orm/entity/EntityRepositoryIndex.v
@@ -8,12 +8,15 @@
             <hr />
             #foreach ($ent in $it.list())
             <li>
-                <a href="$ent.id">
-                    $ent.id
-                </a>
-                $ent
+                <a href="$ent.id">$ent.id</a>
+                <pre>$ent</pre>
             </li>
             #end
         </ul>
+        <hr />
+        <a href="?X=createForm">Create</a>
+        <div>
+            <a href="..">Module Index</a>
+        </div>
     </body>
 </html>
