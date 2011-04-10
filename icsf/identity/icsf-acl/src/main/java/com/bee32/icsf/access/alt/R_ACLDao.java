61e127f03aef892cd216cbf60eff3e51f1e96fe2
Lenik <xjl@99jsj.com>
2011-04-10 18:45:02 +0800

    Separate R_ACLDao from R_Authority.

---
+++ icsf/identity/icsf-acl/src/main/java/com/bee32/icsf/access/alt/R_ACLDao.java
@@ -0,0 +1,104 @@
+package com.bee32.icsf.access.alt;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.orm.hibernate3.HibernateTemplate;
+
+import com.bee32.icsf.access.Permission;
+import com.bee32.icsf.access.resource.Resource;
+import com.bee32.icsf.access.resource.ResourceRegistry;
+import com.bee32.icsf.principal.IPrincipal;
+import com.bee32.icsf.principal.Principal;
+import com.bee32.plover.orm.dao.GenericDao;
+
+@Lazy
+public class R_ACLDao
+        extends GenericDao {
+
+    // @Inject
+    // R_ACEDao aceDao;
+
+    public R_ACL loadACL(Resource resource) {
+        if (resource == null)
+            throw new NullPointerException("resource");
+
+        String resourceName = ResourceRegistry.toName(resource);
+
+        R_ACL acl = new R_ACL(null, resource);
+
+        List<R_ACE> list = getHibernateTemplate().findByNamedParam(//
+                "from R_ACE where" //
+                        + "   (:q = resourceName or :q like resourceName || '.%')", //
+                "q", resourceName);
+
+        for (R_ACE ace : list)
+            acl.add(ace.getPrincipal(), ace.getPermission());
+
+        return acl;
+    }
+
+    public void saveACL(R_ACL acl) {
+        if (acl == null)
+            throw new NullPointerException("acl");
+
+        HibernateTemplate template = getHibernateTemplate();
+
+        Resource resource = acl.getResource();
+        String resourceName = ResourceRegistry.toName(resource);
+
+        List<R_ACE> existing = template.findByNamedParam(//
+                "from R_ACE where" //
+                        + "   (:q = resourceName or :q like resourceName || '.%')", //
+                "q", resourceName);
+
+        Map<IPrincipal, R_ACE> existingMap = new HashMap<IPrincipal, R_ACE>();
+        for (R_ACE ace : existing)
+            existingMap.put(ace.getPrincipal(), ace);
+
+        List<R_ACE> reorg = new ArrayList<R_ACE>();
+        for (Entry<? extends IPrincipal, Permission> entry : acl.getEntries()) {
+            Principal principal = (Principal) entry.getKey();
+            R_ACE existed = existingMap.remove(principal);
+            if (existed != null) {
+                reorg.add(existed);
+            } else {
+                Permission newPermission = entry.getValue();
+                R_ACE newAce = new R_ACE(resource, principal, newPermission);
+                reorg.add(newAce);
+            }
+        }
+
+        template.deleteAll(existing);
+        template.saveOrUpdateAll(reorg);
+    }
+
+    public Permission getPermission(Resource resource, IPrincipal principal) {
+        if (resource == null)
+            throw new NullPointerException("resource");
+        if (principal == null)
+            throw new NullPointerException("principal");
+
+        String resourceName = ResourceRegistry.toName(resource);
+
+        Long principalId = principal.getId();
+        List<R_ACE> list = getHibernateTemplate().findByNamedParam(// ,
+                "from R_ACE where" //
+                        + "   (:q = resourceName or :q like resourceName || '.%')" //
+                        + "   and principal = :principalId", //
+                new String[] { "q", "principalId" }, //
+                new Object[] { resourceName, principalId });
+
+        // Only the first matching entry is returned.
+        for (R_ACE ace : list)
+            return ace.getPermission();
+
+        // No matching entry.
+        return null;
+    }
+
+}
--- icsf/identity/icsf-acl/src/main/java/com/bee32/icsf/access/alt/R_Authority.java
+++ icsf/identity/icsf-acl/src/main/java/com/bee32/icsf/access/alt/R_Authority.java
@@ -1,25 +1,15 @@
 package com.bee32.icsf.access.alt;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-
 import javax.inject.Inject;
 
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.bee32.icsf.access.Permission;
+import com.bee32.icsf.access.acl.IACL;
 import com.bee32.icsf.access.authority.Authority;
 import com.bee32.icsf.access.resource.Resource;
-import com.bee32.icsf.access.resource.ResourceRegistry;
 import com.bee32.icsf.principal.IPrincipal;
-import com.bee32.icsf.principal.Principal;
-import com.bee32.plover.orm.dao.CommonDataManager;
-import com.bee32.plover.orm.dao.HibernateTemplate;
 
 @Lazy
 @Service
@@ -27,89 +17,16 @@ public class R_Authority
         extends Authority {
 
     @Inject
-    CommonDataManager dataManager;
+    R_ACLDao aclDao;
 
-    @Inject
-    R_ACEDao aceDao;
-
-    @Inject
-    HibernateTemplate hibernateTemplate;
-
-    @Transactional(readOnly = true)
     @Override
-    public R_ACL getACL(Resource resource) {
-        if (resource == null)
-            throw new NullPointerException("resource");
-
-        String resourceName = ResourceRegistry.toName(resource);
-
-        R_ACL acl = new R_ACL(null, resource);
-
-        List<R_ACE> list = hibernateTemplate.findByNamedParam(//
-                "from ace where resourceName = :resourceName", //
-                "resourceName", resourceName);
-
-        for (R_ACE ace : list)
-            acl.add(ace.getPrincipal(), ace.getPermission());
-
-        return acl;
+    public IACL getACL(Resource resource) {
+        return aclDao.loadACL(resource);
     }
 
-    @Transactional(readOnly = true)
     @Override
     public Permission getPermission(Resource resource, IPrincipal principal) {
-        if (resource == null)
-            throw new NullPointerException("resource");
-        if (principal == null)
-            throw new NullPointerException("principal");
-
-        String resourceName = ResourceRegistry.toName(resource);
-
-        Long principalId = principal.getId();
-        List<R_ACE> list = hibernateTemplate.findByNamedParam(// ,
-                "from ace where resourceName = :resourceName and principal = :principalId", //
-                new String[] { "resourceName", "principalId" }, //
-                new Object[] { resourceName, principalId });
-
-        // Only the first matching entry is returned.
-        for (R_ACE ace : list)
-            return ace.getPermission();
-
-        // No matching entry.
-        return null;
-    }
-
-    @Transactional
-    public void saveACL(R_ACL acl) {
-        if (acl == null)
-            throw new NullPointerException("acl");
-
-        Resource resource = acl.getResource();
-        String resourceName = ResourceRegistry.toName(resource);
-
-        List<R_ACE> existing = hibernateTemplate.findByNamedParam(//
-                "from ace where resourceName = :resourceName", //
-                "resourceName", resourceName);
-
-        Map<IPrincipal, R_ACE> existingMap = new HashMap<IPrincipal, R_ACE>();
-        for (R_ACE ace : existing)
-            existingMap.put(ace.getPrincipal(), ace);
-
-        List<R_ACE> reorg = new ArrayList<R_ACE>();
-        for (Entry<? extends IPrincipal, Permission> entry : acl.getEntries()) {
-            Principal principal = (Principal) entry.getKey();
-            R_ACE existed = existingMap.remove(principal);
-            if (existed != null) {
-                reorg.add(existed);
-            } else {
-                Permission newPermission = entry.getValue();
-                R_ACE newAce = new R_ACE(resource, principal, newPermission);
-                reorg.add(newAce);
-            }
-        }
-
-        hibernateTemplate.deleteAll(existing);
-        hibernateTemplate.saveOrUpdateAll(reorg);
+        return aclDao.getPermission(resource, principal);
     }
 
 }
