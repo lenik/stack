4f52c4ba5194602738e92c5ff61c5f71a5b9d805
Lenik <xjl@99jsj.com>
2011-04-11 02:09:45 +0800

    Add admin: ap:*=Srwx to normal sample.

--- icsf/identity/icsf-acl/src/main/java/com/bee32/icsf/access/alt/R_ACLSamples.java
+++ icsf/identity/icsf-acl/src/main/java/com/bee32/icsf/access/alt/R_ACLSamples.java
@@ -1,5 +1,6 @@
 package com.bee32.icsf.access.alt;
 
+import static com.bee32.icsf.principal.IcsfPrincipalSamples.adminRole;
 import static com.bee32.icsf.principal.IcsfPrincipalSamples.kate;
 import static com.bee32.icsf.principal.IcsfPrincipalSamples.registeredRole;
 import static com.bee32.icsf.principal.IcsfPrincipalSamples.tom;
@@ -12,11 +13,13 @@ import com.bee32.plover.orm.util.ImportSamples;
 public class R_ACLSamples
         extends EntitySamplesContribution {
 
+    public R_ACE admin_all;
     public R_ACE service_tom_x;
     public R_ACE method1_kate_rx;
     public R_ACE foo_reguser_w;
 
     public R_ACLSamples() {
+        admin_all = new R_ACE("ap:", adminRole, "Slcdrwx");
         service_tom_x = new R_ACE("ap:TestService", tom, "x");
         method1_kate_rx = new R_ACE("ap:TestService.method1", kate, "rx");
         foo_reguser_w = new R_ACE("ap:TestService.foo", registeredRole, "w");
@@ -24,6 +27,7 @@ public class R_ACLSamples
 
     @Override
     protected void preamble() {
+        addNormalSample(admin_all);
         addNormalSample(service_tom_x);
         addNormalSample(method1_kate_rx);
         addNormalSample(foo_reguser_w);
