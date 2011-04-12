4e66ddcb3f6b81838453815eb40bb39431a224a1
Lenik <xjl@99jsj.com>
2011-04-12 09:03:52 +0800

    Simplified WiredSTC: removed the alt-configClass, should use @Import annotation.

--- plover/library/plover-servlet/src/main/java/com/bee32/plover/servlet/test/WiredServletTestCase.java
+++ plover/library/plover-servlet/src/main/java/com/bee32/plover/servlet/test/WiredServletTestCase.java
@@ -1,88 +1,77 @@
 package com.bee32.plover.servlet.test;
 
-import java.util.Collection;
-import java.util.LinkedHashSet;
-import java.util.Set;
-
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import com.bee32.plover.inject.cref.Import;
 import com.bee32.plover.inject.spring.ContextConfigurationUtil;
+import com.bee32.plover.servlet.context.ServletContextUtil;
 import com.bee32.plover.test.WiredTestCase;
 
 @Import(WiredTestCase.class)
 public abstract class WiredServletTestCase
         extends ServletTestCase {
 
-    private final Class<?> configClass;
+    private ApplicationContext applicationContext;
 
     protected WiredServletTestCase() {
-        this.configClass = getClass();
     }
 
-    protected WiredServletTestCase(Class<?> configClass) {
-        if (configClass == null)
-            throw new NullPointerException("configClass");
-        this.configClass = configClass;
-    }
-
+    /**
+     * Happened before applicationContext is initialized.
+     */
     @Override
     protected void configureContext() {
         super.configureContext();
 
-        // setup
-        ContextLoaderListener contextLoaderListener = new RootContextLoaderListener() {
+        if (applicationContext == null) {
 
-            @Override
-            public void contextInitialized(ServletContextEvent event) {
-                super.contextInitialized(event);
+            // Initialize after the context started.
+            stl.addEventListener(new RootContextListener());
 
-                ServletContext sc = event.getServletContext();
-                ApplicationContext applicationContext = WebApplicationContextUtils.getWebApplicationContext(sc);
-                applicationInitialized(applicationContext);
+            RabbitServletContext context = stl.getServletContext();
+            String locations = ContextConfigurationUtil.getConcatConfigLocations(getClass());
+            context.addInitParam("contextConfigLocation", locations);
+
+        } else {
+
+            // Initialize immediately
+            String rootAppCtx = ServletContextUtil.rootApplicationContextKey;
+            stl.setAttribute(rootAppCtx, applicationContext);
+
+            if (applicationContext instanceof WebApplicationContext) {
+                String webRootAppCtx = ServletContextUtil.webRootApplicationContextKey;
+                stl.setAttribute(webRootAppCtx, applicationContext);
             }
 
-            @Override
-            public void contextDestroyed(ServletContextEvent event) {
-                ServletContext sc = event.getServletContext();
-                ApplicationContext applicationContext = WebApplicationContextUtils.getWebApplicationContext(sc);
-                applicationDestroyed(applicationContext);
+        }
+    }
 
-                super.contextDestroyed(event);
-            }
+    class RootContextListener
+            extends RootContextLoaderListener {
 
-        };
+        @Override
+        public void contextInitialized(ServletContextEvent event) {
+            super.contextInitialized(event);
 
-        stl.addEventListener(contextLoaderListener);
-
-        Set<String> mergedLocations = new LinkedHashSet<String>();
-
-        if (configClass != null) {
-            Collection<String> baseLocations = ContextConfigurationUtil.getContextConfigurationLocations(configClass);
-            mergedLocations.addAll(baseLocations);
+            ServletContext sc = event.getServletContext();
+            ApplicationContext applicationContext = WebApplicationContextUtils.getWebApplicationContext(sc);
+            applicationInitialized(applicationContext);
         }
 
-        Class<?> configClass = getClass();
-        Collection<String> userLocations = ContextConfigurationUtil.getContextConfigurationLocations(configClass);
-        mergedLocations.addAll(userLocations);
+        @Override
+        public void contextDestroyed(ServletContextEvent event) {
+            ServletContext sc = event.getServletContext();
+            ApplicationContext applicationContext = WebApplicationContextUtils.getWebApplicationContext(sc);
+            applicationDestroyed(applicationContext);
 
-        StringBuilder locations = new StringBuilder();
-        for (String location : mergedLocations) {
-            String respath = "classpath:" + location;
-
-            if (locations.length() != 0)
-                locations.append(", ");
-
-            locations.append(respath);
+            super.contextDestroyed(event);
         }
 
-        RabbitServletContext context = stl.getServletContext();
-        context.addInitParam("contextConfigLocation", locations.toString());
     }
 
     protected void applicationInitialized(ApplicationContext applicationContext) {
