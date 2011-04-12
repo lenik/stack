dab8c6036767d5dc9ddf79cc47dadb6398fab6ce
Lenik <xjl@99jsj.com>
2011-04-13 04:21:30 +0800

    Add TigerDto For test.

---
+++ plover/modeling/plover-orm/src/test/java/com/bee32/plover/orm/util/TigerDto.java
@@ -0,0 +1,58 @@
+package com.bee32.plover.orm.util;
+
+import com.bee32.plover.orm.feaCat.Tiger;
+
+public class TigerDto
+        extends DataTransferObject<Tiger, Long> {
+
+    String name;
+    String color;
+    int power;
+
+    public TigerDto() {
+        super();
+    }
+
+    public TigerDto(Tiger entity) {
+        super(entity);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    @Override
+    protected void marshalNonNull(Tiger entity) {
+        this.name = entity.getName();
+        this.color = entity.getColor();
+        this.power = entity.getPower();
+    }
+
+    @Override
+    protected void unmarshalNonNull(Tiger entity) {
+        entity.setName(name);
+        entity.setColor(color);
+        entity.setPower(power);
+    }
+
+}
