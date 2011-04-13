73bd4eafd10dae23acda59606d32616428b5715c
Lenik <xjl@99jsj.com>
2011-04-14 00:58:38 +0800

    Prepare AllowListController.

--- sem/process/sem-process/src/main/java/com/bee32/sem/process/verify/builtin/AllowListController.java
+++ sem/process/sem-process/src/main/java/com/bee32/sem/process/verify/builtin/AllowListController.java
@@ -1,11 +1,15 @@
 package com.bee32.sem.process.verify.builtin;
 
+import java.util.List;
+
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
 
@@ -20,8 +24,14 @@ public class AllowListController
     AllowListDao allowListDao;
 
     @RequestMapping("/process/verify/allowListIndex.htm")
-    public void allowListIndex(HttpServletRequest req, HttpServletResponse resp) {
+    @Transactional(readOnly = true)
+    public ModelMap allowListIndex(HttpServletRequest req, HttpServletResponse resp) {
+        ModelMap mm = new ModelMap();
 
+        List<? extends AllowList> list = allowListDao.list();
+        mm.put("list", list);
+
+        return mm;
     }
 
 }
