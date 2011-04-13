1359b904762507cc4391d7e46bb4c9a893cccac1
Lenik <xjl@99jsj.com>
2011-04-13 11:53:58 +0800

    SEMProcessUnit depends on icsf-identity unit.

--- sem/process/sem-process/src/main/java/com/bee32/sem/process/SEMProcessUnit.java
+++ sem/process/sem-process/src/main/java/com/bee32/sem/process/SEMProcessUnit.java
@@ -1,10 +1,13 @@
 package com.bee32.sem.process;
 
+import com.bee32.icsf.IcsfIdentityUnit;
+import com.bee32.plover.orm.unit.ImportUnit;
 import com.bee32.plover.orm.unit.PersistenceUnit;
 import com.bee32.sem.process.verify.builtin.AllowList;
 import com.bee32.sem.process.verify.builtin.MultiLevel;
 import com.bee32.sem.process.verify.builtin.PassToNext;
 
+@ImportUnit(IcsfIdentityUnit.class)
 public class SEMProcessUnit
         extends PersistenceUnit {
 
