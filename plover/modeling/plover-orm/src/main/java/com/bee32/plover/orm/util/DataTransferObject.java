f45e88b4edde588f0280a84b38b59656d3c6650b
Lenik <xjl@99jsj.com>
2011-04-14 12:00:48 +0800

    DTO marshalList() with selection, and get type args using ClassUtil.getOriginPV

--- plover/modeling/plover-orm/src/main/java/com/bee32/plover/orm/util/DataTransferObject.java
+++ plover/modeling/plover-orm/src/main/java/com/bee32/plover/orm/util/DataTransferObject.java
@@ -1,7 +1,7 @@
 package com.bee32.plover.orm.util;
 
 import java.io.Serializable;
-import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -11,6 +11,8 @@ import javax.free.IllegalUsageException;
 import javax.free.NotImplementedException;
 import javax.servlet.http.HttpServletRequest;
 
+import com.bee32.plover.arch.util.ClassUtil;
+import com.bee32.plover.arch.util.Flags32;
 import com.bee32.plover.orm.entity.EntityAccessor;
 import com.bee32.plover.orm.entity.EntityBean;
 
@@ -21,17 +23,34 @@ public abstract class DataTransferObject<E extends EntityBean<K>, K extends Seri
 
     private Class<E> entityClass;
 
+    protected final Flags32 selection;
+
     protected K id;
     protected Integer version;
 
-    @SuppressWarnings("unchecked")
     public DataTransferObject() {
-        ParameterizedType superclass = (ParameterizedType) getClass().getGenericSuperclass();
-        entityClass = (Class<E>) superclass.getActualTypeArguments()[0];
+        this(0);
+    }
+
+    @SuppressWarnings("unchecked")
+    public DataTransferObject(int selection) {
+        Type[] pv = ClassUtil.getOriginPV(getClass());
+        entityClass = (Class<E>) pv[0];
+
+        this.selection = new Flags32(selection);
     }
 
     public DataTransferObject(E entity) {
         this();
+
+        if (entity == null)
+            throw new NullPointerException("entity");
+
+        marshal(entity);
+    }
+
+    public DataTransferObject(E entity, int selection) {
+        this(selection);
 
         if (entity == null)
             throw new NullPointerException("entity");
@@ -59,6 +78,20 @@ public abstract class DataTransferObject<E extends EntityBean<K>, K extends Seri
         this.id = null;
         this.version = null;
         return this;
+    }
+
+    protected <D extends DataTransferObject<E, K>> D birth() {
+        Class<D> selfType = (Class<D>) getClass();
+
+        D vcopy;
+        try {
+            vcopy = selfType.newInstance();
+        } catch (ReflectiveOperationException e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
+
+        vcopy.selection.set(selection);
+        return vcopy;
     }
 
     public void marshal(E entity) {
@@ -167,7 +200,24 @@ public abstract class DataTransferObject<E extends EntityBean<K>, K extends Seri
     public static <D extends DataTransferObject<Ed, Kd>, Ed extends EntityBean<Kd>, Kd extends Serializable> //
     /*    */List<D> marshalList(Class<D> dtoClass, Ed... entities) {
         List<Ed> entityList = Arrays.asList(entities);
-        return marshalList(dtoClass, entityList);
+        return marshalList(dtoClass, null, entityList);
+    }
+
+    /**
+     * Marshal each entity to a DTO object.
+     *
+     * @param dtoClass
+     *            The DTO type to be marshalled to.
+     * @param selection
+     *            Selection for each entity.
+     * @param entities
+     *            Entity array to be marshalled.
+     * @return <code>null</code>.
+     */
+    public static <D extends DataTransferObject<Ed, Kd>, Ed extends EntityBean<Kd>, Kd extends Serializable> //
+    /*    */List<D> marshalList(Class<D> dtoClass, int selection, Ed... entities) {
+        List<Ed> entityList = Arrays.asList(entities);
+        return marshalList(dtoClass, selection, entityList);
     }
 
     /**
@@ -181,6 +231,22 @@ public abstract class DataTransferObject<E extends EntityBean<K>, K extends Seri
      */
     public static <D extends DataTransferObject<Ed, Kd>, Ed extends EntityBean<Kd>, Kd extends Serializable> //
     /*    */List<D> marshalList(Class<D> dtoClass, Iterable<?> entities) {
+        return marshalList(dtoClass, null, entities);
+    }
+
+    /**
+     * Marshal each entity to a DTO object.
+     *
+     * @param dtoClass
+     *            The DTO type to be marshalled to.
+     * @param selection
+     *            Selection for each entity.
+     * @param entities
+     *            Entities to be marshalled.
+     * @return <code>null</code>.
+     */
+    public static <D extends DataTransferObject<Ed, Kd>, Ed extends EntityBean<Kd>, Kd extends Serializable> //
+    /*    */List<D> marshalList(Class<D> dtoClass, Integer selection, Iterable<?> entities) {
 
         if (entities == null)
             return null;
@@ -197,6 +263,9 @@ public abstract class DataTransferObject<E extends EntityBean<K>, K extends Seri
                     Ed entity = (Ed) _entity;
 
                     dto = dtoClass.newInstance();
+                    if (selection != null)
+                        dto.selection.set(selection);
+
                     dto.marshal(entity);
                 }
                 dtoList.add(dto);
