c69f36f514419acaacfb4d7169fd17d810974fcf
Lenik <xjl@99jsj.com>
2011-04-14 16:03:29 +0800

    Rename MultiLevelRanges to MultiLevelRangeMap.

--- sem/process/sem-process/src/main/java/com/bee32/sem/process/verify/builtin/MultiLevelRangeMap.java
+++ sem/process/sem-process/src/main/java/com/bee32/sem/process/verify/builtin/MultiLevelRangeMap.java
@@ -6,22 +6,21 @@ import java.util.TreeMap;
 import javax.free.LongComparator;
 
 import com.bee32.plover.collections.map.RangeToMap;
-import com.bee32.sem.process.verify.VerifyPolicy;
 
-public class MultiLevelRanges
-        extends RangeToMap<Long, VerifyPolicy<?, ?>> {
+public class MultiLevelRangeMap
+        extends RangeToMap<Long, MultiLevelRange> {
 
     private static final long serialVersionUID = 1L;
 
-    public MultiLevelRanges(Map<? extends Long, ? extends VerifyPolicy<?, ?>> map) {
+    public MultiLevelRangeMap(Map<? extends Long, ? extends MultiLevelRange> map) {
         super(LongComparator.getInstance(), map);
     }
 
-    public MultiLevelRanges(TreeMap<Long, VerifyPolicy<?, ?>> treeMap) {
+    public MultiLevelRangeMap(TreeMap<Long, MultiLevelRange> treeMap) {
         super(LongComparator.getInstance(), treeMap);
     }
 
-    public MultiLevelRanges() {
+    public MultiLevelRangeMap() {
         super(LongComparator.getInstance());
     }
 
