763c787b390125a8a4bdcdc3f076bb77e574dce6
Lenik <xjl@99jsj.com>
2011-04-15 03:04:22 +0800

    Add Marshal/UnmarshalException.

---
+++ plover/core/plover-arch/src/main/java/com/bee32/plover/arch/util/MarshalException.java
@@ -0,0 +1,24 @@
+package com.bee32.plover.arch.util;
+
+public class MarshalException
+        extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public MarshalException() {
+        super();
+    }
+
+    public MarshalException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public MarshalException(String message) {
+        super(message);
+    }
+
+    public MarshalException(Throwable cause) {
+        super(cause);
+    }
+
+}
---
+++ plover/core/plover-arch/src/main/java/com/bee32/plover/arch/util/UnmarshalException.java
@@ -0,0 +1,24 @@
+package com.bee32.plover.arch.util;
+
+public class UnmarshalException
+        extends MarshalException {
+
+    private static final long serialVersionUID = 1L;
+
+    public UnmarshalException() {
+        super();
+    }
+
+    public UnmarshalException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public UnmarshalException(String message) {
+        super(message);
+    }
+
+    public UnmarshalException(Throwable cause) {
+        super(cause);
+    }
+
+}
