6d71ae2983786b109c9b2e42fb37e02c8daa6ad8
Lenik <xjl@99jsj.com>
2011-04-16 14:49:06 +0800

    Add VerifyPolicy base Dao, and other related daos.

---
+++ sem/process/sem-process/src/main/java/com/bee32/sem/process/verify/builtin/dao/VerifyPolicyDao.java
@@ -0,0 +1,10 @@
+package com.bee32.sem.process.verify.builtin.dao;
+
+import com.bee32.plover.orm.entity.EntityDao;
+import com.bee32.sem.process.verify.VerifyPolicy;
+import com.bee32.sem.process.verify.VerifyState;
+
+public class VerifyPolicyDao
+        extends EntityDao<VerifyPolicy<?, VerifyState>, Integer> {
+
+}
