e7f3d50dfed241a2fda5d560691a5a66a3988fc9
Lenik <xjl@99jsj.com>
2011-04-16 14:06:52 +0800

    Released com.bee32.plover:plover-x2html-0.2.3
    IJavascriptDependent was refactored to IScriptElement.

---
+++ plover/repr/plover-x2html/src/main/java/com/bee32/plover/format/html/HtmlWidget.java
@@ -0,0 +1,9 @@
+package com.bee32.plover.format.html;
+
+import com.bee32.plover.javascript.ScriptElement;
+
+public abstract class HtmlWidget
+        extends ScriptElement
+        implements IHtmlWidget {
+
+}
--- plover/repr/plover-x2html/src/main/java/com/bee32/plover/format/html/IHtmlWidget.java
+++ plover/repr/plover-x2html/src/main/java/com/bee32/plover/format/html/IHtmlWidget.java
@@ -1,8 +1,8 @@
 package com.bee32.plover.format.html;
 
-import com.bee32.plover.javascript.IJavascriptDependant;
+import com.bee32.plover.javascript.IScriptElement;
 
 public interface IHtmlWidget
-        extends IJavascriptDependant {
+        extends IScriptElement {
 
 }
