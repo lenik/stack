763ec59289d8d9fa0911104d21a107d96c6785c3
Lenik <xjl@99jsj.com>
2011-04-17 17:42:57 +0800

    OverlappedResourceServlet guess content-type by extension.

--- plover/library/plover-servlet/src/main/java/com/bee32/plover/servlet/test/OverlappedResourceServlet.java
+++ plover/library/plover-servlet/src/main/java/com/bee32/plover/servlet/test/OverlappedResourceServlet.java
@@ -3,6 +3,7 @@ package com.bee32.plover.servlet.test;
 import java.io.IOException;
 import java.net.URL;
 
+import javax.free.FilePath;
 import javax.free.OutputStreamTarget;
 import javax.free.URLResource;
 import javax.servlet.ServletException;
@@ -13,6 +14,8 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.bee32.plover.util.Mime;
 
 /**
  * This servlet should be served as the default servlet, which means to replace the default one.
@@ -41,16 +44,24 @@ public class OverlappedResourceServlet
 
         URL resourceUrl = OverlappedBases.searchResource(path);
 
-        if (resourceUrl != null) {
-            URLResource resource = new URLResource(resourceUrl);
-
-            ServletOutputStream out = resp.getOutputStream();
-
-            new OutputStreamTarget(out).forWrite().writeBytes(resource);
+        if (resourceUrl == null) {
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
         }
 
-        else
-            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        URLResource resource = new URLResource(resourceUrl);
+        String extension = FilePath.getExtension(path);
+        Mime mime = Mime.getInstanceByExtension(extension);
+
+        if (mime != null) {
+            String contentType = mime.getName();
+            resp.setContentType(contentType);
+        }
+
+        ServletOutputStream out = resp.getOutputStream();
+        new OutputStreamTarget(out).forWrite().writeBytes(resource);
+
+        // Also guess the encoding?
     }
 
     static URL searchClassInherited(Class<?> clazz, String path) {
