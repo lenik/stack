02315de6621752192baa691a7f07259f4856d9cc
Lenik <xjl@99jsj.com>
2011-04-18 03:40:54 +0800

    VerifyPolicyDto extends EntityDto. Add getter/setters.

--- sem/process/sem-process/src/main/java/com/bee32/sem/process/verify/builtin/web/VerifyPolicyDto.java
+++ sem/process/sem-process/src/main/java/com/bee32/sem/process/verify/builtin/web/VerifyPolicyDto.java
@@ -6,17 +6,33 @@ import javax.free.IVariantLookupMap;
 import javax.free.ParseException;
 import javax.free.TypeConvertException;
 
-import com.bee32.plover.arch.util.DataTransferObject;
+import com.bee32.plover.orm.util.EntityDto;
 import com.bee32.sem.process.verify.VerifyPolicy;
 import com.bee32.sem.process.verify.VerifyState;
 
 public class VerifyPolicyDto
-        extends DataTransferObject<VerifyPolicy<?, VerifyState>> {
+        extends EntityDto<VerifyPolicy<?, VerifyState>, Integer> {
 
     private static final long serialVersionUID = 1L;
 
     String name;
     String description;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
     @Override
     protected void _marshal(VerifyPolicy<?, VerifyState> source) {
@@ -33,6 +49,7 @@ public class VerifyPolicyDto
     @Override
     public void parse(IVariantLookupMap<String> map)
             throws ParseException, TypeConvertException {
+        super.parse(map);
         name = map.getString("name");
         description = map.getString("description");
     }
