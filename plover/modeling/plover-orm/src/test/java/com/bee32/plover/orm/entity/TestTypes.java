bbfc703ed1faa99d1f4f92d3d74b411da112355e
Lenik <xjl@99jsj.com>
2011-04-20 02:43:32 +0800

    Extract test types from EntityRepositoryTest.

---
+++ plover/modeling/plover-orm/src/test/java/com/bee32/plover/orm/entity/TestTypes.java
@@ -0,0 +1,39 @@
+package com.bee32.plover.orm.entity;
+
+interface IFoo
+        extends IEntity<Integer> {
+    @Override
+    Integer getId();
+}
+
+class Foo
+        extends EntityBean<Integer>
+        implements IFoo {
+
+    private static final long serialVersionUID = 1L;
+
+}
+
+interface Bar
+        extends IEntity<Integer> {
+
+    @Override
+    Integer getId();
+
+}
+
+class BarImpl
+        extends EntityBean<Integer>
+        implements Bar {
+
+    private static final long serialVersionUID = 1L;
+
+}
+
+class FooRepo
+        extends EmptyEntityRepository<Foo, Integer> {
+}
+
+class BarImplRepo
+        extends EmptyEntityRepository<BarImpl, Integer> {
+}
