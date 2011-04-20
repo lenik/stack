01411d9ab3bc3e3e2c815a50fa8b14d842d9b904
Lenik <xjl@99jsj.com>
2011-04-20 14:05:28 +0800

    Add new MetaComponent base type.

---
+++ plover/core/plover-arch/src/main/java/com/bee32/plover/arch/MetaComponent.java
@@ -0,0 +1,14 @@
+package com.bee32.plover.arch;
+
+public abstract class MetaComponent
+        extends Component {
+
+    public MetaComponent() {
+        super();
+    }
+
+    public MetaComponent(String name) {
+        super(name);
+    }
+
+}
