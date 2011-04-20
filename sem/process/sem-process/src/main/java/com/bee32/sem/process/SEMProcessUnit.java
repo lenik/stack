619db859850500fbee4251a26bd164a39ffad516
Lenik <xjl@99jsj.com>
2011-04-20 17:33:22 +0800

    process-unit depends on event-unit.

--- sem/process/sem-process/src/main/java/com/bee32/sem/process/SEMProcessUnit.java
+++ sem/process/sem-process/src/main/java/com/bee32/sem/process/SEMProcessUnit.java
@@ -1,15 +1,15 @@
 package com.bee32.sem.process;
 
-import com.bee32.icsf.IcsfIdentityUnit;
 import com.bee32.plover.orm.unit.ImportUnit;
 import com.bee32.plover.orm.unit.PersistenceUnit;
+import com.bee32.sem.event.SEMEventUnit;
 import com.bee32.sem.process.verify.builtin.AllowList;
-import com.bee32.sem.process.verify.builtin.MultiLevel;
 import com.bee32.sem.process.verify.builtin.Level;
+import com.bee32.sem.process.verify.builtin.MultiLevel;
 import com.bee32.sem.process.verify.builtin.PassStep;
 import com.bee32.sem.process.verify.builtin.PassToNext;
 
-@ImportUnit(IcsfIdentityUnit.class)
+@ImportUnit(SEMEventUnit.class)
 public class SEMProcessUnit
         extends PersistenceUnit {
 
