28a811fd25df603b76c36b4759788a2e7171fe2a
Lenik <xjl@99jsj.com>
2011-04-22 21:12:35 +0800

    Request mapping not work: should not extends MultiActionController.

--- plover/library/plover-facelets/src/test/java/com/bee32/plover/web/faces/test/FaceletsTestController.java
+++ plover/library/plover-facelets/src/test/java/com/bee32/plover/web/faces/test/FaceletsTestController.java
@@ -1,20 +1,30 @@
 package com.bee32.plover.web.faces.test;
 
+import java.io.IOException;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
+
+import com.bee32.plover.servlet.util.ServletDiag;
 
 @Controller
-public class FaceletsSpringIntergration
-        extends MultiActionController {
+@Lazy
+public class FaceletsTestController {
 
     @RequestMapping("/version.htm")
     public ModelAndView version(HttpServletRequest req, HttpServletResponse resp) {
         return new ModelAndView();
+    }
+
+    @RequestMapping("/info.htm")
+    public void info(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+        ServletDiag.dump(req, resp);
     }
 
 }
