bdbb1e81fac6311957e3da45a14e5f9664922bea
Lenik <xjl@99jsj.com>
2011-04-22 16:58:34 +0800

    Point SEMEventMenu.states, .priority -> DictController.

--- sem/base/sem-event/src/main/java/com/bee32/sem/event/SEMEventMenu.java
+++ sem/base/sem-event/src/main/java/com/bee32/sem/event/SEMEventMenu.java
@@ -1,17 +1,29 @@
 package com.bee32.sem.event;
 
+import com.bee32.plover.orm.ext.dict.DictController;
+import com.bee32.plover.orm.util.ITypeAbbrAware;
+import com.bee32.plover.servlet.context.LocationContext;
+import com.bee32.sem.event.entity.EventPriority;
+import com.bee32.sem.event.entity.EventState;
 import com.bee32.sem.frame.Contribution;
 import com.bee32.sem.frame.menu.MenuContribution;
 import com.bee32.sem.frame.menu.MenuEntry;
 
 public class SEMEventMenu
-        extends MenuContribution {
+        extends MenuContribution
+        implements ITypeAbbrAware {
+
+    static LocationContext EVENT = WEB_APP.join(SEMEventModule.PREFIX);
+    static LocationContext DICT = WEB_APP.join(DictController.PREFIX);
 
     @Contribution("sa")
     MenuEntry eventAdmin = new MenuEntry("event");
 
     @Contribution("sa/event")
-    MenuEntry priorities = new MenuEntry(0, "priorities");
+    MenuEntry priorities = new MenuEntry(0, "priorities", DICT.join(ABBR.abbr(EventPriority.class) + "/index.htm"));
+
+    @Contribution("sa/event")
+    MenuEntry states = new MenuEntry(0, "states", DICT.join(ABBR.abbr(EventState.class) + "/index.htm"));
 
     @Contribution(".")
     MenuEntry event = new MenuEntry("event");
