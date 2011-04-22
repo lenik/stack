70294e15d6e6fa9b2404e405cb59325040d274c3
Lenik <xjl@99jsj.com>
2011-04-22 16:59:09 +0800

    Make Module.PREFIX ends with /.

---
+++ sem/base/sem-event/src/test/java/com/bee32/sem/event/SEMEventModuleTest.java
@@ -0,0 +1,20 @@
+package com.bee32.sem.event;
+
+import java.io.IOException;
+
+import com.bee32.plover.orm.unit.Using;
+import com.bee32.sem.test.SEMTestCase;
+
+@Using(SEMEventUnit.class)
+public class SEMEventModuleTest
+        extends SEMTestCase {
+
+    public static void main(String[] args)
+            throws IOException {
+
+        new SEMEventModuleTest().browseAndWait(//
+                // SEMEventMenu.EVENT.join("index.htm").getBase() //
+                SEMEventMenu.DICT.join("abcd/index.htm").getBase() //
+                );
+    }
+}
