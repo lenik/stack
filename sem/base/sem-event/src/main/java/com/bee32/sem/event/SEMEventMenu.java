b4a9a998882eeaf5b5fd109623ca7df1ad007f1c
Lenik <xjl@99jsj.com>
2011-04-24 17:52:56 +0800

    Fix SEMEventMenu.priority to point to EventPriorityController.

--- sem/base/sem-event/src/main/java/com/bee32/sem/event/SEMEventMenu.java
+++ sem/base/sem-event/src/main/java/com/bee32/sem/event/SEMEventMenu.java
@@ -3,9 +3,9 @@ package com.bee32.sem.event;
 import com.bee32.plover.orm.ext.dict.CommonDictController;
 import com.bee32.plover.orm.util.ITypeAbbrAware;
 import com.bee32.plover.servlet.context.LocationContext;
-import com.bee32.sem.event.entity.EventPriority;
 import com.bee32.sem.event.entity.EventState;
 import com.bee32.sem.event.web.EventController;
+import com.bee32.sem.event.web.EventPriorityController;
 import com.bee32.sem.frame.Contribution;
 import com.bee32.sem.frame.menu.MenuContribution;
 import com.bee32.sem.frame.menu.MenuEntry;
@@ -21,10 +21,12 @@ public class SEMEventMenu
     MenuEntry eventAdmin = new MenuEntry("event");
 
     @Contribution("sa/event")
-    MenuEntry priorities = new MenuEntry(0, "priorities", DICT.join(ABBR.abbr(EventPriority.class) + "/index.htm"));
+    MenuEntry priorities = new MenuEntry(0, "priorities", //
+            WEB_APP.join(EventPriorityController.PREFIX).join("index.htm"));
 
     @Contribution("sa/event")
-    MenuEntry states = new MenuEntry(0, "states", DICT.join(ABBR.abbr(EventState.class) + "/index.htm"));
+    MenuEntry states = new MenuEntry(0, "states", //
+            DICT.join(ABBR.abbr(EventState.class) + "/index.htm"));
 
     @Contribution(".")
     MenuEntry event = new MenuEntry("event");
