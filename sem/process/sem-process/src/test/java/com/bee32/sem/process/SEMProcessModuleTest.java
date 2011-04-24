076eaa7284c97130db9f90bdc7e247ac389bcc8f
Lenik <xjl@99jsj.com>
2011-04-24 17:56:22 +0800

    Remove away user tests from *ModuleTest. To avoid duplicated startups.

--- sem/process/sem-process/src/test/java/com/bee32/sem/process/SEMProcessModuleTest.java
+++ sem/process/sem-process/src/test/java/com/bee32/sem/process/SEMProcessModuleTest.java
@@ -2,8 +2,6 @@ package com.bee32.sem.process;
 
 import java.io.IOException;
 
-import org.junit.Test;
-
 import com.bee32.plover.orm.unit.Using;
 import com.bee32.sem.process.verify.builtin.web.MultiLevelController;
 import com.bee32.sem.test.SEMTestCase;
@@ -11,23 +9,6 @@ import com.bee32.sem.test.SEMTestCase;
 @Using(SEMProcessUnit.class)
 public class SEMProcessModuleTest
         extends SEMTestCase {
-
-    @Test
-    public void testUser()
-            throws Exception {
-        String loc = "http://localhost:" + stl.getPort() + PREFIX + "/";
-        System.out.println(loc);
-    }
-
-    @Override
-    protected boolean isDebugMode() {
-        return false;
-    }
-
-    @Override
-    protected int getRefreshPeriod() {
-        return 5;
-    }
 
     public static void main(String[] args)
             throws IOException {
