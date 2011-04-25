71a2c1a3a1ce5b9cdc696488b32726c37205e671
Lenik <xjl@99jsj.com>
2011-04-25 09:10:19 +0800

    Renamed EventStateDao to EventStatusDao.

--- sem/base/sem-event/src/main/java/com/bee32/sem/event/dao/EventStatusDao.java
+++ sem/base/sem-event/src/main/java/com/bee32/sem/event/dao/EventStatusDao.java
@@ -3,7 +3,7 @@ package com.bee32.sem.event.dao;
 import com.bee32.plover.orm.entity.EntityDao;
 import com.bee32.sem.event.entity.EventStatus;
 
-public class EventStateDao
+public class EventStatusDao
         extends EntityDao<EventStatus, Integer> {
 
 }
