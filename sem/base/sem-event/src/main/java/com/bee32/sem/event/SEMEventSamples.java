64ae76a51ebb423c0b316a7f26132511d49a6462
Lenik <xjl@99jsj.com>
2011-04-25 09:19:19 +0800

    Add event samples: rain, kill-angel.

--- sem/base/sem-event/src/main/java/com/bee32/sem/event/SEMEventSamples.java
+++ sem/base/sem-event/src/main/java/com/bee32/sem/event/SEMEventSamples.java
@@ -1,9 +1,17 @@
 package com.bee32.sem.event;
 
+import java.util.Arrays;
+import java.util.Date;
+
+import com.bee32.icsf.principal.IcsfPrincipalSamples;
 import com.bee32.plover.orm.util.EntitySamplesContribution;
+import com.bee32.plover.orm.util.ImportSamples;
+import com.bee32.sem.event.entity.Event;
 import com.bee32.sem.event.entity.EventPriority;
 import com.bee32.sem.event.entity.EventStatus;
+import com.bee32.sem.event.entity.Task;
 
+@ImportSamples(IcsfPrincipalSamples.class)
 public class SEMEventSamples
         extends EntitySamplesContribution {
 
@@ -20,10 +28,41 @@ public class SEMEventSamples
     public static final EventStatus FAILED = new EventStatus("failed", "失败", EventState.FAILED);
     public static final EventStatus ERRORED = new EventStatus("errored", "异常", EventState.ERRORED);
 
+    public static Event rain;
+    public static Task killAngel;
+
+    static {
+        rain = new Event();
+        rain.setCategory("天气预报");
+        rain.setPriority(LOW.getPriority());
+        rain.setState(EventState.UNKNOWN);
+        rain.setClosed(true);
+        rain.setSubject("明天早晨会有流星雨，请大家出门带上望远镜。");
+        rain.setMessage("你以为带上望远镜就能看到流行雨吗？少年哟，别做梦了，那是不可能的。");
+        rain.setBeginTime(new Date(System.currentTimeMillis() - 7200));
+        rain.setBeginTime(new Date(System.currentTimeMillis() + 7200));
+        rain.setObservers(Arrays.asList(IcsfPrincipalSamples.solaRobots));
+
+        killAngel = new Task();
+        killAngel.setCategory("特种");
+        killAngel.setPriority(HIGH.getPriority());
+        killAngel.setState(EventState.RUNNING);
+        killAngel.setClosed(false);
+        killAngel.setSubject("EVA 奉命去消灭第18使徒");
+        killAngel.setMessage("[绝密] 最好能活捉。");
+        killAngel.setBeginTime(new Date(System.currentTimeMillis() + 10));
+        killAngel.setObservers(Arrays.asList(//
+                IcsfPrincipalSamples.solaRobots, //
+                IcsfPrincipalSamples.adminRole));
+    }
+
     @Override
     protected void preamble() {
         addNormalSample(URGENT, HIGH, NORMAL, LOW);
         addNormalSample(UNKNOWN, RUNNING, SUSPENDED, CANCELED, DONE, FAILED, ERRORED);
+
+        addNormalSample(rain);
+        addNormalSample(killAngel);
     }
 
 }
