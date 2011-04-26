9b9ef35f662430284aa6ba6735d073e7223a78c7
Lenik <xjl@99jsj.com>
2011-04-26 21:24:38 +0800

    Add EnterpriseServiceFacade which is transaction unawared.

---
+++ plover/modeling/plover-orm/src/main/java/com/bee32/plover/arch/EnterpriseServiceFacade.java
@@ -0,0 +1,11 @@
+package com.bee32.plover.arch;
+
+import org.springframework.context.annotation.Lazy;
+
+import com.bee32.plover.inject.ComponentTemplate;
+
+@ComponentTemplate
+@Lazy
+public abstract class EnterpriseServiceFacade {
+
+}
