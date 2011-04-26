bf0694812afc09209443c03b7da54993672e78e4
Lenik <xjl@99jsj.com>
2011-04-26 21:24:12 +0800

    EnterpriseService as unmarhsal-context.

--- plover/modeling/plover-orm/src/main/java/com/bee32/plover/arch/EnterpriseService.java
+++ plover/modeling/plover-orm/src/main/java/com/bee32/plover/arch/EnterpriseService.java
@@ -2,19 +2,27 @@ package com.bee32.plover.arch;
 
 import java.io.Serializable;
 
+import javax.inject.Inject;
+
 import org.springframework.context.annotation.Lazy;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.bee32.plover.arch.util.DTOs;
 import com.bee32.plover.inject.ComponentTemplate;
+import com.bee32.plover.orm.dao.CommonDataManager;
 import com.bee32.plover.orm.entity.Entity;
 import com.bee32.plover.orm.util.EntityDto;
+import com.bee32.plover.orm.util.IUnmarshalContext;
 
 @Transactional(readOnly = true)
 @ComponentTemplate
 @Lazy
 public abstract class EnterpriseService
-        extends Component {
+        extends Component
+        implements IUnmarshalContext {
+
+    @Inject
+    protected CommonDataManager dataManager;
 
     public EnterpriseService() {
         super();
@@ -22,6 +30,11 @@ public abstract class EnterpriseService
 
     public EnterpriseService(String serviceName) {
         super(serviceName);
+    }
+
+    @Override
+    public <E extends Entity<K>, K extends Serializable> E loadEntity(Class<E> entityType, K id) {
+        return dataManager.fetch(entityType, id);
     }
 
     protected static <D extends EntityDto<E, K>, E extends Entity<K>, K extends Serializable> D marshal(
