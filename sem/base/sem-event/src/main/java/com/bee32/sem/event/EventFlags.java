a9315821ee467b765032795c937df77b1cbd6431
Lenik <xjl@99jsj.com>
2011-04-27 09:22:02 +0800

    EventFlags.NO_LEVEL, FATAL.

--- sem/base/sem-event/src/main/java/com/bee32/sem/event/EventFlags.java
+++ sem/base/sem-event/src/main/java/com/bee32/sem/event/EventFlags.java
@@ -8,10 +8,12 @@ public class EventFlags
     private static final long serialVersionUID = 1L;
 
     public static final int LEVEL_MASK = 0x0000000f;
-    public static final int CLEAR = 0;
-    public static final int NOTICE = 1;
-    public static final int WARNING = 2;
-    public static final int ERROR = 3;
+    public static final int NO_LEVEL = 0;
+    public static final int CLEAR = 1;
+    public static final int NOTICE = 2;
+    public static final int WARNING = 3;
+    public static final int ERROR = 4;
+    public static final int FATAL = 5;
 
     public int getLevel() {
         return bits & LEVEL_MASK;
