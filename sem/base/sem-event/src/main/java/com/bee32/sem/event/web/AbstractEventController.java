4bffce8289d392c8a8d59a0067e2d9e60e934b46
Lenik <xjl@99jsj.com>
2011-04-28 09:54:11 +0800

    Implemented EventController search feature.

--- sem/base/sem-event/src/main/java/com/bee32/sem/event/web/AbstractEventController.java
+++ sem/base/sem-event/src/main/java/com/bee32/sem/event/web/AbstractEventController.java
@@ -1,26 +1,33 @@
 package com.bee32.sem.event.web;
 
 import java.io.IOException;
+import java.util.Date;
 import java.util.List;
 
+import javax.free.ParseException;
 import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.hibernate.criterion.Restrictions;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.bee32.icsf.principal.dao.UserDao;
 import com.bee32.icsf.principal.dto.UserDto;
+import com.bee32.plover.arch.util.TextMap;
 import com.bee32.plover.orm.ext.util.BasicEntityController;
 import com.bee32.plover.orm.ext.util.DataTableDxo;
+import com.bee32.plover.orm.ext.util.SearchModel;
 import com.bee32.plover.orm.util.DTOs;
 import com.bee32.sem.event.dao.EventPriorityDao;
 import com.bee32.sem.event.dao.EventStatusDao;
 import com.bee32.sem.event.dto.AbstractEventDto;
 import com.bee32.sem.event.dto.EventPriorityDto;
 import com.bee32.sem.event.dto.EventStatusDto;
+import com.bee32.sem.event.entity.Activity;
 import com.bee32.sem.event.entity.Event;
+import com.bee32.sem.event.entity.Task;
 
 public abstract class AbstractEventController<E extends Event, Dto extends AbstractEventDto<E>>
         extends BasicEntityController<E, Long, Dto> {
@@ -47,6 +54,37 @@ public abstract class AbstractEventController<E extends Event, Dto extends Abstr
 
         String controlUrl = null;
         tab.push(controlUrl);
+    }
+
+    @Override
+    protected void fillSearchModel(SearchModel model, TextMap request)
+            throws ParseException {
+
+        String stereo = request.getString("stereo");
+        if (stereo != null) {
+            Class<? extends Event> eventClass = null;
+            if ("EVT".equals(stereo))
+                eventClass = Event.class;
+            else if ("TSK".equals(stereo))
+                eventClass = Task.class;
+            else if ("ACT".equals(stereo))
+                eventClass = Activity.class;
+
+            if (eventClass != null)
+                model.setEntityClass(eventClass);
+        }
+
+        Boolean closed = request.getNBoolean("closed");
+        if (closed != null)
+            model.add(Restrictions.eq("closed", closed));
+
+        Integer recent = request.getNInt("recent");
+        if (recent != null) {
+            long recentSeconds = recent * 86400L;
+            Date beginDate = new Date(System.currentTimeMillis() - recentSeconds);
+            model.add(Restrictions.gt("beginTime", beginDate));
+        }
+
     }
 
     @Override
