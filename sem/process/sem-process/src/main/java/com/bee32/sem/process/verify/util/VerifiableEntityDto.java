6b1410dddbc13952b5912e97cc09557a17876c3b
Lenik <xjl@99jsj.com>
2011-04-29 21:34:20 +0800

    Add VerifiableEntityDto.isVerified()

--- sem/process/sem-process/src/main/java/com/bee32/sem/process/verify/util/VerifiableEntityDto.java
+++ sem/process/sem-process/src/main/java/com/bee32/sem/process/verify/util/VerifiableEntityDto.java
@@ -9,6 +9,7 @@ import com.bee32.sem.event.EventState;
 import com.bee32.sem.event.dto.TaskDto;
 import com.bee32.sem.event.entity.Task;
 import com.bee32.sem.process.verify.IVerifyContext;
+import com.bee32.sem.process.verify.VerifyState;
 import com.bee32.sem.process.verify.builtin.dto.VerifyPolicyDto;
 
 public abstract class VerifiableEntityDto<E extends VerifiableEntityBean<K, ? extends IVerifyContext>, K extends Number>
@@ -67,6 +68,10 @@ public abstract class VerifiableEntityDto<E extends VerifiableEntityBean<K, ? ex
 
     public void setVerifyState(EventState verifyState) {
         this.verifyState = verifyState;
+    }
+
+    public boolean isVerified() {
+        return VerifyState.VERIFIED.equals(verifyState);
     }
 
     public String getVerifyError() {
