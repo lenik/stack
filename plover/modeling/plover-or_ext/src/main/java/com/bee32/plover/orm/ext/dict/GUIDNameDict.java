9e3d309d9fef60d97108b11db753f8beba9527b0
Lenik <xjl@99jsj.com>
2011-05-06 16:08:01 +0800

    Add GUIDNameDict, to separate from LongNameDict.

---
+++ plover/modeling/plover-or_ext/src/main/java/com/bee32/plover/orm/ext/dict/GUIDNameDict.java
@@ -0,0 +1,33 @@
+package com.bee32.plover.orm.ext.dict;
+
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+@AttributeOverrides({//
+/*    */@AttributeOverride(name = "id", column = @Column(length = 32)),
+/*    */@AttributeOverride(name = "label", column = @Column(length = 50)) })
+public abstract class GUIDNameDict
+        extends NameDict {
+
+    private static final long serialVersionUID = 1L;
+
+    public GUIDNameDict() {
+        super();
+    }
+
+    public GUIDNameDict(String name, String label) {
+        super(name, label);
+    }
+
+    public GUIDNameDict(String name, String label, String description) {
+        super(name, label, description);
+    }
+
+    protected void initGUID() {
+
+    }
+
+}
