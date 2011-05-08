0b1ed8c94fd2ceb4f9b9b2e01e19588d8aaf58ce
Lenik <xjl@99jsj.com>
2011-05-08 18:57:00 +0800

    Add TxEntity for biz-transaction purpose entities.

---
+++ sem/base/sem-module/src/main/java/com/bee32/sem/base/tx/TxEntity.java
@@ -0,0 +1,27 @@
+package com.bee32.sem.base.tx;
+
+import java.io.Serializable;
+
+import com.bee32.icsf.principal.User;
+import com.bee32.plover.orm.entity.EntityBean;
+
+/**
+ * 事务性实体所具有的特性：
+ *
+ * <ul>
+ * <li>事务的生命周期管理
+ * <li>事务与事件的集成
+ * <li>事务的一般属性：标题、制单人、审核策略
+ */
+public class TxEntity<K extends Serializable>
+        extends EntityBean<K> {
+
+    private static final long serialVersionUID = 1L;
+
+    String name;
+
+    User creator;
+
+    // logs...?
+
+}
