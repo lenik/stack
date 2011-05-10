d81c05415dc9d4768cf80466ded9e379f19eca25
Lenik <xjl@99jsj.com>
2011-05-10 19:08:36 +0800

    Add Entity.idEquals/idHashCode() for re-use.

--- plover/modeling/plover-orm/src/main/java/com/bee32/plover/orm/entity/Entity.java
+++ plover/modeling/plover-orm/src/main/java/com/bee32/plover/orm/entity/Entity.java
@@ -257,20 +257,28 @@ public abstract class Entity<K extends Serializable>
      *         表示无法判定是否自然键等价。
      */
     protected Boolean naturalEquals(EntityBase<K> other) {
+        return idEquals(other);
+    }
+
+    protected Integer naturalHashCode() {
+        return idHashCode();
+    }
+
+    protected final boolean idEquals(EntityBase<K> other) {
         K id1 = getId();
         K id2 = other.getId();
 
         if (id1 == null || id2 == null)
-            return Boolean.FALSE;
+            return false;
 
         return id1.equals(id2);
     }
 
-    protected Integer naturalHashCode() {
+    protected final int idHashCode() {
         K id = getId();
-        if (id == null)
-            return null;
-        return id.hashCode();
+        if (id != null)
+            return id.hashCode();
+        return System.identityHashCode(this);
     }
 
     /**
