6d4e00684661b29cd40eee35b0047498f47c8e4a
Lenik <xjl@99jsj.com>
2011-05-11 01:46:27 +0800

    polish

--- icsf/identity/icsf-acl/src/main/java/com/bee32/icsf/principal/builtins/IsolatedPrincipal.java
+++ icsf/identity/icsf-acl/src/main/java/com/bee32/icsf/principal/builtins/IsolatedPrincipal.java
@@ -2,11 +2,9 @@ package com.bee32.icsf.principal.builtins;
 
 import java.io.Serializable;
 
-import com.bee32.icsf.principal.Principal;
 import com.bee32.icsf.principal.IPrincipal;
 import com.bee32.icsf.principal.IPrincipalVisitor;
-import com.bee32.plover.util.FormatStyle;
-import com.bee32.plover.util.PrettyPrintStream;
+import com.bee32.icsf.principal.Principal;
 
 public class IsolatedPrincipal
         extends Principal
@@ -28,11 +26,6 @@ public class IsolatedPrincipal
     public void accept(IPrincipalVisitor visitor) {
         if (visitor.startPrincipal(this))
             visitor.endPrincipal(this);
-    }
-
-    @Override
-    public void toString(PrettyPrintStream out, FormatStyle format) {
-        out.print("Isolated :: " + name);
     }
 
 }
