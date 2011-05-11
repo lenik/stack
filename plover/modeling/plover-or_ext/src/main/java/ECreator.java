2b5cf3035c31e10178b9546ff93293d994ecf04c
Lenik <xjl@99jsj.com>
2011-05-11 09:23:34 +0800

    Prepare E-creator.

---
+++ plover/modeling/plover-or_ext/src/main/java/ECreator.java
@@ -0,0 +1,10 @@
+import com.bee32.plover.orm.ext.util.PloverEntityCreator;
+
+public class ECreator {
+
+    public static void main(String[] args)
+            throws Exception {
+        PloverEntityCreator.main(args);
+    }
+
+}
