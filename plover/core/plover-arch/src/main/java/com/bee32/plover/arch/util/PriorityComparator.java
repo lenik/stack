4184a19bafdd008cc9e78fc841ea46a4ba5fa789
Lenik <xjl@99jsj.com>
2011-05-20 17:01:03 +0800

    Added IPriority at last, anyway.

---
+++ plover/core/plover-arch/src/main/java/com/bee32/plover/arch/util/PriorityComparator.java
@@ -0,0 +1,22 @@
+package com.bee32.plover.arch.util;
+
+import javax.free.AbstractNonNullComparator;
+
+public class PriorityComparator
+        extends AbstractNonNullComparator<IPriority> {
+
+    @Override
+    public int compareNonNull(IPriority o1, IPriority o2) {
+        int cmp = o1.getPriority() - o2.getPriority();
+        if (cmp != 0)
+            return cmp;
+
+        int h1 = System.identityHashCode(o1);
+        int h2 = System.identityHashCode(o2);
+        cmp = h1 - h2;
+        return cmp;
+    }
+
+    public static final PriorityComparator INSTANCE = new PriorityComparator();
+
+}
