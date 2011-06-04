58c02dac3bee5f77694593bcbeda64ab875d6574
ztf <ztf@bee32.com>
2011-06-04 14:14:46 +0800

    add constructor and two PersonSidType

--- sem/base/sem-people/src/main/java/com/bee32/sem/people/entity/PersonSidType.java
+++ sem/base/sem-people/src/main/java/com/bee32/sem/people/entity/PersonSidType.java
@@ -10,4 +10,26 @@ public class PersonSidType
 
     private static final long serialVersionUID = 1L;
 
+    public PersonSidType() {
+        super();
+    }
+
+    public PersonSidType(String name, String label, String description) {
+        super(name, label, description);
+    }
+
+    public PersonSidType(String name, String label) {
+        super(name, label);
+    }
+
+    public static PersonSidType GENERATIONONE = new PersonSidType("I.D.", "身份证", "天朝基本证件");
+
+    public static PersonSidType GENERATIONTWO = new PersonSidType("I.D.", "身份证", "天朝基本证件");
+
+// public static PersonSidType IDENTITYCARD = new PersonSidType("I.D.", "身份证", "天朝基本证件");
+
+// public static PersonSidType PASSPORT = new PersonSidType("PASS", "护照");
+
+// public static PersonSidType DRIVINGLICENES = new PersonSidType("DRIV", "驾驶证");
+
 }
