311242003c05b59a15014f8483b1db6503cf6654
Lenik <xjl@99jsj.com>
2011-06-10 17:48:16 +0800

    BEC by handlers and add EntityDto/<Dto> autocasts.

--- plover/modeling/plover-or_ext/src/main/java/com/bee32/plover/orm/ext/util/BasicEntityController.java
+++ plover/modeling/plover-or_ext/src/main/java/com/bee32/plover/orm/ext/util/BasicEntityController.java
@@ -2,10 +2,23 @@ package com.bee32.plover.orm.ext.util;
 
 import java.io.Serializable;
 
+import javax.free.ParseException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.bee32.plover.arch.util.TextMap;
 import com.bee32.plover.orm.entity.Entity;
+import com.bee32.plover.orm.ext.basic.ContentHandler;
+import com.bee32.plover.orm.ext.basic.CreateFormHandler;
+import com.bee32.plover.orm.ext.basic.CreateHandler;
+import com.bee32.plover.orm.ext.basic.DataHandler;
+import com.bee32.plover.orm.ext.basic.EditFormHandler;
+import com.bee32.plover.orm.ext.basic.EditHandler;
+import com.bee32.plover.orm.ext.basic.IEntityForming;
+import com.bee32.plover.orm.ext.basic.IEntityListing;
+import com.bee32.plover.orm.ext.basic.IPostUpdating;
+import com.bee32.plover.orm.ext.basic.IndexHandler;
 import com.bee32.plover.orm.util.EntityDto;
 
 public abstract class BasicEntityController<E extends Entity<K>, K extends Serializable, Dto extends EntityDto<E, K>>
@@ -15,24 +28,101 @@ public abstract class BasicEntityController<E extends Entity<K>, K extends Seria
 
     // protected boolean _createOTF;
 
-    public BasicEntityController() {
+    class Impl
+            implements //
+            IEntityListing<E, K>, //
+            IEntityForming<E, K>, //
+            IPostUpdating<E, K> {
+
+        @SuppressWarnings("unchecked")
+        public final void loadEntry(E entity, EntityDto<E, K> _dto) {
+            Dto dto = (Dto) _dto;
+            BasicEntityController.this.loadEntry(entity, dto);
+        }
+
+        @SuppressWarnings("unchecked")
+        public final void fillDataRow(DataTableDxo tab, EntityDto<E, K> _dto) {
+            Dto dto = (Dto) _dto;
+            BasicEntityController.this.fillDataRow(tab, dto);
+        }
+
+        @Override
+        public void fillSearchModel(SearchModel model, TextMap request)
+                throws ParseException {
+            BasicEntityController.this.fillSearchModel(model, request);
+        }
+
+        @SuppressWarnings("unchecked")
+        public final void createForm(E entity, EntityDto<E, K> _dto) {
+            Dto dto = (Dto) _dto;
+            BasicEntityController.this.createForm(entity, dto);
+        };
+
+        @SuppressWarnings("unchecked")
+        @Override
+        public final void loadForm(E entity, EntityDto<E, K> _dto) {
+            Dto dto = (Dto) _dto;
+            BasicEntityController.this.loadForm(entity, dto);
+        }
+
+        @SuppressWarnings("unchecked")
+        @Override
+        public final void saveForm(E entity, EntityDto<E, K> _dto) {
+            Dto dto = (Dto) _dto;
+            BasicEntityController.this.saveForm(entity, dto);
+        }
+
+        @Override
+        public void postUpdate(E entity) {
+            BasicEntityController.this.postUpdate(entity);
+        }
 
     }
 
-    protected void doAction(EntityAction action, E entity, Dto dto, Object... args) {
-        switch (action.getType()) {
-        case CREATE:
-            // fillTemplate(dto);
-            break;
+    Impl impl = new Impl();
 
-        case LOAD:
-            dto.marshal(this, entity);
-            break;
+    public BasicEntityController() {
+        addHandler(new IndexHandler<E, K>());
+        addHandler(new DataHandler<E, K>(impl));
+        addHandler(new ContentHandler<E, K>(impl));
+        addHandler(new CreateFormHandler<E, K>(impl));
+        addHandler(new CreateHandler<E, K>(impl));
+        addHandler(new EditFormHandler<E, K>(impl));
+        addHandler(new EditHandler<E, K>(impl));
+    }
 
-        case SAVE:
-            dto.unmarshalTo(this, entity);
-            break;
-        }
+    protected void loadEntry(E entity, Dto dto) {
+        dto.marshal(this, entity);
+    }
+
+    /**
+     * Fill nothing in default implementation.
+     */
+    protected abstract void fillDataRow(DataTableDxo tab, Dto dto);
+
+    /**
+     * Fill nothing in default implementation.
+     */
+    protected void fillSearchModel(SearchModel model, TextMap request)
+            throws ParseException {
+    }
+
+    protected void createForm(E entity, Dto dto) {
+        // fillTemplate(dto);
+    }
+
+    protected void loadForm(E entity, Dto dto) {
+        dto.marshal(this, entity);
+    }
+
+    protected void saveForm(E entity, Dto dto) {
+        dto.unmarshalTo(this, entity);
+    }
+
+    /**
+     * Do nothing in default implementation.
+     */
+    protected void postUpdate(E entity) {
     }
 
 }
