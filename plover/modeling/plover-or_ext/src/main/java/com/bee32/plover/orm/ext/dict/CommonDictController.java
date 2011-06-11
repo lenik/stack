a4318927e003352bc055e763dff6e1f4ae401b1a
Lenik <xjl@99jsj.com>
2011-06-11 17:39:41 +0800

    Refactor GEC < BEC

--- plover/modeling/plover-or_ext/src/main/java/com/bee32/plover/orm/ext/dict/CommonDictController.java
+++ plover/modeling/plover-or_ext/src/main/java/com/bee32/plover/orm/ext/dict/CommonDictController.java
@@ -1,21 +1,12 @@
 package com.bee32.plover.orm.ext.dict;
 
-import java.io.IOException;
 import java.io.Serializable;
-import java.util.List;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
-import com.bee32.plover.ajax.JsonUtil;
 import com.bee32.plover.orm.ext.PloverORMExtModule;
 import com.bee32.plover.orm.ext.util.DataTableDxo;
 import com.bee32.plover.orm.ext.util.GenericEntityController;
-import com.bee32.plover.orm.util.DTOs;
 
 @RequestMapping(CommonDictController.PREFIX + "*")
 public class CommonDictController<E extends DictEntity<K>, K extends Serializable, Dto extends DictEntityDto<E, K>>
@@ -24,91 +15,11 @@ public class CommonDictController<E extends DictEntity<K>, K extends Serializabl
     public static final String PREFIX = PloverORMExtModule.PREFIX + "1/";
 
     @Override
-    protected ModelAndView _content(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-        K id = parseRequiredId(req.getParameter("id"));
-        E entity = dataManager.fetch(getEntityType(), id);
-        return it(newDto().marshal(entity));
-    }
-
-    @Override
-    protected ModelAndView _data(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-
-        DataTableDxo tab = new DataTableDxo(req);
-
-        List<? extends Dto> all = DTOs.marshalList(getTransferType(), dataManager.loadAll(getEntityType()));
-
-        tab.totalRecords = all.size();
-        tab.totalDisplayRecords = tab.totalRecords;
-
-        for (Dto dto : all) {
-            tab.push(dto.getId());
-            tab.push(dto.getVersion());
-            // XXX tab.push(dto.getName());
-            tab.push(dto.getLabel());
-            tab.push(dto.getDescription());
-            tab.next();
-        }
-
-        return JsonUtil.dump(resp, tab.exportMap());
-    }
-
-    @Override
-    protected ModelAndView _createOrEditForm(ViewData view, HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException {
-
-        boolean create = view.isMethod("create");
-
-        view.put("_create", create);
-        view.put("_verb", create ? "Create" : "Modify");
-
-        Dto dto;
-        if (create) {
-            dto = newDto();
-            // XXX dto.setName("");
-            dto.setLabel("");
-            dto.setDescription("");
-        } else {
-            K id = parseRequiredId(req.getParameter("id"));
-            E entity = dataManager.fetch(getEntityType(), id);
-            dto = newDto().marshal(entity);
-        }
-        view.put("it", dto);
-
-        return view;
-    }
-
-    @Override
-    protected ModelAndView _createOrEdit(ViewData view, HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-
-        boolean create = view.isMethod("create");
-
-        Dto dto = newDto();
-        dto.parse(req);
-
-        E entity;
-        if (create) {
-            entity = newEntity();
-        } else {
-            K id = dto.getId();
-            if (id == null)
-                throw new ServletException("id isn't specified");
-
-            entity = dataManager.get(getEntityType(), id);
-            if (entity == null)
-                throw new IllegalStateException("No entity whose id=" + id);
-
-            Integer requestVersion = dto.getVersion();
-            if (requestVersion != null && requestVersion != entity.getVersion()) {
-                throw new IllegalStateException("Version obsoleted");
-            }
-        }
-
-        dto.unmarshalTo(entity);
-        dataManager.saveOrUpdate(entity);
-        return view;
+    protected void fillDataRow(DataTableDxo tab, Dto dto) {
+        // XXX tab.push(dto.getName());
+        tab.push(dto.getLabel());
+        tab.push(dto.getDescription());
+        tab.next();
     }
 
 }
--- plover/modeling/plover-or_ext/src/main/java/com/bee32/plover/orm/ext/util/GenericEntityController.java
+++ plover/modeling/plover-or_ext/src/main/java/com/bee32/plover/orm/ext/util/GenericEntityController.java
@@ -1,97 +1,77 @@
 package com.bee32.plover.orm.ext.util;
 
-import java.io.IOException;
 import java.io.Serializable;
 
 import javax.free.IllegalUsageException;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.context.annotation.Lazy;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
-import com.bee32.plover.arch.util.ClassUtil;
 import com.bee32.plover.inject.ComponentTemplate;
 import com.bee32.plover.orm.entity.Entity;
-import com.bee32.plover.orm.entity.EntityUtil;
+import com.bee32.plover.orm.ext.basic.EntityActionRequest;
+import com.bee32.plover.orm.ext.basic.EntityActionResult;
+import com.bee32.plover.orm.ext.basic.EntityHandler;
 import com.bee32.plover.orm.util.EntityDto;
 import com.bee32.plover.orm.util.ITypeAbbrAware;
-import com.bee32.plover.orm.util.TransferBy;
 import com.bee32.plover.servlet.context.ILocationConstants;
+import com.bee32.plover.servlet.mvc.ActionResult;
+import com.bee32.plover.servlet.mvc.IActionHandler;
 
 @ComponentTemplate
 @Lazy
 public abstract class GenericEntityController<E extends Entity<K>, K extends Serializable, Dto extends EntityDto<E, K>>
-        extends EntityController<E, K, Dto>
+        extends BasicEntityController<E, K, Dto>
         implements ITypeAbbrAware, ILocationConstants {
 
-    static ThreadLocal<RequestGenerics> tlRequestGenerics = new ThreadLocal<RequestGenerics>();
-
-    public GenericEntityController() {
-    }
-
     @Override
-    protected Class<? extends K> getKeyType() {
-        return tlRequestGenerics.get().getKeyType();
-    }
+    protected ActionResult invokeHandler(IActionHandler handler, EntityActionRequest req, EntityActionResult result)
+            throws Exception {
 
-    @Override
-    protected Class<? extends E> getEntityType() {
-        return tlRequestGenerics.get().<E, K> getEntityType();
-    }
+        HttpServletResponse resp = result.getResponse();
 
-    @Override
-    protected Class<? extends Dto> getTransferType() {
-        return tlRequestGenerics.get().<Dto, E, K> getTransferType();
-    }
-
-    protected void preinit(String type, HttpServletRequest req, HttpServletResponse resp) {
-        if (type == null)
+        String typeAbbr = req.getPathParameter();
+        if (typeAbbr == null)
             throw new NullPointerException("typeAbbr");
 
         Class<? extends E> entityType;
         try {
-            entityType = (Class<? extends E>) ABBR.expand(type);
+            entityType = (Class<? extends E>) ABBR.expand(typeAbbr);
         } catch (ClassNotFoundException e) {
-            throw new IllegalArgumentException("Qualified class name, but not existed " + type, e);
+            throw new IllegalArgumentException("Qualified class name, but not existed " + typeAbbr, e);
         }
 
         if (entityType == null)
-            throw new IllegalUsageException("Bad entity abbrev: " + type);
+            throw new IllegalUsageException("Bad entity abbrev: " + typeAbbr);
 
         if (!Entity.class.isAssignableFrom(entityType))
             throw new IllegalUsageException("Not subclass of entity: " + entityType);
 
-        RequestGenerics requestGenerics = new RequestGenerics();
-        requestGenerics.entityType = entityType;
-        requestGenerics.keyType = EntityUtil.getKeyType(entityType);
+        if (handler instanceof EntityHandler<?, ?>) {
+            @SuppressWarnings("unchecked")
+            EntityHandler<E, K> eHandler = (EntityHandler<E, K>) handler;
+            eHandler.setEntityType(entityType);
+        }
 
-        TransferBy transferBy = entityType.getAnnotation(TransferBy.class);
-        if (transferBy != null)
-            requestGenerics.transferType = transferBy.value();
-        else
-            // fallback to the declared Dto.
-            requestGenerics.transferType = ClassUtil.infer1(getClass(), GenericEntityController.class, 2);
+        ActionResult ret = handler.handleRequest(req, result);
 
-        tlRequestGenerics.set(requestGenerics);
+        ret = postfix("index", ret);
+        return result;
     }
 
-    protected ModelAndView postfix(String defaultView, ModelAndView modelAndView) {
-        if (modelAndView == null) // End of response?
+    protected ActionResult postfix(String defaultView, ActionResult result) {
+        if (result == null) // End of response?
             return null;
 
-        String vn = modelAndView.getViewName();
-        if (vn != null && !vn.startsWith(_prefix))
-            return modelAndView;
+        String viewName = result.getViewName();
+        if (viewName != null && !viewName.startsWith(_prefix))
+            return result;
 
-        if (vn == null)
-            vn = normalizeView(defaultView);
+        if (viewName == null)
+            viewName = normalizeView(defaultView);
         else {
             assert _prefix.endsWith("/");
-            String xbase = vn.substring(_prefix.length());
+            String xbase = viewName.substring(_prefix.length());
 
             assert !xbase.startsWith("/");
             int slash = xbase.indexOf('/');
@@ -101,20 +81,11 @@ public abstract class GenericEntityController<E extends Entity<K>, K extends Ser
                 ;
             else
                 // skip the first item.
-                vn += xbase.substring(0, slash + 1);
+                viewName += xbase.substring(0, slash + 1);
         }
 
-        modelAndView.setViewName(vn);
-        return modelAndView;
+        result.setViewName(viewName);
+        return result;
     }
 
-    @RequestMapping("{type}/index.htm")
-    public ModelAndView index(@PathVariable String type, HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-        preinit(type, req, resp);
-        ModelAndView view = _index(req, resp);
-        return postfix("index", view);
-    }
-
-}
 }
