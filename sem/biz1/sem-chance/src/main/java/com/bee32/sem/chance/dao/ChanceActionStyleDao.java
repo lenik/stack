d684bb14e3b9047a49e0bc856df11f0ebb373831
ztf <ztf@bee32.com>
2011-06-11 11:35:49 +0800

    chance add NameDictDao

---
+++ sem/biz1/sem-chance/src/main/java/com/bee32/sem/chance/dao/ChanceActionStyleDao.java
@@ -0,0 +1,9 @@
+package com.bee32.sem.chance.dao;
+
+import com.bee32.plover.orm.ext.dict.NameDictDao;
+import com.bee32.sem.chance.entity.ChanceActionStyle;
+
+public class ChanceActionStyleDao
+        extends NameDictDao<ChanceActionStyle> {
+
+}
---
+++ sem/biz1/sem-chance/src/main/java/com/bee32/sem/chance/dao/ChanceCategoryDao.java
@@ -0,0 +1,9 @@
+package com.bee32.sem.chance.dao;
+
+import com.bee32.plover.orm.ext.dict.NameDictDao;
+import com.bee32.sem.chance.entity.ChanceCategory;
+
+public class ChanceCategoryDao
+        extends NameDictDao<ChanceCategory> {
+
+}
