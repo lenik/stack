740e92122656067bc8d275a19e094d4bfb644498
Lenik <xjl@99jsj.com>
2011-06-11 17:41:47 +0800

    MailFilterController apply fillFormExtra.

--- sem/base/sem-mail/src/main/java/com/bee32/sem/mail/web/MailFilterController.java
+++ sem/base/sem-mail/src/main/java/com/bee32/sem/mail/web/MailFilterController.java
@@ -1,24 +1,20 @@
 package com.bee32.sem.mail.web;
 
-import java.io.IOException;
 import java.util.List;
 
 import javax.inject.Inject;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.bee32.plover.orm.ext.util.BasicEntityController;
 import com.bee32.plover.orm.ext.util.DataTableDxo;
 import com.bee32.plover.orm.util.DTOs;
+import com.bee32.plover.servlet.mvc.ActionResult;
 import com.bee32.sem.mail.SEMMailModule;
-import com.bee32.sem.mail.dao.MailFolderDao;
 import com.bee32.sem.mail.dao.MailFilterDao;
-import com.bee32.sem.mail.dto.MailFolderDto;
+import com.bee32.sem.mail.dao.MailFolderDao;
 import com.bee32.sem.mail.dto.MailFilterDto;
+import com.bee32.sem.mail.dto.MailFolderDto;
 import com.bee32.sem.mail.entity.MailFilter;
 
 @RequestMapping(MailFilterController.PREFIX + "*")
@@ -42,14 +38,9 @@ public class MailFilterController
     }
 
     @Override
-    protected ModelAndView _createOrEditForm(ViewData view, HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-        super._createOrEditForm(view, req, resp);
-
+    protected void fillFormExtra(ActionResult result) {
         List<MailFolderDto> mailBoxes = DTOs.marshalList(MailFolderDto.class, 0, mailBoxDao.list());
-        view.put("mailBoxes", mailBoxes);
-
-        return view;
+        result.put("mailBoxes", mailBoxes);
     }
 
 }
