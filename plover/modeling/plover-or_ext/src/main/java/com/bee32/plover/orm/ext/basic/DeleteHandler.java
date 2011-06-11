26dc4d66b3a0864581c22b82c9a8198687259b2d
Lenik <xjl@99jsj.com>
2011-06-11 17:36:24 +0800

    Don't delete locked entities.

--- plover/modeling/plover-or_ext/src/main/java/com/bee32/plover/orm/ext/basic/DeleteHandler.java
+++ plover/modeling/plover-or_ext/src/main/java/com/bee32/plover/orm/ext/basic/DeleteHandler.java
@@ -6,6 +6,8 @@ import org.springframework.dao.DataIntegrityViolationException;
 
 import com.bee32.plover.javascript.util.Javascripts;
 import com.bee32.plover.orm.entity.Entity;
+import com.bee32.plover.orm.entity.EntityAccessor;
+import com.bee32.plover.orm.entity.EntityFlags;
 
 public class DeleteHandler<E extends Entity<K>, K extends Serializable>
         extends EntityHandler<E, K> {
@@ -21,16 +23,23 @@ public class DeleteHandler<E extends Entity<K>, K extends Serializable>
             return Javascripts.alertAndBack("非法对象标识: " + idString + ": " + e.getMessage()).dump(result);
         }
 
-        Object entity = dataManager.fetch(eh.getEntityType(), id);
-        if (entity != null)
-            try {
-                dataManager.delete(entity);
-            } catch (DataIntegrityViolationException e) {
-                return Javascripts.alertAndBack("不能删除正在使用中的对象。" + eh.getHint(id)).dump(result);
-            }
+        E entity = dataManager.fetch(eh.getEntityType(), id);
+        if (entity == null)
+            return Javascripts.alertAndBack("对象不存在: " + idString).dump(result);
 
-        result.sendRedirect("index.htm");
-        return null;
+        EntityFlags ef = EntityAccessor.getFlags(entity);
+        if (ef.isLocked())
+            return Javascripts.alertAndBack("对象被锁定: " + idString).dump(result);
+        // if (ef.isDeleted())
+        // return Javascripts.alertAndBack("对象被标记为删除: " + idString).dump(result);
+
+        try {
+            dataManager.delete(entity);
+        } catch (DataIntegrityViolationException e) {
+            return Javascripts.alertAndBack("不能删除正在使用中的对象。" + eh.getHint(id)).dump(result);
+        }
+
+        return result.sendRedirect("index.htm");
     }
 
 }
