96205b38d0fde69b5ff55169453922b2cbecbb1b
Lenik <xjl@99jsj.com>
2011-06-11 17:34:56 +0800

    Add ActionRequest.normalizeView and .pathParam etc.

--- plover/library/plover-servlet/src/main/java/com/bee32/plover/servlet/mvc/ActionRequest.java
+++ plover/library/plover-servlet/src/main/java/com/bee32/plover/servlet/mvc/ActionRequest.java
@@ -7,19 +7,62 @@ import javax.servlet.http.HttpServletRequestWrapper;
 public class ActionRequest
         extends HttpServletRequestWrapper {
 
-    public static final String METHOD_ATTRIBUTE = "__PLOVER_METHOD";
+    static final String METHOD_ATTRIBUTE = "__PLOVER_METHOD";
+
+    String prefix;
+    String pathParameter;
+    String actionName;
+    String method;
 
     public ActionRequest(HttpServletRequest request) {
         super(request);
     }
 
+    /**
+     * @return Non-<code>null</code> prefix name.
+     */
+    public String getPrefix() {
+        if (prefix == null)
+            throw new IllegalStateException("Prefix wasn't set");
+        return prefix;
+    }
+
+    /**
+     * Initialize the prefix.
+     *
+     * @param prefix
+     *            Non-<code>null</code> prefix name.
+     */
+    public void setPrefix(String prefix) {
+        if (prefix == null)
+            throw new NullPointerException("prefix");
+        this.prefix = prefix;
+    }
+
+    public String getPathParameter() {
+        return pathParameter;
+    }
+
+    public void setPathParameter(String pathParameter) {
+        this.pathParameter = pathParameter;
+    }
+
+    public String getActionName() {
+        return actionName;
+    }
+
+    public void setActionName(String actionName) {
+        this.actionName = actionName;
+    }
+
     public String getMethod() {
-        Object method = getAttribute(METHOD_ATTRIBUTE);
+        // Object method = getAttribute(METHOD_ATTRIBUTE);
         return (String) method;
     }
 
     public void setMethod(String method) {
-        setAttribute(METHOD_ATTRIBUTE, method);
+        // setAttribute(METHOD_ATTRIBUTE, method);
+        this.method = method;
     }
 
     /**
@@ -30,6 +73,10 @@ public class ActionRequest
     public boolean methodEquals(String method) {
         String actualMethod = getMethod();
         return Nullables.equals(actualMethod, actualMethod);
+    }
+
+    public String normalizeView(String relativeViewName) {
+        return getPrefix() + relativeViewName;
     }
 
 }
