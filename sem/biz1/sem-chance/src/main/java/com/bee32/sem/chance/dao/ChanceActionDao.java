2c7c7fab1783185264eda09f3f38ee9b71514e9c
ztf <ztf@bee32.com>
2011-06-14 16:10:27 +0800

    Dao polishing

--- sem/biz1/sem-chance/src/main/java/com/bee32/sem/chance/dao/ChanceActionDao.java
+++ sem/biz1/sem-chance/src/main/java/com/bee32/sem/chance/dao/ChanceActionDao.java
@@ -1,10 +1,29 @@
 package com.bee32.sem.chance.dao;
 
+import java.util.Date;
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Restrictions;
+
 import com.bee32.plover.orm.entity.EntityDao;
 import com.bee32.sem.chance.entity.ChanceAction;
 
 public class ChanceActionDao
         extends EntityDao<ChanceAction, Long> {
 
+    public List<ChanceAction> limitedList(int start, int pageSize) {
+        Criteria criteria = getSession().createCriteria(ChanceAction.class);
+        criteria.setFirstResult(start);
+        criteria.setMaxResults(pageSize);
+        return criteria.list();
+    }
 
+    public List<ChanceAction> limitedSearchList(Date date_start, Date date_end, int start, int pageSize) {
+        Criteria criteria = getSession().createCriteria(ChanceAction.class);
+        criteria.add(Restrictions.between("beginTime", date_start, date_end));
+        criteria.setFirstResult(start);
+        criteria.setMaxResults(pageSize);
+        return criteria.list();
+    }
 }
--- sem/biz1/sem-chance/src/main/java/com/bee32/sem/chance/dao/ChanceDao.java
+++ sem/biz1/sem-chance/src/main/java/com/bee32/sem/chance/dao/ChanceDao.java
@@ -1,9 +1,53 @@
 package com.bee32.sem.chance.dao;
 
+import java.util.List;
+
+import javax.free.UnexpectedException;
+
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Restrictions;
+
 import com.bee32.plover.orm.entity.EntityDao;
 import com.bee32.sem.chance.entity.Chance;
 
 public class ChanceDao
         extends EntityDao<Chance, Long> {
+
+    public List<Chance> limitedList(int start, int pageSize) {
+        Criteria criteria = getSession().createCriteria(Chance.class);
+        criteria.setFirstResult(start);
+        criteria.setMaxResults(pageSize);
+        return criteria.list();
+    }
+
+    public List<Chance> limitedList(String keyword, int start, int pageSize) {
+        Criteria criteria = getSession().createCriteria(Chance.class);
+        if (keyword != null && keyword.length() != 0)
+            criteria.add(Restrictions.like("subject", keyword));
+        criteria.setFirstResult(start);
+        criteria.setMaxResults(pageSize);
+        return criteria.list();
+    }
+
+    //FIXME 返回空,奇怪
+    public List<Chance> chanceSearch(String keyword){
+        Criteria criteria = getSession().createCriteria(Chance.class);
+        criteria.add(Restrictions.like("subject", keyword));
+        return criteria.list();
+    }
+
+    public int getChanceCount(String keyword) {
+        Criteria criteria = getSession().createCriteria(Chance.class);
+        if (keyword != null && keyword.length() != 0)
+            criteria.add(Restrictions.like("subject", keyword));
+        criteria.setProjection(Projections.rowCount());
+        Object result = criteria.uniqueResult();
+
+        if (result == null)
+            throw new UnexpectedException("getChanceCount() returns null");
+
+        return ((Number) result).intValue();
+    }
 
 }
