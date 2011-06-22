c2a24306ea4c30b1bc431312be98659cea00c4d3
ztf <ztf@bee32.com>
2011-06-22 14:48:45 +0800

    service 修改

--- sem/biz1/sem-chance/src/main/java/com/bee32/sem/chance/service/ChanceService.java
+++ sem/biz1/sem-chance/src/main/java/com/bee32/sem/chance/service/ChanceService.java
@@ -1,16 +1,24 @@
 package com.bee32.sem.chance.service;
 
+import java.util.Date;
 import java.util.List;
 
 import javax.inject.Inject;
+import javax.servlet.http.HttpSession;
 
 import org.hibernate.criterion.Restrictions;
 
+import com.bee32.icsf.principal.IUserPrincipal;
 import com.bee32.icsf.principal.dao.UserDao;
 import com.bee32.plover.arch.EnterpriseService;
+import com.bee32.plover.servlet.util.ThreadHttpContext;
 import com.bee32.sem.chance.dao.ChanceActionDao;
+import com.bee32.sem.chance.dao.ChanceDao;
+import com.bee32.sem.chance.entity.Chance;
+import com.bee32.sem.chance.entity.ChanceAction;
 import com.bee32.sem.people.dao.PartyDao;
 import com.bee32.sem.people.entity.Party;
+import com.bee32.sem.user.util.SessionLoginInfo;
 
 public class ChanceService
         extends EnterpriseService
@@ -23,26 +31,104 @@ public class ChanceService
     ChanceActionDao chanceActionDao;
 
     @Inject
+    ChanceDao chanceDao;
+
+    @Inject
     PartyDao<Party> partyDao;
 
+// @Override
+// public int getPartyCount() {
+// return (int) partyDao.count();
+// }
+//
+// @Override
+// public List<Party> limitedPartyList(int first, int pageSize){
+// return null;
+// }
+
+// @Override
+// public int limitedPartyKeywordListCount(String keyword) {
+// return (int) partyDao.count(Restrictions.like("name", "%" + keyword + "%"));
+// }
+//
+// @Override
+// public List<Party> limitedPartyKeywordList(String keyword, int displayStart, int displayLength) {
+// return partyDao.limitedKeywordList(Party.class, keyword, displayStart, displayLength);
+// }
+
     @Override
-    public int getAllPartyCount() {
-        return (int) partyDao.count();
+    public int getChanceActionCount() {
+        HttpSession session = ThreadHttpContext.requireSession();
+        IUserPrincipal user = (IUserPrincipal) SessionLoginInfo.requireCurrentUser(session);
+        return (int) chanceActionDao.count(Restrictions.eq("actor.id", user.getId()));
     }
 
     @Override
-    public int getPartyCountByKeyword(String keyword) {
-        return (int) partyDao.count(Restrictions.like("name", keyword));
+    public List<ChanceAction> limitedChanceActionList(int start, int pageSize) {
+        HttpSession session = ThreadHttpContext.requireSession();
+        IUserPrincipal user = (IUserPrincipal) SessionLoginInfo.requireCurrentUser(session);
+        return chanceActionDao.limitedList(user, start, pageSize);
     }
 
     @Override
-    public List<Party> findLimitedParties(int displayStart, int displayLength) {
-        return partyDao.limitedList(Party.class, displayStart, displayLength);
+    public int limitedChanceActionRangeListCount(Date sbt, Date set) {
+        HttpSession session = ThreadHttpContext.requireSession();
+        IUserPrincipal user = (IUserPrincipal) SessionLoginInfo.requireCurrentUser(session);
+        return chanceActionDao.limitedSearchListCount(user, sbt, set);
     }
 
     @Override
-    public List<Party> findPartyByKeywords(String keyword, int displayStart, int displayLength) {
-        return partyDao.limitedKeywordList(Party.class, keyword, displayStart, displayLength);
+    public List<ChanceAction> limitedChanceActionRangeList(Date sbt, Date set, int start, int pageSize) {
+        HttpSession session = ThreadHttpContext.requireSession();
+        IUserPrincipal user = (IUserPrincipal) SessionLoginInfo.requireCurrentUser(session);
+        return chanceActionDao.limitedSearchList(user, sbt, set, start, pageSize);
+    }
+
+    @Override
+    public List<ChanceAction> dateRangeActionList(Date begin, Date end) {
+        HttpSession session = ThreadHttpContext.requireSession();
+        IUserPrincipal user = (IUserPrincipal) SessionLoginInfo.requireCurrentUser(session);
+        return chanceActionDao.dateRangeList(user, begin, end);
+    }
+
+    @Override
+    public List<Chance> keywordChanceList(String keyword) {
+        HttpSession session = ThreadHttpContext.requireSession();
+        IUserPrincipal user = (IUserPrincipal) SessionLoginInfo.requireCurrentUser(session);
+        return chanceDao.keywordList(user, keyword);
+    }
+
+    @Override
+    public List<Party> keywordPartyList(String keyword) {
+        HttpSession session = ThreadHttpContext.requireSession();
+        IUserPrincipal user = (IUserPrincipal) SessionLoginInfo.requireCurrentUser(session);
+        return partyDao.limitedKeywordList(Party.class, user, keyword);
+    }
+
+    @Override
+    public int getChanceCount() {
+        return chanceDao.count();
+    }
+
+    @Override
+    public List<Chance> limitedChanceList(int first, int pageSize) {
+        HttpSession session = ThreadHttpContext.requireSession();
+        IUserPrincipal user = (IUserPrincipal) SessionLoginInfo.requireCurrentUser(session);
+        return chanceDao.limitedList(user, first, pageSize);
+    }
+
+    @Override
+    public int searchedChanceCount() {
+        HttpSession session = ThreadHttpContext.requireSession();
+        IUserPrincipal user = (IUserPrincipal) SessionLoginInfo.requireCurrentUser(session);
+        return chanceDao.count(Restrictions.eq("owner.id", user.getId()));
+    }
+
+    @Override
+    public List<Chance> limitedSearchChanceList(String keyword, int first, int pageSize) {
+        HttpSession session = ThreadHttpContext.requireSession();
+        IUserPrincipal user = (IUserPrincipal) SessionLoginInfo.requireCurrentUser(session);
+        return chanceDao.limitedSearchList(user, keyword, first, pageSize);
     }
 
 }
--- sem/biz1/sem-chance/src/main/java/com/bee32/sem/chance/service/IChanceService.java
+++ sem/biz1/sem-chance/src/main/java/com/bee32/sem/chance/service/IChanceService.java
@@ -1,16 +1,69 @@
 package com.bee32.sem.chance.service;
 
+import java.util.Date;
 import java.util.List;
 
+import com.bee32.sem.chance.entity.Chance;
+import com.bee32.sem.chance.entity.ChanceAction;
 import com.bee32.sem.people.entity.Party;
 
 public interface IChanceService {
 
-    int getAllPartyCount();
+// int getPartyCount();
+// List<Party> limitedPartyList(int displayStart, int displayLength);
 
-    int getPartyCountByKeyword(String keyword);
+// int limitedPartyKeywordListCount(String keyword);
+// List<Party> limitedPartyKeywordList(String keyword, int displayStart, int displayLength);
 
-    List<Party> findLimitedParties(int displayStart, int displayLength);
+    /**
+     * return all ChanceAction number corresponding to the current User
+     * 取得当前用户对应的所有行动记录个数
+     */
+    int getChanceActionCount();
 
-    List<Party> findPartyByKeywords(String keyword, int displayStart, int displayLength);
+    /**
+     * pagination show the ChanceAction corresponding to the current User
+     * 分页显示当前用户对应的行动记录
+     */
+    List<ChanceAction> limitedChanceActionList(int first, int pageSize);
+
+    /**
+     * get the number of records that within the (Date)begin and (Date)end time range, corresponding
+     * to the current User
+     * 取得时间段内,当前用户对应的行动记录个数
+     */
+    int limitedChanceActionRangeListCount(Date begin, Date end);
+
+    /**
+     * pagination show the ChanceAction records corresponding to the current User within time
+     * range (begin to end)
+     * 分页显示时间段内当前用户的行动记录
+     */
+    List<ChanceAction> limitedChanceActionRangeList(Date begin, Date end, int first, int pageSize);
+
+    List<ChanceAction> dateRangeActionList(Date begin, Date end);
+
+    /**
+     * search records,unnecessary to pagination
+     */
+    List<Chance> keywordChanceList(String keyword);
+
+    /**
+     * search records,unnecessary to pagination
+     */
+    List<Party> keywordPartyList(String keyword);
+
+    /**
+     * 当前用户对应的机会个数
+     */
+    int getChanceCount();
+    /**
+     * pagenation chance records corresponding to the current user
+     */
+    List<Chance> limitedChanceList(int first, int pageSize);
+
+    int searchedChanceCount();
+
+    List<Chance> limitedSearchChanceList(String keyword, int first, int pageSize);
+
 }
