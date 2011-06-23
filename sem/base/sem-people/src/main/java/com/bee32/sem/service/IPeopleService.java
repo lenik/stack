2b493ea75da970ef7dd60d6779d973101d80c5a2
Lenik <xjl@99jsj.com>
2011-06-23 17:46:56 +0800

    polishing.

--- sem/base/sem-people/src/main/java/com/bee32/sem/service/IPeopleService.java
+++ sem/base/sem-people/src/main/java/com/bee32/sem/service/IPeopleService.java
@@ -6,9 +6,15 @@ import com.bee32.sem.people.dto.OrgDto;
 import com.bee32.sem.people.dto.PersonDto;
 
 public interface IPeopleService {
-	List<PersonDto> listPersonByCurrentUser(Integer start, Integer count);
-	long listPersonByCurrentUserCount();
+
+    List<PersonDto> listPersonByCurrentUser(Integer start, Integer count);
+
+    long listPersonByCurrentUserCount();
+
     List<OrgDto> listOrgByCurrentUser(Integer start, Integer count);
+
     long listOrgByCurrentUserCount();
-    public List<PersonDto> listPersonByCurrentUser(String keyword);
+
+    List<PersonDto> listPersonByCurrentUser(String keyword);
+
 }
