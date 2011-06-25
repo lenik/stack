f042c84ee6106e84c07c8cae1649a61c6c0de2bd
jack <superjwl@gmail.com>
2011-06-25 17:16:03 +0800

    权限管理制作中

--- sem/base/sem-people/src/main/java/com/bee32/sem/people/web/PermissionAdminBean.java
+++ sem/base/sem-people/src/main/java/com/bee32/sem/people/web/PermissionAdminBean.java
@@ -1,15 +1,22 @@
 package com.bee32.sem.people.web;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 
 import org.primefaces.event.SelectEvent;
 import org.primefaces.event.UnselectEvent;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import com.bee32.icsf.access.Permission;
+import com.bee32.icsf.access.ResourcePermission;
+import com.bee32.icsf.access.alt.R_ACLService;
 import com.bee32.icsf.access.resource.IResourceNamespace;
 import com.bee32.icsf.access.resource.Resource;
 import com.bee32.icsf.access.resource.ScannedResourceRegistry;
@@ -19,6 +26,7 @@ import com.bee32.icsf.principal.User;
 import com.bee32.icsf.principal.dto.GroupDto;
 import com.bee32.icsf.principal.dto.RoleDto;
 import com.bee32.icsf.principal.dto.UserDto;
+import com.bee32.plover.arch.util.ClassUtil;
 import com.bee32.plover.orm.util.DTOs;
 import com.bee32.plover.orm.util.EntityViewBean;
 
@@ -28,40 +36,14 @@ public class PermissionAdminBean extends EntityViewBean {
 
     private static final long serialVersionUID = 1L;
 
-    private UserDto selectedUser;
-    private RoleDto selectedRole;
-    private GroupDto selectedGroup;
+    private List<RPEntry> rpEntries;
+    private RPEntry selectedRpEntry;
 
-    private Resource selectedPermission;
 
 
     @PostConstruct
     public void init() {
 
-    }
-
-    public UserDto getSelectedUser() {
-        return selectedUser;
-    }
-
-    public void setSelectedUser(UserDto selectedUser) {
-        this.selectedUser = selectedUser;
-    }
-
-    public RoleDto getSelectedRole() {
-        return selectedRole;
-    }
-
-    public void setSelectedRole(RoleDto selectedRole) {
-        this.selectedRole = selectedRole;
-    }
-
-    public GroupDto getSelectedGroup() {
-        return selectedGroup;
-    }
-
-    public void setSelectedGroup(GroupDto selectedGroup) {
-        this.selectedGroup = selectedGroup;
     }
 
     public List<UserDto> getUsers() {
@@ -85,35 +67,64 @@ public class PermissionAdminBean extends EntityViewBean {
         return groupDtos;
     }
 
-    public List<Resource> getPermissions() {
-        ScannedResourceRegistry srr = getBean(ScannedResourceRegistry.class);
-
-	List<Resource> resources = new ArrayList<Resource>();
-
-	    for(IResourceNamespace rn: srr.getNamespaces()) {
-		for(Resource res: rn.getResources()) {
-			resources.add(res);
-		}
-	    }
-
-	    return resources;
-    }
-
-    public Resource getSelectedPermission() {
-		return selectedPermission;
+	public List<RPEntry> getRpEntries() {
+		return rpEntries;
 	}
 
-	public void setSelectedPermission(Resource selectedPermission) {
-		this.selectedPermission = selectedPermission;
+	public void setRpEntries(List<RPEntry> rpEntries) {
+		this.rpEntries = rpEntries;
 	}
 
+	public RPEntry getSelectedRpEntry() {
+		return selectedRpEntry;
+	}
 
-
+	public void setSelectedRpEntry(RPEntry selectedRpEntry) {
+		this.selectedRpEntry = selectedRpEntry;
+	}
 
 
 
 
 	public void onRowSelectUser(SelectEvent event) {
+		FacesContext context = FacesContext.getCurrentInstance();
+
+		UserDto selectedUser = (UserDto) event.getObject();
+
+		if(selectedUser == null) {
+			context.addMessage(null, new FacesMessage("提示", "请选择需要设置权限的用户!"));
+            return;
+		}
+
+        ScannedResourceRegistry srr = getBean(ScannedResourceRegistry.class);
+        R_ACLService aclService = getBean(R_ACLService.class);
+
+
+        Map<String, ResourcePermission> havePermissions = new HashMap<String, ResourcePermission>();
+        List<ResourcePermission> haveResourcePermissions = aclService.getResourcePermissions(selectedUser.unmarshal());
+        for(ResourcePermission rp : haveResourcePermissions) {
+		String permissionQulifier = srr.qualify(rp.getResource());
+		havePermissions.put(permissionQulifier, rp);
+        }
+
+	rpEntries = new ArrayList<RPEntry>();
+	    for(IResourceNamespace rn: srr.getNamespaces()) {
+		for(Resource res: rn.getResources()) {
+			String resourceType = ClassUtil.getDisplayName(res.getClass());
+			String permissionQulifier = srr.qualify(res);
+			RPEntry rpEntry = new RPEntry();
+			rpEntry.setResourceType(resourceType);
+			rpEntry.setDisplayName(res.getAppearance().getDisplayName());
+			rpEntry.setQualifiedName(permissionQulifier);
+
+			if(havePermissions.containsKey(permissionQulifier)) {
+				rpEntry.setPermission(havePermissions.get(permissionQulifier).getPermission());
+			} else {
+				rpEntry.setPermission(new Permission(0));
+			}
+		}
+	    }
+
     }
 
     public void onRowUnselectUser(UnselectEvent event) {
