14481330798c533303642c7a3dd21c3e3391def6
Lenik <xjl@99jsj.com>
2011-06-27 17:24:12 +0800

    Polish R_ACLSamples, don't import statics.

--- icsf/identity/icsf-acl/src/main/java/com/bee32/icsf/access/alt/R_ACLSamples.java
+++ icsf/identity/icsf-acl/src/main/java/com/bee32/icsf/access/alt/R_ACLSamples.java
@@ -1,10 +1,5 @@
 package com.bee32.icsf.access.alt;
 
-import static com.bee32.icsf.principal.IcsfPrincipalSamples.adminRole;
-import static com.bee32.icsf.principal.IcsfPrincipalSamples.kate;
-import static com.bee32.icsf.principal.IcsfPrincipalSamples.registeredRole;
-import static com.bee32.icsf.principal.IcsfPrincipalSamples.tom;
-
 import com.bee32.icsf.principal.IcsfPrincipalSamples;
 import com.bee32.plover.orm.util.EntitySamplesContribution;
 import com.bee32.plover.orm.util.ImportSamples;
@@ -13,16 +8,23 @@ import com.bee32.plover.orm.util.ImportSamples;
 public class R_ACLSamples
         extends EntitySamplesContribution {
 
-    public R_ACE admin_all;
-    public R_ACE service_tom_x;
-    public R_ACE method1_kate_rx;
-    public R_ACE foo_reguser_w;
+    static R_ACE admin_all;
+    static R_ACE service_tom_x;
+    static R_ACE method1_kate_rx;
+    static R_ACE foo_reguser_w;
 
     public R_ACLSamples() {
-        admin_all = new R_ACE("ap:", adminRole, "Slcdrwx");
-        service_tom_x = new R_ACE("ap:TestService.", tom, "x");
-        method1_kate_rx = new R_ACE("ap:TestService.method1.", kate, "rx");
-        foo_reguser_w = new R_ACE("ap:TestService.foo.", registeredRole, "w");
+        admin_all = new R_ACE("ap:",//
+                IcsfPrincipalSamples.adminRole, "Slcdrwx");
+
+        service_tom_x = new R_ACE("ap:TestService.", //
+                IcsfPrincipalSamples.tom, "x");
+
+        method1_kate_rx = new R_ACE("ap:TestService.method1.", //
+                IcsfPrincipalSamples.kate, "rx");
+
+        foo_reguser_w = new R_ACE("ap:TestService.foo.", //
+                IcsfPrincipalSamples.registeredRole, "w");
     }
 
     @Override
