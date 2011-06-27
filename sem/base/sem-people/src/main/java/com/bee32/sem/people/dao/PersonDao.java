6f6e6458cda6ae4b8089ee887d29d943ef887398
Lenik <xjl@99jsj.com>
2011-06-27 18:25:30 +0800

    Added PersonDao for DaoManager.

---
+++ sem/base/sem-people/src/main/java/com/bee32/sem/people/dao/PersonDao.java
@@ -0,0 +1,9 @@
+package com.bee32.sem.people.dao;
+
+import com.bee32.plover.orm.entity.EntityDao;
+import com.bee32.sem.people.entity.Person;
+
+public class PersonDao
+        extends EntityDao<Person, Integer> {
+
+}
