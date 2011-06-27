98e9369d96657193f8984d713c71354c49fc8d39
Lenik <xjl@99jsj.com>
2011-06-27 17:21:43 +0800

    CDM <>--->  daoManager.

--- plover/modeling/plover-orm/src/main/java/com/bee32/plover/orm/dao/CommonDataManager.java
+++ plover/modeling/plover-orm/src/main/java/com/bee32/plover/orm/dao/CommonDataManager.java
@@ -8,7 +8,6 @@ import javax.inject.Inject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,20 +20,22 @@ import com.bee32.plover.orm.entity.IEntityAccessService;
 @Service
 @Lazy
 @Transactional(readOnly = true)
-public class CommonDataManager
-// extends HibernateDaoSupport
-        implements ApplicationContextAware {
+public class CommonDataManager {
 
     static Logger logger = LoggerFactory.getLogger(CommonDataManager.class);
 
     @Inject
     ApplicationContext applicationContext;
 
+    @Inject
+    DaoManager daoManager;
+
     public <E extends Entity<? extends K>, K extends Serializable> //
     IEntityAccessService<E, K> access(Class<? extends E> entityType) {
-        EntityDao<E, K> dao = (EntityDao<E, K>) daoMap.get(entityType);
+
+        EntityDao<? super E, ? super K> dao = daoManager.getNearestDao(entityType);
         if (dao == null)
-            throw new IllegalUsageException("No EntityDao exists for entity " + entityType);
+            throw new IllegalUsageException("No suitable EntityDao for entity " + entityType);
 
         EasTxWrapper<E, K> wrapper = applicationContext.getBean(EasTxWrapper.class);
         wrapper.setDao(dao);
