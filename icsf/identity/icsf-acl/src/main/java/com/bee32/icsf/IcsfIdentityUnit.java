f554d7fb4dbe1a71e2d6629fb9d45f89812f25b4
Lenik <xjl@99jsj.com>
2011-06-29 17:50:25 +0800

    Add to identity-unit: UserPassword, DACL, DACE.

--- icsf/identity/icsf-acl/src/main/java/com/bee32/icsf/IcsfIdentityUnit.java
+++ icsf/identity/icsf-acl/src/main/java/com/bee32/icsf/IcsfIdentityUnit.java
@@ -1,6 +1,9 @@
 package com.bee32.icsf;
 
 import com.bee32.icsf.access.alt.R_ACE;
+import com.bee32.icsf.access.dacl.DACE;
+import com.bee32.icsf.access.dacl.DACL;
+import com.bee32.icsf.login.UserPassword;
 import com.bee32.icsf.principal.Group;
 import com.bee32.icsf.principal.Principal;
 import com.bee32.icsf.principal.Role;
@@ -24,9 +27,13 @@ public class IcsfIdentityUnit
         // add(Realm.class);
 
         add(UserEmail.class);
+        add(UserPassword.class);
 
         // add(R_ACL.class);
         add(R_ACE.class);
+
+        add(DACL.class);
+        add(DACE.class);
     }
 
 }
