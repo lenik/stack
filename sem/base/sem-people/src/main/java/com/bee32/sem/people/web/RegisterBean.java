27cc9c6ef4e73d5431676a84f55d1bb9885b9062
jack <superjwl@gmail.com>
2011-06-30 11:13:16 +0800

    增加用户注册功能

---
+++ sem/base/sem-people/src/main/java/com/bee32/sem/people/web/RegisterBean.java
@@ -0,0 +1,94 @@
+package com.bee32.sem.people.web;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import com.bee32.icsf.login.UserPassword;
+import com.bee32.icsf.principal.User;
+import com.bee32.plover.orm.util.EntityViewBean;
+
+@Component
+@Scope("view")
+public class RegisterBean extends EntityViewBean {
+
+	private static final long serialVersionUID = 1L;
+
+	private String username;
+	private String fullname;
+	private String password;
+	private String passwordConfirm;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public String getFullname() {
+		return fullname;
+	}
+
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPasswordConfirm() {
+		return passwordConfirm;
+	}
+
+	public void setPasswordConfirm(String passwordConfirm) {
+		this.passwordConfirm = passwordConfirm;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+
+
+
+	public void register(ActionEvent even) {
+		FacesMessage msg = null;
+
+		if(!password.equals(passwordConfirm)) {
+			msg = new FacesMessage(FacesMessage.SEVERITY_WARN, "注册错误", "密码和密码确认不匹配");
+			FacesContext.getCurrentInstance().addMessage(null, msg);
+			return;
+		}
+
+		User user = serviceFor(User.class).get(username);
+		if(user != null) {
+			msg = new FacesMessage(FacesMessage.SEVERITY_WARN, "注册错误", "此用户名已经存在");
+			FacesContext.getCurrentInstance().addMessage(null, msg);
+			return;
+		}
+
+
+		User newUser = new User(username);
+		newUser.setFullName(fullname);
+		serviceFor(User.class).save(newUser);
+
+		UserPassword pass = new UserPassword(newUser, password);
+		serviceFor(UserPassword.class).save(pass);
+
+
+		username = "";
+		fullname = "";
+		password = "";
+		passwordConfirm = "";
+
+		msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "注册成功", "注册新用户成功");
+		FacesContext.getCurrentInstance().addMessage(null, msg);
+
+	}
+}
