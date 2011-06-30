978f622721085e31a1d318e15c162f7eea851b16
Lenik <xjl@99jsj.com>
2011-06-30 17:08:45 +0800

    Chanced UserPassword.user 1:1 -> User.

--- icsf/identity/icsf-acl/src/main/java/com/bee32/icsf/login/UserPassword.java
+++ icsf/identity/icsf-acl/src/main/java/com/bee32/icsf/login/UserPassword.java
@@ -7,6 +7,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.persistence.Transient;
@@ -55,7 +56,7 @@ public class UserPassword
         this.master = master;
     }
 
-    @ManyToOne
+    @OneToOne
     @JoinColumn(nullable = false)
     public User getUser() {
         return user;
