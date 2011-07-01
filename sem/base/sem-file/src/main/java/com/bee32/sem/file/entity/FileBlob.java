f5e7955aa6fef150ad12af9352df27621b3ca3f3
Lenik <xjl@99jsj.com>
2011-07-01 16:17:31 +0800

    Add FileBlob.new* methods.

--- sem/base/sem-file/src/main/java/com/bee32/sem/file/entity/FileBlob.java
+++ sem/base/sem-file/src/main/java/com/bee32/sem/file/entity/FileBlob.java
@@ -1,22 +1,36 @@
 package com.bee32.sem.file.entity;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.Reader;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.free.IFile;
+import javax.free.IStreamInputSource;
+import javax.free.IStreamOutputTarget;
 import javax.free.InputBytesFile;
+import javax.free.JavaioFile;
 import javax.persistence.AttributeOverride;
 import javax.persistence.AttributeOverrides;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Transient;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.bee32.plover.orm.cache.Redundant;
 import com.bee32.plover.orm.ext.digest.MD5Entity;
-import com.bee32.sem.file.io.LocalFileContainer;
+import com.bee32.sem.file.io.LocalStorage;
 import com.bee32.sem.file.util.ImageBlob;
 
+/**
+ * 文件数据
+ */
 @Entity
 @AttributeOverrides({//
 /*    */@AttributeOverride(name = "label", column = @Column(length = 100)) })
@@ -24,6 +38,8 @@ public class FileBlob
         extends MD5Entity {
 
     private static final long serialVersionUID = 1L;
+
+    static Logger logger = LoggerFactory.getLogger(FileBlob.class);
 
     public static final int HEADER_SIZE = 4000;
 
@@ -36,6 +52,9 @@ public class FileBlob
     // BASE CONTENT MINING.
     String encoding;
     String contentType;
+
+    int refCount;
+    List<UserFile> usage = new ArrayList<UserFile>();
 
     /**
      * 文件长度 （字节）。
@@ -87,34 +106,72 @@ public class FileBlob
     }
 
     @Override
-    public InputStream readContent()
+    protected InputStream readContent()
+            throws IOException {
+        return newInputStream();
+    }
+
+    @Transient
+    LocalStorage container = LocalStorage.INSTANCE;
+
+    IFile temporaryFile;
+
+    IFile getTemporaryFile() {
+        return temporaryFile;
+    }
+
+    void setTemporaryFile(IFile temporaryFile) {
+        this.temporaryFile = temporaryFile;
+    }
+
+    public IFile resolve()
+            throws IOException {
+
+        if (temporaryFile != null)
+            return temporaryFile;
+
+        String digest = getDigest();
+        if (digest == null)
+            throw new NullPointerException("digest");
+
+        IFile file;
+
+        boolean headerLoaded = header != null;
+        if (headerLoaded && length <= HEADER_SIZE) {
+            InputBytesFile memoryFile = new InputBytesFile(digest, header);
+            file = memoryFile;
+
+        } else {
+            file = container.resolveHash(digest);
+        }
+
+        String encoding = getEncoding("utf-8");
+        file.setPreferredCharset(encoding);
+        return file;
+    }
+
+    public InputStream newInputStream()
             throws IOException {
         IFile file = resolve();
         return file.toSource().newInputStream();
     }
 
-    public Reader readTextContent()
+    public Reader newReader()
             throws IOException {
-        InputStream in = readContent();
-        String encoding = getEncoding();
-        if (encoding == null)
-            encoding = "utf-8";
-        InputStreamReader reader = new InputStreamReader(in, encoding);
-        return reader;
+        IFile file = resolve();
+        return file.toSource().newReader();
     }
 
-    @Transient
-    LocalFileContainer container = LocalFileContainer.INSTANCE;
-
-    public IFile resolve()
+    OutputStream newOutputStream()
             throws IOException {
-        String digest = getDigest();
-        if (length <= HEADER_SIZE) {
-            InputBytesFile memoryFile = new InputBytesFile(digest, header);
-            return memoryFile;
-        } else {
-            return container.resolveHash(digest);
-        }
+        IFile file = resolve();
+        return file.toTarget().newOutputStream();
+    }
+
+    Writer newWriter()
+            throws IOException {
+        IFile file = resolve();
+        return file.toTarget().newWriter();
     }
 
     /**
@@ -123,6 +180,13 @@ public class FileBlob
     @Column(length = 16)
     public String getEncoding() {
         return encoding;
+    }
+
+    public String getEncoding(String defaultEncoding) {
+        if (encoding == null)
+            return defaultEncoding;
+        else
+            return encoding;
     }
 
     public void setEncoding(String encoding) {
@@ -141,4 +205,60 @@ public class FileBlob
         this.contentType = contentType;
     }
 
+    @Redundant
+    @Column(nullable = false)
+    public int getRefCount() {
+        return refCount;
+    }
+
+    void setRefCount(int refCount) {
+        this.refCount = refCount;
+    }
+
+    /**
+     * Commit a temporary file to form a FileBlob.
+     *
+     * @param localFile
+     *            Local file to commit.
+     * @param deleteAfterCommitted
+     *            Delete the original local file after succeeded to commit.
+     * @throws IOException
+     */
+    public static FileBlob commit(File localFile, boolean deleteAfterCommitted)
+            throws IOException {
+        FileBlob fileBlob = new FileBlob();
+
+        IFile temporaryFile = new JavaioFile(localFile);
+        fileBlob.setTemporaryFile(temporaryFile);
+        String digest = fileBlob.getOrComputeDigest();
+        fileBlob.setTemporaryFile(null);
+
+        // Forecast file to be committed.
+        IFile file = fileBlob.resolve();
+        // file.setCreateParentsMode(true);
+        new File(file.getParentFile().getPath().toString()).mkdirs();
+
+        // Prepare streaming.
+        IStreamInputSource source = temporaryFile.toSource();
+        IStreamOutputTarget target = file.toTarget();
+
+        logger.debug("Committing " + digest + ": " + localFile + " -> " + file.getParentFile());
+
+        int headerMin = HEADER_SIZE;
+        if (localFile.length() <= HEADER_SIZE)
+            headerMin = (int) localFile.length();
+
+        // Always create the file even if it's very small.
+        target.forWrite().writeBytes(source);
+
+        byte[] header = source.forRead().readBytes(headerMin);
+        fileBlob.setHeader(header);
+
+        if (!deleteAfterCommitted) {
+            logger.debug("Delete the committed file: " + localFile);
+            localFile.delete();
+        }
+
+        return fileBlob;
+    }
 }
