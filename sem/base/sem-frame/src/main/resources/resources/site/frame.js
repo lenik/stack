fcde6945a15d07b7ca54cc0cb10cd556b4d3ce14
jack <superjwl@gmail.com>
2011-07-06 15:44:25 +0800

    改正logout的错误

--- sem/base/sem-frame/src/main/resources/resources/site/frame.js
+++ sem/base/sem-frame/src/main/resources/resources/site/frame.js
@@ -7,7 +7,7 @@ $(function() {
             "success" : function(data) {
                 if (data.result.toUpperCase() == "SUCCESS") {
                     alert("成功退出!");
-                    window.location = data.gotoAddress;
+                    window.location = document.WEB_APP;
                 } else {
                     alert(data.message);
                 }
---
+++ sem/base/sem-people/src/main/java/com/bee32/sem/people/web/LogoutController.java
@@ -0,0 +1,33 @@
+package com.bee32.sem.people.web;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
+
+import com.bee32.icsf.login.SessionLoginInfo;
+import com.bee32.plover.ajax.SuccessOrFailMessage;
+
+@Controller
+@Lazy
+public class LogoutController extends MultiActionController {
+
+	@RequestMapping("/logout.do")
+	public void logout(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		new SuccessOrFailMessage("SUCCESS") {
+
+			@Override
+			protected String eval() throws Exception {
+				SessionLoginInfo.setCurrentUser(null);
+
+				return null;
+			}
+		}.jsonDump(res);
+
+	}
+}
