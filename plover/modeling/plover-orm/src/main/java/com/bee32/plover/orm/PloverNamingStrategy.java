c003d6b8d9d7a7419bfd42b7c5b590a02db08f23
Lenik <xjl@99jsj.com>
2011-07-08 17:34:18 +0800

    Add plover naming-strategy debug info.

--- plover/modeling/plover-orm/src/main/java/com/bee32/plover/orm/PloverNamingStrategy.java
+++ plover/modeling/plover-orm/src/main/java/com/bee32/plover/orm/PloverNamingStrategy.java
@@ -2,71 +2,87 @@ package com.bee32.plover.orm;
 
 import org.hibernate.cfg.ImprovedNamingStrategy;
 import org.hibernate.cfg.NamingStrategy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class PloverNamingStrategy
         extends ImprovedNamingStrategy {
 
     private static final long serialVersionUID = 1L;
 
+    static Logger logger = LoggerFactory.getLogger(PloverNamingStrategy.class);
+
     public String escapeName(String name) {
+        StackTraceElement[] st = Thread.currentThread().getStackTrace();
+        String method = st[2].getMethodName();
+        logger.debug(method + ": " + name);
         name = name.replace("`", "");
         return "`" + name + "`";
     }
 
     @Override
     public String classToTableName(String className) {
-        return escapeName(super.classToTableName(className));
+        String name = super.classToTableName(className);
+        return escapeName(name);
     }
 
     @Override
     public String propertyToColumnName(String propertyName) {
-        return escapeName(super.propertyToColumnName(propertyName));
+        String name = super.propertyToColumnName(propertyName);
+        return escapeName(name);
     }
 
     @Override
     public String tableName(String tableName) {
-        return escapeName(super.tableName(tableName));
+        String name = super.tableName(tableName);
+        return escapeName(name);
     }
 
     @Override
     public String columnName(String columnName) {
-        return escapeName(super.columnName(columnName));
+        String name = super.columnName(columnName);
+        return escapeName(name);
     }
 
     @Override
     public String collectionTableName(String ownerEntity, String ownerEntityTable, String associatedEntity,
             String associatedEntityTable, String propertyName) {
-        return escapeName(super.collectionTableName(ownerEntity, ownerEntityTable, associatedEntity,
-                associatedEntityTable, propertyName));
+        String name = super.collectionTableName(ownerEntity, ownerEntityTable, associatedEntity, associatedEntityTable,
+                propertyName);
+        return escapeName(name);
     }
 
     @Override
     public String joinKeyColumnName(String joinedColumn, String joinedTable) {
-        return escapeName(super.joinKeyColumnName(joinedColumn, joinedTable));
+        String name = super.joinKeyColumnName(joinedColumn, joinedTable);
+        return escapeName(name);
     }
 
     @Override
     public String foreignKeyColumnName(String propertyName, String propertyEntityName, String propertyTableName,
             String referencedColumnName) {
-        return escapeName(super.foreignKeyColumnName(propertyName, propertyEntityName, propertyTableName,
-                referencedColumnName));
+        String name = super.foreignKeyColumnName(propertyName, propertyEntityName, propertyTableName,
+                referencedColumnName);
+        return escapeName(name);
     }
 
     @Override
     public String logicalColumnName(String columnName, String propertyName) {
-        return escapeName(super.logicalColumnName(columnName, propertyName));
+        String name = super.logicalColumnName(columnName, propertyName);
+        return escapeName(name);
     }
 
     @Override
-    public String logicalCollectionTableName(String tableName, String ownerEntityTable, String associatedEntityTable,
+    public String logicalCollectionTableName(String tableName, String ownerEntityTable, String assocEntityTable,
             String propertyName) {
-        return escapeName(super.logicalCollectionTableName(tableName, ownerEntityTable, associatedEntityTable,
-                propertyName));
+        String name = super.logicalCollectionTableName(tableName, ownerEntityTable, assocEntityTable, propertyName);
+        return escapeName(name);
     }
 
     @Override
     public String logicalCollectionColumnName(String columnName, String propertyName, String referencedColumn) {
-        return escapeName(super.logicalCollectionColumnName(columnName, propertyName, referencedColumn));
+        String name = super.logicalCollectionColumnName(columnName, propertyName, referencedColumn);
+        return escapeName(name);
     }
 
     public static NamingStrategy getInstance(String dialect) {
