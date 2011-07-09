e220d5c335d2590b4d0b67554ce1f1939e1507cf
Lenik <xjl@99jsj.com>
2011-07-09 12:13:39 +0800

    StockJob  as mapped superclass.

--- sem/resource/sem-inventory/src/main/java/com/bee32/sem/inventory/tx/entity/StockJob.java
+++ sem/resource/sem-inventory/src/main/java/com/bee32/sem/inventory/tx/entity/StockJob.java
@@ -1,8 +1,13 @@
 package com.bee32.sem.inventory.tx.entity;
 
+import javax.persistence.MappedSuperclass;
+
 import com.bee32.plover.orm.ext.color.UIEntityAuto;
 
-public class StockJob
+@MappedSuperclass
+public abstract class StockJob
         extends UIEntityAuto<Long> {
+
+    private static final long serialVersionUID = 1L;
 
 }
