fd2a22b96b516278f8bd4bd5772feef9cde01e81
jack <superjwl@gmail.com>
2011-07-11 18:03:10 +0800

    角色管理初步

--- sem/base/sem-people/src/main/java/com/bee32/sem/people/web/RoleAdminBean.java
+++ sem/base/sem-people/src/main/java/com/bee32/sem/people/web/RoleAdminBean.java
@@ -23,19 +23,67 @@ public class RoleAdminBean
 	private static final long serialVersionUID = 1L;
 
 	private TreeNode root;
+	private RoleDto role;
+	private RoleDto selectedRole;
+	private RoleDto selectedParentRole;
+
+
+	public TreeNode getRoot() {
+		if(root == null) {
+			loadRoleTree();
+		}
+		return root;
+	}
+
+	public RoleDto getRole() {
+		if(role == null) {
+			_newRole();
+		}
+		return role;
+	}
+
+	public void setRole(RoleDto role) {
+		this.role = role;
+	}
+
+	public RoleDto getSelectedRole() {
+		return selectedRole;
+	}
+
+	public void setSelectedRole(RoleDto selectedRole) {
+		this.selectedRole = selectedRole;
+	}
+
+	public RoleDto getSelectedParentRole() {
+		return selectedParentRole;
+	}
+
+	public void setSelectedParentRole(RoleDto selectedParentRole) {
+		this.selectedParentRole = selectedParentRole;
+	}
+
+
 
 	private void loadRoleTree() {
 		root = new DefaultTreeNode("根", null);
 
-		List<Role> roles = serviceFor(Role.class).list(Restrictions.isNull("parent"));	//get top roles
+		List<Role> roles = serviceFor(Role.class).list(Restrictions.isNull("inheritedRole"));	//get top roles
         List<RoleDto> roleDtos = DTOs.marshalList(RoleDto.class, roles);
-        for(RoleDto roleDto : roleDtos) {
-		TreeNode roleNode = new DefaultTreeNode(roleDto, root);
 
-//        	if(roleDto) {
-//
-//        	}
+        for(RoleDto roleDto : roleDtos) {
+		loadRoleRecursive(roleDto, root);
         }
+	}
+
+	private void loadRoleRecursive(RoleDto roleDto, TreeNode parentTreeNode) {
+		TreeNode roleNode = new DefaultTreeNode(roleDto, parentTreeNode);
+
+	List<RoleDto> subRoles = roleDto.getDerivedRoles();
+	if(subRoles.size() > 0) {
+		for(RoleDto subRole : subRoles) {
+			loadRoleRecursive(subRole, roleNode);
+		}
+	}
 	}
 
 	@PostConstruct
@@ -43,9 +91,13 @@ public class RoleAdminBean
 		loadRoleTree();
 	}
 
-
-	public TreeNode getRoot() {
-		return root;
+	private void _newRole() {
+		role = new RoleDto();
 	}
+
+	public void doNewRole() {
+		_newRole();
+	}
+
 
 }
