4065ced344baa3bf3ce8b1c55eb4ad0e199a9705
Lenik <xjl@99jsj.com>
2011-07-12 07:59:14 +0800

    Add SEMInventoryUnitTest.

---
+++ sem/resource/sem-inventory/src/test/java/com/bee32/sem/inventory/SEMInventoryUnitTest.java
@@ -0,0 +1,12 @@
+package com.bee32.sem.inventory;
+
+import com.bee32.plover.orm.unit.PUnitDumper;
+
+public class SEMInventoryUnitTest {
+
+    public static void main(String[] args) {
+        SEMInventoryUnit unit = new SEMInventoryUnit();
+        System.out.println(PUnitDumper.format(unit));
+    }
+
+}
