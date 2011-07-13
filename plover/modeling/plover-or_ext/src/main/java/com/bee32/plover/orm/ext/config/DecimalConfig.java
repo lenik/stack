8a553991ab479d19ef95cca4cb2a35dabb1941a9
Lenik <xjl@99jsj.com>
2011-07-13 15:22:54 +0800

    Added DecimalContext.*MATH_CONTEXT.

--- plover/modeling/plover-or_ext/src/main/java/com/bee32/plover/orm/ext/config/DecimalConfig.java
+++ plover/modeling/plover-or_ext/src/main/java/com/bee32/plover/orm/ext/config/DecimalConfig.java
@@ -1,11 +1,15 @@
 package com.bee32.plover.orm.ext.config;
 
+import java.math.MathContext;
+
 public interface DecimalConfig {
 
     int QTY_ITEM_SCALE = 16;
     int QTY_ITEM_PRECISION = 4;
+    MathContext QTY_ITEM_CONTEXT = new MathContext(QTY_ITEM_PRECISION);
 
     int MONEY_ITEM_SCALE = 16;
     int MONEY_ITEM_PRECISION = 4;
+    MathContext MONEY_ITEM_CONTEXT = new MathContext(MONEY_ITEM_PRECISION);
 
 }
