aa0b8077f4a9a3b24c5d4482f879992149dd9b5f
Lenik <xjl@99jsj.com>
2011-07-13 15:24:11 +0800

    MCArray < Iterable, +add/subtract(MCArray).

--- sem/resource/sem-world/src/main/java/com/bee32/sem/world/monetary/MCArray.java
+++ sem/resource/sem-world/src/main/java/com/bee32/sem/world/monetary/MCArray.java
@@ -3,6 +3,7 @@ package com.bee32.sem.world.monetary;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Currency;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.TreeMap;
@@ -10,11 +11,12 @@ import java.util.TreeMap;
 import javax.free.AbstractNonNullComparator;
 
 public class MCArray
-        implements Serializable {
+        implements Serializable, ICurrencyAware, Iterable<MCValue> {
 
     private static final long serialVersionUID = 1L;
 
     Map<Currency, BigDecimal> map = new TreeMap<Currency, BigDecimal>(CurrencyCodeComparator.INSTANCE);
+    Currency conversion = DEFAULT_CURRENCY;
 
     @Override
     public MCArray clone() {
@@ -23,28 +25,96 @@ public class MCArray
         return copy;
     }
 
+    static class Iter
+            implements Iterator<MCValue> {
+
+        Iterator<Entry<Currency, BigDecimal>> entryIter;
+
+        public Iter(Iterator<Entry<Currency, BigDecimal>> entryIter) {
+            if (entryIter == null)
+                throw new NullPointerException("entryIter");
+            this.entryIter = entryIter;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return entryIter.hasNext();
+        }
+
+        @Override
+        public MCValue next() {
+            Entry<Currency, BigDecimal> entry = entryIter.next();
+            return new VirtualMCV(entry);
+        }
+
+        @Override
+        public void remove() {
+            entryIter.remove();
+        }
+
+    }
+
+    @Override
+    public Iterator<MCValue> iterator() {
+        return new Iter(map.entrySet().iterator());
+    }
+
+    /**
+     * Get the currency used for conversion.
+     *
+     * When
+     *
+     * @return <code>null</code> if conversion id disabled.
+     */
+    public Currency getConversion() {
+        return conversion;
+    }
+
+    /**
+     * @param conversion
+     *            The currency used for conversion.
+     */
+    public void setConversion(Currency conversion) {
+        this.conversion = conversion;
+    }
+
     public void add(MCValue mcv) {
         if (mcv == null)
             throw new NullPointerException("mcv");
-        BigDecimal cval = map.get(mcv.currency);
+        BigDecimal cval = map.get(mcv.getCurrency());
         if (cval == null) {
-            map.put(mcv.currency, mcv.value);
+            map.put(mcv.getCurrency(), mcv.getValue());
         } else {
-            cval = cval.add(mcv.value);
-            map.put(mcv.currency, cval);
+            cval = cval.add(mcv.getValue());
+            map.put(mcv.getCurrency(), cval);
         }
     }
 
     public void subtract(MCValue mcv) {
         if (mcv == null)
             throw new NullPointerException("mcv");
-        BigDecimal cval = map.get(mcv.currency);
+        BigDecimal cval = map.get(mcv.getCurrency());
         if (cval == null) {
-            map.put(mcv.currency, mcv.value.negate());
+            map.put(mcv.getCurrency(), mcv.getValue().negate());
         } else {
-            cval = cval.subtract(mcv.value);
-            map.put(mcv.currency, cval);
+            cval = cval.subtract(mcv.getValue());
+            map.put(mcv.getCurrency(), cval);
         }
+    }
+
+    public final void add(MCArray mca) {
+        for (MCValue mcv : mca)
+            add(mcv);
+    }
+
+    public final void subtract(MCArray mca) {
+        for (MCValue mcv : mca)
+            subtract(mcv);
+    }
+
+    public void multiply(int k) {
+        BigDecimal decimal = new BigDecimal(k);
+        multiply(decimal);
     }
 
     public void multiply(double k) {
@@ -60,6 +130,41 @@ public class MCArray
             val = val.multiply(k);
             entry.setValue(val);
         }
+    }
+
+}
+
+class VirtualMCV
+        extends MCValue {
+
+    private static final long serialVersionUID = 1L;
+
+    final Entry<Currency, BigDecimal> entry;
+
+    public VirtualMCV(Entry<Currency, BigDecimal> entry) {
+        if (entry == null)
+            throw new NullPointerException("entry");
+        this.entry = entry;
+    }
+
+    @Override
+    public Currency getCurrency() {
+        return entry.getKey();
+    }
+
+    @Override
+    public void setCurrency(Currency currency) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public BigDecimal getValue() {
+        return entry.getValue();
+    }
+
+    @Override
+    public void setValue(BigDecimal value) {
+        entry.setValue(value);
     }
 
 }
