07b45b05026ee15d123514c99a5ef1f6e91f8cbc
Lenik <xjl@99jsj.com>
2011-07-14 16:49:34 +0800

    It should be precision.scale

--- plover/modeling/plover-or_ext/src/main/java/com/bee32/plover/orm/ext/config/DecimalConfig.java
+++ plover/modeling/plover-or_ext/src/main/java/com/bee32/plover/orm/ext/config/DecimalConfig.java
@@ -4,12 +4,12 @@ import java.math.MathContext;
 
 public interface DecimalConfig {
 
-    int QTY_ITEM_SCALE = 16;
-    int QTY_ITEM_PRECISION = 4;
+    int QTY_ITEM_PRECISION = 16;
+    int QTY_ITEM_SCALE = 4;
     MathContext QTY_ITEM_CONTEXT = new MathContext(QTY_ITEM_PRECISION);
 
-    int MONEY_ITEM_SCALE = 16;
-    int MONEY_ITEM_PRECISION = 4;
+    int MONEY_ITEM_PRECISION = 16;
+    int MONEY_ITEM_SCALE = 4;
     MathContext MONEY_ITEM_CONTEXT = new MathContext(MONEY_ITEM_PRECISION);
 
 }
