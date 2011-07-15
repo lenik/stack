ad88db8a23e9d04d8621e1721a0ee0824009f472
Lenik <xjl@99jsj.com>
2011-07-15 23:13:19 +0800

    Complemented SimpleGroup.ctor* <Principal.

--- icsf/identity/icsf-acl/src/test/java/com/bee32/icsf/principal/SimpleGroup.java
+++ icsf/identity/icsf-acl/src/test/java/com/bee32/icsf/principal/SimpleGroup.java
@@ -14,12 +14,16 @@ public class SimpleGroup
         super();
     }
 
-    public SimpleGroup(String name, SimpleUser owner, SimpleUser... memberUsers) {
-        super(name, owner, memberUsers);
-    }
-
     public SimpleGroup(String name) {
         super(name);
+    }
+
+    public SimpleGroup(String name, String fullName) {
+        super(name, fullName);
+    }
+
+    public SimpleGroup(String name, String fullName, User owner, User... memberUsers) {
+        super(name, fullName, owner, memberUsers);
     }
 
 }
