ad929cde6fea9d2069319c22f79da2f8687256ab
Lenik <xjl@99jsj.com>
2011-07-15 23:14:05 +0800

    Clean SEMWorldSample the std instances are auto discoverred.

---
+++ sem/resource/sem-world/src/main/java/com/bee32/sem/world/thing/SEMWorldThingSamples.java
@@ -0,0 +1,12 @@
+package com.bee32.sem.world.thing;
+
+import com.bee32.plover.orm.util.SampleContribution;
+
+public class SEMWorldThingSamples
+        extends SampleContribution {
+
+    @Override
+    protected void preamble() {
+    }
+
+}
