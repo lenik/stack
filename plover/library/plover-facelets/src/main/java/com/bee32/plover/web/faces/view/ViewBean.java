60c16c2de1c948084e72cf66c3de147035ca8b15
Lenik <xjl@99jsj.com>
2011-07-16 12:48:43 +0800

    Added ViewBean.findComponent/findComponentEx.

--- plover/library/plover-facelets/src/main/java/com/bee32/plover/web/faces/view/ViewBean.java
+++ plover/library/plover-facelets/src/main/java/com/bee32/plover/web/faces/view/ViewBean.java
@@ -2,6 +2,10 @@ package com.bee32.plover.web.faces.view;
 
 import java.io.Serializable;
 
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIViewRoot;
+import javax.faces.context.FacesContext;
+
 import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Lazy;
@@ -9,6 +13,7 @@ import org.springframework.context.annotation.Scope;
 
 import com.bee32.plover.inject.ComponentTemplate;
 import com.bee32.plover.servlet.util.ThreadHttpContext;
+import com.bee32.plover.web.faces.utils.ComponentHelper;
 import com.bee32.plover.web.faces.utils.FacesContextSupport;
 
 @ComponentTemplate
@@ -31,6 +36,25 @@ public abstract class ViewBean
         ApplicationContext context = ThreadHttpContext.requireApplicationContext();
         AutowireCapableBeanFactory factory = context.getAutowireCapableBeanFactory();
         factory.autowireBean(this);
+    }
+
+    protected UIComponent findComponent(String expr) {
+        FacesContext context = FacesContext.getCurrentInstance();
+        UIViewRoot viewRoot = context.getViewRoot();
+        UIComponent component = viewRoot.findComponent(expr);
+
+        if (component == null)
+            throw new IllegalArgumentException("Illegal component id expr: " + expr);
+
+        return component;
+    }
+
+    protected ComponentHelper findComponentEx(String expr) {
+        UIComponent component = findComponent(expr);
+        if (component == null)
+            return null;
+
+        return new ComponentHelper(component);
     }
 
 }
