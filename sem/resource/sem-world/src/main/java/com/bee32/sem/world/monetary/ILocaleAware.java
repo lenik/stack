68c33d3cea31bfd1dd6566770b227f779907ef39
Lenik <xjl@99jsj.com>
2011-07-18 00:00:51 +0800

    Added ILocaleAware (it should be moved to plover)

---
+++ sem/resource/sem-world/src/main/java/com/bee32/sem/world/monetary/ILocaleAware.java
@@ -0,0 +1,10 @@
+package com.bee32.sem.world.monetary;
+
+import java.util.Locale;
+
+public interface ILocaleAware {
+
+    Locale en_US = Locale.forLanguageTag("en-US");
+    Locale zh_CN = Locale.forLanguageTag("zh-CN");
+
+}
