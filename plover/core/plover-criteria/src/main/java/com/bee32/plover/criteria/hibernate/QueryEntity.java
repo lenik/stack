9a1e1533aa3570c88f5c60cfebf8136da2facfcc
Lenik <xjl@99jsj.com>
2011-07-18 13:38:23 +0800

    Moved CriteriaTemplate to plover-criteria.

---
+++ plover/core/plover-criteria/src/main/java/com/bee32/plover/criteria/hibernate/QueryEntity.java
@@ -0,0 +1,13 @@
+package com.bee32.plover.criteria.hibernate;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+public @interface QueryEntity {
+
+    Class<?>[] value();
+
+}
