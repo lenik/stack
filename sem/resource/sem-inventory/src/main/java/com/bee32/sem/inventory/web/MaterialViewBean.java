5f1a066c690526a5b846454b2697c380fc1a3df8
ztf <ztf@bee32.com>
2011-07-18 17:19:12 +0800

    modify viewBean

--- sem/resource/sem-inventory/src/main/java/com/bee32/sem/inventory/web/MaterialViewBean.java
+++ sem/resource/sem-inventory/src/main/java/com/bee32/sem/inventory/web/MaterialViewBean.java
@@ -1,5 +1,6 @@
 package com.bee32.sem.inventory.web;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
@@ -13,12 +14,16 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import com.bee32.plover.orm.util.DTOs;
+import com.bee32.sem.inventory.dto.MaterialAttributeDto;
 import com.bee32.sem.inventory.dto.MaterialCategoryDto;
 import com.bee32.sem.inventory.dto.MaterialDto;
+import com.bee32.sem.inventory.entity.Material;
 import com.bee32.sem.inventory.entity.MaterialCategory;
 import com.bee32.sem.sandbox.MultiTabEntityViewBean;
 import com.bee32.sem.sandbox.UIHelper;
 import com.bee32.sem.world.thing.Unit;
+import com.bee32.sem.world.thing.UnitConv;
+import com.bee32.sem.world.thing.UnitConvDto;
 import com.bee32.sem.world.thing.UnitDto;
 
 @Component
@@ -28,20 +33,41 @@ public class MaterialViewBean
 
     private static final long serialVersionUID = 1L;
 
+    public static final String ATTR = "attr";
+
     private boolean searching;
     private String materialNamePattern;
     private List<MaterialDto> materialList;
     private TreeNode root;
     private TreeNode selectedMaterialNode;
     private MaterialDto tempMaterial = new MaterialDto().create();
+    private UnitDto newUnit = new UnitDto().create();
+    private MaterialAttributeDto tempAttr;
 
     MaterialViewBean() {
         root = new DefaultTreeNode("root", null);
+        tempAttr = new MaterialAttributeDto(tempMaterial,"","");
     }
 
     @PostConstruct
     public void initialize() {
         buildTree();
+    }
+
+    public void doAddAttr(){
+        tempMaterial.addAttribute(tempAttr);
+    }
+
+    public void doAddUhnit() {
+        tempMaterial.setUnit(newUnit);
+        Unit unit = newUnit.unmarshal();
+        serviceFor(Unit.class).save(unit);
+    }
+
+    public void doAddMaterial(){
+        Material material = tempMaterial.unmarshal();
+        serviceFor(Material.class).saveOrUpdate(material);
+
     }
 
     public void buildTree() {
@@ -69,6 +95,28 @@ public class MaterialViewBean
         List<Unit> unitList = serviceFor(Unit.class).list();
         List<UnitDto> unitDtoList = DTOs.marshalList(UnitDto.class, unitList);
         return UIHelper.selectItemsFromDict(unitDtoList);
+    }
+
+    public List<SelectItem> getCategories() {
+        List<MaterialCategory> categoryList = serviceFor(MaterialCategory.class).list();
+        List<MaterialCategoryDto> categoryDtoList = DTOs.marshalList(MaterialCategoryDto.class, 0, categoryList);
+        List<SelectItem> itemList = new ArrayList<SelectItem>();
+        for (MaterialCategoryDto materialCategoryDto : categoryDtoList)
+            itemList.add(new SelectItem(materialCategoryDto.getId(), materialCategoryDto.getName()));
+        return itemList;
+    }
+
+    public List<SelectItem> getStandardUnits() {
+        List<Unit> unitList = serviceFor(Unit.class).list(//
+                Restrictions.eq("scale", 0.0));
+        List<UnitDto> unitDtoList = DTOs.marshalList(UnitDto.class, unitList);
+        return UIHelper.selectItemsFromDict(unitDtoList);
+    }
+
+    public List<SelectItem> getUnitConvs(){
+        List<UnitConv> unitConvList = serviceFor(UnitConv.class).list();
+        List<UnitConvDto> unitConvDtoList = DTOs.marshalList(UnitConvDto.class, unitConvList);
+        return UIHelper.selectItemsFromDict(unitConvDtoList);
     }
 
     public boolean isSearching() {
@@ -117,6 +165,22 @@ public class MaterialViewBean
 
     public void setTempMaterial(MaterialDto tempMaterial) {
         this.tempMaterial = tempMaterial;
+    }
+
+    public UnitDto getNewUnit() {
+        return newUnit;
+    }
+
+    public void setNewUnit(UnitDto newUnit) {
+        this.newUnit = newUnit;
+    }
+
+    public MaterialAttributeDto getTempAttr() {
+        return tempAttr;
+    }
+
+    public void setTempAttr(MaterialAttributeDto tempAttr) {
+        this.tempAttr = tempAttr;
     }
 
 }
