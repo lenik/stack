739e40e83cde89dd6f18f56845eebc6503a15d9b
Lenik <xjl@99jsj.com>
2011-07-18 08:27:56 +0800

    Add DecimalConfig.*_TOTAL_* constants.

--- plover/modeling/plover-or_ext/src/main/java/com/bee32/plover/orm/ext/config/DecimalConfig.java
+++ plover/modeling/plover-or_ext/src/main/java/com/bee32/plover/orm/ext/config/DecimalConfig.java
@@ -8,8 +8,16 @@ public interface DecimalConfig {
     int QTY_ITEM_SCALE = 4;
     MathContext QTY_ITEM_CONTEXT = new MathContext(QTY_ITEM_PRECISION);
 
+    int QTY_TOTAL_PRECISION = 20;
+    int QTY_TOTAL_SCALE = 4;
+    MathContext QTY_TOTAL_CONTEXT = new MathContext(QTY_TOTAL_PRECISION);
+
     int MONEY_ITEM_PRECISION = 16;
     int MONEY_ITEM_SCALE = 4;
     MathContext MONEY_ITEM_CONTEXT = new MathContext(MONEY_ITEM_PRECISION);
+
+    int MONEY_TOTAL_PRECISION = 20;
+    int MONEY_TOTAL_SCALE = 4;
+    MathContext MONEY_TOTAL_CONTEXT = new MathContext(MONEY_TOTAL_PRECISION);
 
 }
