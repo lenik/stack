5c3339b89701ac44e99a59308a3321d62d275640
Lenik <xjl@99jsj.com>
2011-07-18 08:29:15 +0800

    StockJob couldn't be inherited as JOINED, cuz StockJob+ may have multiple StockOrder refs.

--- sem/resource/sem-inventory/src/main/java/com/bee32/sem/inventory/tx/entity/StockJob.java
+++ sem/resource/sem-inventory/src/main/java/com/bee32/sem/inventory/tx/entity/StockJob.java
@@ -5,6 +5,10 @@ import javax.persistence.MappedSuperclass;
 import com.bee32.plover.orm.ext.color.UIEntityAuto;
 
 @MappedSuperclass
+// @Entity
+// @Inheritance(strategy = InheritanceType.JOINED)
+// @DiscriminatorColumn(name = "stereo", length = 3)
+// @DiscriminatorValue("n/a")
 public abstract class StockJob
         extends UIEntityAuto<Long> {
 
