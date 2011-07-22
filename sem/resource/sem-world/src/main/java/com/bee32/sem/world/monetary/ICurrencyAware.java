6294c78ab5f9088b81b28ee1921004214854b638
Lenik <xjl@99jsj.com>
2011-07-22 18:03:16 +0800

    Add all Currency constants to ICurrencyAware.codes.*

--- sem/resource/sem-world/src/main/java/com/bee32/sem/world/monetary/ICurrencyAware.java
+++ sem/resource/sem-world/src/main/java/com/bee32/sem/world/monetary/ICurrencyAware.java
@@ -4,56 +4,662 @@ import java.util.Currency;
 
 public interface ICurrencyAware {
 
-    /** 澳大利亚元 */
-    Currency AUD = Currency.getInstance("AUD");
-
-    /** 加拿大元 */
-    Currency CAD = Currency.getInstance("CAD");
-
-    /** 瑞士法郎 */
-    Currency CHF = Currency.getInstance("CHF");
-
     /** 人民币 */
     Currency CNY = Currency.getInstance("CNY");
 
-    /** 丹麦克朗 */
-    Currency DKK = Currency.getInstance("DKK");
-
-    /** 欧元 */
-    Currency EUR = Currency.getInstance("EUR");
+    /** 法国法郎 */
+    Currency FRF = Currency.getInstance("FRF");
 
     /** 英镑 */
     Currency GBP = Currency.getInstance("GBP");
 
-    /** 港币 */
-    Currency HKD = Currency.getInstance("HKD");
-
     /** 日元 */
     Currency JPY = Currency.getInstance("JPY");
 
-    /** 韩国元 */
+    /** 韩圆 */
     Currency KRW = Currency.getInstance("KRW");
-
-    /** 挪威克朗 */
-    Currency NOK = Currency.getInstance("NOK");
-
-    /** 新西兰元 */
-    Currency NZD = Currency.getInstance("NZD");
-
-    /** 菲律宾比索 */
-    Currency PHP = Currency.getInstance("PHP");
-
-    /** 瑞典克朗 */
-    Currency SEK = Currency.getInstance("SEK");
-
-    /** 新加坡元 */
-    Currency SGD = Currency.getInstance("SGD");
-
-    /** 泰国铢 */
-    Currency THB = Currency.getInstance("THB");
 
     /** 美元 */
     Currency USD = Currency.getInstance("USD");
+
+    interface codes {
+
+        /** 安道尔比塞塔 */
+        Currency ADP = Currency.getInstance("ADP");
+
+        /** 阿联酋迪拉姆 */
+        Currency AED = Currency.getInstance("AED");
+
+        /** 阿富汗尼 (1927-2002) */
+        Currency AFA = Currency.getInstance("AFA");
+
+        /** 阿富汗尼 */
+        Currency AFN = Currency.getInstance("AFN");
+
+        /** 阿尔巴尼亚列克 */
+        Currency ALL = Currency.getInstance("ALL");
+
+        /** 亚美尼亚德拉姆 */
+        Currency AMD = Currency.getInstance("AMD");
+
+        /** 荷兰安替兰盾 */
+        Currency ANG = Currency.getInstance("ANG");
+
+        /** 安哥拉宽扎 */
+        Currency AOA = Currency.getInstance("AOA");
+
+        /** 阿根廷比索 */
+        Currency ARS = Currency.getInstance("ARS");
+
+        /** 奥地利先令 */
+        Currency ATS = Currency.getInstance("ATS");
+
+        /** 澳大利亚元 */
+        Currency AUD = Currency.getInstance("AUD");
+
+        /** 阿鲁巴基尔德元 */
+        Currency AWG = Currency.getInstance("AWG");
+
+        /** AYM */
+        Currency AYM = Currency.getInstance("AYM");
+
+        /** 阿塞拜疆马纳特 (1993-2006) */
+        Currency AZM = Currency.getInstance("AZM");
+
+        /** 阿塞拜疆马纳特 */
+        Currency AZN = Currency.getInstance("AZN");
+
+        /** 波士尼亚-赫塞哥维纳兑换券 */
+        Currency BAM = Currency.getInstance("BAM");
+
+        /** 巴巴多斯元 */
+        Currency BBD = Currency.getInstance("BBD");
+
+        /** 孟加拉塔卡 */
+        Currency BDT = Currency.getInstance("BDT");
+
+        /** 比利时法郎 */
+        Currency BEF = Currency.getInstance("BEF");
+
+        /** 保加利亚硬列弗 */
+        Currency BGL = Currency.getInstance("BGL");
+
+        /** 保加利亚新列弗 */
+        Currency BGN = Currency.getInstance("BGN");
+
+        /** 巴林第纳尔 */
+        Currency BHD = Currency.getInstance("BHD");
+
+        /** 布隆迪法郎 */
+        Currency BIF = Currency.getInstance("BIF");
+
+        /** 百慕大元 */
+        Currency BMD = Currency.getInstance("BMD");
+
+        /** 文莱元 */
+        Currency BND = Currency.getInstance("BND");
+
+        /** 玻利维亚诺 */
+        Currency BOB = Currency.getInstance("BOB");
+
+        /** 玻利维亚 Mvdol（资金） */
+        Currency BOV = Currency.getInstance("BOV");
+
+        /** 巴西雷亚尔 */
+        Currency BRL = Currency.getInstance("BRL");
+
+        /** 巴哈马元 */
+        Currency BSD = Currency.getInstance("BSD");
+
+        /** 不丹努扎姆 */
+        Currency BTN = Currency.getInstance("BTN");
+
+        /** 博茨瓦纳普拉 */
+        Currency BWP = Currency.getInstance("BWP");
+
+        /** 白俄罗斯新卢布 (1994-1999) */
+        Currency BYB = Currency.getInstance("BYB");
+
+        /** 白俄罗斯卢布 */
+        Currency BYR = Currency.getInstance("BYR");
+
+        /** 伯利兹元 */
+        Currency BZD = Currency.getInstance("BZD");
+
+        /** 加拿大元 */
+        Currency CAD = Currency.getInstance("CAD");
+
+        /** 刚果法郎 */
+        Currency CDF = Currency.getInstance("CDF");
+
+        /** 瑞士法郎 */
+        Currency CHF = Currency.getInstance("CHF");
+
+        /** 智利 Unidades de Fomento（资金） */
+        Currency CLF = Currency.getInstance("CLF");
+
+        /** 智利比索 */
+        Currency CLP = Currency.getInstance("CLP");
+
+        /** 人民币 */
+        Currency CNY = Currency.getInstance("CNY");
+
+        /** 哥伦比亚比索 */
+        Currency COP = Currency.getInstance("COP");
+
+        /** 哥斯达黎加科朗 */
+        Currency CRC = Currency.getInstance("CRC");
+
+        /** 旧塞尔维亚第纳尔 */
+        Currency CSD = Currency.getInstance("CSD");
+
+        /** 古巴比索 */
+        Currency CUP = Currency.getInstance("CUP");
+
+        /** 佛得角埃斯库多 */
+        Currency CVE = Currency.getInstance("CVE");
+
+        /** 塞浦路斯镑 */
+        Currency CYP = Currency.getInstance("CYP");
+
+        /** 捷克克郎 */
+        Currency CZK = Currency.getInstance("CZK");
+
+        /** 德国马克 */
+        Currency DEM = Currency.getInstance("DEM");
+
+        /** 吉布提法郎 */
+        Currency DJF = Currency.getInstance("DJF");
+
+        /** 丹麦克朗 */
+        Currency DKK = Currency.getInstance("DKK");
+
+        /** 多米尼加比索 */
+        Currency DOP = Currency.getInstance("DOP");
+
+        /** 阿尔及利亚第纳尔 */
+        Currency DZD = Currency.getInstance("DZD");
+
+        /** 爱沙尼亚克朗 */
+        Currency EEK = Currency.getInstance("EEK");
+
+        /** 埃及镑 */
+        Currency EGP = Currency.getInstance("EGP");
+
+        /** 厄立特里亚纳克法 */
+        Currency ERN = Currency.getInstance("ERN");
+
+        /** 西班牙比塞塔 */
+        Currency ESP = Currency.getInstance("ESP");
+
+        /** 埃塞俄比亚比尔 */
+        Currency ETB = Currency.getInstance("ETB");
+
+        /** 欧元 */
+        Currency EUR = Currency.getInstance("EUR");
+
+        /** 芬兰马克 */
+        Currency FIM = Currency.getInstance("FIM");
+
+        /** 斐济元 */
+        Currency FJD = Currency.getInstance("FJD");
+
+        /** 福克兰镑 */
+        Currency FKP = Currency.getInstance("FKP");
+
+        /** 法国法郎 */
+        Currency FRF = Currency.getInstance("FRF");
+
+        /** 英镑 */
+        Currency GBP = Currency.getInstance("GBP");
+
+        /** 乔治亚拉瑞 */
+        Currency GEL = Currency.getInstance("GEL");
+
+        /** 加纳塞第 */
+        Currency GHC = Currency.getInstance("GHC");
+
+        /** 加纳塞地 */
+        Currency GHS = Currency.getInstance("GHS");
+
+        /** 直布罗陀镑 */
+        Currency GIP = Currency.getInstance("GIP");
+
+        /** 冈比亚达拉西 */
+        Currency GMD = Currency.getInstance("GMD");
+
+        /** 几内亚法郎 */
+        Currency GNF = Currency.getInstance("GNF");
+
+        /** 希腊德拉克马 */
+        Currency GRD = Currency.getInstance("GRD");
+
+        /** 危地马拉格查尔 */
+        Currency GTQ = Currency.getInstance("GTQ");
+
+        /** 几内亚比绍比索 */
+        Currency GWP = Currency.getInstance("GWP");
+
+        /** 圭亚那元 */
+        Currency GYD = Currency.getInstance("GYD");
+
+        /** 港元 */
+        Currency HKD = Currency.getInstance("HKD");
+
+        /** 洪都拉斯拉伦皮拉 */
+        Currency HNL = Currency.getInstance("HNL");
+
+        /** 克罗地亚库纳 */
+        Currency HRK = Currency.getInstance("HRK");
+
+        /** 海地古德 */
+        Currency HTG = Currency.getInstance("HTG");
+
+        /** 匈牙利福林 */
+        Currency HUF = Currency.getInstance("HUF");
+
+        /** 印度尼西亚盾 */
+        Currency IDR = Currency.getInstance("IDR");
+
+        /** 爱尔兰镑 */
+        Currency IEP = Currency.getInstance("IEP");
+
+        /** 以色列新谢克尔 */
+        Currency ILS = Currency.getInstance("ILS");
+
+        /** 印度卢比 */
+        Currency INR = Currency.getInstance("INR");
+
+        /** 伊拉克第纳尔 */
+        Currency IQD = Currency.getInstance("IQD");
+
+        /** 伊朗里亚尔 */
+        Currency IRR = Currency.getInstance("IRR");
+
+        /** 冰岛克朗 */
+        Currency ISK = Currency.getInstance("ISK");
+
+        /** 意大利里拉 */
+        Currency ITL = Currency.getInstance("ITL");
+
+        /** 牙买加元 */
+        Currency JMD = Currency.getInstance("JMD");
+
+        /** 约旦第纳尔 */
+        Currency JOD = Currency.getInstance("JOD");
+
+        /** 日元 */
+        Currency JPY = Currency.getInstance("JPY");
+
+        /** 肯尼亚先令 */
+        Currency KES = Currency.getInstance("KES");
+
+        /** 吉尔吉斯斯坦索姆 */
+        Currency KGS = Currency.getInstance("KGS");
+
+        /** 柬埔寨瑞尔 */
+        Currency KHR = Currency.getInstance("KHR");
+
+        /** 科摩罗法郎 */
+        Currency KMF = Currency.getInstance("KMF");
+
+        /** 朝鲜圆 */
+        Currency KPW = Currency.getInstance("KPW");
+
+        /** 韩圆 */
+        Currency KRW = Currency.getInstance("KRW");
+
+        /** 科威特第纳尔 */
+        Currency KWD = Currency.getInstance("KWD");
+
+        /** 开曼元 */
+        Currency KYD = Currency.getInstance("KYD");
+
+        /** 哈萨克斯坦坚戈 */
+        Currency KZT = Currency.getInstance("KZT");
+
+        /** 老挝基普 */
+        Currency LAK = Currency.getInstance("LAK");
+
+        /** 黎巴嫩镑 */
+        Currency LBP = Currency.getInstance("LBP");
+
+        /** 斯里兰卡卢比 */
+        Currency LKR = Currency.getInstance("LKR");
+
+        /** 利比亚元 */
+        Currency LRD = Currency.getInstance("LRD");
+
+        /** 莱索托洛蒂 */
+        Currency LSL = Currency.getInstance("LSL");
+
+        /** 立陶宛立特 */
+        Currency LTL = Currency.getInstance("LTL");
+
+        /** 卢森堡法郎 */
+        Currency LUF = Currency.getInstance("LUF");
+
+        /** 拉脱维亚拉特 */
+        Currency LVL = Currency.getInstance("LVL");
+
+        /** 利比亚第纳尔 */
+        Currency LYD = Currency.getInstance("LYD");
+
+        /** 摩洛哥迪拉姆 */
+        Currency MAD = Currency.getInstance("MAD");
+
+        /** 摩尔多瓦列伊 */
+        Currency MDL = Currency.getInstance("MDL");
+
+        /** 马达加斯加阿里亚里 */
+        Currency MGA = Currency.getInstance("MGA");
+
+        /** 马达加斯加法郎 */
+        Currency MGF = Currency.getInstance("MGF");
+
+        /** 马其顿戴代纳尔 */
+        Currency MKD = Currency.getInstance("MKD");
+
+        /** 缅甸开亚特 */
+        Currency MMK = Currency.getInstance("MMK");
+
+        /** 蒙古图格里克 */
+        Currency MNT = Currency.getInstance("MNT");
+
+        /** 澳门元 */
+        Currency MOP = Currency.getInstance("MOP");
+
+        /** 毛里塔尼亚乌吉亚 */
+        Currency MRO = Currency.getInstance("MRO");
+
+        /** 马耳他里拉 */
+        Currency MTL = Currency.getInstance("MTL");
+
+        /** 毛里求斯卢比 */
+        Currency MUR = Currency.getInstance("MUR");
+
+        /** 马尔代夫拉菲亚 */
+        Currency MVR = Currency.getInstance("MVR");
+
+        /** 马拉维克瓦查 */
+        Currency MWK = Currency.getInstance("MWK");
+
+        /** 墨西哥比索 */
+        Currency MXN = Currency.getInstance("MXN");
+
+        /** 墨西哥 Unidad de Inversion (UDI)（资金） */
+        Currency MXV = Currency.getInstance("MXV");
+
+        /** 马来西亚林吉特 */
+        Currency MYR = Currency.getInstance("MYR");
+
+        /** 旧莫桑比克美提卡 */
+        Currency MZM = Currency.getInstance("MZM");
+
+        /** 莫桑比克美提卡 */
+        Currency MZN = Currency.getInstance("MZN");
+
+        /** 纳米比亚元 */
+        Currency NAD = Currency.getInstance("NAD");
+
+        /** 尼日利亚奈拉 */
+        Currency NGN = Currency.getInstance("NGN");
+
+        /** 尼加拉瓜金科多巴 */
+        Currency NIO = Currency.getInstance("NIO");
+
+        /** 荷兰盾 */
+        Currency NLG = Currency.getInstance("NLG");
+
+        /** 挪威克朗 */
+        Currency NOK = Currency.getInstance("NOK");
+
+        /** 尼泊尔卢比 */
+        Currency NPR = Currency.getInstance("NPR");
+
+        /** 新西兰元 */
+        Currency NZD = Currency.getInstance("NZD");
+
+        /** 阿曼里亚尔 */
+        Currency OMR = Currency.getInstance("OMR");
+
+        /** 巴拿马巴波亚 */
+        Currency PAB = Currency.getInstance("PAB");
+
+        /** 秘鲁新索尔 */
+        Currency PEN = Currency.getInstance("PEN");
+
+        /** 巴布亚新几内亚基那 */
+        Currency PGK = Currency.getInstance("PGK");
+
+        /** 菲律宾比索 */
+        Currency PHP = Currency.getInstance("PHP");
+
+        /** 巴基斯坦卢比 */
+        Currency PKR = Currency.getInstance("PKR");
+
+        /** 波兰兹罗提 */
+        Currency PLN = Currency.getInstance("PLN");
+
+        /** 葡萄牙埃斯库多 */
+        Currency PTE = Currency.getInstance("PTE");
+
+        /** 巴拉圭瓜拉尼 */
+        Currency PYG = Currency.getInstance("PYG");
+
+        /** 卡塔尔里亚尔 */
+        Currency QAR = Currency.getInstance("QAR");
+
+        /** 旧罗马尼亚列伊 */
+        Currency ROL = Currency.getInstance("ROL");
+
+        /** 罗马尼亚列伊 */
+        Currency RON = Currency.getInstance("RON");
+
+        /** 塞尔维亚第纳尔 */
+        Currency RSD = Currency.getInstance("RSD");
+
+        /** 俄国卢布 */
+        Currency RUB = Currency.getInstance("RUB");
+
+        /** 俄国卢布 (1991-1998) */
+        Currency RUR = Currency.getInstance("RUR");
+
+        /** 卢旺达法郎 */
+        Currency RWF = Currency.getInstance("RWF");
+
+        /** 沙特里亚尔 */
+        Currency SAR = Currency.getInstance("SAR");
+
+        /** 所罗门群岛元 */
+        Currency SBD = Currency.getInstance("SBD");
+
+        /** 塞舌尔卢比 */
+        Currency SCR = Currency.getInstance("SCR");
+
+        /** 苏丹第纳尔 */
+        Currency SDD = Currency.getInstance("SDD");
+
+        /** Sudanese Pound */
+        Currency SDG = Currency.getInstance("SDG");
+
+        /** 瑞典克朗 */
+        Currency SEK = Currency.getInstance("SEK");
+
+        /** 新加坡元 */
+        Currency SGD = Currency.getInstance("SGD");
+
+        /** 圣赫勒拿群岛磅 */
+        Currency SHP = Currency.getInstance("SHP");
+
+        /** 斯洛文尼亚托拉尔 */
+        Currency SIT = Currency.getInstance("SIT");
+
+        /** 斯洛伐克克朗 */
+        Currency SKK = Currency.getInstance("SKK");
+
+        /** 塞拉利昂利昂 */
+        Currency SLL = Currency.getInstance("SLL");
+
+        /** 索马里先令 */
+        Currency SOS = Currency.getInstance("SOS");
+
+        /** 苏里南元 */
+        Currency SRD = Currency.getInstance("SRD");
+
+        /** 苏里南盾 */
+        Currency SRG = Currency.getInstance("SRG");
+
+        /** 圣多美和普林西比多布拉 */
+        Currency STD = Currency.getInstance("STD");
+
+        /** 萨尔瓦多科朗 */
+        Currency SVC = Currency.getInstance("SVC");
+
+        /** 叙利亚镑 */
+        Currency SYP = Currency.getInstance("SYP");
+
+        /** 斯威士兰里兰吉尼 */
+        Currency SZL = Currency.getInstance("SZL");
+
+        /** 泰铢 */
+        Currency THB = Currency.getInstance("THB");
+
+        /** 塔吉克斯坦索莫尼 */
+        Currency TJS = Currency.getInstance("TJS");
+
+        /** 土库曼斯坦马纳特 */
+        Currency TMM = Currency.getInstance("TMM");
+
+        /** 突尼斯第纳尔 */
+        Currency TND = Currency.getInstance("TND");
+
+        /** 汤加潘加 */
+        Currency TOP = Currency.getInstance("TOP");
+
+        /** 帝汶埃斯库多 */
+        Currency TPE = Currency.getInstance("TPE");
+
+        /** 土耳其里拉 */
+        Currency TRL = Currency.getInstance("TRL");
+
+        /** 新土耳其里拉 */
+        Currency TRY = Currency.getInstance("TRY");
+
+        /** 特立尼达和多巴哥元 */
+        Currency TTD = Currency.getInstance("TTD");
+
+        /** 新台币 */
+        Currency TWD = Currency.getInstance("TWD");
+
+        /** 坦桑尼亚先令 */
+        Currency TZS = Currency.getInstance("TZS");
+
+        /** 乌克兰格里夫尼亚 */
+        Currency UAH = Currency.getInstance("UAH");
+
+        /** 乌干达先令 */
+        Currency UGX = Currency.getInstance("UGX");
+
+        /** 美元 */
+        Currency USD = Currency.getInstance("USD");
+
+        /** 美元（次日） */
+        Currency USN = Currency.getInstance("USN");
+
+        /** 美元（当日） */
+        Currency USS = Currency.getInstance("USS");
+
+        /** 乌拉圭比索 */
+        Currency UYU = Currency.getInstance("UYU");
+
+        /** 乌兹别克斯苏姆 */
+        Currency UZS = Currency.getInstance("UZS");
+
+        /** 委内瑞拉博利瓦 */
+        Currency VEB = Currency.getInstance("VEB");
+
+        /** 委内瑞拉强势玻利瓦 */
+        Currency VEF = Currency.getInstance("VEF");
+
+        /** 越南盾 */
+        Currency VND = Currency.getInstance("VND");
+
+        /** 瓦努阿图瓦图 */
+        Currency VUV = Currency.getInstance("VUV");
+
+        /** 西萨摩亚塔拉 */
+        Currency WST = Currency.getInstance("WST");
+
+        /** 中非金融合作法郎 */
+        Currency XAF = Currency.getInstance("XAF");
+
+        /** 银 */
+        Currency XAG = Currency.getInstance("XAG");
+
+        /** 黄金 */
+        Currency XAU = Currency.getInstance("XAU");
+
+        /** 欧洲复合单位 */
+        Currency XBA = Currency.getInstance("XBA");
+
+        /** 欧洲货币联盟 */
+        Currency XBB = Currency.getInstance("XBB");
+
+        /** 欧洲计算单位 (XBC) */
+        Currency XBC = Currency.getInstance("XBC");
+
+        /** 欧洲计算单位 (XBD) */
+        Currency XBD = Currency.getInstance("XBD");
+
+        /** 东加勒比元 */
+        Currency XCD = Currency.getInstance("XCD");
+
+        /** 特别提款权 */
+        Currency XDR = Currency.getInstance("XDR");
+
+        /** 法国金法郎 */
+        Currency XFO = Currency.getInstance("XFO");
+
+        /** 法国 UIC 法郎 */
+        Currency XFU = Currency.getInstance("XFU");
+
+        /** 非洲金融共同体法郎 */
+        Currency XOF = Currency.getInstance("XOF");
+
+        /** 钯 */
+        Currency XPD = Currency.getInstance("XPD");
+
+        /** 太平洋法郎 */
+        Currency XPF = Currency.getInstance("XPF");
+
+        /** 铂 */
+        Currency XPT = Currency.getInstance("XPT");
+
+        /** 为测试保留的代码 */
+        Currency XTS = Currency.getInstance("XTS");
+
+        /** 货币未知或无效 */
+        Currency XXX = Currency.getInstance("XXX");
+
+        /** 也门里亚尔 */
+        Currency YER = Currency.getInstance("YER");
+
+        /** 南斯拉夫偌威第纳尔 */
+        Currency YUM = Currency.getInstance("YUM");
+
+        /** 南非兰特 */
+        Currency ZAR = Currency.getInstance("ZAR");
+
+        /** 赞比亚克瓦查 */
+        Currency ZMK = Currency.getInstance("ZMK");
+
+        /** 津巴布韦元 */
+        Currency ZWD = Currency.getInstance("ZWD");
+
+        /** ZWN */
+        Currency ZWN = Currency.getInstance("ZWN");
+    }
 
     Currency NATIVE_CURRENCY = CurrencyConfig.NATIVE;
 
