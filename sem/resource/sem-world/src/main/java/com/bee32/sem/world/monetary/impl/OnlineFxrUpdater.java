9eb0dfe8955cd5295ec7dbbc08401c5aa6e0ee0a
Lenik <xjl@99jsj.com>
2011-07-22 18:06:09 +0800

    OnlinxFxrUpdater commit only if it's newer than the latest.

--- sem/resource/sem-world/src/main/java/com/bee32/sem/world/monetary/impl/OnlineFxrUpdater.java
+++ sem/resource/sem-world/src/main/java/com/bee32/sem/world/monetary/impl/OnlineFxrUpdater.java
@@ -1,14 +1,19 @@
 package com.bee32.sem.world.monetary.impl;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Currency;
+import java.util.Date;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.bee32.plover.arch.EnterpriseService;
+import com.bee32.plover.criteria.hibernate.Order;
 import com.bee32.sem.world.monetary.FxrQueryException;
+import com.bee32.sem.world.monetary.FxrRecord;
 import com.bee32.sem.world.monetary.FxrTable;
 import com.bee32.sem.world.monetary.ICurrencyAware;
 import com.bee32.sem.world.monetary.IFxrUpdater;
@@ -45,18 +50,42 @@ public abstract class OnlineFxrUpdater
             return;
         }
 
-        Currency unitCurrency = table.getUnitCurrency();
-        if (unitCurrency != NATIVE_CURRENCY) {
-            logger.error("FXR Update is skipped because of bad unit currency in FXR table: " + unitCurrency);
+        Currency quoteCurrency = table.getQuoteCurrency();
+        if (quoteCurrency != NATIVE_CURRENCY) {
+            logger.error("FXR Update is skipped because of bad quote currency in FXR table: " + quoteCurrency);
             return;
         }
 
         commit(table);
     }
 
-    @Transactional(readOnly = false)
-    void commit(FxrTable table) {
+    static Date lastUpdatedDate;
 
+    @Transactional(readOnly = false)
+    synchronized void commit(FxrTable table) {
+
+        if (lastUpdatedDate == null) {
+            FxrRecord mostRecentRecord = asFor(FxrRecord.class).getFirst(Order.desc("date"));
+            if (mostRecentRecord == null)
+                lastUpdatedDate = new Date(0); // 1970-1-1
+            else
+                lastUpdatedDate = mostRecentRecord.getDate();
+        }
+
+        List<FxrRecord> newRecords = new ArrayList<FxrRecord>();
+        Date maxDate = lastUpdatedDate;
+        for (FxrRecord record : table.getRecords()) {
+            // record.date > lastUpdatedDate?
+            Date date = record.getDate();
+            if (date.compareTo(lastUpdatedDate) > 0) {
+                newRecords.add(record);
+                if (date.compareTo(maxDate) > 0)
+                    maxDate = date;
+            }
+        }
+
+        asFor(FxrRecord.class).saveAll(newRecords);
+        lastUpdatedDate = maxDate;
     }
 
 }
