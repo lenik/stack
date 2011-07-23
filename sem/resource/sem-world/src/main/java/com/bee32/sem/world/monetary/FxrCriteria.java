2420015aac97df8065cf63eb34086f48443fb108
Lenik <xjl@99jsj.com>
2011-07-23 17:55:20 +0800

    Added FxrCriteria.

---
+++ sem/resource/sem-world/src/main/java/com/bee32/sem/world/monetary/FxrCriteria.java
@@ -0,0 +1,15 @@
+package com.bee32.sem.world.monetary;
+
+import java.util.Date;
+
+import com.bee32.plover.criteria.hibernate.CriteriaSpec;
+import com.bee32.plover.criteria.hibernate.ICriteriaElement;
+
+public class FxrCriteria
+        extends CriteriaSpec {
+
+    public static ICriteriaElement beforeThan(Date date, int limit) {
+        return compose(lessThan("date", date));
+    }
+
+}
