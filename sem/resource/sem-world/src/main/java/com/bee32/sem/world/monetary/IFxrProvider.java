d49c85752f1501d9a23af78077c2973e4fbee665
Lenik <xjl@99jsj.com>
2011-07-23 17:54:28 +0800

    Added IFxrProvider.getFxr for history.

--- sem/resource/sem-world/src/main/java/com/bee32/sem/world/monetary/IFxrProvider.java
+++ sem/resource/sem-world/src/main/java/com/bee32/sem/world/monetary/IFxrProvider.java
@@ -1,8 +1,43 @@
 package com.bee32.sem.world.monetary;
 
 import java.util.Currency;
+import java.util.Date;
+import java.util.List;
 
 public interface IFxrProvider {
+
+    /**
+     * This is the same to {@link FxrTable#getQuoteCurrency()}.
+     *
+     * The actual provider should use the native currency as quote currency.
+     *
+     * @return Non-<code>null</code> quote currency in use.
+     */
+    Currency getQuoteCurrency();
+
+    /**
+     * Get the FXR table towards the floor and several tables before of a specific date.
+     *
+     * @return The FXR table towards the floor of a specific date. <code>null</code> if none.
+     */
+    List<FxrTable> getFxrTableSeries(Date date, int nprev)
+            throws FxrQueryException;
+
+    /**
+     * Get the FXR table towards the floor of a specific date.
+     *
+     * @return The FXR table towards the floor of a specific date. <code>null</code> if none.
+     */
+    FxrTable getFxrTable(Date date)
+            throws FxrQueryException;
+
+    /**
+     * Get the FXR of a specific unit currency towards the floor of a specific date.
+     *
+     * @return <code>null</code> if none.
+     */
+    Float getFxr(Date date, Currency unitCurrency, FxrUsage usage)
+            throws FxrQueryException;
 
     /**
      * Get the latest FXR table.
@@ -13,24 +48,15 @@ public interface IFxrProvider {
             throws FxrQueryException;
 
     /**
-     * This is the same to {@link FxrTable#getUnitCurrency()}.
-     *
-     * The actual provider should use the native currency as unit currency.
-     *
-     * @return Non-<code>null</code> unit currency in use.
-     */
-    Currency getUnitCurrency();
-
-    /**
      * Get the latest FXR entry.
      * <p>
      * The same to {@link #getLatestFxrTable()}, {@link FxrTable#getQuote(Currency)}.
      *
-     * @param quoteCurrency
-     *            The quoting currency to be queried.
-     * @return <code>null</code> if the FXR for the specified quote currency is not defined.
+     * @param unitCurrency
+     *            The unit currency to be queried.
+     * @return <code>null</code> if the FXR for the specified unit currency is not defined.
      */
-    Float getLatestFxr(Currency quoteCurrency)
+    Float getLatestFxr(Currency unitCurrency, FxrUsage usage)
             throws FxrQueryException;
 
 }
