8b341909b11d1a4742124bbf2f9c87c4903079d7
ztf <ztf@bee32.com>
2011-07-23 17:19:58 +0800

    add materialCategory menu entry

--- sem/resource/sem-inventory/src/main/java/com/bee32/sem/inventory/SEMInventoryMenu.java
+++ sem/resource/sem-inventory/src/main/java/com/bee32/sem/inventory/SEMInventoryMenu.java
@@ -12,14 +12,13 @@ public class SEMInventoryMenu
 
     static Location INVENTORY_ = WEB_APP.join(SEMInventoryModule.PREFIX_);
 
-
-
     public static MenuNode INVENTORY = menu(SEMFrameMenu.MAIN, "inventory");
     public static MenuNode MATERIAL = menu(INVENTORY, "material");
     public static MenuNode SETTINGS = menu(INVENTORY, "settings");
 
     static MenuNode materialAdmin = entry(MATERIAL, 1, "materialAdmin", INVENTORY_.join("material/index-rich.jsf"));
 
+    static MenuNode materialCategory = entry(SETTINGS, 1, "materialCategory", INVENTORY_.join("settings/materialCategory/index-rich.jsf"));
     static MenuNode stockWarehouse = entry(SETTINGS, 1, "stockWarehouse", INVENTORY_.join("settings/stockWarehouse/index-rich.jsf"));
     static MenuNode stockLocation = entry(SETTINGS, 1, "stockLocation", INVENTORY_.join("settings/stockLocation/index-rich.jsf"));
 
