0345c7183d8c6904f6ac2bf6414a57bca658ff45
Lenik <xjl@99jsj.com>
2011-07-24 08:08:48 +0800

    LoginBean review.

--- sem/base/sem-people/src/main/java/com/bee32/sem/people/web/LoginBean.java
+++ sem/base/sem-people/src/main/java/com/bee32/sem/people/web/LoginBean.java
@@ -3,8 +3,6 @@ package com.bee32.sem.people.web;
 import java.util.List;
 import java.util.Random;
 
-import javax.faces.application.FacesMessage;
-import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 
 import org.apache.commons.codec.digest.DigestUtils;
@@ -20,72 +18,71 @@ import com.bee32.plover.orm.util.EntityViewBean;
 
 @Component
 @Scope("view")
-public class LoginBean extends EntityViewBean {
+public class LoginBean
+        extends EntityViewBean {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private String username;
-	private String password;
-	private String challenge = "C" + new Random().nextInt();
+    private String username;
+    private String password;
+    private String challenge = "C" + new Random().nextInt();
 
-	public String getUsername() {
-		return username;
-	}
+    public String getUsername() {
+        return username;
+    }
 
-	public void setUsername(String username) {
-		this.username = username;
-	}
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    public String getPassword() {
+        return password;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-	public String getChallenge() {
-		return challenge;
-	}
+    public String getChallenge() {
+        return challenge;
+    }
 
-	public void setChallenge(String challenge) {
-		this.challenge = challenge;
-	}
+    public void setChallenge(String challenge) {
+        this.challenge = challenge;
+    }
 
+    public void login(ActionEvent even) {
+        boolean loggedIn = false;
 
+        SessionLoginInfo.setCurrentUser(null);
 
-	public void login(ActionEvent even) {
-		FacesMessage msg = null;
-		RequestContext context = RequestContext.getCurrentInstance();
-		boolean loggedIn = false;
+        User user = serviceFor(User.class).getByName(username);
+        if (user == null) {
+            uiLogger.error("用户不存在");
+            return;
+        }
 
-		SessionLoginInfo.setCurrentUser(null);
+        List<UserPassword> plist = serviceFor(UserPassword.class).list(LoginCriteria.forUser(user));
+        if (plist.isEmpty()) {
+            uiLogger.error("登录错误:密码不存在");
+            return;
+        }
 
-		User user = serviceFor(User.class).get(username);
-		if(user == null) {
-			msg = new FacesMessage(FacesMessage.SEVERITY_WARN, "登录错误", "用户不存在");
-		} else {
-			List<UserPassword> plist = serviceFor(UserPassword.class).list(
-					LoginCriteria.forUser(user));
-			if(plist.isEmpty()) {
-				msg = new FacesMessage(FacesMessage.SEVERITY_WARN, "登录错误", "密码不存在");
-			} else {
-				String p1 = plist.get(0).getPasswd();
-				String _p2 = challenge + p1 + challenge;
-				String p2 = DigestUtils.shaHex(_p2);
+        String p1 = plist.get(0).getPasswd();
+        String _p2 = challenge + p1 + challenge;
+        String p2 = DigestUtils.shaHex(_p2);
 
-				if(p2.equals(password)) {
-					SessionLoginInfo.setCurrentUser(user);
+        if (!p2.equals(password)) {
+            uiLogger.error("密码输入错误");
+            return;
+        }
 
-					loggedIn = true;
-					msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "登录成功", "登录成功");
-				} else {
-					msg = new FacesMessage(FacesMessage.SEVERITY_WARN, "登录错误", "密码输入错误");
-				}
-			}
-		}
+        SessionLoginInfo.setCurrentUser(user);
+        loggedIn = true;
+        uiLogger.info("登录成功");
 
-		FacesContext.getCurrentInstance().addMessage(null, msg);
-		context.addCallbackParam("loggedIn", loggedIn);
-	}
+        RequestContext requestContext = RequestContext.getCurrentInstance();
+        requestContext.addCallbackParam("loggedIn", loggedIn);
+    }
+
 }
