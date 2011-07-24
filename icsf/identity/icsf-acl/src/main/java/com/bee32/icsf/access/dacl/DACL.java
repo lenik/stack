902390a54f4c67c484028fa22b97dbfdc6553f4b
Lenik <xjl@99jsj.com>
2011-07-24 12:36:43 +0800

    Rebased DACL < TreeEntityAuto.

--- icsf/identity/icsf-acl/src/main/java/com/bee32/icsf/access/dacl/DACL.java
+++ icsf/identity/icsf-acl/src/main/java/com/bee32/icsf/access/dacl/DACL.java
@@ -3,52 +3,23 @@ package com.bee32.icsf.access.dacl;
 import java.util.List;
 
 import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
 import org.hibernate.annotations.Cascade;
 import org.hibernate.annotations.CascadeType;
 
-import com.bee32.icsf.principal.Principal;
-import com.bee32.plover.orm.ext.color.UIEntityAuto;
+import com.bee32.plover.orm.ext.tree.TreeEntityAuto;
 
 @Entity
 public class DACL
-        extends UIEntityAuto<Integer> {
+        extends TreeEntityAuto<Integer, DACL> {
 
     private static final long serialVersionUID = 1L;
 
-    // Not yet used.
-    DACL parent;
-
-    Principal owner;
     List<DACE> entries;
 
-    /**
-     * Not used, yet.
-     */
-    @ManyToOne
-    public DACL getParent() {
-        return parent;
-    }
-
-    public void setParent(DACL parent) {
-        this.parent = parent;
-    }
-
-    @ManyToOne
-    @JoinColumn(nullable = false)
-    public Principal getOwner() {
-        return owner;
-    }
-
-    public void setOwner(Principal owner) {
-        this.owner = owner;
-    }
-
     @OneToMany(mappedBy = "dacl")
-    @Cascade({ CascadeType.ALL, CascadeType.DELETE_ORPHAN })
+    @Cascade(CascadeType.ALL)
     public List<DACE> getEntries() {
         return entries;
     }
@@ -60,6 +31,8 @@ public class DACL
     }
 
     public boolean addEntry(DACE entry) {
+        if (entry == null)
+            throw new NullPointerException("entry");
         if (entries.contains(entry))
             return false;
         entries.add(entry);
@@ -67,6 +40,8 @@ public class DACL
     }
 
     public boolean removeEntry(DACE entry) {
+        if (entry == null)
+            throw new NullPointerException("entry");
         return entries.remove(entry);
     }
 
--- icsf/identity/icsf-acl/src/main/java/com/bee32/icsf/access/dacl/DACLDto.java
+++ icsf/identity/icsf-acl/src/main/java/com/bee32/icsf/access/dacl/DACLDto.java
@@ -5,19 +5,16 @@ import java.util.List;
 import javax.free.NotImplementedException;
 import javax.free.ParseException;
 
-import com.bee32.icsf.principal.dto.AbstractPrincipalDto;
 import com.bee32.plover.arch.util.TextMap;
-import com.bee32.plover.orm.ext.color.UIEntityDto;
+import com.bee32.plover.orm.ext.tree.TreeEntityDto;
 
 public class DACLDto
-        extends UIEntityDto<DACL, Integer> {
+        extends TreeEntityDto<DACL, Integer, DACLDto> {
 
     private static final long serialVersionUID = 1L;
 
     public static final int ENTRIES = 1;
 
-    DACLDto parent;
-    AbstractPrincipalDto<?> owner;
     List<DACEDto> entries;
 
     public DACLDto() {
@@ -28,20 +25,14 @@ public class DACLDto
         super(selection);
     }
 
-    @SuppressWarnings("unchecked")
     @Override
     protected void _marshal(DACL source) {
-        parent = mref(DACLDto.class, source.getParent());
-        owner = mref(AbstractPrincipalDto.class, source.getOwner());
-
         if (selection.contains(ENTRIES))
             entries = marshalList(DACEDto.class, source.getEntries());
     }
 
     @Override
     protected void _unmarshalTo(DACL target) {
-        merge(target, "parent", parent);
-        merge(target, "owner", owner);
         mergeList(target, "entries", entries);
     }
 
@@ -49,26 +40,6 @@ public class DACLDto
     protected void _parse(TextMap map)
             throws ParseException {
         throw new NotImplementedException();
-    }
-
-    public DACLDto getParent() {
-        return parent;
-    }
-
-    public void setParent(DACLDto parent) {
-        if (parent == null)
-            throw new NullPointerException("parent");
-        this.parent = parent;
-    }
-
-    public AbstractPrincipalDto<?> getOwner() {
-        return owner;
-    }
-
-    public void setOwner(AbstractPrincipalDto<?> owner) {
-        if (owner == null)
-            throw new NullPointerException("owner");
-        this.owner = owner;
     }
 
     public List<DACEDto> getEntries() {
