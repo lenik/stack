abb42d5aa62d8bb3ec4142db1ed61caad5ef1a00
ztf <ztf@bee32.com>
2011-07-26 17:10:18 +0800

    物料设置基本完成

--- sem/resource/sem-inventory/src/main/java/com/bee32/sem/inventory/web/MaterialSettingsBean.java
+++ sem/resource/sem-inventory/src/main/java/com/bee32/sem/inventory/web/MaterialSettingsBean.java
@@ -3,10 +3,12 @@ package com.bee32.sem.inventory.web;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
+import javax.faces.model.SelectItem;
 
 import org.primefaces.event.FlowEvent;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+import org.zkoss.lang.Strings;
 
 import com.bee32.plover.orm.ext.tree.TreeCriteria;
 import com.bee32.plover.orm.util.DTOs;
@@ -16,6 +18,13 @@ import com.bee32.sem.inventory.dto.MaterialCategoryDto;
 import com.bee32.sem.inventory.entity.MaterialCategory;
 import com.bee32.sem.inventory.web.dialogs.MaterialCategoryTreeModel;
 import com.bee32.sem.sandbox.MultiTabEntityViewBean;
+import com.bee32.sem.sandbox.UIHelper;
+import com.bee32.sem.world.thing.ScaleItem;
+import com.bee32.sem.world.thing.Unit;
+import com.bee32.sem.world.thing.UnitConv;
+import com.bee32.sem.world.thing.UnitConvDto;
+import com.bee32.sem.world.thing.UnitCriteria;
+import com.bee32.sem.world.thing.UnitDto;
 
 @Component
 @Scope("view")
@@ -25,27 +34,39 @@ public class MaterialSettingsBean
     private static final long serialVersionUID = 1L;
     static final String BUTTON_CATEGORY_EDIT = "main:editCategory";
     static final String BUTTON_CATEGORY_DELETE = "main:deleteCategory";
-    static final String BUTTON_UNIT_DELETE = "main:deleteUnit";
-    static final String BUTTON_UNITCONV_EDIT = "main:editUnitConv";
-    static final String BUTTON_UNITCONV_DELETE = "main:deleteUnitConv";
+
+    boolean unitConvEditable;
+    boolean categoryOptionable;
+    boolean cantEdit;
 
     MaterialCategoryDto activeCategory = new MaterialCategoryDto().create();
+    List<UnitDto> unitList;
+    UnitDto activeUnit = new UnitDto().create();;
+    UnitDto selectedUnit;
+
+    List<UnitConvDto> unitConvList;
+    UnitConvDto activeUnitConv = new UnitConvDto().create();
+    UnitConvDto selectedUnitConv;
+
+    ScaleItem activeScaleItem;
+    ScaleItem selectedScale;
 
     MaterialCategoryTreeModel materialCategorySelectTree;
     MaterialCategoryTreeModel materialCategoryMainTree;
 
     public MaterialSettingsBean() {
+        activeScaleItem = new ScaleItem();
+        UnitDto unit = new UnitDto().create();
+        activeScaleItem.setUnit(unit);
+
         innitSelectCategoryTree();
         initMainTree();
+        initUnitList();
+        initUnitConvList();
     }
 
     @PostConstruct
     public void initPage() {
-        findComponentEx(BUTTON_CATEGORY_EDIT).setEnabled(false);
-        findComponentEx(BUTTON_CATEGORY_DELETE).setEnabled(false);
-        findComponentEx(BUTTON_UNIT_DELETE).setEnabled(false);
-        findComponentEx(BUTTON_UNITCONV_EDIT).setEnabled(false);
-        findComponentEx(BUTTON_UNITCONV_DELETE).setEnabled(false);
     }
 
     public void initMainTree() {
@@ -84,6 +105,107 @@ public class MaterialSettingsBean
         });
     }
 
+    public void initUnitConvList() {
+        List<UnitConv> ucl = serviceFor(UnitConv.class).list();
+        unitConvList = DTOs.marshalList(UnitConvDto.class, ucl);
+    }
+
+    public void setUnitConvUneditable() {
+        unitConvEditable = true;
+    }
+
+    public void createScaleItem() {
+        activeScaleItem = new ScaleItem();
+        UnitDto unitDto = new UnitDto().create();
+        activeScaleItem.setUnit(unitDto);
+    }
+
+    public void createUnitConvForm() {
+        unitConvEditable = false;
+        cantEdit = false;
+        activeUnitConv = new UnitConvDto().create();
+    }
+
+//    public void editUnitConvForm() {
+//        activeUnitConv = selectedUnitConv;
+//        activeUnitConv = reload(activeUnitConv);
+//    }
+
+    public void doAddScaleItem() {
+        String id = activeScaleItem.getUnit().getId();
+        Unit _unit = serviceFor(Unit.class).getOrFail(id);
+        UnitDto unit = DTOs.marshal(UnitDto.class, _unit);
+        activeScaleItem.setUnit(unit);
+        activeUnitConv.addScaleItem(activeScaleItem);
+    }
+
+    public void doSaveUnitConv() {
+        try {
+            UnitConv ucv = activeUnitConv.unmarshal();
+            serviceFor(UnitConv.class).saveOrUpdate(ucv);
+            initUnitConvList();
+            uiLogger.info("保存单位还算表" + activeUnitConv.getLabel() + "成功");
+        } catch (Exception e) {
+            uiLogger.error("保存单位还算表" + activeUnitConv.getLabel() + "失败:" + e.getMessage(), e);
+        }
+    }
+
+    public void destroyUnitConv() {
+        try {
+            String convId = selectedUnitConv.getId();
+            UnitConv uc = serviceFor(UnitConv.class).getOrFail(convId);
+            serviceFor(UnitConv.class).delete(uc);
+            initUnitConvList();
+            uiLogger.info("删除单位还算表" + selectedUnitConv.getLabel() + "成功");
+        } catch (Exception e) {
+            uiLogger.error("删除单位还算表" + selectedUnitConv.getLabel() + "失败" + e.getMessage(), e);
+        }
+    }
+
+    public void initUnitList() {
+        List<Unit> ul = serviceFor(Unit.class).list();
+        unitList = DTOs.marshalList(UnitDto.class, ul);
+    }
+
+    public void createUnitForm() {
+        cantEdit = false;
+        activeUnit = new UnitDto().create();
+    }
+
+//    public void editUnitForm() {
+//        activeUnit = selectedUnit;
+//        activeUnit = reload(activeUnit);
+//    }
+
+    public void doSaveUnit() {
+        String stuId = activeUnit.getStdUnit().getId();
+        UnitDto stdUnit = null;
+        if (!Strings.isEmpty(stuId))
+            stdUnit = DTOs.marshal(UnitDto.class, serviceFor(Unit.class).getOrFail(activeUnit.getStdUnit().getId()));
+
+        try {
+            activeUnit.setStdUnit(stdUnit);
+            Unit enity = activeUnit.unmarshal();
+            serviceFor(Unit.class).saveOrUpdate(enity);
+            initUnitList();
+            uiLogger.info("保存单位" + activeUnit.getLabel() + "成功!");
+        } catch (Exception e) {
+            uiLogger.error("保存单位" + activeUnit.getLabel() + "错误:" + e.getMessage(), e);
+        }
+    }
+
+    public void destroyUnit() {
+        try {
+            String unitId = selectedUnit.getId();
+            Unit unitl = serviceFor(Unit.class).getOrFail(unitId);
+            serviceFor(Unit.class).delete(unitl);
+            initUnitList();
+            uiLogger.info("删除单位" + selectedUnit.getLabel() + "成功");
+        } catch (Exception e) {
+            uiLogger.error("删除单位" + selectedUnit.getLabel() + "失败:" + e.getMessage(), e);
+        }
+    }
+
     public void createCategoryForm() {
         activeCategory = new MaterialCategoryDto().create();
     }
@@ -120,22 +242,33 @@ public class MaterialSettingsBean
         }
     }
 
-// public List<SelectItem> getCodeGeneratories(){
-// List<CodeGenerator> generatories = serviceFor(CodeGenerator.class).list();
-// }
-
     public void onCategoryNodeSelect() {
-        findComponentEx(BUTTON_CATEGORY_EDIT).setEnabled(true);
-        findComponentEx(BUTTON_CATEGORY_DELETE).setEnabled(true);
+        categoryOptionable = true;
     }
 
     public void onCategoryNodeUnselect() {
-        findComponentEx(BUTTON_CATEGORY_EDIT).setEnabled(false);
-        findComponentEx(BUTTON_CATEGORY_DELETE).setEnabled(false);
+        categoryOptionable = false;
+    }
+
+    public void setDictNameCantEdit() {
+        cantEdit = true;
+        unitConvEditable = false;
     }
 
     public String onFlowProcess(FlowEvent event) {
         return event.getNewStep();
+    }
+
+    public List<SelectItem> getStandardUnits() {
+        List<Unit> stdUnitList = serviceFor(Unit.class).list(UnitCriteria.standardUnits);
+        List<UnitDto> stdUnitDtoList = DTOs.marshalList(UnitDto.class, stdUnitList);
+        return UIHelper.selectItemsFromDict(stdUnitDtoList);
+    }
+
+    public List<SelectItem> getUnits() {
+        List<Unit> unitList = serviceFor(Unit.class).list();
+        List<UnitDto> unitDtoList = DTOs.marshalList(UnitDto.class, unitList);
+        return UIHelper.selectItemsFromDict(unitDtoList);
     }
 
     public MaterialCategoryDto getActiveCategory() {
@@ -146,12 +279,106 @@ public class MaterialSettingsBean
         this.activeCategory = activeCategory;
     }
 
+    public List<UnitDto> getUnitList() {
+        return unitList;
+    }
+
+    public void setUnitList(List<UnitDto> unitList) {
+        this.unitList = unitList;
+    }
+
+    public UnitDto getActiveUnit() {
+        return activeUnit;
+    }
+
+    public void setActiveUnit(UnitDto activeUnit) {
+        this.activeUnit = activeUnit;
+    }
+
+    public UnitDto getSelectedUnit() {
+        return selectedUnit;
+    }
+
+    public void setSelectedUnit(UnitDto sUnit) {
+        this.selectedUnit = sUnit;
+        this.activeUnit = reload(selectedUnit);
+    }
+
+    public List<UnitConvDto> getUnitConvList() {
+        return unitConvList;
+    }
+
+    public void setUnitConvList(List<UnitConvDto> unitConvList) {
+        this.unitConvList = unitConvList;
+    }
+
+    public UnitConvDto getActiveUnitConv() {
+        return activeUnitConv;
+    }
+
+    public void setActiveUnitConv(UnitConvDto activeUnitConv) {
+        this.activeUnitConv = activeUnitConv;
+    }
+
+    public UnitConvDto getSelectedUnitConv() {
+        return selectedUnitConv;
+    }
+
+    public void setSelectedUnitConv(UnitConvDto selectedUnitConv) {
+        this.selectedUnitConv = selectedUnitConv;
+        this.activeUnitConv = reload(selectedUnitConv);
+    }
+
     public MaterialCategoryTreeModel getMaterialCategorySelectTree() {
         return materialCategorySelectTree;
     }
 
     public MaterialCategoryTreeModel getMaterialCategoryMainTree() {
         return materialCategoryMainTree;
+    }
+
+    public ScaleItem getActiveScaleItem() {
+        return activeScaleItem;
+    }
+
+    public void setActiveScaleItem(ScaleItem activeScaleItem) {
+        this.activeScaleItem = activeScaleItem;
+    }
+
+    public ScaleItem getSelectedScale() {
+        return selectedScale;
+    }
+
+    public void setSelectedScale(ScaleItem selectedScale) {
+        this.selectedScale = selectedScale;
+        activeScaleItem = new ScaleItem();
+        activeScaleItem.setScale(selectedScale.getScale());
+        UnitDto unit = reload(selectedScale.getUnit());
+        activeScaleItem.setUnit(unit);
+    }
+
+    public boolean isUnitConvEditable() {
+        return unitConvEditable;
+    }
+
+    public void setUnitConvEditable(boolean unitConvEditable) {
+        this.unitConvEditable = unitConvEditable;
+    }
+
+    public boolean isCategoryOptionable() {
+        return categoryOptionable;
+    }
+
+    public void setCategoryOptionable(boolean categoryOptionable) {
+        this.categoryOptionable = categoryOptionable;
+    }
+
+    public boolean isCantEdit() {
+        return cantEdit;
+    }
+
+    public void setCantEdit(boolean cantEdit) {
+        this.cantEdit = cantEdit;
     }
 
 }
