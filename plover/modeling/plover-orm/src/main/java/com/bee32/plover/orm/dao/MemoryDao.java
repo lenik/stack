0e3f603bfc6bede85a47c484bf5f9d1304d592b3
Lenik <xjl@99jsj.com>
2011-07-26 20:12:28 +0800

    MemoryDao implementation. (except criteria).

--- plover/modeling/plover-orm/src/main/java/com/bee32/plover/orm/dao/MemoryDao.java
+++ plover/modeling/plover-orm/src/main/java/com/bee32/plover/orm/dao/MemoryDao.java
@@ -20,20 +20,23 @@ import org.hibernate.LockMode;
 import org.hibernate.ReplicationMode;
 import org.springframework.dao.DataAccessException;
 
+import com.bee32.plover.criteria.hibernate.CriteriaComposite;
+import com.bee32.plover.criteria.hibernate.Equals;
 import com.bee32.plover.criteria.hibernate.ICriteriaElement;
-import com.bee32.plover.orm.config.CustomizedSessionFactoryBean;
+import com.bee32.plover.criteria.hibernate.Limit;
 import com.bee32.plover.orm.entity.Entity;
+import com.bee32.plover.orm.entity.EntityAccessor;
 import com.bee32.plover.orm.entity.EntityRepository;
 import com.bee32.plover.orm.entity.EntityUtil;
 import com.bee32.plover.orm.entity.IEntityAccessService;
+import com.bee32.plover.orm.entity.NonUniqueException;
 import com.bee32.plover.orm.unit.PersistenceUnit;
 
 public class MemoryDao
         extends EntityRepository<Entity<?>, Serializable>
         implements IEntityAccessService<Entity<?>, Serializable> {
 
-// Set to true, the root node would be for Entity.class.
-    static final boolean virtualRoot = true;
+    // Set to true, the root node would be for Entity.class.
     static final Map<Class<? extends Entity<?>>, Map<Serializable, Entity<?>>> database;
     static {
         database = new HashMap<Class<? extends Entity<?>>, Map<Serializable, Entity<?>>>();
@@ -43,13 +46,6 @@ public class MemoryDao
     final List<MemoryDao> children = new ArrayList<MemoryDao>();
 
     final Map<Serializable, Entity<?>> table;
-
-    static void scanAll() {
-        PersistenceUnit unit = CustomizedSessionFactoryBean.getForceUnit();
-        for (Class<?> entityType : unit.getClasses()) {
-
-        }
-    }
 
     private MemoryDao(Class<? extends Entity<?>> entityType) {
         super(entityType, EntityUtil.getKeyType(entityType));
@@ -67,23 +63,275 @@ public class MemoryDao
                 @SuppressWarnings("unchecked")
                 Class<? extends Entity<?>> superType = (Class<? extends Entity<?>>) _superType;
 
-                boolean included = true;
-                if (!virtualRoot)
-                    included = _superType.getAnnotation(javax.persistence.Entity.class) != null;
+                boolean isInheritedFromSuper = true;
+                isInheritedFromSuper = _superType.getAnnotation(javax.persistence.Entity.class) != null;
 
-                if (included)
+                if (isInheritedFromSuper)
                     parent = getInstance(superType);
             }
         }
         this.parent = parent;
-
         if (parent != null)
             parent.addChild(this);
     }
 
+    void addChild(MemoryDao childDao) {
+        if (children.contains(childDao))
+            throw new IllegalUsageError("Duplicated child memdao: " + childDao);
+        children.add(childDao);
+    }
+
+    protected MemoryDao access(Entity<?> entity) {
+        if (entity == null)
+            throw new NullPointerException("entity");
+
+        Class<? extends Entity<?>> entityType = (Class<? extends Entity<?>>) entity.getClass();
+        if (this.entityType.equals(entityType))
+            return this;
+
+        if (!this.entityType.isAssignableFrom(entityType))
+            throw new IllegalArgumentException("Can't handle entity of " + entityType //
+                    + " by a " + this.entityType.getSimpleName() + " memory-dao.");
+
+        MemoryDao specDao = getInstance(entityType);
+        return specDao;
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        return table.containsKey(key);
+    }
+
+    @Override
+    public Entity<?> get(Serializable key) {
+        Entity<?> _o = table.get(key);
+        if (_o == null)
+            return null;
+        else
+            return dup(_o);
+    }
+
+    @Override
+    public List<Entity<?>> list() {
+        ArrayList<Entity<?>> copy = new ArrayList<Entity<?>>();
+        for (Entity<?> _o : table.values()) {
+            copy.add(dup(_o));
+        }
+        return copy;
+    }
+
+    @Override
+    public synchronized boolean deleteByKey(Serializable key) {
+        Entity<?> removed = table.remove(key);
+
+        for (MemoryDao child : children)
+            child.deleteByKey(key);
+
+        return removed != null;
+    }
+
+    @Override
+    public synchronized void deleteAll() {
+        table.clear();
+
+        for (MemoryDao child : children)
+            child.deleteAll();
+    }
+
+    @Override
+    public int count() {
+        return table.size();
+    }
+
+    // IEntityRepo_H
+
+    @Override
+    public final Entity<?> lazyLoad(Serializable id) {
+        return get(id);
+    }
+
+    @Override
+    public final Entity<?> retrieve(Serializable key, LockMode lockMode)
+            throws DataAccessException {
+        return get(key);
+    }
+
+    @Override
+    public final void update(Entity<?> entity, LockMode lockMode)
+            throws DataAccessException {
+        update(entity);
+    }
+
+    @Override
+    public final boolean delete(Object entity, LockMode lockMode)
+            throws DataAccessException {
+        return delete(entity);
+    }
+
+    @Deprecated
+    @Override
+    public synchronized Entity<?> merge(Entity<?> entity)
+            throws DataAccessException {
+        MemoryDao spec = access(entity);
+        if (spec != this)
+            return spec.merge(entity);
+
+        @SuppressWarnings("unchecked")
+        Entity<Serializable> _o = (Entity<Serializable>) dup(entity);
+
+        Serializable id = _o.getId();
+        if (id == null) {
+            id = SimpleIdGenerator.generate(entity);
+            EntityAccessor.setId(_o, id);
+        }
+
+        _put(id, _o);
+        return dup(_o);
+    }
+
+    void _put(Serializable id, Entity<?> _o) {
+        MemoryDao node = this;
+        while (node != null) {
+            node.table.put(id, _o);
+            node = node.parent;
+        }
+    }
+
+    @Deprecated
+    @Override
+    public void evict(Entity<?> entity)
+            throws DataAccessException {
+        // do nothing here.
+        for (MemoryDao child : children)
+            child.evict(entity);
+    }
+
+    @Deprecated
+    @Override
+    public void replicate(Entity<?> entity, ReplicationMode replicationMode)
+            throws DataAccessException {
+        // do nothing here.
+        for (MemoryDao child : children)
+            child.replicate(entity, replicationMode);
+    }
+
+    @Deprecated
+    @Override
+    public void flush() {
+        // do nothing here.
+        for (MemoryDao child : children)
+            child.flush();
+    }
+
+    // IEntityAccessService.
+
+    @Override
+    public Entity<?> getUnique(ICriteriaElement... criteriaElements) {
+        List<Entity<?>> list = list(criteriaElements);
+        if (list.isEmpty())
+            return null;
+        if (list.size() > 1)
+            throw new NonUniqueException();
+        return list.get(0);
+    }
+
+    @Override
+    public Entity<?> getFirst(ICriteriaElement... criteriaElements) {
+        CriteriaComposite optim = new CriteriaComposite(criteriaElements, new Limit(0, 1));
+        List<Entity<?>> list = list(optim);
+        if (list.isEmpty())
+            return null;
+        else
+            return list.get(0);
+    }
+
+    @Override
+    public Entity<?> getByName(String name) {
+        return getFirst(new Equals("name", name));
+    }
+
+    @Override
+    public int count(ICriteriaElement... criteriaElements) {
+        return list(criteriaElements).size();
+    }
+
+    @Override
+    public final boolean deleteById(Serializable id) {
+        return deleteByKey(id);
+    }
+
+    @Override
+    public void deleteAll(ICriteriaElement... criteriaElements) {
+        List<Entity<?>> list = list(criteriaElements);
+        for (Entity<?> entity : list)
+            delete(entity);
+    }
+
+    @Override
+    public List<Entity<?>> list(ICriteriaElement... criteriaElements) {
+        throw new NotImplementedException();
+    }
+
+    // ER.
+
+    @Override
+    public Serializable save(Entity<?> _entity) {
+        @SuppressWarnings("unchecked")
+        Entity<Serializable> entity = (Entity<Serializable>) _entity;
+
+        MemoryDao spec = access(entity);
+        if (spec != this)
+            return spec.save(entity);
+
+        Serializable id = entity.getId();
+        if (id != null) {
+            // is not auto-generated?
+        }
+
+        if (id == null) {
+            id = SimpleIdGenerator.generate(entity);
+            EntityAccessor.setId(entity, id);
+        }
+
+        Entity<Serializable> _o = dup(entity);
+        _put(id, _o);
+        return id;
+    }
+
+    @Override
+    public void update(Entity<?> entity) {
+        MemoryDao spec = access(entity);
+        if (spec != this) {
+            spec.update(entity);
+            return;
+        }
+
+        Serializable id = entity.getId();
+        if (id == null)
+            throw new IllegalStateException("Entity hasn't been saved yet: " + entity);
+
+        Entity<?> _o = dup(entity);
+        _put(id, _o);
+    }
+
+    @Override
+    public void refresh(Entity<?> entity) {
+        MemoryDao spec = access(entity);
+        if (spec != this) {
+            spec.refresh(entity);
+            return;
+        }
+        Serializable id = entity.getId();
+        if (id == null)
+            throw new IllegalStateException("Entity hasn't been persisted: " + entity);
+
+        Entity<?> existing = getOrFail(id);
+        entity.populate(existing); // may not been well implemented by specific entity types.
+    }
+
     static final Map<Class<? extends Entity<?>>, MemoryDao> instances = new HashMap<Class<? extends Entity<?>>, MemoryDao>();
 
-    static synchronized MemoryDao getInstance(Class<? extends Entity<?>> entityType) {
+    public static synchronized MemoryDao getInstance(Class<? extends Entity<?>> entityType) {
         MemoryDao instance = instances.get(entityType);
         if (instance == null) {
             instance = new MemoryDao(entityType);
@@ -91,10 +339,16 @@ public class MemoryDao
         return instance;
     }
 
-    void addChild(MemoryDao childDao) {
-        if (children.contains(childDao))
-            throw new IllegalUsageError("Duplicated child memdao: " + childDao);
-        children.add(childDao);
+    public static synchronized void refreshForUnit(PersistenceUnit unit) {
+        instances.clear();
+
+        for (Class<?> _class : unit.getClasses()) {
+
+            @SuppressWarnings("unchecked")
+            Class<? extends Entity<?>> entityType = (Class<? extends Entity<?>>) _class;
+
+            getInstance(entityType);
+        }
     }
 
     /**
@@ -122,182 +376,6 @@ public class MemoryDao
         } catch (ClassNotFoundException e) {
             throw new UnexpectedException(e.getMessage(), e);
         }
-    }
-
-    @Override
-    public boolean containsKey(Object key) {
-        if (table.containsKey(key))
-            return true;
-
-        for (MemoryDao child : children)
-            if (child.contains(key))
-                return true;
-
-        return false;
-    }
-
-    @Override
-    public Entity<?> get(Serializable key) {
-        Entity<?> _o = table.get(key);
-        if (_o == null)
-            for (MemoryDao child : children) {
-                _o = child.get(key);
-                if (_o != null)
-                    break;
-            }
-        if (_o == null)
-            return null;
-        else
-            return dup(_o);
-    }
-
-    @Override
-    public List<Entity<?>> list() {
-        ArrayList<Entity<?>> copy = new ArrayList<Entity<?>>();
-        for (Entity<?> _o : table.values()) {
-            copy.add(dup(_o));
-        }
-        return copy;
-    }
-
-    @Override
-    public boolean deleteByKey(Serializable key) {
-        Entity<?> removed = table.remove(key);
-        if (removed != null)
-            return true;
-
-        for (MemoryDao child : children) {
-            if (child.deleteByKey(key))
-                return true;
-        }
-
-        return false;
-    }
-
-    @Override
-    public void deleteAll() {
-        table.clear();
-
-        for (MemoryDao child : children)
-            child.deleteAll();
-    }
-
-    @Override
-    public int count() {
-        return table.size();
-    }
-
-    // IEntityRepo_H
-
-    @Override
-    public Entity<?> lazyLoad(Serializable id) {
-        return get(id);
-    }
-
-    @Override
-    public Entity<?> retrieve(Serializable key, LockMode lockMode)
-            throws DataAccessException {
-        return get(key);
-    }
-
-    @Override
-    public void update(Entity<?> entity, LockMode lockMode)
-            throws DataAccessException {
-        update(entity);
-    }
-
-    @Override
-    public boolean delete(Object entity, LockMode lockMode)
-            throws DataAccessException {
-        return delete(entity);
-    }
-
-    @Override
-    public Entity<?> merge(Entity<?> entity)
-            throws DataAccessException {
-
-        Class<?> entityType = entity.getClass();
-        // getInstance(entityType)
-
-        Entity<?> _o = dup(entity);
-
-        throw new NotImplementedException();
-    }
-
-    @Override
-    public void evict(Entity<?> entity)
-            throws DataAccessException {
-        // do nothing here.
-    }
-
-    @Override
-    public void replicate(Entity<?> entity, ReplicationMode replicationMode)
-            throws DataAccessException {
-        // do nothing here.
-    }
-
-    @Override
-    public void flush() {
-        // do nothing here.
-    }
-
-    // IEas.
-
-    @Override
-    public Entity<?> getUnique(ICriteriaElement... criteriaElements) {
-        throw new NotImplementedException();
-    }
-
-    @Override
-    public Entity<?> getFirst(ICriteriaElement... criteriaElements) {
-        throw new NotImplementedException();
-    }
-
-    @Override
-    public Entity<?> getByName(String name) {
-        throw new NotImplementedException();
-    }
-
-    @Override
-    public List<Entity<?>> list(ICriteriaElement... criteriaElements) {
-        throw new NotImplementedException();
-    }
-
-    @Override
-    public int count(ICriteriaElement... criteriaElements) {
-        throw new NotImplementedException();
-    }
-
-    @Override
-    public boolean deleteById(Serializable id) {
-        return deleteByKey(id);
-    }
-
-    @Override
-    public void deleteAll(ICriteriaElement... criteriaElements) {
-        throw new NotImplementedException();
-    }
-
-    // ER.
-
-    @Override
-    public Serializable save(Entity<?> entity) {
-        Class<?> entityType = entity.getClass();
-        // getInstance
-
-        Entity<?> _o = dup(entity);
-
-        throw new NotImplementedException();
-    }
-
-    @Override
-    public void update(Entity<?> entity) {
-        // do nothing.
-    }
-
-    @Override
-    public void refresh(Entity<?> entity) {
-        // do nothing.
     }
 
 }
