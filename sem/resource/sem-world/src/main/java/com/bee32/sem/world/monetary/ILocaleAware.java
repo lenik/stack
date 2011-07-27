e291cbb8f46f659f6bdda648ef8d703bed895462
Lenik <xjl@99jsj.com>
2011-07-27 10:41:48 +0800

    Added ILocaleAware.NATIVE_LOCALE.

--- sem/resource/sem-world/src/main/java/com/bee32/sem/world/monetary/ILocaleAware.java
+++ sem/resource/sem-world/src/main/java/com/bee32/sem/world/monetary/ILocaleAware.java
@@ -4,6 +4,8 @@ import java.util.Locale;
 
 public interface ILocaleAware {
 
+    Locale NATIVE_LOCALE = Locale.getDefault();
+
     Locale en_US = Locale.forLanguageTag("en-US");
     Locale zh_CN = Locale.forLanguageTag("zh-CN");
 
