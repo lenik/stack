5e3e421471b97ea0edac245184f5581225a4e328
Lenik <xjl@99jsj.com>
2011-07-29 17:35:47 +0800

    Fixed pre-init null: LoginEntityAddon use dummy admin.

--- icsf/identity/icsf-acl/src/main/java/com/bee32/icsf/login/LoginEntityAddon.java
+++ icsf/identity/icsf-acl/src/main/java/com/bee32/icsf/login/LoginEntityAddon.java
@@ -3,34 +3,49 @@ package com.bee32.icsf.login;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.bee32.icsf.principal.IUserPrincipal;
 import com.bee32.icsf.principal.User;
 import com.bee32.plover.orm.entity.AbstractEntityLifecycleAddon;
 import com.bee32.plover.orm.entity.Entity;
+import com.bee32.plover.orm.entity.EntityAccessor;
 
 public class LoginEntityAddon
         extends AbstractEntityLifecycleAddon {
 
     static Logger logger = LoggerFactory.getLogger(LoginEntityAddon.class);
 
+    static final User dummy = new User("dummy");
+    static {
+        EntityAccessor.setId(dummy, -1);
+    }
+
+    User getCurrentUser() {
+        User user = (User) SessionLoginInfo.getUserOpt();
+        if (user != null) {
+            assert user.getId() != null;
+            return user;
+        }
+
+        if (User.admin == null)
+            return dummy;
+
+        Integer adminId = User.admin.getId();
+        if (adminId == null)
+            return dummy;
+
+        return User.admin;
+    }
+
     @Override
     public void entityCreate(Entity<?> entity) {
         super.entityCreate(entity);
 
-        IUserPrincipal owner = SessionLoginInfo.getUserOpt();
-        if (owner == null)
-            owner = User.admin;
+        // Ignore if the owner is already specified.
+        if (entity.getOwnerId() != 0)
+            return;
 
-        if (owner != null) {
-            Integer ownerId = owner.getId();
-            if (ownerId == null) {
-                // Until now, the necessary user/dacl hasn't been created yet.
-                return;
-            }
-
-            logger.debug("Set owner of new entity: " + owner.getDisplayName());
-            entity.setOwnerId(ownerId);
-        }
+        User owner = getCurrentUser();
+        logger.debug("Set owner of new entity: " + owner.getDisplayName());
+        entity.setOwnerId(owner.getId());
     }
 
 }
