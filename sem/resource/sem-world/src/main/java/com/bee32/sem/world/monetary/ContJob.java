6fda1156aa7b5b2f71711a71baf288eba1656902
Lenik <xjl@99jsj.com>
2011-08-04 17:42:34 +0800

    Prepare of FXR scheduler.

---
+++ sem/resource/sem-world/src/main/java/com/bee32/sem/world/monetary/ContJob.java
@@ -0,0 +1,15 @@
+package com.bee32.sem.world.monetary;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+public class ContJob
+        implements Job {
+
+    @Override
+    public void execute(JobExecutionContext jobexecutioncontext)
+            throws JobExecutionException {
+    }
+
+}
