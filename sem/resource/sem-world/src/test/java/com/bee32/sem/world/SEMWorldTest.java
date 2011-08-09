0bb7d8cbfb38120479189ee28d488e2cba703686
Lenik <xjl@99jsj.com>
2011-08-09 17:33:36 +0800

    Add SEMWorldTest.

---
+++ sem/resource/sem-world/src/test/java/com/bee32/sem/world/SEMWorldTest.java
@@ -0,0 +1,20 @@
+package com.bee32.sem.world;
+
+import java.io.IOException;
+
+import com.bee32.plover.web.faces.test.FaceletsTestCase;
+
+public class SEMWorldTest
+        extends FaceletsTestCase {
+
+    @Override
+    protected int getRefreshPeriod() {
+        return 10;
+    }
+
+    public static void main(String[] args)
+            throws IOException {
+        new SEMWorldTest().browseAndWait();
+    }
+
+}
