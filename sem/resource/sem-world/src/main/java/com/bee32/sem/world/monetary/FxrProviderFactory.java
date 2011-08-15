ddfd9c4736ac19e6ccd490d6f684da7c7cf0faeb
Lenik <xjl@99jsj.com>
2011-08-15 08:52:17 +0800

    FxrProviderFactory construct the test provider OTF.

--- sem/resource/sem-world/src/main/java/com/bee32/sem/world/monetary/FxrProviderFactory.java
+++ sem/resource/sem-world/src/main/java/com/bee32/sem/world/monetary/FxrProviderFactory.java
@@ -1,22 +1,20 @@
 package com.bee32.sem.world.monetary;
 
-import java.nio.channels.IllegalSelectorException;
+import java.io.IOException;
 import java.util.Map;
+
+import javax.free.UnexpectedException;
 
 import org.springframework.context.ApplicationContext;
 
 import com.bee32.plover.servlet.util.ThreadHttpContext;
+import com.bee32.sem.world.monetary.impl.DiscreteFxrProvider;
+import com.bee32.sem.world.monetary.impl.FxrSamplesSource;
 
 public class FxrProviderFactory {
 
     static ApplicationContext getApplicationContext() {
         ApplicationContext context = ThreadHttpContext.getApplicationContext();
-
-        if (context == null) {
-            // WiredTestCase...?
-            throw new IllegalSelectorException();
-        }
-
         return context;
     }
 
@@ -26,12 +24,37 @@ public class FxrProviderFactory {
     public static IFxrProvider getFxrProvider() {
         ApplicationContext appctx = getApplicationContext();
 
+        if (appctx == null)
+            return getTestFxrProvider();
+
         Map<String, IFxrProvider> beans = appctx.getBeansOfType(IFxrProvider.class);
         if (beans.isEmpty())
             throw new IllegalStateException("No available FXP Provider.");
 
         IFxrProvider first = beans.values().iterator().next();
         return first;
+    }
+
+    static IFxrProvider testFxrProvider;
+
+    static IFxrProvider getTestFxrProvider() {
+        if (testFxrProvider == null) {
+            FxrSamplesSource testSource = new FxrSamplesSource();
+            DiscreteFxrProvider fxp = new DiscreteFxrProvider();
+
+            try {
+                for (int i = 0; i < testSource.getCount(); i++) {
+                    FxrTable table = testSource.download();
+                    assert table != null;
+                    fxp.commit(table);
+                }
+            } catch (IOException e) {
+                throw new UnexpectedException(e);
+            }
+
+            testFxrProvider = fxp;
+        }
+        return testFxrProvider;
     }
 
 }
