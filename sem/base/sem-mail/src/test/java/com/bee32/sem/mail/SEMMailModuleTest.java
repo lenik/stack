203ebe732956231b6925fd034a1bb69358e3de95
ztf <ztf@bee32.com>
2011-08-25 17:18:30 +0800

    set Locale to zh-CN

--- sem/base/sem-mail/src/test/java/com/bee32/sem/mail/SEMMailModuleTest.java
+++ sem/base/sem-mail/src/test/java/com/bee32/sem/mail/SEMMailModuleTest.java
@@ -1,6 +1,7 @@
 package com.bee32.sem.mail;
 
 import java.io.IOException;
+import java.util.Locale;
 
 import com.bee32.plover.orm.unit.Using;
 import com.bee32.sem.mail.web.MailFilterController;
@@ -9,6 +10,21 @@ import com.bee32.sem.test.SEMTestCase;
 @Using(SEMMailUnit.class)
 public class SEMMailModuleTest
         extends SEMTestCase {
+
+    static {
+        Locale zh = Locale.forLanguageTag("zh-CN");
+        Locale.setDefault(zh);
+    }
+
+    @Override
+    protected int getRefreshPeriod() {
+        return 10;
+    }
+
+    @Override
+    protected String getLoggedInUser() {
+        return "admin";
+    }
 
     public static void main(String[] args)
             throws IOException {
