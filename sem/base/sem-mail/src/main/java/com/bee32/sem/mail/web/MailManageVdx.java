f63b839a2d57c83eecdf5e9e2f3eb9670b067322
ztf <ztf@bee32.com>
2011-08-29 17:20:57 +0800

    add method getWritebox,getWriteboxIndex

--- sem/base/sem-mail/src/main/java/com/bee32/sem/mail/web/MailManageVdx.java
+++ sem/base/sem-mail/src/main/java/com/bee32/sem/mail/web/MailManageVdx.java
@@ -1,5 +1,7 @@
 package com.bee32.sem.mail.web;
 
+import java.util.List;
+
 import javax.faces.component.UIComponent;
 
 import org.primefaces.component.tabview.Tab;
@@ -14,7 +16,8 @@ public abstract class MailManageVdx
 
     static final String MAIN_TAB_VIEW = "mainForm:mainTabView";
     static final String BOX_TAB = "mainForm:boxTab";
-    static final String DETAIL_TAB = "mainForm:detailTab";
+    static final String CONTENT_TAB = "mainForm:contentTab";
+    static final String WRITE_TAB = "mainForm:writeTab";
 
     protected TabView getMainTabView() {
         return (TabView) findComponent(MAIN_TAB_VIEW);
@@ -24,8 +27,24 @@ public abstract class MailManageVdx
         return (Tab) findComponent(BOX_TAB);
     }
 
-    protected UIComponent getDetailTab() {
-        return findComponent(DETAIL_TAB);
+    protected UIComponent getContentTab() {
+        return findComponent(CONTENT_TAB);
     }
 
+    protected UIComponent getWriteTab() {
+        return findComponent(WRITE_TAB);
+    }
+
+    protected int getWriteboxIndex(String tabTitle) {
+        int index = 0;
+        TabView mainTabView = (TabView) findComponent(MAIN_TAB_VIEW);
+        List<UIComponent> subTabs = mainTabView.getChildren();
+        for (UIComponent component : subTabs) {
+            Tab tab = (Tab) component;
+            String title = tab.getTitle();
+            if (title.equals(tabTitle))
+                index = subTabs.indexOf(component);
+        }
+        return index;
+    }
 }
