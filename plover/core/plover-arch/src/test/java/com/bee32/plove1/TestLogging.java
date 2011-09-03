c280dc876d214ddad710eb8bce46ee2fbb16e842
Lenik <xjl@99jsj.com>
2011-09-03 08:20:35 +0800

    Logging overrides test. (failed)

---
+++ plover/core/plover-arch/src/test/java/com/bee32/plove1/TestLogging.java
@@ -0,0 +1,21 @@
+package com.bee32.plove1;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestLogging {
+
+    static Logger logger = LoggerFactory.getLogger(TestLogging.class);
+
+    public static void main(String[] args) {
+        System.out.println("BEGIN");
+        logger.info("Info message");
+        logger.warn("Warn message");
+        logger.error("Error message");
+        logger.debug("--- Debug message 1");
+        logger.debug("--- Debug message 2");
+        logger.debug("--- Debug message 3");
+        System.out.println("END");
+    }
+
+}
