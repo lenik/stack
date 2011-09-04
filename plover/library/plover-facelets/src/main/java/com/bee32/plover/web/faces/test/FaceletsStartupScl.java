df2668e86234391ae9a5b9637104e12502c4cf98
Lenik <xjl@99jsj.com>
2011-09-04 23:09:17 +0800

    Myfaces StartupSCL is optional, it could be auto loaded.

--- plover/library/plover-facelets/src/main/java/com/bee32/plover/web/faces/test/FaceletsStartupScl.java
+++ plover/library/plover-facelets/src/main/java/com/bee32/plover/web/faces/test/FaceletsStartupScl.java
@@ -22,7 +22,11 @@ public class FaceletsStartupScl
             throw new UnsupportedOperationException("JSF-RI isn't supported anymore");
 
         case APACHE_MYFACES:
-            myfacesScl.contextInitialized(sce);
+            /**
+             * Normally, servlet containers will automatically load and process .tld files at
+             * startup time, and therefore register and run this class automatically.
+             */
+            // myfacesScl.contextInitialized(sce);
             break;
         }
     }
@@ -31,7 +35,7 @@ public class FaceletsStartupScl
     public void contextDestroyed(ServletContextEvent sce) {
         switch (FaceletsTestCase.faceletsProvider) {
         case APACHE_MYFACES:
-            myfacesScl.contextDestroyed(sce);
+            // myfacesScl.contextDestroyed(sce);
         }
     }
 
