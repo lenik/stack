af9dc06ed76774918879e9b5231ec7f7f7b5e7b5
Lenik <xjl@99jsj.com>
2011-09-06 08:14:53 +0800

    Added PloverP6spySl4fjLogger to replace the ugly original sql format.

---
+++ plover/modeling/plover-orm/src/main/java/com/bee32/plover/thirdparty/p6spy/PloverP6spySlf4jLogger.java
@@ -0,0 +1,75 @@
+package com.bee32.plover.thirdparty.p6spy;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.p6spy.engine.logging.appender.FormattedLogger;
+import com.p6spy.engine.logging.appender.P6Logger;
+
+public class PloverP6spySlf4jLogger
+        extends FormattedLogger
+        implements P6Logger {
+
+    static Logger logger = LoggerFactory.getLogger("p6spy");
+
+    static long bootTime = System.currentTimeMillis();
+
+    static DateFormat timeFormat;
+    static {
+        timeFormat = new SimpleDateFormat("HH:mm:ss.MM");
+    }
+
+    /**
+     * Original Format: 1315265848160|63|1|statement||select relname from pg_class where relkind='S'
+     */
+    @Override
+    public void logSQL(int connectionId, String now, long elapsed, String category, String prepared, String sql) {
+        StringBuilder sb = new StringBuilder(500);
+
+        String time;
+        try {
+            long _now = Long.parseLong(now);
+            time = timeFormat.format(_now - bootTime);
+        } catch (Exception e) {
+            e.printStackTrace();
+            time = "Error:" + e.getMessage();
+        }
+
+        // 00:02:14.431 63ms
+        sb.append(time);
+        sb.append(' ');
+        sb.append(elapsed);
+        sb.append("ms");
+
+        if (connectionId != -1) {
+            sb.append(" Con");
+            sb.append(connectionId);
+        }
+
+        if (!"statement".equals(category)) {
+            sb.append('|');
+            sb.append(category);
+        }
+
+        sb.append(": ");
+        sql = sql.replace('\n', ' ').trim();
+        sb.append(sql);
+
+        String text = sb.toString();
+        logText(text);
+    }
+
+    @Override
+    public void logText(String text) {
+        logger.info(text);
+    }
+
+    @Override
+    public void logException(Exception e) {
+        logger.error(e.getMessage(), e);
+    }
+
+}
