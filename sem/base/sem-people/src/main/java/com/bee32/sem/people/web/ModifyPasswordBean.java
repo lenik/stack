e07d441e282ad61a6f59dbee01c9e884e327efca
Lenik <xjl@99jsj.com>
2011-09-06 20:47:08 +0800

    ModifyPassword: utilized LoginInfo.chain.

--- sem/base/sem-people/src/main/java/com/bee32/sem/people/web/ModifyPasswordBean.java
+++ sem/base/sem-people/src/main/java/com/bee32/sem/people/web/ModifyPasswordBean.java
@@ -3,23 +3,17 @@ package com.bee32.sem.people.web;
 import java.util.List;
 import java.util.Random;
 
-import javax.faces.application.FacesMessage;
-import javax.faces.context.FacesContext;
-
 import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Component;
 
 import com.bee32.icsf.login.LoginCriteria;
 import com.bee32.icsf.login.LoginInfo;
 import com.bee32.icsf.login.UserPassword;
-import com.bee32.plover.orm.util.DTOs;
 import com.bee32.plover.orm.util.EntityViewBean;
 import com.bee32.plover.ox1.principal.User;
-import com.bee32.plover.ox1.principal.UserDto;
 
-@Component
 @Scope("view")
-public class ModifyPasswordBean extends EntityViewBean {
+public class ModifyPasswordBean
+        extends EntityViewBean {
 
     private static final long serialVersionUID = 1L;
 
@@ -28,75 +22,62 @@ public class ModifyPasswordBean extends EntityViewBean {
     private String confirmPass;
     private String challenge = "C" + new Random().nextInt();
 
-	public String getOldPass() {
-		return oldPass;
-	}
-	public void setOldPass(String oldPass) {
-		this.oldPass = oldPass;
-	}
-	public String getNewPass() {
-		return newPass;
-	}
-	public void setNewPass(String newPass) {
-		this.newPass = newPass;
-	}
-	public String getConfirmPass() {
-		return confirmPass;
-	}
-	public void setConfirmPass(String confirmPass) {
-		this.confirmPass = confirmPass;
-	}
+    public String getOldPass() {
+        return oldPass;
+    }
 
-	public String getChallenge() {
-		return challenge;
-	}
+    public void setOldPass(String oldPass) {
+        this.oldPass = oldPass;
+    }
 
-	public void setChallenge(String challenge) {
-		this.challenge = challenge;
-	}
+    public String getNewPass() {
+        return newPass;
+    }
 
+    public void setNewPass(String newPass) {
+        this.newPass = newPass;
+    }
 
+    public String getConfirmPass() {
+        return confirmPass;
+    }
 
-	public UserDto getCurrentUser() {
-		User u = (User) LoginInfo.getInstance().getUser();
-		User user = serviceFor(User.class).getOrFail(u.getId());
-		UserDto cu = DTOs.marshal(UserDto.class, user);
+    public void setConfirmPass(String confirmPass) {
+        this.confirmPass = confirmPass;
+    }
 
-		return cu;
-	}
+    public String getChallenge() {
+        return challenge;
+    }
 
+    public void setChallenge(String challenge) {
+        this.challenge = challenge;
+    }
 
-	public void modify() {
-		FacesMessage msg = null;
+    public void modify() {
+        if (!newPass.equals(confirmPass)) {
+            uiLogger.error("新密码和新密码确认不匹配");
+            return;
+        }
 
-		if(!newPass.equals(confirmPass)) {
-			msg = new FacesMessage(FacesMessage.SEVERITY_WARN, "修改密码错误", "新密码和新密码确认不匹配");
-			FacesContext.getCurrentInstance().addMessage(null, msg);
-			return;
-		}
+        User u = LoginInfo.getInstance().getInternalUser();
+        List<UserPassword> plist = serviceFor(UserPassword.class).list(LoginCriteria.forUser(u));
+        if (plist.isEmpty()) {
+            uiLogger.warn("密码不存在");
+            return;
+        }
+        String p1 = plist.get(0).getPasswd();
 
-		User u = (User) LoginInfo.getInstance().getUser();
-		List<UserPassword> plist = serviceFor(UserPassword.class).list(
-				LoginCriteria.forUser(u));
-		if(plist.isEmpty()) {
-			msg = new FacesMessage(FacesMessage.SEVERITY_WARN, "修改密码错误", "密码不存在");
-			FacesContext.getCurrentInstance().addMessage(null, msg);
-		} else {
-			String p1 = plist.get(0).getPasswd();
+        if (p1.equals(oldPass)) {
+            // 用户输入的旧密码正确
+            plist.get(0).setPasswd(newPass);
+            serviceFor(UserPassword.class).saveOrUpdate(plist.get(0));
 
-			if(p1.equals(oldPass)) {
-				//用户输入的旧密码正确
-				plist.get(0).setPasswd(newPass);
-				serviceFor(UserPassword.class).saveOrUpdate(plist.get(0));
-
-				msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "修改密码成功", "修改密码成功");
-				FacesContext.getCurrentInstance().addMessage(null, msg);
-			} else {
-				//旧密码不正确
-				msg = new FacesMessage(FacesMessage.SEVERITY_WARN, "修改密码错误", "旧密码输入错误");
-				FacesContext.getCurrentInstance().addMessage(null, msg);
-			}
-		}
-	}
+            uiLogger.info("修改密码成功");
+        } else {
+            // 旧密码不正确
+            uiLogger.error("旧密码输入错误");
+        }
+    }
 
 }
