33ee8d957193f9f5a279d9b2cfa654258c256432
ztf <ztf@bee32.com>
2011-09-08 13:56:55 +0800

    add mailService

---
+++ sem/base/sem-mail/src/main/java/com/bee32/sem/mail/web/MailService.java
@@ -0,0 +1,14 @@
+package com.bee32.sem.mail.web;
+
+public class MailService {
+
+    public static boolean doSendMail() {
+        // XXX
+        return false;
+    }
+
+    public static boolean doSaveDraft() {
+        // XXX
+        return false;
+    }
+}
