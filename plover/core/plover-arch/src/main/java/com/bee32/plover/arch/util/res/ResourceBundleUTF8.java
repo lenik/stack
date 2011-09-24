56e7f43d8d884a9975093162e598e9d622d29f63
Lenik <xjl@99jsj.com>
2011-09-24 17:47:20 +0800

    Separate UTF8Control from RBUTF8.

--- plover/core/plover-arch/src/main/java/com/bee32/plover/arch/util/res/ResourceBundleUTF8.java
+++ plover/core/plover-arch/src/main/java/com/bee32/plover/arch/util/res/ResourceBundleUTF8.java
@@ -1,10 +1,5 @@
 package com.bee32.plover.arch.util.res;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
 import java.util.Locale;
 import java.util.MissingResourceException;
 import java.util.PropertyResourceBundle;
@@ -254,42 +249,5 @@ public class ResourceBundleUTF8 {
     public static ResourceBundle getBundle(String baseName, Locale locale, ClassLoader loader) {
         return ResourceBundle.getBundle(baseName, locale, UTF8Control.INSTANCE);
     }
-
-}
-
-class UTF8Control
-        extends Control {
-
-    public ResourceBundle newBundle(String baseName, Locale locale, String format, ClassLoader loader, boolean reload)
-            throws IllegalAccessException, InstantiationException, IOException {
-// The below code is copied from default Control#newBundle() implementation.
-// Only the PropertyResourceBundle line is changed to read the file as UTF-8.
-        String bundleName = toBundleName(baseName, locale);
-        String resourceName = toResourceName(bundleName, ResourceBundleUTF8.BUNDLE_EXTENSION);
-        ResourceBundle bundle = null;
-        InputStream stream = null;
-        if (reload) {
-            URL url = loader.getResource(resourceName);
-            if (url != null) {
-                URLConnection connection = url.openConnection();
-                if (connection != null) {
-                    connection.setUseCaches(false);
-                    stream = connection.getInputStream();
-                }
-            }
-        } else {
-            stream = loader.getResourceAsStream(resourceName);
-        }
-        if (stream != null) {
-            try {
-                bundle = new PropertyResourceBundle(new InputStreamReader(stream, "UTF-8"));
-            } finally {
-                stream.close();
-            }
-        }
-        return bundle;
-    }
-
-    public static final UTF8Control INSTANCE = new UTF8Control();
 
 }
