68285dd4b93c36d3a21bc2518048a5981d80b64f
Lenik <xjl@99jsj.com>
2011-09-27 09:25:21 +0800

    Added abbr dumper util for entity classes in -uber.

---
+++ sem/build/sem-uber/src/test/java/com/bee32/sem/uber/SEMUberUnitDump.java
@@ -0,0 +1,48 @@
+package com.bee32.sem.uber;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import javax.free.AbstractNonNullComparator;
+
+import com.bee32.plover.orm.util.ITypeAbbrAware;
+
+public class SEMUberUnitDump
+        implements ITypeAbbrAware {
+
+    public static void main(String[] args) {
+        SEMUberUnit unit = new SEMUberUnit();
+        List<Class<?>> classes = new ArrayList<Class<?>>(unit.getClasses());
+
+        Collections.sort(classes, SimpleNameComparator.INSTANCE);
+
+        for (Class<?> clazz : classes) {
+            String abbr = ABBR.register(clazz);
+            String simpleName = clazz.getSimpleName();
+            if (!simpleName.equals(abbr))
+                System.out.println("      Entity " + simpleName + " -> " + abbr);
+        }
+    }
+
+}
+
+class SimpleNameComparator
+        extends AbstractNonNullComparator<Class<?>> {
+
+    @Override
+    public int compareNonNull(Class<?> o1, Class<?> o2) {
+        String n1 = o1.getSimpleName();
+        String n2 = o2.getSimpleName();
+        int cmp = n1.compareTo(n2);
+        if (cmp != 0)
+            return cmp;
+
+        n1 = o1.getName();
+        n2 = o2.getName();
+        return n1.compareTo(n2);
+    }
+
+    public static final SimpleNameComparator INSTANCE = new SimpleNameComparator();
+
+}
