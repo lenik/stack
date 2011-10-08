185c01b68deffee3642c7f74465883f518aa60ab
Lenik <xjl@99jsj.com>
2011-10-09 07:53:18 +0800

    Overrided validation are supported.

--- plover/library/plover-facelets/src/test/java/com/bee32/plover/web/faces/test/ValidBean.java
+++ plover/library/plover-facelets/src/test/java/com/bee32/plover/web/faces/test/ValidBean.java
@@ -1,28 +1,25 @@
 package com.bee32.plover.web.faces.test;
 
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 import org.springframework.context.annotation.Scope;
 
-import com.bee32.plover.web.faces.view.ViewBean;
+import com.bee32.plover.util.TextUtil;
 
 @Scope("view")
 public class ValidBean
-        extends ViewBean {
+        extends ValidBase {
 
     private static final long serialVersionUID = 1L;
 
-    @Size(min = 1, message = "Please enter the Email")
-    @Pattern(regexp = "[a-zA-Z0-9]+@[a-zA-Z0-9]+\\.[a-zA-Z0-9]+", message = "Email format is invalid.")
     private String email;
 
-    @Size(min = 5, max = 20, message = "Please enter a valid username (5-20 characters)")
-    private String userName;
-
-    @Size(min = 5, max = 20, message = "Please enter a valid password (5-10 characters)")
     private String password;
 
+    @Size(min = 1, message = "Please enter the Email")
+    @Pattern(regexp = "[a-zA-Z0-9]+@[a-zA-Z0-9]+\\.[a-zA-Z0-9]+", message = "Email format is invalid.")
     public String getEmail() {
         return email;
     }
@@ -31,15 +28,19 @@ public class ValidBean
         this.email = email;
     }
 
+    @NotNull
+    @Size(min = 2, max = 20, message = "Please enter a valid username (2-20 characters)")
     public String getUserName() {
-        return userName;
+        return super.getUserName();
     }
 
-    // @Size(min = 5, max = 20, message = "Please enter a valid username (5-20 characters)")
+    @Override
     public void setUserName(String userName) {
-        this.userName = userName;
+        userName = TextUtil.normalizeSpace(userName, true);
+        super.setUserName(userName);
     }
 
+    @Size(min = 5, max = 20, message = "Please enter a valid password (5-10 characters)")
     public String getPassword() {
         return password;
     }
