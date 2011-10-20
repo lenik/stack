0b8c46b435d8184cf74f8a4ee0863f6609f6cd31
Lenik <xjl@99jsj.com>
2011-10-20 13:42:44 +0800

    Using/OSIV wacs are now enabled on purpose.

--- plover/modeling/plover-orm/src/main/java/com/bee32/plover/orm/context/OSIVWac.java
+++ plover/modeling/plover-orm/src/main/java/com/bee32/plover/orm/context/OSIVWac.java
@@ -1,10 +1,13 @@
 package com.bee32.plover.orm.context;
 
-import com.bee32.plover.servlet.test.AbstractWebAppConfigurer;
+import com.bee32.plover.inject.spring.ImportUtil;
+import com.bee32.plover.orm.util.WiredDaoTestCase;
+import com.bee32.plover.servlet.test.OuterWac;
 import com.bee32.plover.servlet.test.ServletTestLibrary;
+import com.bee32.plover.servlet.test.WiredServletTestCase;
 
 public class OSIVWac
-        extends AbstractWebAppConfigurer {
+        extends OuterWac<WiredServletTestCase> {
 
     @Override
     public int getOrder() {
@@ -13,7 +16,18 @@ public class OSIVWac
     }
 
     @Override
-    public void configureServlets(ServletTestLibrary stl) {
+    public void configureServlets(ServletTestLibrary stl, WiredServletTestCase outer) {
+        Class<?> outerType = outer.getClass();
+        boolean usingRequired = false;
+
+        for (Class<?> imp : ImportUtil.getImportClasses(outerType)) {
+            if (WiredDaoTestCase.class.isAssignableFrom(imp))
+                usingRequired = true;
+        }
+
+        if (!usingRequired)
+            return;
+
         stl.addFilter(OSIVFilter.class, "/*", 0);
     }
 
--- plover/modeling/plover-orm/src/main/java/com/bee32/plover/orm/unit/UsingWac.java
+++ plover/modeling/plover-orm/src/main/java/com/bee32/plover/orm/unit/UsingWac.java
@@ -2,7 +2,9 @@ package com.bee32.plover.orm.unit;
 
 import javax.free.IllegalUsageException;
 
+import com.bee32.plover.inject.spring.ImportUtil;
 import com.bee32.plover.orm.config.CustomizedSessionFactoryBean;
+import com.bee32.plover.orm.util.WiredDaoTestCase;
 import com.bee32.plover.servlet.test.OuterWac;
 import com.bee32.plover.servlet.test.ServletTestLibrary;
 import com.bee32.plover.servlet.test.WiredServletTestCase;
@@ -18,6 +20,16 @@ public class UsingWac
     @Override
     protected void configureContext(ServletTestLibrary stl, WiredServletTestCase outer) {
         Class<?> outerType = outer.getClass();
+        boolean usingRequired = false;
+
+        for (Class<?> imp : ImportUtil.getImportClasses(outerType)) {
+            if (WiredDaoTestCase.class.isAssignableFrom(imp))
+                usingRequired = true;
+        }
+
+        if (!usingRequired)
+            return;
+
         Using useUnit = outerType.getAnnotation(Using.class);
         if (useUnit == null)
             throw new IllegalUsageException("@Using isn't defined on " + outerType);
