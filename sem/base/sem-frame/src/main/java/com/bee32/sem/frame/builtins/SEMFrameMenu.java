b65f11d73a5cbbec91eba60f9f01543bbe164038
Lenik <xjl@99jsj.com>
2011-10-20 13:46:27 +0800

    Added view/theme menus in sem frame.

--- sem/base/sem-frame/src/main/java/com/bee32/sem/frame/builtins/SEMFrameMenu.java
+++ sem/base/sem-frame/src/main/java/com/bee32/sem/frame/builtins/SEMFrameMenu.java
@@ -1,5 +1,16 @@
 package com.bee32.sem.frame.builtins;
 
+import java.io.Serializable;
+import java.util.Map.Entry;
+
+import javax.faces.event.AbortProcessingException;
+import javax.faces.event.ActionEvent;
+import javax.faces.event.ActionListener;
+
+import com.bee32.plover.web.faces.misc.GuestPreferences;
+import com.bee32.plover.web.faces.misc.ThemeData;
+import com.bee32.plover.web.faces.utils.FacesContextSupport;
+import com.bee32.sem.frame.action.Action;
 import com.bee32.sem.frame.menu.MenuContribution;
 import com.bee32.sem.frame.menu.MenuNode;
 
@@ -14,13 +25,49 @@ public class SEMFrameMenu
     public static final MenuNode PROCESS = menu(MAIN, "process");
     public static final MenuNode SUPPORT = menu(MAIN, "support");
     public static final MenuNode BIZ1 = menu(MAIN, "biz1");
+    public static final MenuNode VIEW = menu(MAIN, "view");
     public static final MenuNode HELP = menu(MAIN, "help");
+
+    public static final MenuNode THEME = menu(VIEW, "theme");
 
     static String ABOUT_NAME = "SEM-Frame 0.3.2";
     static MenuNode ABOUT = entry(HELP, "aboutFrame", JAVASCRIPT.join("alert('" + ABOUT_NAME + "')"));
 
     @Override
     protected void preamble() {
+        for (Entry<String, String> ent : ThemeData.getThemes().entrySet()) {
+            String label = ent.getKey();
+            String name = ent.getValue();
+            MenuNode node = entry(THEME, label, null);
+            Action action = new Action();
+            action.setActionListener(new ThemeSwitcherActionListener(name));
+            node.setAction(action);
+        }
+    }
+
+}
+
+class ThemeSwitcherActionListener
+        implements ActionListener, Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    String themeName;
+
+    public ThemeSwitcherActionListener() {
+    }
+
+    public ThemeSwitcherActionListener(String themeName) {
+        if (themeName == null)
+            throw new NullPointerException("themeName");
+        this.themeName = themeName;
+    }
+
+    @Override
+    public void processAction(ActionEvent actionEvent)
+            throws AbortProcessingException {
+        GuestPreferences pref = FacesContextSupport.getBean(GuestPreferences.class);
+        pref.setTheme(themeName);
     }
 
 }
