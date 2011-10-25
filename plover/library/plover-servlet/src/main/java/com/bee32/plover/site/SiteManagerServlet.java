867f4c63c026a3d2b8e8ee40f3ff0678740ee791
Lenik <xjl@99jsj.com>
2011-10-25 17:16:28 +0800

    Refactor SiteManagerServlet in OO style.

--- plover/library/plover-servlet/src/main/java/com/bee32/plover/site/SiteManagerServlet.java
+++ plover/library/plover-servlet/src/main/java/com/bee32/plover/site/SiteManagerServlet.java
@@ -4,13 +4,14 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Map;
 
+import javax.free.Doc;
+import javax.free.Order;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.bee32.plover.arch.util.TextMap;
-import com.bee32.plover.util.PrettyPrintStream;
 
 public class SiteManagerServlet
         extends HttpServlet {
@@ -35,35 +36,44 @@ public class SiteManagerServlet
         out.println();
     }
 
-    static Map<String, Class<? extends SiteTemplate>> tmap;
+    static MultiPage pages;
     static {
-        tmap.put("create", Create.class);
+        pages.add(About.class);
+        pages.add(Create.class);
     }
 
-    class Create
+    @Order(100)
+    @Doc("关于")
+    static class About
+            extends SiteTemplate {
+
+        public About(TextMap args) {
+            super(args);
+        }
+
+    }
+
+    @Doc("新建实例")
+    static class Create
             extends SiteTemplate {
 
         public Create(TextMap args) {
             super(args);
         }
 
-    }
+        @Override
+        protected void _content() {
+            SiteInstance site = getSiteInstance();
 
-    public void about() {
-        //
-    }
+            // if `label` is provided, set it anyway.
+            String label = args.getString("label");
+            if (label != null) {
+                label = label.trim();
+                site.setProperty("label", label);
+            }
 
-    public void about(SiteInstance site, TextMap map, PrettyPrintStream out) {
-        String label = map.getString("label");
-        if (label != null) {
-            label = label.trim();
-            site.setProperty("label", label);
+            simpleForm("#", "label", "Label", label);
         }
-
-        simpleForm(out, "label", "Label", label);
-    }
-
-    protected void simpleForm(PrettyPrintStream out, Object... entries) {
 
     }
 
