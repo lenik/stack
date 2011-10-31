0c45b9a84129c484d4bdc0a1e2dd49c85ffa94ca
Lenik <xjl@99jsj.com>
2011-10-31 17:26:37 +0800

    Renamed AnnotatedFaceletExceptionHandler to AnnotatedFeh.

--- plover/library/plover-facelets/src/main/java/com/bee32/plover/web/faces/AnnotatedFeh.java
+++ plover/library/plover-facelets/src/main/java/com/bee32/plover/web/faces/AnnotatedFeh.java
@@ -1,32 +1,16 @@
 package com.bee32.plover.web.faces;
 
-import javax.free.IllegalUsageException;
-
-import com.bee32.plover.inject.ComponentTemplate;
+import com.bee32.plover.inject.ServiceTemplate;
 import com.bee32.plover.web.faces.utils.FacesUILogger;
 
 /**
  * You must declare {@link ForException} annotation.
  */
-@ComponentTemplate
+@ServiceTemplate
 // Must be eager, so as to inject to PloverExceptionHandler.handlerMap as soon as possible.
 // @Lazy
-public abstract class AnnotatedFaceletExceptionHandler
+public abstract class AnnotatedFeh
         implements IFaceletExceptionHandler {
-
-    /**
-     * Auto discovery and configure here.
-     */
-    public AnnotatedFaceletExceptionHandler() {
-
-        ForException annotation = getClass().getAnnotation(ForException.class);
-        if (annotation == null)
-            throw new IllegalUsageException("No annotation " + ForException.class + " is declared on " + getClass());
-
-        Class<? extends Throwable> exceptionType = annotation.value();
-
-        PloverExceptionHandler.register(exceptionType, this);
-    }
 
     public ExceptionHandleResult skip() {
         return ExceptionHandleResult.SKIP;
