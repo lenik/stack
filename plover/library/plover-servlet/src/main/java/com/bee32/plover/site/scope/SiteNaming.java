3d2027681d36aa56ed6db9009b15234e1b395b24
Lenik <xjl@99jsj.com>
2011-11-01 15:33:51 +0800

    Clean up: request.localName isn't used in SiteNaming.

--- plover/library/plover-servlet/src/main/java/com/bee32/plover/site/scope/SiteNaming.java
+++ plover/library/plover-servlet/src/main/java/com/bee32/plover/site/scope/SiteNaming.java
@@ -4,9 +4,11 @@ import javax.servlet.http.HttpServletRequest;
 
 public class SiteNaming {
 
+    public static final String DEFAULT = "default";
+
     public static String getSiteAlias(HttpServletRequest request) {
-        String host = request.getServerName();
-        String localName = request.getLocalName();
+        String host = request.getServerName(); // "localhost"
+        // String localName = request.getLocalName(); // "127.0.0.1"
         int port = request.getLocalPort();
         return host + ":" + port;
     }
