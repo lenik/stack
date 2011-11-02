231562cb1c3a4c848d6690d32f7ad2e4a46b1d44
Lenik <xjl@99jsj.com>
2011-11-02 16:55:53 +0800

    Config data source per site.

---
+++ plover/modeling/plover-orm/src/main/java/com/bee32/plover/orm/config/SiteDataSource.java
@@ -0,0 +1,91 @@
+package com.bee32.plover.orm.config;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.bee32.plover.servlet.util.ThreadHttpContext;
+import com.bee32.plover.site.DBDialect;
+import com.bee32.plover.site.LoadSiteException;
+import com.bee32.plover.site.OptimizationLevel;
+import com.bee32.plover.site.SiteInstance;
+import com.bee32.plover.site.VerboseLevel;
+import com.bee32.plover.site.scope.PerSite;
+import com.bee32.plover.thirdparty.hibernate.util.HibernateProperties;
+import com.p6spy.engine.common.P6SpyOptions;
+import com.p6spy.engine.common.PropertiesRefreshListener;
+import com.p6spy.engine.common.PropertiesRefresher;
+
+@Component
+@PerSite
+public class SiteDataSource
+        extends BasicDataSource
+        implements HibernateProperties {
+
+    static Logger logger = LoggerFactory.getLogger(SiteDataSource.class);
+
+    SiteInstance site;
+    String realDriver;
+
+    public SiteDataSource()
+            throws LoadSiteException {
+        site = ThreadHttpContext.getSiteInstance();
+
+        DBDialect dialect = site.getDbDialect();
+        realDriver = dialect.getDriverClass();
+
+        if (isP6SpyEnabled()) {
+            logger.info("P6spy is enabled, configure it.");
+
+            setDriverClassName("com.p6spy.engine.spy.P6SpyDriver");
+
+            // realDriver 在 configurer 中转。
+            TestP6SpyConfigurer testConfigurer = new TestP6SpyConfigurer();
+
+            PropertiesRefresher.addPropertiesRefreshListener(testConfigurer);
+
+        } else {
+            logger.info("P6spy is disabled.");
+            setDriverClassName(realDriver);
+        }
+
+        String database = site.getDbName();
+        String user = site.getDbUser();
+        String password = site.getDbPass();
+        String urlFormat = dialect.getUrlFormat();
+        String url = String.format(urlFormat, database);
+
+        setUrl(url);
+        setUsername(user);
+        setPassword(password);
+
+        setDefaultAutoCommit(false);
+
+        OptimizationLevel opt = site.getOptimizationLevel();
+        if (opt.compareTo(OptimizationLevel.LOW) <= 0) {
+            // Set very small pool initial size for "debug" mode.
+            setInitialSize(1);
+        }
+    }
+
+    protected boolean isP6SpyEnabled() {
+        VerboseLevel verbose = site.getVerboseLevel();
+        boolean showSql = verbose.compareTo(VerboseLevel.SQL) >= 0;
+        return showSql;
+    }
+
+    class TestP6SpyConfigurer
+            implements PropertiesRefreshListener {
+
+        @Override
+        public void refresh() {
+            refreshP6SpyProperties();
+        }
+    }
+
+    protected void refreshP6SpyProperties() {
+        P6SpyOptions.addRealDriver(realDriver);
+    }
+
+}
--- sem/base/sem-module/src/main/java/com/bee32/sem/module/SEMModuleServer.java
+++ sem/base/sem-module/src/main/java/com/bee32/sem/module/SEMModuleServer.java
@@ -2,20 +2,13 @@ package com.bee32.sem.module;
 
 import java.io.IOException;
 
-import com.bee32.plover.orm.config.test.TestDataSource;
 import com.bee32.sem.test.SEMTestCase;
 
 public class SEMModuleServer
         extends SEMTestCase {
 
     public SEMModuleServer() {
-        String name = getDataSource();
-        TestDataSource.NAME = name;
         contextPath = "";
-    }
-
-    protected String getDataSource() {
-        return "main";
     }
 
     @Override
