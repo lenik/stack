bd3a7641f437b14c3abf881155d6e32f45b4d223
Lenik <xjl@99jsj.com>
2011-11-03 17:34:45 +0800

    General Dao but not abstract.

--- sem/base/sem-event/src/main/java/com/bee32/sem/event/dao/AbstractEventDao.java
+++ sem/base/sem-event/src/main/java/com/bee32/sem/event/dao/AbstractEventDao.java
@@ -3,7 +3,7 @@ package com.bee32.sem.event.dao;
 import com.bee32.plover.orm.entity.EntityDao;
 import com.bee32.sem.event.entity.Event;
 
-public abstract class AbstractEventDao<E extends Event>
+public class AbstractEventDao<E extends Event>
         extends EntityDao<E, Long> {
 
 }
