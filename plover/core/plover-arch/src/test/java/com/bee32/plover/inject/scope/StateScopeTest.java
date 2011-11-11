c8fab7c371a96f27cd625bad5c868bc5dfacfe36
Lenik <xjl@99jsj.com>
2011-11-11 18:04:24 +0800

    StateScope performance test.

--- plover/core/plover-arch/src/test/java/com/bee32/plover/inject/scope/StateScopeTest.java
+++ plover/core/plover-arch/src/test/java/com/bee32/plover/inject/scope/StateScopeTest.java
@@ -39,6 +39,19 @@ public class StateScopeTest
     @Test
     public void testScopeProxy() {
         manager.setCurrentState("a");
+        StateHint hint = app.getBean(StateHint.class);
+        hint.setHint("a-hint");
+
+        manager.setCurrentState("b");
+        hint.setHint("b-hint");
+
+        manager.setCurrentState("a");
+        assertEquals("a-hint", hint.getHint());
+    }
+
+    @Test
+    public void testScopeRefetch() {
+        manager.setCurrentState("a");
         StateHint a = app.getBean(StateHint.class);
         a.setHint("a-hint");
 
@@ -54,13 +67,15 @@ public class StateScopeTest
     /**
      * Result: Interface proxy is slightly faster.
      * <ul>
+     * <li>NONE/1: 301513, 435516
      * <li>CLASS/1: 43126, 44792, 51576, 52630, 22733
      * <li>IFACE/1: 118927, 284336, 222006
+     * <li>NONE/5: 3249465, 2737048, 2711960
      * <li>CLASS/5: 2268238, 2261569, 2220071
      * <li>IFACE/5: 2316550, 1979359, 2353264, 2346242
      * </ul>
      */
-    @Test
+    // @Test
     public void testProxyPerformance() {
         long start = System.currentTimeMillis();
         long count = 0;
