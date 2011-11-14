1441072be1d93bea97ca97731f635b7d3acd259f
jack <superjwl@gmail.com>
2011-11-14 16:58:31 +0800

    asset会计科目管理功能完成

---
+++ sem/resource/sem-asset/src/main/java/com/bee32/sem/asset/SEMAssetSamples.java
@@ -0,0 +1,21 @@
+package com.bee32.sem.asset;
+
+import com.bee32.plover.orm.util.SampleContribution;
+import com.bee32.sem.asset.entity.AccountSubject;
+
+public class SEMAssetSamples
+        extends SampleContribution {
+
+    public static AccountSubject s1 = new AccountSubject();
+
+    static {
+
+    }
+
+
+
+    @Override
+    protected void preamble() {
+    }
+
+}
