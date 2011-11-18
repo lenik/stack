04503e89ab6a8cb67820104938e8729984009b9e
Lenik <xjl@99jsj.com>
2011-11-18 12:31:33 +0800

    Added xref graph demo case.

---
+++ sem/build/sem-uber/src/test/java/com/bee32/sem/uber/EntityGraphDumpDemo.java
@@ -0,0 +1,64 @@
+package com.bee32.sem.uber;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.springframework.dao.DataAccessException;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.bee32.plover.orm.entity.Entity;
+import com.bee32.plover.orm.unit.EntityGraphTool;
+import com.bee32.plover.orm.unit.EntityPartialRefs;
+import com.bee32.plover.orm.unit.EntityXrefMap;
+import com.bee32.plover.orm.unit.Using;
+import com.bee32.plover.orm.util.WiredDaoFeat;
+import com.bee32.plover.ox1.principal.User;
+import com.bee32.plover.test.ICoordinator;
+import com.bee32.plover.util.FormatStyle;
+
+@Using(SEMUberUnit.class)
+public class EntityGraphDumpDemo
+        extends WiredDaoFeat<EntityGraphDumpDemo> {
+
+    @Inject
+    EntityGraphTool tool;
+
+    public void dumpXrefs(Entity<?> entity)
+            throws DataAccessException {
+        if (entity == null)
+            throw new NullPointerException("entity");
+        EntityXrefMap xrefMap = tool.getReferences(entity);
+
+        System.out.println("References of " + entity.toString(FormatStyle.SIMPLE));
+        for (EntityPartialRefs partialRefs : xrefMap.values()) {
+            String label = partialRefs.getLabel();
+            List<? extends Entity<?>> list = partialRefs.getList();
+
+            System.out.println("Partial references by " + label + ": ");
+            for (Entity<?> ref : list) {
+                String line = ref.toString(FormatStyle.SIMPLE);
+                System.out.println("    " + line);
+            }
+            System.out.println();
+        }
+    }
+
+    @Transactional(readOnly = true)
+    public void listAllForAdmin() {
+        dumpXrefs(User.admin);
+    }
+
+    public static void main(String[] args)
+            throws IOException {
+        new EntityGraphDumpDemo().mainLoop(new ICoordinator<EntityGraphDumpDemo>() {
+            @Override
+            public void main(EntityGraphDumpDemo feat)
+                    throws Exception {
+                feat.listAllForAdmin();
+            }
+        });
+    }
+
+}
