22fa7efe299496eab58f5f4ed0827f997e6f99ec
Lenik <xjl@99jsj.com>
2011-12-01 17:36:04 +0800

    Utilized MatchMode in PeopleCriteria.

--- sem/base/sem-people/src/main/java/com/bee32/sem/people/util/PeopleCriteria.java
+++ sem/base/sem-people/src/main/java/com/bee32/sem/people/util/PeopleCriteria.java
@@ -1,10 +1,11 @@
 package com.bee32.sem.people.util;
 
+import org.hibernate.criterion.MatchMode;
+
 import com.bee32.plover.criteria.hibernate.CriteriaElement;
 import com.bee32.plover.criteria.hibernate.CriteriaSpec;
 import com.bee32.plover.criteria.hibernate.ICriteriaElement;
 import com.bee32.plover.criteria.hibernate.LeftHand;
-import com.bee32.plover.criteria.hibernate.Like;
 import com.bee32.sem.people.entity.OrgUnit;
 import com.bee32.sem.people.entity.Party;
 import com.bee32.sem.people.entity.PartyTagname;
@@ -12,11 +13,14 @@ import com.bee32.sem.people.entity.PartyTagname;
 public class PeopleCriteria
         extends CriteriaSpec {
 
+    @LeftHand(Party.class)
     public static CriteriaElement namedLike(String keyword) {
         if (keyword == null || keyword.isEmpty())
             return null;
         else
-            return or(like("name", "%" + keyword + "%"), like("fullName", "%" + keyword + "%"));
+            return or(//
+                    like("name", keyword, MatchMode.ANYWHERE),//
+                    like("fullName", keyword, MatchMode.ANYWHERE));
     }
 
     @LeftHand(PartyTagname.class)
@@ -32,10 +36,10 @@ public class PeopleCriteria
 
     @LeftHand(OrgUnit.class)
     public static ICriteriaElement internalOrgUnitWithName(String namePattern) {
-        return compose(
-                alias("org", "org1"),
-                alias("org1.tags", "tag"),
-                new Like("name", "%" + namePattern + "%"),
+        return compose(//
+                alias("org", "org1"), //
+                alias("org1.tags", "tag"), //
+                like("name", namePattern, MatchMode.ANYWHERE), //
                 in("tag.id", PartyTagname.INTERNAL.getId()));
     }
 
@@ -45,10 +49,10 @@ public class PeopleCriteria
                 in("tag.id", PartyTagname.CUSTOMER.getId()));
     }
 
+    @LeftHand(Party.class)
     public static ICriteriaElement suppliers() {
         return compose(alias("tags", "tag"), //
                 in("tag.id", PartyTagname.SUPPLIER.getId()));
     }
-
 
 }
