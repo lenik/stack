cd2c2a399e6d3d2847faffe33b7e00eb47606599
Lenik <xjl@99jsj.com>
2011-12-06 17:15:54 +0800

    DataService and CDM should be proxied? (to be checked)

--- plover/modeling/plover-orm/src/main/java/com/bee32/plover/arch/DataService.java
+++ plover/modeling/plover-orm/src/main/java/com/bee32/plover/arch/DataService.java
@@ -4,9 +4,11 @@ import java.io.Serializable;
 
 import javax.inject.Inject;
 
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.bee32.plover.inject.ComponentTemplate;
+import com.bee32.plover.inject.spring.ScopeProxy;
 import com.bee32.plover.orm.dao.CommonDataManager;
 import com.bee32.plover.orm.dao.MemdbDataManager;
 import com.bee32.plover.orm.entity.Entity;
@@ -19,6 +21,7 @@ import com.bee32.plover.site.scope.PerSite;
 @Transactional(readOnly = true)
 @ComponentTemplate
 @PerSite
+@ScopeProxy(ScopedProxyMode.TARGET_CLASS)
 public abstract class DataService
         extends Component
         implements IEntityMarshalContext {
