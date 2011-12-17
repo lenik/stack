685401c9182958aa57b20eb1891b4b427845ff09
Lenik <xjl@99jsj.com>
2011-12-17 15:21:23 +0800

    Single..withNumber: bind the number with specific property.

--- sem/process/sem-process/src/main/java/com/bee32/sem/process/verify/util/SingleVerifierWithNumberSupport.java
+++ sem/process/sem-process/src/main/java/com/bee32/sem/process/verify/util/SingleVerifierWithNumberSupport.java
@@ -3,6 +3,7 @@ package com.bee32.sem.process.verify.util;
 import javax.persistence.Embeddable;
 import javax.persistence.Transient;
 
+import com.bee32.plover.arch.util.dto.IPropertyAccessor;
 import com.bee32.plover.orm.entity.Entity;
 import com.bee32.sem.process.verify.builtin.IJudgeNumber;
 import com.bee32.sem.process.verify.builtin.ISingleVerifierWithNumber;
@@ -21,6 +22,12 @@ public class SingleVerifierWithNumberSupport
         this.judgeNumberImpl = entity;
     }
 
+    public <E extends Entity<?>> SingleVerifierWithNumberSupport(E entity,
+            IPropertyAccessor<? extends Number> property, String description) {
+        super(entity);
+        judgeNumberImpl = new JudgeNumberInProperty(entity, property, description);
+    }
+
     @Transient
     @Override
     public String getNumberDescription() {
@@ -31,6 +38,35 @@ public class SingleVerifierWithNumberSupport
     @Override
     public Number getJudgeNumber() {
         return judgeNumberImpl.getJudgeNumber();
+    }
+
+}
+
+class JudgeNumberInProperty
+        implements IJudgeNumber {
+
+    final Entity<?> entity;
+    final IPropertyAccessor<? extends Number> property;
+    final String description;
+
+    public JudgeNumberInProperty(Entity<?> entity, IPropertyAccessor<? extends Number> property, String description) {
+        if (entity == null)
+            throw new NullPointerException("entity");
+        if (description == null)
+            throw new NullPointerException("description");
+        this.entity = entity;
+        this.property = property;
+        this.description = description;
+    }
+
+    @Override
+    public String getNumberDescription() {
+        return description;
+    }
+
+    @Override
+    public Number getJudgeNumber() {
+        return property.get(entity);
     }
 
 }
