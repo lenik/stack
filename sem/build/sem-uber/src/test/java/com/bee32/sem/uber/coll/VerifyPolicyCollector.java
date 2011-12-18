cb65e787b94fa4caa4b2e036e2f236366d657ffc
Lenik <xjl@99jsj.com>
2011-12-19 00:45:08 +0800

    Verify policy collector: re-target to IVerifyPolicy.

--- sem/build/sem-uber/src/test/java/com/bee32/sem/uber/coll/VerifyPolicyCollector.java
+++ sem/build/sem-uber/src/test/java/com/bee32/sem/uber/coll/VerifyPolicyCollector.java
@@ -3,10 +3,10 @@ package com.bee32.sem.uber.coll;
 import java.io.IOException;
 
 import com.bee32.plover.test.ServiceCollector;
-import com.bee32.sem.process.verify.VerifyPolicy;
+import com.bee32.sem.process.verify.IVerifyPolicy;
 
 public class VerifyPolicyCollector
-        extends ServiceCollector<VerifyPolicy> {
+        extends ServiceCollector<IVerifyPolicy> {
 
     public static void main(String[] args)
             throws IOException {
