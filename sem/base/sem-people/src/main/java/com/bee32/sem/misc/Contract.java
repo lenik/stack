cafd0301f458ba945e97247225411509e6d27c5e
Lenik <xjl@99jsj.com>
2011-12-19 15:20:05 +0800

    Moved Contract to sem.misc temporarily.

---
+++ sem/base/sem-people/src/main/java/com/bee32/sem/misc/Contract.java
@@ -0,0 +1,25 @@
+package com.bee32.sem.misc;
+
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.SequenceGenerator;
+
+import com.bee32.plover.ox1.color.MomentInterval;
+
+/**
+ * 合同
+ */
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "stereo", length = 4)
+@DiscriminatorValue("-")
+@SequenceGenerator(name = "idgen", sequenceName = "contract_seq", allocationSize = 1)
+public abstract class Contract
+        extends MomentInterval {
+
+    private static final long serialVersionUID = 1L;
+
+}
