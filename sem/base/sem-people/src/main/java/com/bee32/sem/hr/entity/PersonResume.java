14a0f31c5d5b4e19cb9c029c933f4af830867934
Lenik <xjl@99jsj.com>
2011-12-19 15:48:37 +0800

    Changed Employee to PersonResume.

---
+++ sem/base/sem-people/src/main/java/com/bee32/sem/hr/entity/PersonResume.java
@@ -0,0 +1,24 @@
+package com.bee32.sem.hr.entity;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Entity;
+
+import com.bee32.plover.ox1.color.UIEntityAuto;
+import com.bee32.sem.people.entity.Person;
+
+@Entity
+public class PersonResume
+        extends UIEntityAuto<Integer> {
+
+    private static final long serialVersionUID = 1L;
+
+    Person person;
+    List<PersonRecord> records;
+    List<PersonSkill> skills;
+
+    Date employedDate;
+    Date resignedDate;
+
+}
