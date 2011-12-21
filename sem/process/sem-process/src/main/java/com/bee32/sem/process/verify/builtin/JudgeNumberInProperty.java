8328bcadd9bb346486412f6aca25197b5b58e69b
Lenik <xjl@99jsj.com>
2011-12-21 16:45:50 +0800

    Separate JudgeNumberInProperty into its own source file.

---
+++ sem/process/sem-process/src/main/java/com/bee32/sem/process/verify/builtin/JudgeNumberInProperty.java
@@ -0,0 +1,33 @@
+package com.bee32.sem.process.verify.builtin;
+
+import com.bee32.plover.arch.util.dto.IPropertyAccessor;
+import com.bee32.plover.orm.entity.Entity;
+
+public class JudgeNumberInProperty
+        implements IJudgeNumber {
+
+    final Entity<?> entity;
+    final IPropertyAccessor<? extends Number> property;
+    final String description;
+
+    public JudgeNumberInProperty(Entity<?> entity, IPropertyAccessor<? extends Number> property, String description) {
+        if (entity == null)
+            throw new NullPointerException("entity");
+        if (description == null)
+            throw new NullPointerException("description");
+        this.entity = entity;
+        this.property = property;
+        this.description = description;
+    }
+
+    @Override
+    public String getNumberDescription() {
+        return description;
+    }
+
+    @Override
+    public Number getJudgeNumber() {
+        return property.get(entity);
+    }
+
+}
--- sem/process/sem-process/src/main/java/com/bee32/sem/process/verify/builtin/SingleVerifierWithNumberSupport.java
+++ sem/process/sem-process/src/main/java/com/bee32/sem/process/verify/builtin/SingleVerifierWithNumberSupport.java
@@ -39,32 +39,3 @@ public class SingleVerifierWithNumberSupport
     }
 
 }
-
-class JudgeNumberInProperty
-        implements IJudgeNumber {
-
-    final Entity<?> entity;
-    final IPropertyAccessor<? extends Number> property;
-    final String description;
-
-    public JudgeNumberInProperty(Entity<?> entity, IPropertyAccessor<? extends Number> property, String description) {
-        if (entity == null)
-            throw new NullPointerException("entity");
-        if (description == null)
-            throw new NullPointerException("description");
-        this.entity = entity;
-        this.property = property;
-        this.description = description;
-    }
-
-    @Override
-    public String getNumberDescription() {
-        return description;
-    }
-
-    @Override
-    public Number getJudgeNumber() {
-        return property.get(entity);
-    }
-
-}
