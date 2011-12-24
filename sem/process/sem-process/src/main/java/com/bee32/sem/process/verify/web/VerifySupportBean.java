2e77c0b401cab2ba581d341b450806c66acc5dbc
Lenik <xjl@99jsj.com>
2011-12-24 17:18:09 +0800

    Added the base VerifySupportBean.

---
+++ sem/process/sem-process/src/main/java/com/bee32/sem/process/verify/web/VerifySupportBean.java
@@ -0,0 +1,49 @@
+package com.bee32.sem.process.verify.web;
+
+import javax.free.IllegalUsageException;
+
+import com.bee32.plover.orm.entity.Entity;
+import com.bee32.plover.orm.util.EntityViewBean;
+import com.bee32.sem.process.verify.AbstractVerifyContext;
+import com.bee32.sem.process.verify.IVerifiable;
+import com.bee32.sem.process.verify.IVerifyContext;
+import com.bee32.sem.process.verify.service.IVerifyService;
+
+/**
+ * @see AbstractVerifyContext
+ */
+public abstract class VerifySupportBean
+        extends EntityViewBean {
+
+    private static final long serialVersionUID = 1L;
+
+    protected IVerifyService getVerifyService() {
+        return getBean(IVerifyService.class);
+    }
+
+    public AbstractVerifyContext getRequestVerifyContext() {
+        Entity<?> entity = getRequestEntity(false);
+        if (entity == null)
+            return null;
+        if (!(entity instanceof IVerifiable))
+            throw new IllegalUsageException(String.format("%s isn't applicable for non-verifiable entities.", //
+                    VerifySupportBean.class.getSimpleName()));
+
+        IVerifiable<?> verifiable = (IVerifiable<?>) entity;
+        IVerifyContext _context = verifiable.getVerifyContext();
+        if (_context instanceof AbstractVerifyContext)
+            throw new UnsupportedOperationException(String.format(//
+                    "Only %s or its descendents are supported by verify-support bean.", //
+                    AbstractVerifyContext.class.getSimpleName()));
+        AbstractVerifyContext context = (AbstractVerifyContext) _context;
+        return context;
+    }
+
+    public boolean isVerified() {
+        IVerifyService verifyService = getVerifyService();
+        AbstractVerifyContext context = getRequestVerifyContext();
+        boolean verified = verifyService.isVerified(context);
+        return verified;
+    }
+
+}
