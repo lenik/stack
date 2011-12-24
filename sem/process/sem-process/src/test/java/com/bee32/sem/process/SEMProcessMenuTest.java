17ffede6f483fb504111f05e34b47087ff9f508f
Lenik <xjl@99jsj.com>
2011-12-24 17:18:35 +0800

    Added reverse-map tests for pagedir.

--- sem/process/sem-process/src/test/java/com/bee32/sem/process/SEMProcessMenuTest.java
+++ sem/process/sem-process/src/test/java/com/bee32/sem/process/SEMProcessMenuTest.java
@@ -12,15 +12,42 @@ import com.bee32.sem.process.verify.preference.VerifyPolicyPref;
 public class SEMProcessMenuTest
         extends Assert {
 
-    @Test
-    public void testGetListPage() {
+    {
         SEMProcessModule module = new SEMProcessModule();
         module.preamble();
+    }
+
+    @Test
+    public void testGetListPage() {
         IObjectPageDirectory pageDir = PageDirectory.getPageDirectory(VerifyPolicyPref.class);
         Location listLoc = pageDir.getPageForView(StandardViews.LIST);
         String listBase = listLoc.getBase();
         // System.out.println(listLoc);
         assertEquals("/3/15/2/1/pref/index-rich.jsf", listBase);
+    }
+
+    @Test
+    public void testWhichClass1() {
+        Class<?> c1 = PageDirectory.whichClass("/3/15/2/1/pref/index-rich.jsf");
+        assertEquals(VerifyPolicyPref.class, c1);
+    }
+
+    @Test
+    public void testWhichClass2() {
+        Class<?> c2 = PageDirectory.whichClass("/3/15/2/1/pref/");
+        assertEquals(VerifyPolicyPref.class, c2);
+    }
+
+    @Test
+    public void testWhichClassNoSep() {
+        Class<?> c3 = PageDirectory.whichClass("/3/15/2/1/pref");
+        assertNull(c3);
+    }
+
+    @Test
+    public void testWhichClassNoEntry() {
+        Class<?> c3 = PageDirectory.whichClass("/3/15/2/1/other");
+        assertNull(c3);
     }
 
 }
