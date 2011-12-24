70afea66bb31d89f9d67bf303c5a8f2bca8efad5
Lenik <xjl@99jsj.com>
2011-12-24 17:13:49 +0800

    Added reverse-search support in PageDirectory.

--- plover/library/plover-servlet/src/main/java/com/bee32/plover/restful/resource/PageDirectory.java
+++ plover/library/plover-servlet/src/main/java/com/bee32/plover/restful/resource/PageDirectory.java
@@ -1,17 +1,30 @@
 package com.bee32.plover.restful.resource;
 
 import javax.free.ClassLocal;
+import javax.free.PrefixMap;
+
+import com.bee32.plover.rtx.location.Location;
 
 public class PageDirectory {
 
     static ClassLocal<IObjectPageDirectory> classMap = new ClassLocal<IObjectPageDirectory>();
+    static PrefixMap<Class<?>> basePathReverseMap = new PrefixMap<Class<?>>();
 
     public static void register(Class<?> clazz, IObjectPageDirectory pageDir) {
         classMap.put(clazz, pageDir);
+        Location baseLocation = pageDir.getBaseLocation();
+        String basePath = baseLocation.getBase() + "/";
+        basePathReverseMap.put(basePath, clazz);
     }
 
-    public static void unregister(Class<?> clazz) {
-        classMap.remove(clazz);
+    public static boolean unregister(Class<?> clazz) {
+        IObjectPageDirectory pageDir = classMap.remove(clazz);
+        if (pageDir == null)
+            return false;
+        Location baseLocation = pageDir.getBaseLocation();
+        String basePath = baseLocation.getBase() + "/";
+        basePathReverseMap.remove(basePath);
+        return true;
     }
 
     /**
@@ -40,6 +53,16 @@ public class PageDirectory {
                 return getPageDirectory(superclass, reusable);
         }
         return null;
+    }
+
+    /**
+     * Find the most exactly matched entry registered with any prefix of the given path.
+     *
+     * @return <code>null</code> if no matching entry registered.
+     */
+    public static Class<?> whichClass(String path) {
+        Class<?> clazz = basePathReverseMap.floor(path);
+        return clazz;
     }
 
 }
