9c8cbeb2cfca277adfa9d27c4d330f03bb95059d
Lenik <xjl@99jsj.com>
2011-12-27 16:46:41 +0800

    Removed read-only @Transactional from WiredDaoFeat by default.

--- plover/modeling/plover-orm/src/main/java/com/bee32/plover/orm/util/WiredDaoFeat.java
+++ plover/modeling/plover-orm/src/main/java/com/bee32/plover/orm/util/WiredDaoFeat.java
@@ -4,21 +4,24 @@ import java.io.Serializable;
 
 import javax.inject.Inject;
 
-import org.springframework.transaction.annotation.Transactional;
-
 import com.bee32.plover.inject.cref.Import;
 import com.bee32.plover.orm.dao.CommonDataManager;
 import com.bee32.plover.orm.entity.Entity;
 import com.bee32.plover.orm.entity.IEntityAccessService;
+import com.bee32.plover.site.scope.SiteNaming;
 import com.bee32.plover.test.FeaturePlayer;
 
 @Import(WiredDaoTestCase.class)
-@Transactional(readOnly = true)
+// @Transactional(readOnly = true)
 public abstract class WiredDaoFeat<T extends WiredDaoFeat<T>>
         extends FeaturePlayer<T> {
 
     @Inject
     protected CommonDataManager dataManager;
+
+    static {
+        SiteNaming.setDefaultSiteName("feat");
+    }
 
     protected <E extends Entity<? extends K>, K extends Serializable> //
     IEntityAccessService<E, K> asFor(Class<E> entityType) {
