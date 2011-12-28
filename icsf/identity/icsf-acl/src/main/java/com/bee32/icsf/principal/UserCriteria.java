bf6276243caf2b41cd43bde23eb1ee56b9e3979c
Lenik <xjl@99jsj.com>
2011-12-28 17:10:15 +0800

    Added EventCriteria.observedBy(...), and UserCriteria.

---
+++ icsf/identity/icsf-acl/src/main/java/com/bee32/icsf/principal/UserCriteria.java
@@ -0,0 +1,21 @@
+package com.bee32.icsf.principal;
+
+import com.bee32.icsf.login.SessionUser;
+import com.bee32.icsf.principal.PrincipalCriteria;
+import com.bee32.icsf.principal.User;
+import com.bee32.plover.criteria.hibernate.CriteriaElement;
+import com.bee32.plover.criteria.hibernate.ICriteriaElement;
+
+public class UserCriteria {
+
+    public static ICriteriaElement impliedByCurrentUser(String propertyName) {
+        User currentUser = SessionUser.getInstance().getInternalUser();
+        return PrincipalCriteria.inImSet(propertyName, currentUser);
+    }
+
+    public static CriteriaElement responsibleByCurrentUser(String propertyName) {
+        User currentUser = SessionUser.getInstance().getInternalUser();
+        return PrincipalCriteria.inInvSet(propertyName, currentUser);
+    }
+
+}
--- sem/base/sem-event/src/main/java/com/bee32/sem/event/util/EventCriteria.java
+++ sem/base/sem-event/src/main/java/com/bee32/sem/event/util/EventCriteria.java
@@ -2,12 +2,32 @@ package com.bee32.sem.event.util;
 
 import java.util.Date;
 
+import com.bee32.icsf.login.SessionUser;
+import com.bee32.icsf.principal.PrincipalCriteria;
+import com.bee32.icsf.principal.User;
 import com.bee32.plover.criteria.hibernate.CriteriaElement;
 import com.bee32.plover.criteria.hibernate.CriteriaSpec;
+import com.bee32.plover.criteria.hibernate.ICriteriaElement;
+import com.bee32.plover.criteria.hibernate.LeftHand;
+import com.bee32.sem.event.entity.Event;
 
 public class EventCriteria
         extends CriteriaSpec {
 
+    @LeftHand(Event.class)
+    public static ICriteriaElement observedBy(User user) {
+        return compose(//
+                alias("observers", "observer"), //
+                PrincipalCriteria.inImSet("observer", user));
+    }
+
+    @LeftHand(Event.class)
+    public static ICriteriaElement observedByCurrentUser() {
+        User currentUser = SessionUser.getInstance().getInternalUser();
+        return observedBy(currentUser);
+    }
+
+    @LeftHand(Event.class)
     public static CriteriaElement typeOf(String type) {
         if (type == null)
             return null;
@@ -17,6 +37,7 @@ public class EventCriteria
         return equals("_type", type);
     }
 
+    @LeftHand(Event.class)
     public static CriteriaElement closed(Boolean closed) {
         return _equals("closed", closed);
     }
