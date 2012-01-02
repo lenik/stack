ec1dfac87fa54cacf415435cc52af54bdc6e8ed0
jack <superjwl@gmail.com>
2012-01-02 18:51:40 +0800

    物料管理继续制作中 +

--- sem/resource/sem-inventory/src/main/java/com/bee32/sem/inventory/web/MaterialExAdminBean.java
+++ sem/resource/sem-inventory/src/main/java/com/bee32/sem/inventory/web/MaterialExAdminBean.java
@@ -12,6 +12,7 @@ import com.bee32.plover.orm.util.EntityViewBean;
 import com.bee32.plover.ox1.tree.TreeCriteria;
 import com.bee32.sem.inventory.dto.MaterialCategoryDto;
 import com.bee32.sem.inventory.dto.MaterialDto;
+import com.bee32.sem.inventory.entity.Material;
 import com.bee32.sem.inventory.entity.MaterialCategory;
 import com.bee32.sem.sandbox.UIHelper;
 import com.bee32.sem.world.thing.Unit;
@@ -20,6 +21,8 @@ import com.bee32.sem.world.thing.UnitDto;
 public class MaterialExAdminBean extends EntityViewBean {
 
     private static final long serialVersionUID = 1L;
+
+    static final String TREETABLE_CATEGORY = "mainForm:categoryTree";
 
     TreeNode root;
 
@@ -64,6 +67,10 @@ public class MaterialExAdminBean extends EntityViewBean {
     }
 
     private void loadMaterialCategoryTree() {
+        loadMaterialCategoryTree(null);
+    }
+
+    private void loadMaterialCategoryTree(Integer categoryId) {
         root = new DefaultTreeNode("categoryRoot", null);
 
         List<MaterialCategory> topCategories = serviceFor(
@@ -74,17 +81,25 @@ public class MaterialExAdminBean extends EntityViewBean {
                 topCategories);
 
         for (MaterialCategoryDto materialCategoryDto : topCategoryDtos) {
-            loadMaterialCategoryRecursive(materialCategoryDto, root);
+            loadMaterialCategoryRecursive(materialCategoryDto, root, categoryId);
         }
     }
 
-    private void loadMaterialCategoryRecursive(MaterialCategoryDto materialCategoryDto, TreeNode parentTreeNode) {
-        TreeNode materialCategoryNode = new DefaultTreeNode(materialCategoryDto, parentTreeNode);
+    private void loadMaterialCategoryRecursive(MaterialCategoryDto materialCategoryDto, TreeNode parentTreeNode, Integer categoryId) {
+        TreeNode materialCategoryNode = null;
+        if (categoryId != null && categoryId.equals(materialCategoryDto.getId())) {
+            //materialCategoryDto = reload(materialCategoryDto, MaterialCategoryDto.MATERIALS);
+            materialCategoryNode = new DefaultTreeNode(materialCategoryDto, parentTreeNode);
+            materialCategoryNode.setSelected(true);
+            materials = materialCategoryDto.getMaterials();
+        } else {
+            materialCategoryNode = new DefaultTreeNode(materialCategoryDto, parentTreeNode);
+        }
 
         List<MaterialCategoryDto> subMaterialCategories = materialCategoryDto.getChildren();
         if (subMaterialCategories != null) {
             for (MaterialCategoryDto subMaterialCategory : subMaterialCategories) {
-                loadMaterialCategoryRecursive(subMaterialCategory, materialCategoryNode);
+                loadMaterialCategoryRecursive(subMaterialCategory, materialCategoryNode, categoryId);
             }
         }
     }
@@ -102,7 +117,32 @@ public class MaterialExAdminBean extends EntityViewBean {
     }
 
     public void saveMaterial() {
+        if (material.getCategory() == null || material.getCategory().getId() == null) {
+            uiLogger.error("物料所属分类不能为空!");
+            return;
+        }
 
+        if (
+                material.getUnit() == null
+                || material.getUnit().getId() == null
+                || material.getUnit().getId().isEmpty()) {
+            uiLogger.error("主单位不能为空!");
+            return;
+        }
+
+        try {
+            Material _m = material.unmarshal();
+
+            serviceFor(Material.class).saveOrUpdate(_m);
+            serviceFor(MaterialCategory.class).evict(material.getCategory().unmarshal());
+
+            MaterialCategoryDto category = (MaterialCategoryDto)selectedMaterialCategoryNode.getData();
+            loadMaterialCategoryTree(category.getId());
+
+            uiLogger.info("保存物料成功.");
+        } catch (Exception e) {
+            uiLogger.error("保存物料出错!", e);
+        }
     }
 
     public List<SelectItem> getUnits() {
@@ -117,4 +157,18 @@ public class MaterialExAdminBean extends EntityViewBean {
         material.setCategory(category);
     }
 
+    public void deleteMaterial() {
+        try {
+            serviceFor(Material.class).deleteById(material.getId());
+            serviceFor(MaterialCategory.class).evict(material.getCategory().unmarshal());
+
+            MaterialCategoryDto category = (MaterialCategoryDto)selectedMaterialCategoryNode.getData();
+            loadMaterialCategoryTree(category.getId());
+
+            uiLogger.info("删除物料成功.");
+
+        } catch (Exception e) {
+            uiLogger.error("删除物料出错!", e);
+        }
+    }
 }
