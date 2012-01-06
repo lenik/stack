1553d4a0ca4d10915853d1aac3d1c37f8f6fb040
Lenik <xjl@99jsj.com>
2012-01-06 15:08:50 +0800

    ClassScanner make the subclass closure OTF. Now iface-query is supported.

--- plover/core/plover-arch/src/main/java/com/bee32/plover/xutil/ClassScanner.java
+++ plover/core/plover-arch/src/main/java/com/bee32/plover/xutil/ClassScanner.java
@@ -9,8 +9,12 @@ import java.util.Set;
 
 import javax.free.Pred1;
 
+import com.bee32.plover.arch.ModuleManager;
+
 public class ClassScanner {
 
+    Set<Class<?>> parsedClasses = new HashSet<Class<?>>();
+    Set<Class<?>> rootClasses = new HashSet<Class<?>>();
     Map<Class<?>, Set<Class<?>>> subclassMap = new HashMap<Class<?>, Set<Class<?>>>();
 
     public synchronized Set<Class<?>> getSubclasses(Class<?> clazz) {
@@ -22,21 +26,46 @@ public class ClassScanner {
         return subclasses;
     }
 
-    int addSubclass(Class<?> clazz, Class<?> subclass) {
-        Set<Class<?>> set = getSubclasses(clazz);
-        int counter = 0;
-        if (set.add(subclass))
-            counter++;
-
-        Class<?> superclass = clazz.getSuperclass();
-        if (superclass != null)
-            counter += addSubclass(superclass, subclass);
-
-        return counter;
+    public Set<Class<?>> getSubclassClosure(Class<?> clazz) {
+        Set<Class<?>> closure = new HashSet<Class<?>>();
+        _getSubclassClosure(clazz, closure);
+        return closure;
     }
 
-    public int contribute(Class<?> clazz) {
-        return addSubclass(clazz, clazz);
+    void _getSubclassClosure(Class<?> clazz, Set<Class<?>> closure) {
+        if (closure.add(clazz)) {
+            for (Class<?> subclass : getSubclasses(clazz))
+                _getSubclassClosure(subclass, closure);
+        }
+    }
+
+    public synchronized int parse(Class<?> clazz) {
+        return _parse(clazz);
+    }
+
+    private int _parse(Class<?> clazz) {
+        if (clazz == ModuleManager.class) {
+            System.out.println(1);
+        }
+        int counter = 0;
+        if (parsedClasses.add(clazz)) {
+            Class<?> superclass = clazz.getSuperclass();
+            if (superclass != null) {
+                Set<Class<?>> set = getSubclasses(superclass);
+                if (set.add(clazz))
+                    counter++;
+                counter += _parse(superclass);
+            } else
+                rootClasses.add(clazz);
+
+            for (Class<?> iface : clazz.getInterfaces()) {
+                Set<Class<?>> set = getSubclasses(iface);
+                if (set.add(clazz))
+                    counter++;
+                counter += _parse(iface);
+            }
+        }
+        return counter;
     }
 
     public void scan(String packageName)
@@ -44,7 +73,7 @@ public class ClassScanner {
         ResourceScanner.scanTypes(packageName, new Pred1<Class<?>>() {
             @Override
             public boolean test(Class<?> clazz) {
-                int adds = contribute(clazz);
+                int adds = _parse(clazz);
                 return adds > 0;
             }
         });
@@ -64,6 +93,18 @@ public class ClassScanner {
     public void dump() {
         for (Entry<Class<?>, Set<Class<?>>> entry : subclassMap.entrySet()) {
             System.out.println(entry.getKey() + ": " + entry.getValue().size() + " entries");
+        }
+    }
+
+    public void dump(Class<?> root) {
+        dump(root, "");
+    }
+
+    void dump(Class<?> clazz, String prefix) {
+        System.out.println(prefix + clazz.getCanonicalName());
+        prefix += "    ";
+        for (Class<?> subclass : getSubclasses(clazz)) {
+            dump(subclass, prefix);
         }
     }
 
