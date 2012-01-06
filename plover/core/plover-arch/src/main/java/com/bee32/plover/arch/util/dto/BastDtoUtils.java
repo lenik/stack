b7be5a363e3adae00fe860af3db76e1f50e99f7d
Lenik <xjl@99jsj.com>
2012-01-06 15:07:55 +0800

    Refactor Dummy/Dto the same way as DummyEntity*.

--- plover/core/plover-arch/src/main/java/com/bee32/plover/arch/util/dto/BastDtoUtils.java
+++ plover/core/plover-arch/src/main/java/com/bee32/plover/arch/util/dto/BastDtoUtils.java
@@ -9,57 +9,7 @@ import com.bee32.plover.arch.util.TextMap;
 
 public class BastDtoUtils {
 
-    static BaseDto<Object, Object> instance;
-    static {
-        instance = new BaseDto<Object, Object>() {
-
-            private static final long serialVersionUID = 1L;
-
-            @Override
-            protected void _marshal(Object source) {
-            }
-
-            @Override
-            protected void _unmarshalTo(Object target) {
-            }
-
-            @Override
-            protected void _parse(TextMap map)
-                    throws ParseException {
-            }
-
-            @Override
-            public boolean isNullRef() {
-                return false;
-            }
-
-            @Override
-            public <D extends BaseDto<? extends Object, Object>> D ref(Object source) {
-                return null;
-            }
-
-            @Override
-            protected Object mergeDeref(Object given) {
-                return null;
-            }
-
-            @Override
-            public Serializable getKey() {
-                return null;
-            }
-
-            @Override
-            protected boolean idEquals(BaseDto<Object, Object> other) {
-                return false;
-            }
-
-            @Override
-            protected int idHashCode() {
-                return 0;
-            }
-
-        };
-    }
+    static DummyDto dummy = new DummyDto();
 
     static <S, D extends BaseDto<S, C>, C> D mar(int selection, S source) {
         Class<S> sourceType = (Class<S>) source.getClass();
@@ -69,12 +19,65 @@ public class BastDtoUtils {
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e.getMessage(), e);
         }
-        return instance.marshal(dtoType, selection, source);
+        return dummy.marshal(dtoType, selection, source);
     }
 
     @SuppressWarnings("unchecked")
     static <S, D extends BaseDto<S, C>, C> D mar(S source) {
         return (D) mar(-1, source);
+    }
+
+}
+
+class Dummy {
+}
+
+class DummyDto
+        extends BaseDto<Dummy, Object> {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void _marshal(Dummy source) {
+    }
+
+    @Override
+    protected void _unmarshalTo(Dummy target) {
+    }
+
+    @Override
+    protected void _parse(TextMap map)
+            throws ParseException {
+    }
+
+    @Override
+    public boolean isNullRef() {
+        return false;
+    }
+
+    @Override
+    public <D extends BaseDto<? extends Dummy, Object>> D ref(Dummy source) {
+        return null;
+    }
+
+    @Override
+    protected Dummy mergeDeref(Dummy given) {
+        return null;
+    }
+
+    @Override
+    public Serializable getKey() {
+        return null;
+    }
+
+    @Override
+    protected boolean idEquals(BaseDto<Dummy, Object> other) {
+        return false;
+    }
+
+    @Override
+    protected int idHashCode() {
+        return 0;
     }
 
 }
--- plover/modeling/plover-orm/src/main/java/com/bee32/plover/orm/util/DTOs.java
+++ plover/modeling/plover-orm/src/main/java/com/bee32/plover/orm/util/DTOs.java
@@ -25,107 +25,107 @@ public abstract class DTOs {
 
     public static <_S, _D extends BaseDto<? super _S, _C>, _C> _D _marshal(Class<_D> dtoClass, int selection,
             _S source, Boolean refButFilled) {
-        return new DummyDto().marshal(dtoClass, selection, source, refButFilled);
+        return new DummyEntityDto().marshal(dtoClass, selection, source, refButFilled);
     }
 
     public static <_S, _D extends BaseDto<? super _S, _C>, _C> _D marshal(Class<_D> dtoClass, int selection, _S source) {
-        return new DummyDto().marshal(dtoClass, selection, source);
+        return new DummyEntityDto().marshal(dtoClass, selection, source);
     }
 
     public static <_S, _D extends BaseDto<? super _S, _C>, _C> _D marshal(Class<_D> dtoClass, _S source) {
-        return new DummyDto().marshal(dtoClass, source);
+        return new DummyEntityDto().marshal(dtoClass, source);
     }
 
     public static <_S, _D extends BaseDto<? super _S, _C>, _C> _D marshal(Class<_D> dtoClass, _S source,
             Boolean refButFilled) {
-        return new DummyDto().marshal(dtoClass, source, refButFilled);
+        return new DummyEntityDto().marshal(dtoClass, source, refButFilled);
     }
 
     // @Deprecated
     public static <_S, _D extends BaseDto<? super _S, _C>, _C> _D mref(Class<_D> dtoClass, int selection, _S source) {
-        return new DummyDto().mref(dtoClass, selection, source);
+        return new DummyEntityDto().mref(dtoClass, selection, source);
     }
 
     public static <_S, _D extends BaseDto<? super _S, _C>, _C> _D mref(Class<_D> dtoClass, _S source) {
-        return new DummyDto().mref(dtoClass, source);
+        return new DummyEntityDto().mref(dtoClass, source);
     }
 
     public static <_S, _D extends BaseDto<_S, _C>, _C> List<_D> _marshalList(Class<_D> dtoClass, int selection,
             Iterable<? extends _S> sources, Boolean refButFilled) {
-        return new DummyDto()._marshalList(dtoClass, selection, sources, refButFilled);
+        return new DummyEntityDto()._marshalList(dtoClass, selection, sources, refButFilled);
     }
 
     public static <_S, _D extends BaseDto<? super _S, _C>, _C> List<_D> marshalList(Class<_D> dtoClass, int selection,
             Iterable<? extends _S> sources) {
-        return new DummyDto().marshalList(dtoClass, selection, sources);
+        return new DummyEntityDto().marshalList(dtoClass, selection, sources);
     }
 
     public static <_S, _D extends BaseDto<? super _S, _C>, _C> List<_D> marshalList(Class<_D> dtoClass,
             Iterable<? extends _S> sources) {
-        return new DummyDto().marshalList(dtoClass, sources);
+        return new DummyEntityDto().marshalList(dtoClass, sources);
     }
 
     public static <_S, _D extends BaseDto<? super _S, _C>, _C> List<_D> mrefList(Class<_D> dtoClass, int selection,
             Iterable<? extends _S> sources) {
-        return new DummyDto().mrefList(dtoClass, selection, sources);
+        return new DummyEntityDto().mrefList(dtoClass, selection, sources);
     }
 
     public static <_S, _D extends BaseDto<? super _S, _C>, _C> List<_D> mrefList(Class<_D> dtoClass,
             Iterable<? extends _S> sources) {
-        return new DummyDto().mrefList(dtoClass, sources);
+        return new DummyEntityDto().mrefList(dtoClass, sources);
     }
 
     public static <_S, _D extends BaseDto<? super _S, _C>, _C> Set<_D> marshalSet(Class<_D> dtoClass, int selection,
             Iterable<? extends _S> sources, Boolean refButFilled) {
-        return new DummyDto().marshalSet(dtoClass, selection, sources, refButFilled);
+        return new DummyEntityDto().marshalSet(dtoClass, selection, sources, refButFilled);
     }
 
     public static <_S, _D extends BaseDto<? super _S, _C>, _C> Set<_D> marshalSet(Class<_D> dtoClass, int selection,
             Iterable<? extends _S> sources) {
-        return new DummyDto().marshalSet(dtoClass, selection, sources);
+        return new DummyEntityDto().marshalSet(dtoClass, selection, sources);
     }
 
     public static <_S, _D extends BaseDto<? super _S, _C>, _C> Set<_D> marshalSet(Class<_D> dtoClass,
             Iterable<? extends _S> sources) {
-        return new DummyDto().marshalSet(dtoClass, sources);
+        return new DummyEntityDto().marshalSet(dtoClass, sources);
     }
 
     public static <_S, _D extends BaseDto<? super _S, _C>, _C> Set<_D> marshalSet(Class<_D> dtoClass, int selection,
             Iterable<? extends _S> sources, boolean refButFilled) {
-        return new DummyDto().marshalSet(dtoClass, selection, sources, refButFilled);
+        return new DummyEntityDto().marshalSet(dtoClass, selection, sources, refButFilled);
     }
 
     public static <_S, _D extends BaseDto<_S, _C>, _C> Set<_D> marshalSet(Class<_D> dtoClass,
             Iterable<? extends _S> sources, boolean refButFilled) {
-        return new DummyDto().marshalSet(dtoClass, sources, refButFilled);
+        return new DummyEntityDto().marshalSet(dtoClass, sources, refButFilled);
     }
 
     public static <_S, __s, _C> void merge(_S target, IPropertyAccessor<__s> property, BaseDto<__s, _C> propertyDto) {
-        new DummyDto().merge(target, property, propertyDto);
+        new DummyEntityDto().merge(target, property, propertyDto);
     }
 
     public static <_S, __s, _C> void merge(_S target, String propertyName, BaseDto<__s, _C> propertyDto) {
-        new DummyDto().merge(target, propertyName, propertyDto);
+        new DummyEntityDto().merge(target, propertyName, propertyDto);
     }
 
     public static <_E extends Entity<?>, _d extends EntityDto<_e, _k>, _e extends Entity<_k>, _k extends Serializable> void mergeList(
             _E target, IPropertyAccessor<List<_e>> property, Iterable<? extends _d> dtoList) {
-        new DummyDto().mergeList(target, property, dtoList);
+        new DummyEntityDto().mergeList(target, property, dtoList);
     }
 
     public static <_E extends Entity<?>, _d extends EntityDto<_e, _k>, _e extends Entity<_k>, _k extends Serializable> void mergeList(
             _E target, String propertyName, Iterable<? extends _d> dtoList) {
-        new DummyDto().mergeList(target, propertyName, dtoList);
+        new DummyEntityDto().mergeList(target, propertyName, dtoList);
     }
 
     public static <_E extends Entity<?>, _d extends EntityDto<_e, _k>, _e extends Entity<_k>, _k extends Serializable> void mergeSet(
             _E target, IPropertyAccessor<Set<_e>> property, Iterable<? extends _d> dtoList) {
-        new DummyDto().mergeSet(target, property, dtoList);
+        new DummyEntityDto().mergeSet(target, property, dtoList);
     }
 
     public static <_E extends Entity<?>, _d extends EntityDto<_e, _k>, _e extends Entity<_k>, _k extends Serializable> void mergeSet(
             _E target, String propertyName, Iterable<? extends _d> dtoList) {
-        new DummyDto().mergeSet(target, propertyName, dtoList);
+        new DummyEntityDto().mergeSet(target, propertyName, dtoList);
     }
 
     public static boolean equals(EntityDto<?, ?> a, EntityDto<?, ?> b) {
@@ -142,7 +142,7 @@ public abstract class DTOs {
 
 }
 
-class Dummy
+class DummyEntity
         extends EntitySpec<Serializable> {
 
     private static final long serialVersionUID = 1L;
@@ -158,22 +158,22 @@ class Dummy
 
 }
 
-class DummyDto
-        extends EntityDto<Dummy, Serializable> {
+class DummyEntityDto
+        extends EntityDto<DummyEntity, Serializable> {
 
     private static final long serialVersionUID = 1L;
 
-    public DummyDto() {
+    public DummyEntityDto() {
         enter(getDefaultSession());
     }
 
     @Override
-    protected void _marshal(Dummy source) {
+    protected void _marshal(DummyEntity source) {
         throw new UnexpectedException();
     }
 
     @Override
-    protected void _unmarshalTo(Dummy target) {
+    protected void _unmarshalTo(DummyEntity target) {
         throw new UnexpectedException();
     }
 
