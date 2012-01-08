b7c8aa872e64106fe5a5500a7deea15dd433be0b
Lenik <xjl@99jsj.com>
2012-01-08 23:27:06 +0800

    Fixed: SingleVerifierRankedPolicyTest don't have to be wired.

--- sem/process/sem-process/src/test/java/com/bee32/sem/process/verify/builtin/SingleVerifierRankedPolicyTest.java
+++ sem/process/sem-process/src/test/java/com/bee32/sem/process/verify/builtin/SingleVerifierRankedPolicyTest.java
@@ -2,28 +2,22 @@ package com.bee32.sem.process.verify.builtin;
 
 import java.util.List;
 
-import org.junit.Ignore;
+import org.junit.Assert;
 import org.junit.Test;
 
-import com.bee32.plover.orm.unit.Using;
-import com.bee32.plover.orm.util.WiredDaoTestCase;
-import com.bee32.sem.process.SEMProcessUnit;
 import com.bee32.sem.process.SEMVerifyPolicySamples;
 
-@Ignore
-@Using(SEMProcessUnit.class)
 public class SingleVerifierRankedPolicyTest
-        extends WiredDaoTestCase {
+        extends Assert {
 
     @Test
     public void testLimitOrder() {
-        Integer id = SEMVerifyPolicySamples.macLevel.getId();
-        SingleVerifierRankedPolicy mlevel = asFor(SingleVerifierRankedPolicy.class).get(id);
-        List<SingleVerifierLevel> ranges = mlevel.getLevels();
-        long limit1 = ranges.get(0).getLimit();
-        SingleVerifierLevel limit2 = ranges.get(1);
+        SingleVerifierRankedPolicy policy = SEMVerifyPolicySamples.macLevel;
+        List<SingleVerifierLevel> levels = policy.getLevels();
+        long limit1 = levels.get(0).getLimit();
+        SingleVerifierLevel level2 = levels.get(1);
         assertEquals(1000, limit1);
-        assertEquals(10000, limit2);
+        assertEquals(10000, level2.getLimit());
     }
 
 }
