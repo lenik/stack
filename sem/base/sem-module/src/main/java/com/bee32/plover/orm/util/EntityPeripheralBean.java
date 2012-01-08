910cb50fd14eec6ebb73bc34801b5e31f815e3c5
Lenik <xjl@99jsj.com>
2012-01-09 00:54:07 +0800

    EntityPeripheralBean: don't infer context beans from page directory.

--- sem/base/sem-module/src/main/java/com/bee32/plover/orm/util/EntityPeripheralBean.java
+++ sem/base/sem-module/src/main/java/com/bee32/plover/orm/util/EntityPeripheralBean.java
@@ -6,9 +6,6 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
-import com.bee32.plover.orm.annotation.EntityTypePattern;
-import com.bee32.plover.orm.annotation.ForEntity;
-import com.bee32.plover.orm.entity.Entity;
 import com.bee32.plover.restful.resource.PageDirectory;
 import com.bee32.plover.servlet.util.ThreadHttpContext;
 import com.bee32.plover.web.faces.view.ViewBean;
@@ -26,7 +23,11 @@ public abstract class EntityPeripheralBean
 
     private static final long serialVersionUID = 1L;
 
-    protected Class<?> getContextType() {
+    protected EntityViewBeanRegistry getViewBeanRegistry() {
+        return getBean(EntityViewBeanRegistry.class);
+    }
+
+    protected Class<?> getContextTypeFromPageDirectory() {
         // FacesContext.getCurrentInstance().getExternalContext().getRequest();
         HttpServletRequest request = ThreadHttpContext.getRequest();
         String path = request.getServletPath();
@@ -34,58 +35,19 @@ public abstract class EntityPeripheralBean
         return whichClass;
     }
 
-    protected EntityViewBeanRegistry getViewBeanRegistry() {
-        return getBean(EntityViewBeanRegistry.class);
+    protected List<?> getSelection() {
+        return getSelection(new Class<?>[0]);
     }
 
-    /**
-     * @return <code>null</code> if there is no view bean which is annotated with the corresponding
-     *         &#64;{@link ForEntity}.
-     */
-    public List<Class<? extends EntityViewBean>> getContextViewBeanTypes() {
-        Class<?> mainType = getContextType();
-        if (mainType == null)
-            return null;
-        if (!Entity.class.isAssignableFrom(mainType))
-            return null;
-
-        @SuppressWarnings("unchecked")
-        Class<? extends Entity<?>> entityType = (Class<? extends Entity<?>>) mainType;
-
-        EntityTypePattern pattern = new EntityTypePattern(entityType);
-
-        return getViewBeanRegistry().getViewBeanClasses(pattern);
-    }
-
-    /**
-     * @return <code>null</code> if there is no view bean which is annotated with the corresponding
-     *         &#64;{@link ForEntity}.
-     */
-    public List<? extends EntityViewBean> getContextViewBeans() {
-        return getContextViewBeans(new Class<?>[0]);
-    }
-
-    public List<? extends EntityViewBean> getContextViewBeans(Class<?>... interfaces) {
-        List<Class<? extends EntityViewBean>> viewBeanTypes = getContextViewBeanTypes();
-        if (viewBeanTypes == null)
-            return null;
-
-        List<EntityViewBean> viewBeans = new ArrayList<EntityViewBean>(viewBeanTypes.size());
-        for (Class<? extends EntityViewBean> viewBeanType : viewBeanTypes) {
-            if (interfaces != null && interfaces.length != 0) {
-                boolean interesting = true;
-                for (Class<?> iface : interfaces)
-                    if (!iface.isAssignableFrom(viewBeanType)) {
-                        interesting = false;
-                        break;
-                    }
-                if (!interesting)
-                    continue;
-            }
-            EntityViewBean viewBean = getBean(viewBeanType);
-            viewBeans.add(viewBean);
+    protected List<?> getSelection(Class<?>... interfaces) {
+        List<Object> all = new ArrayList<Object>();
+        List<?> viewBeans = getMetadata().getViewBeans(EntityViewBean.class);
+        for (Object _viewBean : viewBeans) {
+            EntityViewBean viewBean = (EntityViewBean) _viewBean;
+            List<?> selection = viewBean.getSelection(interfaces);
+            all.addAll(selection);
         }
-        return viewBeans;
+        return all;
     }
 
 }
