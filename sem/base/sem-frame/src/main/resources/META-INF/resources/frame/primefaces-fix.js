f4412dc199fe9424c23d3abf0b1219f1ff2cc360
Lenik <xjl@99jsj.com>
2012-01-17 12:26:23 +0800

    Fixed/workaround: primefaces 3.0.1 dataTable doesn't render before dialog.show()

---
+++ sem/base/sem-frame/src/main/resources/META-INF/resources/frame/primefaces-fix.js
@@ -0,0 +1,30 @@
+// actionListener = dialog.hideValidated(args)
+// instance.__proto__ = object.getClass()
+// type.prototype = Object.class
+
+(function() {
+
+    var _show = PrimeFaces.widget.Dialog.prototype.show;
+
+    PrimeFaces.widget.Dialog.prototype.show = function() {
+        _show.apply(this, arguments);
+
+        var datatable = this.jq.find('  .ui-datatable');
+        if (datatable.length == 0)
+            return;
+        datatable = datatable[0];
+
+        var form = $(datatable).parents('form');
+        if (form.length == 0)
+            return;
+        form = form[0];
+
+        PrimeFaces.ajax.AjaxRequest({
+            formId : form.id,
+            source : this.id,
+            process : '@all',
+            update : datatable.id
+        });
+    };
+
+})(jQuery);
