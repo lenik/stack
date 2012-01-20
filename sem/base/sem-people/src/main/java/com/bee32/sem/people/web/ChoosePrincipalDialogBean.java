75c914ef3c77702a25917813eea5c4756e39a24a
Lenik <xjl@99jsj.com>
2012-01-20 22:26:05 +0800

    Added stereo in choosePrincipalDialog.

--- sem/base/sem-people/src/main/java/com/bee32/sem/people/web/ChoosePrincipalDialogBean.java
+++ sem/base/sem-people/src/main/java/com/bee32/sem/people/web/ChoosePrincipalDialogBean.java
@@ -3,9 +3,15 @@ package com.bee32.sem.people.web;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.bee32.icsf.principal.Group;
+import com.bee32.icsf.principal.GroupDto;
 import com.bee32.icsf.principal.Principal;
 import com.bee32.icsf.principal.PrincipalCriteria;
 import com.bee32.icsf.principal.PrincipalDto;
+import com.bee32.icsf.principal.Role;
+import com.bee32.icsf.principal.RoleDto;
+import com.bee32.icsf.principal.User;
+import com.bee32.icsf.principal.UserDto;
 import com.bee32.sem.misc.SimpleEntityViewBean;
 
 public class ChoosePrincipalDialogBean
@@ -16,6 +22,7 @@ public class ChoosePrincipalDialogBean
     static Logger logger = LoggerFactory.getLogger(ChoosePrincipalDialogBean.class);
 
     String caption = "Please choose a principal..."; // NLS: 选择用户或组
+    String stereo;
 
     public ChoosePrincipalDialogBean() {
         super(Principal.class, PrincipalDto.class, 0);
@@ -30,6 +37,32 @@ public class ChoosePrincipalDialogBean
 
     public void setCaption(String caption) {
         this.caption = caption;
+    }
+
+    public void setStereo(String stereo) {
+        if (stereo == null)
+            stereo = "";
+        switch (stereo) {
+        case "":
+        case "-":
+            entityClass = Principal.class;
+            dtoClass = PrincipalDto.class;
+            break;
+        case "U":
+            entityClass = User.class;
+            dtoClass = UserDto.class;
+            break;
+        case "G":
+            entityClass = Group.class;
+            dtoClass = GroupDto.class;
+            break;
+        case "R":
+            entityClass = Role.class;
+            dtoClass = RoleDto.class;
+            break;
+        default:
+            throw new IllegalArgumentException("Bad stereo: " + stereo);
+        }
     }
 
     @Override
