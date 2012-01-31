8c086bffde03daf8e840d73824317a449463d595
Lenik <xjl@99jsj.com>
2012-02-01 02:39:31 +0800

    Initial of listView. (Pending cuz cc:renderFacet problem)

---
+++ sem/base/sem-frame/src/main/java/com/bee32/sem/frame/ui/ListModel.java
@@ -0,0 +1,75 @@
+package com.bee32.sem.frame.ui;
+
+import java.util.List;
+
+import com.bee32.plover.web.faces.utils.FacesContextSupport;
+import com.bee32.plover.web.faces.utils.FacesUILogger;
+import com.bee32.plover.web.faces.utils.SelectableList;
+
+public class ListModel<T>
+        extends FacesContextSupport {
+
+    final List<T> list;
+    final Class<T> elementType;
+    T selection;
+    T activeObject;
+
+    public ListModel(List<T> list, Class<T> elementType) {
+        if (list == null)
+            throw new NullPointerException("list");
+        if (elementType == null)
+            throw new NullPointerException("elementType");
+        this.list = list;
+        this.elementType = elementType;
+    }
+
+    public List<T> getList() {
+        return list;
+    }
+
+    public SelectableList<T> getSelectableList() {
+        return SelectableList.decorate(getList());
+    }
+
+    public void addElement(T element) {
+        list.add(element);
+    }
+
+    public void removeElement(T element) {
+        list.remove(element);
+    }
+
+    public T getSelection() {
+        return selection;
+    }
+
+    public void setSelection(T selection) {
+        this.selection = selection;
+    }
+
+    public void removeSelection() {
+        removeElement(selection);
+    }
+
+    public T getActiveObject() {
+        return activeObject;
+    }
+
+    public void showIndexForm() {
+        activeObject = null;
+    }
+
+    public void showCreateForm() {
+        try {
+            activeObject = elementType.newInstance();
+            addElement(activeObject);
+        } catch (ReflectiveOperationException e) {
+            new FacesUILogger(false).error("创建失败", activeObject);
+        }
+    }
+
+    public void showEditForm() {
+        activeObject = selection;
+    }
+
+}
---
+++ sem/base/sem-frame/src/test/java/com/bee32/sem/frame/ui/ListViewTestBean.java
@@ -0,0 +1,34 @@
+package com.bee32.sem.frame.ui;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.free.Person;
+
+import org.apache.myfaces.view.facelets.tag.composite.InsertFacetHandler;
+import org.apache.myfaces.view.facelets.tag.jsf.core.FacetHandler;
+
+import com.bee32.plover.web.faces.view.ViewBean;
+
+public class ListViewTestBean
+        extends ViewBean {
+
+    private static final long serialVersionUID = 1L;
+
+    List<Person> persons;
+
+    public ListViewTestBean() {
+        persons = new ArrayList<>();
+        persons.add(new Person("lenik", 31, false));
+        persons.add(new Person("lily", 18, true));
+    }
+
+    public ListModel<Person> getPersonModel() {
+        return new ListModel<Person>(persons, Person.class);
+    }
+
+    {
+        InsertFacetHandler ifh;
+        FacetHandler ffh;
+    }
+}
