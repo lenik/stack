b6b02e55aa5b86506c686dc0383041bae403b1ba
Lenik <xjl@99jsj.com>
2012-02-05 19:37:47 +0800

    ajax-validation should warn developer for missing args.

--- sem/base/sem-frame/src/main/resources/META-INF/resources/frame/ajax-validation.js
+++ sem/base/sem-frame/src/main/resources/META-INF/resources/frame/ajax-validation.js
@@ -3,6 +3,10 @@
 // type.prototype = Object.class
 
 PrimeFaces.widget.Dialog.prototype.hideValidated = function(args) {
+    if (args == null) {
+        alert("hideValidated 需要参数 args");
+        return;
+    }
     if (args.validationFailed) {
         this.jq.effect("shake", {
             times : 2
