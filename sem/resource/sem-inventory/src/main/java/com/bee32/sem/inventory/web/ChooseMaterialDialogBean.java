bc99a89f66a633a6b23b857a2aac5a8b44d65021
Lenik <xjl@99jsj.com>
2012-02-07 00:07:38 +0800

    Added ChooseMaterialDialogBean.productLike.

--- sem/resource/sem-inventory/src/main/java/com/bee32/sem/inventory/web/ChooseMaterialDialogBean.java
+++ sem/resource/sem-inventory/src/main/java/com/bee32/sem/inventory/web/ChooseMaterialDialogBean.java
@@ -1,8 +1,16 @@
 package com.bee32.sem.inventory.web;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.bee32.plover.criteria.hibernate.ICriteriaElement;
+import com.bee32.plover.criteria.hibernate.InCollection;
+import com.bee32.plover.criteria.hibernate.NotEquals;
+import com.bee32.sem.inventory.Classification;
 import com.bee32.sem.inventory.dto.MaterialDto;
 import com.bee32.sem.inventory.entity.Material;
 import com.bee32.sem.misc.SimpleEntityViewBean;
@@ -16,8 +24,28 @@ public class ChooseMaterialDialogBean
 
     String header = "Please choose a material..."; // NLS: 选择用户或组
 
+    Boolean productLike;
+
     public ChooseMaterialDialogBean() {
         super(Material.class, MaterialDto.class, 0);
+    }
+
+    static Set<Character> productLikeClasses;
+    static {
+        productLikeClasses = new HashSet<Character>();
+        productLikeClasses.add(Classification.PRODUCT.getValue());
+        productLikeClasses.add(Classification.SEMI.getValue());
+    }
+
+    @Override
+    protected void composeBaseCriteriaElements(List<ICriteriaElement> elements) {
+        super.composeBaseCriteriaElements(elements);
+        if (productLike != null) {
+            if (productLike)
+                elements.add(new InCollection("_classification", productLikeClasses));
+            else
+                elements.add(new NotEquals("_classification", Classification.PRODUCT.getValue()));
+        }
     }
 
     // Properties
@@ -28,6 +56,22 @@ public class ChooseMaterialDialogBean
 
     public void setHeader(String header) {
         this.header = header;
+    }
+
+    /**
+     * Values:
+     * <ul>
+     * <li>true = 成品/半成品
+     * <li>false = not 成品
+     * <li>null = 不限
+     * </ul>
+     */
+    public Boolean getProductLike() {
+        return productLike;
+    }
+
+    public void setProductLike(Boolean productLike) {
+        this.productLike = productLike;
     }
 
 }
