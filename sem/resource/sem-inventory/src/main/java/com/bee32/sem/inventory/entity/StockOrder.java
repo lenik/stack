66bca2416be0046dea1eb4c9cfe8a2c396af9c75
Lenik <xjl@99jsj.com>
2012-02-08 14:40:16 +0800

    Renamed the entity name of StockOrder to PlainStockOrder.

--- sem/resource/sem-inventory/src/main/java/com/bee32/sem/inventory/entity/StockOrder.java
+++ sem/resource/sem-inventory/src/main/java/com/bee32/sem/inventory/entity/StockOrder.java
@@ -3,7 +3,7 @@ package com.bee32.sem.inventory.entity;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
-@Entity
+@Entity(name = "PlainStockOrder")
 @DiscriminatorValue("-")
 public class StockOrder
         extends AbstractStockOrder<StockOrderItem> {
