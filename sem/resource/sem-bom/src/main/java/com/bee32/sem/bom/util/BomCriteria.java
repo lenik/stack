34047c0af9b66272410995deb99c4690c388dbe6
Lenik <xjl@99jsj.com>
2012-02-10 16:06:54 +0800

    BomCriteria  various name changes.

--- sem/resource/sem-bom/src/main/java/com/bee32/sem/bom/util/BomCriteria.java
+++ sem/resource/sem-bom/src/main/java/com/bee32/sem/bom/util/BomCriteria.java
@@ -1,24 +1,27 @@
 package com.bee32.sem.bom.util;
 
+import org.hibernate.criterion.MatchMode;
+
 import com.bee32.plover.criteria.hibernate.CriteriaElement;
 import com.bee32.plover.criteria.hibernate.CriteriaSpec;
 import com.bee32.plover.criteria.hibernate.Equals;
 import com.bee32.plover.criteria.hibernate.ICriteriaElement;
 import com.bee32.plover.criteria.hibernate.LeftHand;
-import com.bee32.plover.criteria.hibernate.Like;
 import com.bee32.sem.bom.entity.Part;
 
 public class BomCriteria
         extends CriteriaSpec {
 
     @LeftHand(Part.class)
-    public static ICriteriaElement findPartUseMaterialName(String pattern) {
-        return compose(alias("target", "material"), //
-                new Like("material.label", "%" + pattern + "%"));
+    public static ICriteriaElement partsMaterialLike(String pattern, boolean ignoreCase) {
+        return compose(
+                alias("target", "material"), //
+                ignoreCase ? likeIgnoreCase("material.label", pattern, MatchMode.ANYWHERE) : like("material.label",
+                        pattern, MatchMode.ANYWHERE));
     }
 
     @LeftHand(Part.class)
-    public static ICriteriaElement listPartByCategory(int materialCategoryId) {
+    public static ICriteriaElement partsCategoryOf(int materialCategoryId) {
         return compose(alias("target", "material"), //
                 new Equals("material.category.id", materialCategoryId));
     }
