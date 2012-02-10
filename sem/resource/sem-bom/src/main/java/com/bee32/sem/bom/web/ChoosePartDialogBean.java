599a281c37170dad46c66614cbb4870cd25a4b2a
Lenik <xjl@99jsj.com>
2012-02-10 16:05:55 +0800

    Choose part dialog: name/label match the target material.

--- sem/resource/sem-bom/src/main/java/com/bee32/sem/bom/web/ChoosePartDialogBean.java
+++ sem/resource/sem-bom/src/main/java/com/bee32/sem/bom/web/ChoosePartDialogBean.java
@@ -3,8 +3,11 @@ package com.bee32.sem.bom.web;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.bee32.plover.criteria.hibernate.Or;
+import com.bee32.plover.ox1.util.CommonCriteria;
 import com.bee32.sem.bom.dto.PartDto;
 import com.bee32.sem.bom.entity.Part;
+import com.bee32.sem.bom.util.BomCriteria;
 import com.bee32.sem.misc.SimpleEntityViewBean;
 
 public class ChoosePartDialogBean
@@ -15,6 +18,7 @@ public class ChoosePartDialogBean
     static Logger logger = LoggerFactory.getLogger(ChoosePartDialogBean.class);
 
     String header = "Please choose a component part..."; // NLS: 选择用户或组
+    String mode;
 
     public ChoosePartDialogBean() {
         super(Part.class, PartDto.class, 0);
@@ -28,6 +32,22 @@ public class ChoosePartDialogBean
 
     public void setHeader(String header) {
         this.header = header;
+    }
+
+    public String getMode() {
+        return mode;
+    }
+
+    public void setMode(String mode) {
+        this.mode = mode;
+    }
+
+    @Override
+    public void addNameOrLabelRestriction() {
+        addSearchFragment("名称含有 " + searchPattern, Or.of(//
+                CommonCriteria.labelledWith(searchPattern, true), //
+                BomCriteria.partsMaterialLike(searchPattern, true)));
+        searchPattern = null;
     }
 
 }
