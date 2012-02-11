8f2500972769ed05ac652f1c67da2d0de1ba9ad2
Lenik <xjl@99jsj.com>
2012-02-11 18:47:50 +0800

    PrincipalCriteria: Added im/inv set fallback.

--- plover/modeling/plover-ox1/src/main/java/com/bee32/icsf/principal/PrincipalCriteria.java
+++ plover/modeling/plover-ox1/src/main/java/com/bee32/icsf/principal/PrincipalCriteria.java
@@ -1,11 +1,14 @@
 package com.bee32.icsf.principal;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
 import org.apache.commons.lang.StringUtils;
 import org.hibernate.criterion.MatchMode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.bee32.plover.criteria.hibernate.CriteriaElement;
 import com.bee32.plover.criteria.hibernate.CriteriaSpec;
@@ -15,6 +18,8 @@ import com.bee32.plover.ox1.util.CommonCriteria;
 
 public class PrincipalCriteria
         extends CriteriaSpec {
+
+    static Logger logger = LoggerFactory.getLogger(PrincipalCriteria.class);
 
     @LeftHand(Principal.class)
     public static ICriteriaElement namedLike(String pattern) {
@@ -39,7 +44,13 @@ public class PrincipalCriteria
             throw new NullPointerException("propertyName");
         if (principal == null)
             return null;
-        Collection<Principal> imSet = principal.getImSet();
+        Collection<Principal> imSet;
+        try {
+            imSet = principal.getImSet();
+        } catch (Exception e) {
+            logger.error("Failed to get im-set, fall back to self.", e);
+            imSet = Arrays.asList(principal); // self only.
+        }
         List<Integer> idSet = new ArrayList<Integer>();
         for (Principal im : imSet)
             idSet.add(im.getId());
@@ -53,7 +64,13 @@ public class PrincipalCriteria
             throw new NullPointerException("propertyName");
         if (principal == null)
             return null;
-        Collection<Principal> invSet = principal.getInvSet();
+        Collection<Principal> invSet;
+        try {
+            invSet = principal.getInvSet();
+        } catch (Exception e) {
+            logger.error("Failed to get inv-set, fall back to self.", e);
+            invSet = Arrays.asList(principal); // self only.
+        }
         List<Integer> idSet = new ArrayList<Integer>();
         for (Principal inv : invSet)
             idSet.add(inv.getId());
