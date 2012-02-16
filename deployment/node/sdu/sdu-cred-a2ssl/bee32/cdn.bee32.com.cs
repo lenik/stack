80c21984cb6027474d64ffa50dfb56eb68a58e80
Lenik <xjl@99jsj.com>
2012-02-16 17:29:22 +0800

    Reduced cdn.bee32.com.crt payloads.

--- deployment/node/sdu/sdu-cred-a2ssl/bee32/cdn.bee32.com.cs
+++ deployment/node/sdu/sdu-cred-a2ssl/bee32/cdn.bee32.com.cs
@@ -4,15 +4,13 @@
     DN_C=CN
     DN_ST=浙江
     DN_O=海宁市智恒软件有限公司
-    DN_OU=智恒云计算-公共数据分发网
-    DN_CN=智恒云计算-公共数据分发网
+    DN_OU=公共数据
+    DN_CN=智恒云计算-公共数据分发
     DN_emailAddress=ca@bee32.com
 
     domains=(
         cdn.bee32.com
         st1.cdn.bee32.com
-        st2.cdn.bee32.com
-        st3.cdn.bee32.com
     )
 
     main "$@"
