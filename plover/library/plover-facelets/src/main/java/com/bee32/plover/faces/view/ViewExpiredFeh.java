32eb30d63dcc81fa9fa0ee51d20795e1927f79a7
Lenik <xjl@99jsj.com>
2012-02-20 13:26:48 +0800

    Prepared ViewExpired FEH.

---
+++ plover/library/plover-facelets/src/main/java/com/bee32/plover/faces/view/ViewExpiredFeh.java
@@ -0,0 +1,22 @@
+package com.bee32.plover.faces.view;
+
+import javax.faces.application.ViewExpiredException;
+
+import com.bee32.plover.arch.exception.ExceptionHandleResult;
+import com.bee32.plover.arch.exception.ForException;
+import com.bee32.plover.faces.FaceletExceptionContext;
+import com.bee32.plover.faces.FaceletsExceptionHandler;
+import com.bee32.plover.faces.utils.FacesUILogger;
+
+@ForException(ViewExpiredException.class)
+public class ViewExpiredFeh
+        extends FaceletsExceptionHandler {
+
+    @Override
+    public ExceptionHandleResult handle(Throwable exception, FaceletExceptionContext context) {
+        FacesUILogger uiLogger = getUILogger();
+        uiLogger.error("页面已过期，请重新载入页面后重试。");
+        return ExceptionHandleResult.HANDLED;
+    }
+
+}
