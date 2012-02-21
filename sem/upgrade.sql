4b01a2d619db3b22a4de2acd01cd36f004fc583a
Lenik <xjl@99jsj.com>
2012-02-22 02:01:18 +0800

    Updated upgrade.sql

--- sem/upgrade.sql
+++ sem/upgrade.sql
@@ -1,21 +1,9 @@
+alter table purchase_request_item drop column parent;
+alter table purchase_request_item rename column purchase_request to parent;
 
-alter table unit add column hint varchar(20) not null default '';
-update unit set hint='长度' where id='m' or std_unit='m';
-update unit set hint='面积' where id='m2' or std_unit='m2';
-update unit set hint='体积' where id='m3' or std_unit='m3';
-update unit set hint='质量' where id='g' or std_unit='g';
-update unit set hint='力' where id='N' or std_unit='N';
-update unit set hint='容量' where id='L' or std_unit='mL';
-update unit set hint='数量' where id='PCS' or std_unit='PCS';
-update unit set hint='数量' where hint='';
+alter table purchase_request_item rename column quantity to required_quantity;
+alter table purchase_request_item rename column plan_quantity to quantity;
 
-alter table material alter column unit_hint drop not null;
-update material set unit_hint=null;
-
-alter table stock_warehouse alter column name drop not null;
-update stock_warehouse set label=name;
-update stock_warehouse set name=null;
-
-update stock_location set label=address;
-alter table stock_location drop column address;
+alter table purchase_inquiry drop column parent;
+alter table purchase_inquiry rename column purchase_request_item to parent;
 
