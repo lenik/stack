6c6e412d3270d1c8b58f27571fa8bebe62e45c86
Lenik <xjl@99jsj.com>
2012-02-22 09:04:49 +0800

    upgrade.sql: Renamed batch to batch0.

--- sem/upgrade.sql
+++ sem/upgrade.sql
@@ -1,3 +1,6 @@
+alter table stock_order_item rename column batch to batch0;
+alter table stock_order_item drop column cbatch;
+
 alter table purchase_request_item drop column parent;
 alter table purchase_request_item rename column purchase_request to parent;
 
@@ -6,4 +9,3 @@ alter table purchase_request_item rename column plan_quantity to quantity;
 
 alter table purchase_inquiry drop column parent;
 alter table purchase_inquiry rename column purchase_request_item to parent;
-
