6f8dcceeb75e56ff4d6381a77c04a7c04cf72862
Lenik <xjl@99jsj.com>
2012-02-24 11:17:14 +0800

    New feature: ox1-sql select centity-set by unit.

--- plover/modeling/plover-ox1/src/main/java/com/bee32/plover/ox1/util/Ox1SQLServlet.java
+++ plover/modeling/plover-ox1/src/main/java/com/bee32/plover/ox1/util/Ox1SQLServlet.java
@@ -1,10 +1,16 @@
 package com.bee32.plover.ox1.util;
 
-import javax.free.Doc;
+import java.util.Collections;
+import java.util.Set;
 
+import javax.free.Doc;
+import javax.persistence.Entity;
+
+import com.bee32.plover.arch.util.ClassCatalog;
 import com.bee32.plover.html.HtmlTemplate;
 import com.bee32.plover.orm.PloverNamingStrategy;
 import com.bee32.plover.orm.config.CustomizedSessionFactoryBean;
+import com.bee32.plover.orm.unit.PersistenceUnit;
 import com.bee32.plover.ox1.c.CEntity;
 import com.bee32.plover.site.SimpleServlet;
 
@@ -29,21 +35,49 @@ public class Ox1SQLServlet
             PloverNamingStrategy naming = PloverNamingStrategy.getDefaultInstance();
 
             String template = getRequest().getParameter("template");
+            String unitName = getRequest().getParameter("unit");
+
             if (template != null) {
                 pre();
-                for (Class<?> entityType : CustomizedSessionFactoryBean.getForceUnit().getClasses()) {
-                    if (CEntity.class.isAssignableFrom(entityType)) {
-                        String entityName = naming.classToTableName(entityType.getCanonicalName());
-                        entityName = entityName.replace("`", "\""); // PL/SQL.
-                        String sql = String.format(template, entityName);
-                        text(sql);
-                        text(";\n");
+
+                PersistenceUnit root = CustomizedSessionFactoryBean.getForceUnit();
+                Set<Class<?>> selection = Collections.emptySet();
+                if (unitName == null) {
+                    selection = root.getClasses();
+                } else {
+                    for (ClassCatalog catalog : root.getAllDependencies()) {
+                        if (unitName.equals(catalog.getName())) {
+                            selection = catalog.getLocalClasses();
+                            break;
+                        }
                     }
+                }
+
+                for (Class<?> entityType : selection) {
+                    if (!CEntity.class.isAssignableFrom(entityType))
+                        continue;
+
+                    Class<?> anySuper = entityType.getSuperclass();
+                    boolean inherited = false;
+                    while (anySuper != null) {
+                        if (anySuper.getAnnotation(Entity.class) != null) {
+                            inherited = true;
+                            break;
+                        }
+                        anySuper = anySuper.getSuperclass();
+                    }
+                    if (inherited)
+                        continue;
+
+                    String entityName = naming.classToTableName(entityType.getCanonicalName());
+                    entityName = entityName.replace("`", "\""); // PL/SQL.
+                    String sql = String.format(template, entityName);
+                    text(sql);
+                    text(";\n");
                 }
                 end();
             }
         }
-
     }
 
 }
