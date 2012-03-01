c7821d1e6b3a155bbf4aff5fe05469ea8ece6033
Lenik <xjl@99jsj.com>
2012-03-01 17:14:24 +0800

    Updated upgrade sql.

--- sem/upgrade.sql
+++ sem/upgrade.sql
@@ -13,3 +13,11 @@ alter table purchase_inquiry rename column purchase_request_item to parent;
 
 -- 2
 alter table make_order_item rename column external_specification to external_model_spec;
+
+-- 3
+drop table plover_conf;
+
+update material_category set label=name;
+alter table material_category drop column name;
+alter table palette_entry drop column name;
+alter table acl drop column name;
