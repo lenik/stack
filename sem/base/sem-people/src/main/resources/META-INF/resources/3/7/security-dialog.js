cdf88e87f1c5bc2949e9dfd3e469074bfde70d82
Lenik <xjl@99jsj.com>
2012-03-03 00:49:34 +0800

    Auto load security-dialog.js by securityOptionDialogs.

--- sem/base/sem-people/src/main/resources/META-INF/resources/3/7/security-dialog.js
+++ sem/base/sem-people/src/main/resources/META-INF/resources/3/7/security-dialog.js
@@ -1,11 +1,11 @@
 function loadAclAndShow(dialog) {
 
     var form = dialog.jq.find('form')[0];
-    var loadAclButton = dialog.jq.find('#securityDialog\\:form\\:loadAclButton')[0];
+    var actionSource = dialog.jq.find('.x-load-acl-button')[0];
 
     PrimeFaces.ajax.AjaxRequest({
         formId : form.id,
-        source : loadAclButton.id,
+        source : actionSource.id,
         process : '@all',
         update : form.id
     });
