337610e92155ee11d09cb44e07fff34323892c98
Lenik <xjl@99jsj.com>
2012-03-16 15:44:36 +0800

    Renamed MakeInstance(-Step).def -> .model.

--- sem/biz1/sem-makebiz/src/main/java/com/bee32/sem/makebiz/entity/MakeInstanceStep.java
+++ sem/biz1/sem-makebiz/src/main/java/com/bee32/sem/makebiz/entity/MakeInstanceStep.java
@@ -1,14 +1,17 @@
 package com.bee32.sem.makebiz.entity;
 
+import javax.persistence.Entity;
+
 import com.bee32.plover.ox1.color.MomentInterval;
 import com.bee32.sem.make.entity.MakeStep;
 
+@Entity
 public class MakeInstanceStep
         extends MomentInterval {
 
     private static final long serialVersionUID = 1L;
 
     MakeInstance instance;
-    MakeStep def;
+    MakeStep model;
 
 }
