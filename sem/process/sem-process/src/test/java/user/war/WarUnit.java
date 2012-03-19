6ea36af6d922af22c52cf583fcd4a159e94ad4dc
Lenik <xjl@99jsj.com>
2012-03-19 17:32:22 +0800

    Import into WarUnit: PeopleUnit.

--- sem/process/sem-process/src/test/java/user/war/WarUnit.java
+++ sem/process/sem-process/src/test/java/user/war/WarUnit.java
@@ -5,9 +5,13 @@ import user.war.entity.BuildMission;
 
 import com.bee32.plover.orm.unit.ImportUnit;
 import com.bee32.plover.orm.unit.PersistenceUnit;
+import com.bee32.sem.people.SEMPeopleUnit;
 import com.bee32.sem.process.SEMProcessUnit;
 
-@ImportUnit(SEMProcessUnit.class)
+@ImportUnit({ SEMProcessUnit.class, //
+        /** 因为 People 无法单独运行，故这里引入 people-unit。 */
+        SEMPeopleUnit.class, //
+})
 public class WarUnit
         extends PersistenceUnit {
 
