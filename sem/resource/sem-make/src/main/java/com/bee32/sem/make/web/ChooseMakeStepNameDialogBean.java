1709878fd2b3468f8b2dd3f6f9a76fb856a4988d
jack <superjwl@gmail.com>
2012-03-27 14:08:05 +0800

    add ChooseMakeStepNameDialog

---
+++ sem/resource/sem-make/src/main/java/com/bee32/sem/make/web/ChooseMakeStepNameDialogBean.java
@@ -0,0 +1,41 @@
+package com.bee32.sem.make.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.bee32.plover.criteria.hibernate.Or;
+import com.bee32.plover.ox1.util.CommonCriteria;
+import com.bee32.sem.make.dto.MakeStepNameDto;
+import com.bee32.sem.make.entity.MakeStepName;
+import com.bee32.sem.make.util.BomCriteria;
+import com.bee32.sem.misc.ChooseEntityDialogBean;
+
+public class ChooseMakeStepNameDialogBean
+        extends ChooseEntityDialogBean {
+
+    private static final long serialVersionUID = 1L;
+
+    static Logger logger = LoggerFactory.getLogger(ChooseMakeStepNameDialogBean.class);
+
+    String mode;
+
+    public ChooseMakeStepNameDialogBean() {
+        super(MakeStepName.class, MakeStepNameDto.class, 0);
+    }
+
+    @Override
+    public void addNameOrLabelRestriction() {
+        addSearchFragment("名称含有 " + searchPattern, Or.of(//
+                CommonCriteria.labelledWith(searchPattern, true), //
+                BomCriteria.targetLabel(searchPattern, true)));
+        searchPattern = null;
+    }
+
+    public String getMode() {
+        return mode;
+    }
+
+    public void setMode(String mode) {
+        this.mode = mode;
+    }
+}
