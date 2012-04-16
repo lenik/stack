34381b33b4172a01f4b39f2e9dad32be75647e91
Lenik <xjl@99jsj.com>
2012-04-16 11:27:04 +0800

    Workaround: Run file tests in war test.

--- sem/process/sem-process/src/test/java/user/war/WarUnit.java
+++ sem/process/sem-process/src/test/java/user/war/WarUnit.java
@@ -5,12 +5,15 @@ import user.war.entity.BuildMission;
 
 import com.bee32.plover.orm.unit.ImportUnit;
 import com.bee32.plover.orm.unit.PersistenceUnit;
+import com.bee32.sem.file.SEMFileUnit;
 import com.bee32.sem.people.SEMPeopleUnit;
 import com.bee32.sem.process.SEMProcessUnit;
 
 @ImportUnit({ SEMProcessUnit.class, //
         /** 因为 People 无法单独运行，故这里引入 people-unit。 */
         SEMPeopleUnit.class, //
+        /** 因为 File 无法单独运行，故这里引入 file-unit。 */
+        SEMFileUnit.class, //
 })
 public class WarUnit
         extends PersistenceUnit {
