372a6a31b8a948d251e7e880e2d7f0a7c206816f
ztf <ztf@bee32.com>
2012-05-30 17:06:11 +0800

    internalPerson : add menu entry

--- sem/base/sem-people/src/main/java/com/bee32/sem/people/SEMPeopleMenu.java
+++ sem/base/sem-people/src/main/java/com/bee32/sem/people/SEMPeopleMenu.java
@@ -5,6 +5,11 @@ import com.bee32.plover.rtx.location.Location;
 import com.bee32.sem.frame.builtins.SEMFrameMenu;
 import com.bee32.sem.frame.menu.MenuContribution;
 import com.bee32.sem.frame.menu.MenuNode;
+import com.bee32.sem.hr.entity.JobPerformance;
+import com.bee32.sem.hr.entity.JobPost;
+import com.bee32.sem.hr.entity.JobTitle;
+import com.bee32.sem.hr.entity.PersonEducationType;
+import com.bee32.sem.hr.entity.PersonSkillCategory;
 import com.bee32.sem.people.entity.ContactCategory;
 import com.bee32.sem.people.entity.OrgType;
 import com.bee32.sem.people.entity.PartyRecordCategory;
@@ -31,4 +36,12 @@ public class SEMPeopleMenu
     /**/static MenuNode partyRecordCategory = entry(SETTINGS, 5, "partyRecordCategory",
             getDictIndex(PartyRecordCategory.class));
 
+    static MenuNode INTERNALPERSON = menu(PEOPLE, 25, "internal");
+    /**/static MenuNode internalPersonAdmin = entry(INTERNALPERSON, 1, "internalPersonAdmin", PEOPLE_.join("internalPerson/index-rich.jsf"));
+    /**/static MenuNode INTERNALDICTS = menu(INTERNALPERSON, 2, "internalDicts");
+    /*    */static MenuNode jobTitle = entry(INTERNALDICTS, 1, "jobTitle", getDictIndex(JobTitle.class));
+    /*    */static MenuNode jobPost = entry(INTERNALDICTS, 2, "jobPost", getDictIndex(JobPost.class));
+    /*    */static MenuNode educationType = entry(INTERNALDICTS, 3, "educationType", getDictIndex(PersonEducationType.class));
+    /*    */static MenuNode jobPerformance = entry(INTERNALDICTS, 4, "jobPerformance", getDictIndex(JobPerformance.class));
+    /*    */static MenuNode skillCategory = entry(INTERNALDICTS, 5, "skillCategory", getDictIndex(PersonSkillCategory.class));
 }
