24e9c0adc963c96621aeb78203bc83863036a6ef
ztf <ztf@bee32.com>
2012-07-06 09:46:55 +0800

    修改采购原则和采购方式的顺序

--- sem/biz1/sem-chance/src/main/java/com/bee32/sem/chance/entity/ProcurementMethods.java
+++ sem/biz1/sem-chance/src/main/java/com/bee32/sem/chance/entity/ProcurementMethods.java
@@ -5,9 +5,9 @@ import com.bee32.plover.orm.sample.StandardSamples;
 public class ProcurementMethods
         extends StandardSamples {
 
+    public final ProcurementMethod OTHER = new ProcurementMethod("other", "其他");
     public final ProcurementMethod JDJG = new ProcurementMethod("jdjg", "甲订甲供");
     public final ProcurementMethod JDYG = new ProcurementMethod("jdyg", "甲订乙供");
     public final ProcurementMethod YDYG = new ProcurementMethod("ydyg", "乙订乙供");
-    public final ProcurementMethod OTHER = new ProcurementMethod("other", "其他");
 
 }
