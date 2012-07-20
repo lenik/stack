297a89f14201727e5d6e0a5dea228ce8fdb253cc
jack <superjwl@gmail.com>
2012-07-20 17:44:52 +0800

    应收单和应付单加入预计收款或付款时间

--- sem/resource/sem-account/src/main/java/com/bee32/sem/account/dto/AccountAbleDto.java
+++ sem/resource/sem-account/src/main/java/com/bee32/sem/account/dto/AccountAbleDto.java
@@ -1,9 +1,16 @@
 package com.bee32.sem.account.dto;
 
+import java.util.Date;
+
+import com.bee32.sem.account.entity.AccountAble;
+import com.bee32.sem.account.entity.CurrentAccount;
+
 public class AccountAbleDto
         extends CurrentAccountDto {
 
     private static final long serialVersionUID = 1L;
+
+    Date expectedDate;
 
     public AccountAbleDto() {
         super();
@@ -11,5 +18,31 @@ public class AccountAbleDto
 
     public AccountAbleDto(int mask) {
         super(mask);
+    }
+
+    @Override
+    protected void _marshal(CurrentAccount source) {
+        super._marshal(source);
+
+        AccountAble o = (AccountAble)source;
+
+        expectedDate = o.getExpectedDate();
+    }
+
+    @Override
+    protected void _unmarshalTo(CurrentAccount target) {
+        super._unmarshalTo(target);
+
+        AccountAble o = (AccountAble)target;
+
+        o.setExpectedDate(expectedDate);
+    }
+
+    public Date getExpectedDate() {
+        return expectedDate;
+    }
+
+    public void setExpectedDate(Date expectedDate) {
+        this.expectedDate = expectedDate;
     }
 }
--- sem/resource/sem-account/src/main/java/com/bee32/sem/account/entity/AccountAble.java
+++ sem/resource/sem-account/src/main/java/com/bee32/sem/account/entity/AccountAble.java
@@ -1,7 +1,11 @@
 package com.bee32.sem.account.entity;
 
+import java.util.Date;
+
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 /**
  * 应收单应付单基类
@@ -13,5 +17,21 @@ import javax.persistence.Entity;
 public class AccountAble extends CurrentAccount {
 
     private static final long serialVersionUID = 1L;
+
+    Date expectedDate;
+
+    /**
+     * 预计收款或付款时间,用于账龄分析
+     * @return
+     */
+    @Temporal(TemporalType.TIMESTAMP)
+    public Date getExpectedDate() {
+        return expectedDate;
+    }
+
+    public void setExpectedDate(Date expectedDate) {
+        this.expectedDate = expectedDate;
+    }
+
 
 }
