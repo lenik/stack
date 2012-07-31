ef99ada6b4d53a31de93732f8f3a5cee3733e7de
jack <superjwl@gmail.com>
2012-07-31 14:19:22 +0800

    应收余额表加入合计行

--- sem/resource/sem-account/src/main/java/com/bee32/sem/account/web/BalanceSheetReceBean.java
+++ sem/resource/sem-account/src/main/java/com/bee32/sem/account/web/BalanceSheetReceBean.java
@@ -24,6 +24,7 @@ public class BalanceSheetReceBean
     boolean verified;
 
     List<Object[]> result = new ArrayList<Object[]>();
+    List<Object[]> summary = new ArrayList<Object[]>();
 
     public boolean isVerified() {
         return verified;
@@ -41,6 +42,14 @@ public class BalanceSheetReceBean
         this.result = result;
     }
 
+    public List<Object[]> getSummary() {
+        return summary;
+    }
+
+    public void setSummary(List<Object[]> summary) {
+        this.summary = summary;
+    }
+
     @Transactional
     // (readOnly = true)
     public void query() {
@@ -48,8 +57,8 @@ public class BalanceSheetReceBean
 
         sb.append("select  ");
         sb.append(" a2.party as partyId,b.label as partyName, ");
-        sb.append(" a2.init,a2.rable,a2.red,a2.init+a2.rable+a2.red as balance from ( ");
-        sb.append(" select party,sum(init) as init,sum(rable) as rable,sum(red) as red, sum(init)+sum(rable)+sum(red) as balance from ( ");
+        sb.append(" a2.init,a2.rable,a2.red,a2.balance from ( ");
+        sb.append(" select party,sum(init) as init,sum(rable) as rable,sum(red) as red, sum(init)+sum(rable)-sum(red) as balance from ( ");
         sb.append("     select party,amount as init,0 as rable,0 as red from current_account  ");
         sb.append("     where stereo='RINIT' ");
         if(verified) {
@@ -63,7 +72,7 @@ public class BalanceSheetReceBean
         }
         sb.append("     group by party ");
         sb.append("     union all ");
-        sb.append("     select party,0 as init, 0 as rable, sum(amount) as red from current_account  ");
+        sb.append("     select party,0 as init, 0 as rable, -sum(amount) as red from current_account  ");
         sb.append("     where stereo='RED'  ");
         if(verified) {
             sb.append("     and verify_eval_state=33554434 ");
@@ -76,7 +85,32 @@ public class BalanceSheetReceBean
 
         Session session = SessionFactoryUtils.getSession(sessionFactory, false);
         SQLQuery sqlQuery = session.createSQLQuery(sb.toString());
-
         result = sqlQuery.list();
+
+
+        sb.delete(0, sb.length());  //清空string builder
+        sb.append("select  ");
+        sb.append(" sum(init) as init,sum(rable) as rable,sum(red) as red, sum(init)+sum(rable)-sum(red) as balance from ( ");
+        sb.append(" select amount as init,0 as rable,0 as red from current_account  ");
+        sb.append(" where stereo='RINIT' ");
+        if(verified) {
+            sb.append("     and verify_eval_state=33554434 ");
+        }
+        sb.append(" union all ");
+        sb.append(" select 0 as init, sum(amount) as rable, 0 as red from current_account  ");
+        sb.append(" where stereo='RABLE'  ");
+        if(verified) {
+            sb.append("     and verify_eval_state=33554434 ");
+        }
+        sb.append(" union all ");
+        sb.append(" select 0 as init, 0 as rable, -sum(amount) as red from current_account  ");
+        sb.append(" where stereo='RED' ");
+        if(verified) {
+            sb.append("     and verify_eval_state=33554434 ");
+        }
+        sb.append(") a1  ");
+
+        sqlQuery = session.createSQLQuery(sb.toString());
+        summary = sqlQuery.list();
     }
 }
