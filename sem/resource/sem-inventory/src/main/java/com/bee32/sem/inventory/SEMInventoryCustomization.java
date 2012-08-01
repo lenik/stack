4332b789f90c8a00d7954fcb855105d479c4f6e1
Lenik <xjl@99jsj.com>
2012-08-01 14:13:05 +0800

    Added Term customization for SEM Inventory.

--- sem/resource/sem-inventory/src/main/java/com/bee32/sem/inventory/SEMInventoryCustomization.java
+++ sem/resource/sem-inventory/src/main/java/com/bee32/sem/inventory/SEMInventoryCustomization.java
@@ -1,8 +1,17 @@
 package com.bee32.sem.inventory;
 
 import com.bee32.sem.module.AbstractModuleCustomization;
+import com.bee32.sem.module.ITermProvider;
 
 public class SEMInventoryCustomization
         extends AbstractModuleCustomization {
 
+    public SEMInventoryCustomization() {
+        super(SEMInventoryModule.class);
+    }
+
+    @Override
+    public ITermProvider getTermProvider() {
+        return new SEMInventoryTerms();
+    }
 }
--- sem/resource/sem-inventory/src/main/java/com/bee32/sem/inventory/SEMInventoryTerms.java
+++ sem/resource/sem-inventory/src/main/java/com/bee32/sem/inventory/SEMInventoryTerms.java
@@ -1,65 +1,44 @@
 package com.bee32.sem.inventory;
 
+import org.springframework.stereotype.Component;
+
 import com.bee32.plover.site.cfg.SitePropertyPrefix;
 import com.bee32.sem.module.AbstractModuleTerms;
 import com.bee32.sem.module.Term;
 
+@Component("inventoryTerms")
 @SitePropertyPrefix("inventory.term")
 public class SEMInventoryTerms
         extends AbstractModuleTerms {
 
     @Term
     public String getWarehouse() {
-        return getProperty("warehouse");
-    }
-
-    public void setWarehouse(String warehouse) {
-        setProperty("warehouse", warehouse);
+        return getTermLabel("warehouse");
     }
 
     @Term
     public String getBatch1() {
-        return getProperty("batch1");
-    }
-
-    public void setBatch1(String batch1) {
-        setProperty("batch1", batch1);
+        return getTermLabel("batch1");
     }
 
     @Term
     public String getBatch2() {
-        return getProperty("batch2");
-    }
-
-    public void setBatch2(String batch2) {
-        setProperty("batch2", batch2);
+        return getTermLabel("batch2");
     }
 
     @Term
     public String getBatch3() {
-        return getProperty("batch3");
-    }
-
-    public void setBatch3(String batch3) {
-        setProperty("batch3", batch3);
+        return getTermLabel("batch3");
     }
 
     @Term
     public String getBatch4() {
-        return getProperty("batch4");
-    }
-
-    public void setBatch4(String batch4) {
-        setProperty("batch4", batch4);
+        return getTermLabel("batch4");
     }
 
     @Term
     public String getBatch5() {
-        return getProperty("batch5");
-    }
-
-    public void setBatch5(String batch5) {
-        setProperty("batch5", batch5);
+        return getTermLabel("batch5");
     }
 
 }
