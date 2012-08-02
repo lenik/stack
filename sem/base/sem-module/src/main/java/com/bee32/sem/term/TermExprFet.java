55852ebe48e23247429301af9d0aed3662f49634
Lenik <xjl@99jsj.com>
2012-08-02 17:17:35 +0800

    Added TermExprFet to translate faces error messages.

---
+++ sem/base/sem-module/src/main/java/com/bee32/sem/term/TermExprFet.java
@@ -0,0 +1,16 @@
+package com.bee32.sem.term;
+
+import com.bee32.plover.faces.AbstractFet;
+
+public class TermExprFet
+        extends AbstractFet {
+
+    TermMessageInterpolator interpolator = TermMessageInterpolator.getInstance();
+
+    @Override
+    public String translate(Throwable exception, String message) {
+        message = interpolator.process(message);
+        return message;
+    }
+
+}
