ff903e4cda5d7f60b52ffd66854c409602cc1930
Lenik <xjl@99jsj.com>
2012-08-02 14:45:52 +0800

    Polishing

--- sem/build/sem-uber/src/test/java/com/bee32/sem/uber/coll/ApCollector.java
+++ sem/build/sem-uber/src/test/java/com/bee32/sem/uber/coll/ApCollector.java
@@ -2,11 +2,11 @@ package com.bee32.sem.uber.coll;
 
 import java.io.IOException;
 
-import com.bee32.plover.arch.AppProfile;
+import com.bee32.plover.arch.IAppProfile;
 import com.bee32.plover.test.ServiceCollector;
 
 public class ApCollector
-        extends ServiceCollector<AppProfile> {
+        extends ServiceCollector<IAppProfile> {
 
     public static void main(String[] args)
             throws IOException {
