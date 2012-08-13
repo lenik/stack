f5ccaf4eea306f74762dbd0323ea9540838e0b25
ztf <ztf@bee32.com>
2012-08-13 17:16:35 +0800

    add wage to uber

--- sem/build/sem-uber/src/main/java/com/bee32/sem/uber/SEMUberUnit.java
+++ sem/build/sem-uber/src/main/java/com/bee32/sem/uber/SEMUberUnit.java
@@ -6,11 +6,12 @@ import com.bee32.sem.account.SEMAccountUnit;
 import com.bee32.sem.asset.SEMAssetUnit;
 import com.bee32.sem.chance.SEMChanceUnit;
 import com.bee32.sem.purchase.SEMPurchaseUnit;
+import com.bee32.sem.wage.SEMWageUnit;
 
 /**
  * Import units of biz1, biz2, etc..
  */
-@ImportUnit({ SEMChanceUnit.class, SEMPurchaseUnit.class, SEMAssetUnit.class, SEMAccountUnit.class })
+@ImportUnit({ SEMChanceUnit.class, SEMPurchaseUnit.class, SEMAssetUnit.class, SEMAccountUnit.class, SEMWageUnit.class })
 public class SEMUberUnit
         extends PersistenceUnit {
 
