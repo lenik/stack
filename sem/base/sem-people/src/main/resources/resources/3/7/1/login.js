c947b8e1a50b2515bcc99352c003bea91c2e25c7
Lenik <xjl@99jsj.com>
2012-08-17 17:14:55 +0800

    Rewrite the login page.

---
+++ sem/base/sem-people/src/main/resources/resources/3/7/1/login.js
@@ -0,0 +1,16 @@
+function calcDigest() {
+    var p = $.sha1($("#loginForm\\:passPlain").val());
+
+    var c = $("#loginForm\\:challenge").val();
+    var p2 = $.sha1(c + p + c);
+
+    $("#loginForm\\:password").val(p2);
+}
+
+function handleLoginRequest(xhr, status, args) {
+    if (args.validationFailed || !args.loggedIn) {
+
+    } else {
+        window.location.href = "#{location.WEB_APP}/";
+    }
+}
