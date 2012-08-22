e18be9dd3766d4b70248a920536a20c6e968af28
ztf <ztf@bee32.com>
2012-08-22 09:02:19 +0800

    Skill level add DecimalConfig

--- sem/base/sem-people/src/main/java/com/bee32/sem/hr/entity/PersonSkillCategoryLevel.java
+++ sem/base/sem-people/src/main/java/com/bee32/sem/hr/entity/PersonSkillCategoryLevel.java
@@ -1,15 +1,22 @@
 package com.bee32.sem.hr.entity;
 
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+
 import com.bee32.plover.orm.entity.EntityAuto;
+import com.bee32.plover.ox1.config.DecimalConfig;
 
 public class PersonSkillCategoryLevel
-        extends EntityAuto<Integer> {
+        extends EntityAuto<Integer>
+        implements DecimalConfig {
 
     private static final long serialVersionUID = 1L;
 
     PersonSkillCategory category;
     int score;
     String label;
+    BigDecimal bonus = BigDecimal.ZERO;
 
     public PersonSkillCategory getCategory() {
         return category;
@@ -33,6 +40,15 @@ public class PersonSkillCategoryLevel
 
     public void setLabel(String label) {
         this.label = label;
+    }
+
+    @Column(precision = MONEY_ITEM_PRECISION, scale = MONEY_ITEM_SCALE)
+    public BigDecimal getBonus() {
+        return bonus;
+    }
+
+    public void setBonus(BigDecimal bonus) {
+        this.bonus = bonus;
     }
 
 }
