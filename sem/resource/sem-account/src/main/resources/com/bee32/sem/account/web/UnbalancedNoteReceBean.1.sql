6e68090794ef9e7cf9b4a2268c548dbd76147b03
jack <superjwl@gmail.com>
2012-08-24 15:50:17 +0800

    未结算应收票据查询功能完成

---
+++ sem/resource/sem-account/src/main/resources/com/bee32/sem/account/web/UnbalancedNoteReceBean.1.sql
@@ -0,0 +1,16 @@
+select
+    a.begin_time,a.end_time,c.label as customer,
+    -a.amount as amount,a.bill_no,d.label as bill_type,a.accept_bank,
+    e.label as accept_org,f.label as person
+ from current_account a
+left join note_balancing b
+    on a.id=b.note
+left join party c
+    on a.party=c.id
+left join bill_type d
+    on a.bill_type=d.id
+left join party e
+    on a.accept_org=e.id
+left join party f
+    on a.person=f.id
+where a.stereo='RNOTE' and b.id is null AND_VERIFIED
