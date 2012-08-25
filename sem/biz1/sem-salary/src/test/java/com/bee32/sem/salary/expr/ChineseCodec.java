9265aaaff13dbf0fe7d066141e2cbdd9d53a54b3
Lenik <xjl@99jsj.com>
2012-08-25 15:28:10 +0800

    Added ChineseCodec to support chinese var names in expr.

---
+++ sem/biz1/sem-salary/src/test/java/com/bee32/sem/salary/expr/ChineseCodec.java
@@ -0,0 +1,63 @@
+package com.bee32.sem.salary.expr;
+
+import java.util.regex.Pattern;
+
+import javax.free.PatternProcessor;
+
+public class ChineseCodec {
+
+    static Pattern encodedChar = Pattern.compile("_u[0-9a-f]{4}_");
+
+    static class Decoder
+            extends PatternProcessor {
+
+        public Decoder() {
+            super(encodedChar);
+        }
+
+        @Override
+        protected void matched(String part) {
+            String hex = part.substring(2, part.length() - 1);
+            int num = Integer.parseInt(hex, 16);
+            char ch = (char) num;
+            print(ch);
+        }
+
+    }
+
+    public static String encode(String s) {
+        int n = s.length();
+        StringBuilder encoded = new StringBuilder(n);
+        for (int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+            if (ch < '\u0080')
+                encoded.append(ch);
+            else {
+                encoded.append("_u");
+                String hex = Integer.toHexString(ch);
+                switch (hex.length()) {
+                case 0:
+                    assert false;
+                case 1:
+                    encoded.append('0');
+                case 2:
+                    encoded.append('0');
+                case 3:
+                    encoded.append('0');
+                    encoded.append(hex);
+                    break;
+                default:
+                    hex = hex.substring(hex.length() - 4);
+                    encoded.append(hex);
+                }
+                encoded.append('_');
+            }
+        }
+        return encoded.toString();
+    }
+
+    public static String decode(String s) {
+        return new Decoder().process(s);
+    }
+
+}
---
+++ sem/biz1/sem-salary/src/test/java/com/bee32/sem/salary/expr/ChineseCoderTest.java
@@ -0,0 +1,69 @@
+package com.bee32.sem.salary.expr;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ChineseCoderTest
+        extends Assert {
+
+    @Test
+    public void testEncodeEnglish() {
+        String encoded = ChineseCodec.encode("hello");
+        assertEquals("hello", encoded);
+    }
+
+    @Test
+    public void testDecodeEnglish() {
+        String decoded = ChineseCodec.decode("hello");
+        assertEquals("hello", decoded);
+    }
+
+    @Test
+    public void testEncodeZh1() {
+        String encoded = ChineseCodec.encode("你");
+        assertEquals("_u4f60_", encoded);
+    }
+
+    @Test
+    public void testEncodeZh2() {
+        String encoded = ChineseCodec.encode("你好");
+        assertEquals("_u4f60__u597d_", encoded);
+    }
+
+    @Test
+    public void testEncodeMixed() {
+        String encoded = ChineseCodec.encode("a你b好");
+        assertEquals("a_u4f60_b_u597d_", encoded);
+    }
+
+    @Test
+    public void testEncodeAmbiguous() {
+        String decoded = ChineseCodec.decode("_u123_");
+        assertEquals("_u123_", decoded);
+    }
+
+    @Test
+    public void testDecodeZh1() {
+        String decoded = ChineseCodec.decode("_u4f60_");
+        assertEquals("你", decoded);
+    }
+
+    @Test
+    public void testDecodeZh2() {
+        String decoded = ChineseCodec.decode("_u4f60__u597d_");
+        assertEquals("你好", decoded);
+    }
+
+    @Test
+    public void testDecodeMixed() {
+        String decoded = ChineseCodec.decode("a_u4f60_b_u597d_");
+        assertEquals("a你b好", decoded);
+    }
+
+    @Test
+    public void testDecodeAmbiguous() {
+        String decoded = ChineseCodec.decode("_u123_");
+        assertEquals("_u123_", decoded);
+    }
+
+}
