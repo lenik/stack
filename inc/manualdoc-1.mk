1e6f3bdbe5be411a2c96fb2d760ec96010e75fa4
Lenik <xjl@99jsj.com>
2012-09-15 00:09:25 +0800

    Generated history.tex from changelogs.

--- inc/manualdoc-1.mk
+++ inc/manualdoc-1.mk
@@ -4,11 +4,22 @@ g_incdir := $(dir $(lastword $(MAKEFILE_LIST)))
 
 tex_template := $(g_incdir)/manualdoc-1.tex
 
-manual.pdf: manual.tex $(tex_template)
-	xelatex -jobname=manual '\nonstopmode \def\modtitle{$(TITLE)} \def\modsubtitle{$(SUBTITLE)} \input $(tex_template)'
+manual-1.pdf: manual.tex history.tex $(tex_template)
+	xelatex -jobname=manual-1 '\nonstopmode \def\modtitle{$(TITLE)} \def\modsubtitle{$(SUBTITLE)} \input $(tex_template)'
 
-changes.tex:
+history.tex: manual.tex
+	CWD="$$PWD"; cd ..; \
+	gitcl . | gitcl2 -r -t '' -T \
+		--Tv=版本 \
+		--Td=日期 \
+		--Tl=变更项目 \
+		--Tw=10cm \
+		--Ts='c|c|>{\sl}p{10cm}' \
+		--Th='\caption{版本历史} \\ ' \
+		--Tb='$$\circ$$' \
+		>"$$CWD/history.tex"
 
 clean:
 	rm -f *.aux *.log *.lot *.ind *.idx *.ilg *.pdf *.bbl *.blg *.out
+	rm -f history.tex
 
