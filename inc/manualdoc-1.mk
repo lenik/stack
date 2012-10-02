60c75a496806fa19c2cdbc8637e66b91cc9ebb8a
Lenik <xjl@99jsj.com>
2012-10-02 11:46:41 +0800

    Utilized cooltex to build tex files.

--- inc/manualdoc-1.mk
+++ inc/manualdoc-1.mk
@@ -6,7 +6,7 @@ tex_template := $(g_incdir)/manualdoc-1.tex
 tex_libmanual := $(g_incdir)/libmanual.tex
 
 manual-1.pdf: manual.tex history.tex $(tex_template) $(tex_libmanual)
-	xelatex -jobname=manual-1 '\nonstopmode \def\DIRNAME{$(g_incdir)} \def\modtitle{$(TITLE)} \def\modsubtitle{$(SUBTITLE)} \input $(tex_template)'
+	cooltex -abvPjmanual-1 -DDIRNAME="$(g_incdir)" -Dmodtitle="$(TITLE)" -Dmodsubtitle="$(SUBTITLE)" "$(tex_template)"
 
 history.tex: manual.tex
 	CWD="$$PWD"; cd ..; \
