4a079bee8994132246e6306fb6940b8a25a970c4
jack <superjwl@gmail.com>
2012-10-18 15:48:36 +0800

    解决付款单付款功能错误，解决相关权限问题

--- sem/resource/sem-asset/src/main/java/com/bee32/sem/asset/web/PaymentNoteAdminBean.java
+++ sem/resource/sem-asset/src/main/java/com/bee32/sem/asset/web/PaymentNoteAdminBean.java
@@ -6,6 +6,7 @@ import com.bee32.sem.asset.entity.FundFlow;
 import com.bee32.sem.asset.entity.PaymentNote;
 import com.bee32.sem.asset.service.AssetService;
 import com.bee32.sem.misc.ScrollEntityViewBean;
+import com.bee32.sem.people.dto.PersonDto;
 
 @ForEntity(FundFlow.class)
 public class PaymentNoteAdminBean
@@ -13,13 +14,45 @@ public class PaymentNoteAdminBean
 
     private static final long serialVersionUID = 1L;
 
+    PersonDto whoPay;
+
     public PaymentNoteAdminBean() {
         super(PaymentNote.class, PaymentNoteDto.class, 0);
     }
 
+    public PersonDto getWhoPay() {
+        return whoPay;
+    }
+
+    public void setWhoPay(PersonDto whoPay) {
+        this.whoPay = whoPay;
+    }
+
+    /**
+     * 显示付款对话框时，把付款人copy到临时变量中
+     */
+    public void showPayDialog() {
+        PaymentNoteDto note = getOpenedObject();
+        whoPay = note.getWhoPay();
+    }
+
     public void pay() {
+        if (whoPay.isNull()) {
+            uiLogger.error("没有付款人");
+            return;
+        }
+
+        PersonDto whoPayOld = null;
         PaymentNoteDto note =  getOpenedObject();
-        BEAN(AssetService.class).pay(note);
+        try {
+            whoPayOld = note.getWhoPay();   //保存原来的付款人
+            note.setWhoPay(whoPay);
+            BEAN(AssetService.class).pay(note);
+            uiLogger.info("付款成功");
+        } catch (Exception e) {
+            note.setWhoPay(whoPayOld);  //如果出错，则还原旧的付款人
+            uiLogger.error("付款错误", e);
+        }
     }
 
 }
