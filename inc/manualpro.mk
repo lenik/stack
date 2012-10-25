4fdc1f648de55fc924eb5e2c5d21a4e1f62d2081
Lenik <xjl@99jsj.com>
2012-10-25 15:45:19 +0800

    Enhanced application profile book style & building.

--- inc/manualpro.mk
+++ inc/manualpro.mk
@@ -5,8 +5,28 @@ g_incdir := $(patsubst %/,%,$(dir $(lastword $(MAKEFILE_LIST))))
 tex_template := $(g_incdir)/manualpro.tex
 tex_libmanual := $(g_incdir)/libmanual.tex
 
-manualpro.pdf: profile.tex $(tex_template) $(tex_libmanual)
-	xelatex -jobname=manualpro '\nonstopmode \def\DIRNAME{$(g_incdir)} \def\modtitle{$(TITLE)} \def\modsubtitle{$(SUBTITLE)} \input $(tex_template)'
+all: manualpro.pdf
+
+manualpro.pdf: profile.tex history.tex $(tex_template) $(tex_libmanual)
+	cooltex -abvjmanualpro -DDIRNAME="$(g_incdir)" -Dmodtitle="$(TITLE)" -Dmodsubtitle="$(SUBTITLE)" "$(tex_template)"
+
+# stack/manuals/profiles/FooBar/...
+history.tex: modules
+	CWD="$$PWD"; cd ../../..; \
+	gitcl `cat $$CWD/modules` | gitcl2 -r -t '' -T \
+		--Tv=版本 \
+		--Td=日期 \
+		--Tl=变更项目 \
+		--Ts='c|c|>{\sl}p{11.5cm}' \
+		--Th='\caption{版本历史} \\ ' \
+		--Tb='$$\circ$$' \
+		>"$$CWD/history.tex"
+
+modules: profile.tex
+	grep modulechapter $< | while IFS='{' read modchap title dir; do \
+		echo "$${dir%\}}"; \
+	done >$@
 
 clean:
 	rm -f *.aux *.log *.lot *.ind *.idx *.ilg *.pdf *.bbl *.blg *.out
+	rm -f history.tex
