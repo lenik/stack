b9664eb0fb8e2b3251d403c41262319932fd950d
ztf <ztf@bee32.com>
2012-10-26 16:08:30 +0800

    设置出勤状态和图标由多对一到一对一。

--- sem/biz1/sem-salary/src/main/java/com/bee32/sem/attendance/entity/AttendanceType.java
+++ sem/biz1/sem-salary/src/main/java/com/bee32/sem/attendance/entity/AttendanceType.java
@@ -64,25 +64,25 @@ public class AttendanceType
         site.saveConfig();
     }
 
-    public static final AttendanceType notAvailable = new AttendanceType('-', "不可用", "div.jpg", false);
+    public static final AttendanceType notAvailable = new AttendanceType('-', "不可用", "notavailable.jpg", false);
 
     /**
      * 正常出勤
      */
-    public static final AttendanceType normal = new AttendanceType('A', "内勤", "right.jpg", true); // 正常出勤
-    public static final AttendanceType trip = new AttendanceType('B', "外勤", "right.jpg", true); // 外勤
+    public static final AttendanceType normal = new AttendanceType('A', "内勤", "attendance.jpg", true); // 正常出勤
+    public static final AttendanceType trip = new AttendanceType('B', "外勤", "trip.jpg", true); // 外勤
 
     /**
      * 缺勤：请假
      */
-    public static final AttendanceType leave1 = new AttendanceType('C', "请假", "rabbish.jpg", false); // 一般请假
-    public static final AttendanceType leave2 = new AttendanceType('D', "婚假", "rabbish.jpg", false); // 婚假
-    public static final AttendanceType leave3 = new AttendanceType('E', "产假", "rabbish.jpg", false); // 产假
+    public static final AttendanceType leave1 = new AttendanceType('C', "请假", "aleave.jpg", false); // 一般请假
+    public static final AttendanceType leave2 = new AttendanceType('D', "婚假", "mleave.jpg", false); // 婚假
+    public static final AttendanceType leave3 = new AttendanceType('E', "产假", "home.jpg", false); // 产假
 
     /**
      * 缺勤：其他
      */
-    public static final AttendanceType late1 = new AttendanceType('F', "迟到", "leave.jpg", true); // 迟到
+    public static final AttendanceType late1 = new AttendanceType('F', "迟到", "late.jpg", true); // 迟到
 
     /**
      * 缺勤：矿工
@@ -93,13 +93,12 @@ public class AttendanceType
      * 应加班
      */
     public static final AttendanceType over1 = new AttendanceType('H', "晚加班", "star.jpg", true); // 晚加班
-    public static final AttendanceType over2 = new AttendanceType('I', "节日加班", "star.jpg", true); // 节日加班
-    public static final AttendanceType over3 = new AttendanceType('J', "假日加班", "star.jpg", true); // 假日加班
-    public static final AttendanceType over4 = new AttendanceType('K', "休息日加班", "star.jpg", true); // 休息日加班
+    public static final AttendanceType over2 = new AttendanceType('I', "节假日加班", "flag.jpg", true); // 节日加班
+    public static final AttendanceType over4 = new AttendanceType('J', "休息日加班", "hstar.jpg", true); // 休息日加班
 
     /**
      * 其他
      */
-    public static final AttendanceType rest2 = new AttendanceType('Z', "公休", "stop.jpg", false); // 公休
+    public static final AttendanceType rest2 = new AttendanceType('Z', "公休", "rest.jpg", false); // 公休
 
 }
