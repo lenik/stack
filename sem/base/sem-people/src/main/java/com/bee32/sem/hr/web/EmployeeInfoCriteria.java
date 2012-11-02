ec7a827598d83394308dadb4b1a7b22fd0940a45
ztf <ztf@bee32.com>
2012-11-02 16:10:07 +0800

    format

--- sem/base/sem-people/src/main/java/com/bee32/sem/hr/web/EmployeeInfoCriteria.java
+++ sem/base/sem-people/src/main/java/com/bee32/sem/hr/web/EmployeeInfoCriteria.java
@@ -1,13 +1,17 @@
 package com.bee32.sem.hr.web;
 
+import java.util.Date;
+
+import com.bee32.plover.criteria.hibernate.CriteriaElement;
 import com.bee32.plover.criteria.hibernate.CriteriaSpec;
 import com.bee32.plover.criteria.hibernate.ICriteriaElement;
 
-public class EmployeeInfoCriteria extends CriteriaSpec {
+public class EmployeeInfoCriteria
+        extends CriteriaSpec {
 
-    public static ICriteriaElement listInfo() {
-        return compose(alias("person", "person"),
-                equals("person.employee", true));
+    public static ICriteriaElement listInfo(Date date) {
+        CriteriaElement or = or(greaterThan("resignedDate", date), equals("resignedDate", null));
+        return compose(alias("person", "person"), equals("person.employee", true), or);
     }
 
 }
