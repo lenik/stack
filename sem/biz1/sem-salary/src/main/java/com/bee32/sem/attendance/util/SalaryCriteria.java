a3a4e65c9e52184f2eb35c4da6016ec49083eb25
ztf <ztf@bee32.com>
2012-11-02 16:23:22 +0800

    移除不用的方法。

--- sem/biz1/sem-salary/src/main/java/com/bee32/sem/attendance/util/SalaryCriteria.java
+++ sem/biz1/sem-salary/src/main/java/com/bee32/sem/attendance/util/SalaryCriteria.java
@@ -1,9 +1,5 @@
 package com.bee32.sem.attendance.util;
 
-import java.util.Date;
-
-import javax.free.Pair;
-
 import org.hibernate.criterion.CriteriaSpecification;
 
 import com.bee32.plover.criteria.hibernate.CriteriaSpec;
@@ -11,18 +7,12 @@ import com.bee32.plover.criteria.hibernate.Equals;
 import com.bee32.plover.criteria.hibernate.ICriteriaElement;
 import com.bee32.plover.criteria.hibernate.LeftHand;
 import com.bee32.sem.salary.entity.MonthSalary;
-import com.bee32.sem.salary.util.SalaryDateUtil;
 
 public class SalaryCriteria
         extends CriteriaSpec {
 
     public static ICriteriaElement listSalaryByYearAndMonth(int yearMonth) {
         return new Equals("yearMonth", yearMonth);
-    }
-
-    public static ICriteriaElement listByDate(Date date) {
-        Pair<Integer, Integer> yearAndMonth = SalaryDateUtil.getYearAndMonth(date);
-        return new Equals("yearMonth", yearAndMonth.getFirst() * 100 + yearAndMonth.getSecond());
     }
 
     @LeftHand(MonthSalary.class)
