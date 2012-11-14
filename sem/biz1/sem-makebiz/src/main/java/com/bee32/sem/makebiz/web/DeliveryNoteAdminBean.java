f6345c76627b6044139b290250919b9a9919ff71
ztf <ztf@bee32.com>
2012-11-14 12:38:37 +0800

    Remove field: pdfFile

--- sem/biz1/sem-makebiz/src/main/java/com/bee32/sem/makebiz/web/DeliveryNoteAdminBean.java
+++ sem/biz1/sem-makebiz/src/main/java/com/bee32/sem/makebiz/web/DeliveryNoteAdminBean.java
@@ -1,26 +1,33 @@
 package com.bee32.sem.makebiz.web;
 
-import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 import java.math.BigDecimal;
+import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
+import javax.faces.context.FacesContext;
+import javax.free.UnexpectedException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+
+import net.sf.jasperreports.engine.JRDataSource;
+import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperCompileManager;
-import net.sf.jasperreports.engine.JasperExportManager;
-import net.sf.jasperreports.engine.JasperFillManager;
-import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.JasperRunManager;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 
 import org.apache.commons.lang.StringUtils;
-import org.primefaces.model.DefaultStreamedContent;
-import org.primefaces.model.StreamedContent;
 
 import com.bee32.plover.orm.annotation.ForEntity;
 import com.bee32.plover.orm.util.DTOs;
+import com.bee32.plover.util.Mime;
+import com.bee32.plover.util.Mimes;
 import com.bee32.sem.frame.ui.ListMBean;
 import com.bee32.sem.inventory.dto.StockOrderItemDto;
 import com.bee32.sem.makebiz.dto.DeliveryNoteDto;
@@ -33,14 +40,14 @@ import com.bee32.sem.makebiz.entity.MakeOrderItem;
 import com.bee32.sem.makebiz.service.MakebizService;
 import com.bee32.sem.misc.ScrollEntityViewBean;
 import com.bee32.sem.misc.UnmarshalMap;
+import com.bee32.sem.world.monetary.FxrQueryException;
 
 @ForEntity(DeliveryNote.class)
 public class DeliveryNoteAdminBean
         extends ScrollEntityViewBean {
 
     private static final long serialVersionUID = 1L;
-
-    StreamedContent pdfFile;
+    private static final Mime PDF = Mimes.application_pdf;
 
     int tabIndex;
 
@@ -56,17 +63,16 @@ public class DeliveryNoteAdminBean
         this.tabIndex = tabIndex;
     }
 
-
     public void setApplyMakeOrder(MakeOrderDto makeOrderRef) {
         DeliveryNoteDto deliveryNote = getOpenedObject();
         MakeOrderDto makeOrder = reload(makeOrderRef, MakeOrderDto.NOT_DELIVERIED_ITEMS);
 
-//        for (MakeOrderItemDto item : makeOrder.getItems()) {
-//            if (item.getMaterial().isNull()) {
-//                uiLogger.error("定单明细没有指定物料.");
-//                return;
-//            }
-//        }
+// for (MakeOrderItemDto item : makeOrder.getItems()) {
+// if (item.getMaterial().isNull()) {
+// uiLogger.error("定单明细没有指定物料.");
+// return;
+// }
+// }
 
         List<DeliveryNoteItemDto> deliveryNoteItems = makeOrder.arrangeDeliveryNote();
         if (deliveryNoteItems.isEmpty()) {
@@ -110,69 +116,86 @@ public class DeliveryNoteAdminBean
         }
     }
 
-
-    public void exportToPdf() {
+    public void exportToPdf()
+            throws FxrQueryException, IOException {
         DeliveryNoteDto note = this.getOpenedObject();
-        JRBeanCollectionDataSource beanCollectionDataSource=new JRBeanCollectionDataSource(note.getItems());
+        JRBeanCollectionDataSource beanCollectionDataSource = new JRBeanCollectionDataSource(note.getItems());
 
-        ClassLoader ccl = getClass().getClassLoader(); //Thread.currentThread().getContextClassLoader();
-        InputStream reportStream = ccl.getResourceAsStream("resources/3/15/6/3/delivery/report1.jrxml");
+        InputStream report = getClass().getClassLoader().getResourceAsStream(
+                "resources/3/15/6/3/delivery/report1.jrxml");
+
+        Map<String, Object> reportParams = new HashMap<String, Object>();
+        reportParams.put("id", note.getId());
+        reportParams.put("createDate", note.getCreatedDate());
+        reportParams.put("arrivalDate", note.getArrivalDate());
+        reportParams.put("owner", note.getOwnerDisplayName());
+        reportParams.put("label", note.getLabel());
+        reportParams.put("customer", note.getCustomer().getDisplayName());
+        reportParams.put("description", note.getDescription());
+        reportParams.put("nativeTotal", note.getNativeTotal());
+        doExport(report, reportParams, beanCollectionDataSource, "deliveryNote", PDF);
+    }
+
+    public void exportToPdfNoPrice()
+            throws IOException, FxrQueryException {
+        DeliveryNoteDto note = this.getOpenedObject();
+        JRBeanCollectionDataSource beanCollectionDataSource = new JRBeanCollectionDataSource(note.getItems());
+
+        InputStream report = getClass().getClassLoader().getResourceAsStream(
+                "resources/3/15/6/3/delivery/report1_no_price.jrxml");
+
+        Map<String, Object> reportParams = new HashMap<String, Object>();
+        reportParams.put("id", note.getId());
+        reportParams.put("createDate", note.getCreatedDate());
+        reportParams.put("arrivalDate", note.getArrivalDate());
+        reportParams.put("owner", note.getOwnerDisplayName());
+        reportParams.put("label", note.getLabel());
+        reportParams.put("customer", note.getCustomer().getDisplayName());
+        reportParams.put("description", note.getDescription());
+        reportParams.put("nativeTotal", note.getNativeTotal());
+
+        doExport(report, reportParams, beanCollectionDataSource, "deliveryNode", PDF);
+    }
+
+    void doExport(InputStream template, Map<String, Object> params, JRDataSource datasource, String fileName,
+            Mime contentType)
+            throws IOException {
+        FacesContext context = FacesContext.getCurrentInstance();
+
+        byte[] outputByteArray = null;
+        JasperReport report;
+        ServletOutputStream outputStream;
+        HttpServletResponse response;
+
+        response = (HttpServletResponse) context.getExternalContext().getResponse();
+
+        /**
+         * @see RFC 5987 2.3
+         */
+        String encodedFilename;
+        try {
+            encodedFilename = URLEncoder.encode(fileName, "utf-8");
+            System.out.println(encodedFilename);
+        } catch (UnsupportedEncodingException e) {
+            throw new UnexpectedException(e);
+        }
+
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType(contentType.getContentType());
+        response.setHeader("Content-Disposition",
+                "attachment; filename*=UTF-8''" + encodedFilename + "." + contentType.getPreferredExtension());
 
         try {
-            JasperReport report = JasperCompileManager.compileReport(reportStream);
-            Map<String, Object> reportParams = new HashMap<String, Object>();
-            reportParams.put("id", note.getId());
-            reportParams.put("createDate", note.getCreatedDate());
-            reportParams.put("arrivalDate", note.getArrivalDate());
-            reportParams.put("owner", note.getOwnerDisplayName());
-            reportParams.put("label", note.getLabel());
-            reportParams.put("customer", note.getCustomer().getDisplayName());
-            reportParams.put("description", note.getDescription());
-            reportParams.put("nativeTotal", note.getNativeTotal());
-
-            JasperPrint jasperPrint = JasperFillManager.fillReport(report, reportParams, beanCollectionDataSource);
-            byte[] pdfByteArray = JasperExportManager.exportReportToPdf(jasperPrint);
-
-            InputStream stream = new ByteArrayInputStream(pdfByteArray);
-            pdfFile = new DefaultStreamedContent(stream, "application/pdf", "deliveryNote.pdf");
-        } catch (Exception e) {
-            e.printStackTrace();
+            report = JasperCompileManager.compileReport(template);
+            outputByteArray = JasperRunManager.runReportToPdf(report, params, datasource);
+            outputStream = response.getOutputStream();
+            outputStream.write(outputByteArray);
+            outputStream.flush();
+        } catch (JRException e) {
+            uiLogger.error("无法生成报表", e);
         }
+        context.responseComplete();
     }
-
-    public void exportToPdfNoPrice() {
-        DeliveryNoteDto note = this.getOpenedObject();
-        JRBeanCollectionDataSource beanCollectionDataSource=new JRBeanCollectionDataSource(note.getItems());
-
-        ClassLoader ccl = getClass().getClassLoader(); //Thread.currentThread().getContextClassLoader();
-        InputStream reportStream = ccl.getResourceAsStream("resources/3/15/6/3/delivery/report1_no_price.jrxml");
-
-        try {
-            JasperReport report = JasperCompileManager.compileReport(reportStream);
-            Map<String, Object> reportParams = new HashMap<String, Object>();
-            reportParams.put("id", note.getId());
-            reportParams.put("createDate", note.getCreatedDate());
-            reportParams.put("arrivalDate", note.getArrivalDate());
-            reportParams.put("owner", note.getOwnerDisplayName());
-            reportParams.put("label", note.getLabel());
-            reportParams.put("customer", note.getCustomer().getDisplayName());
-            reportParams.put("description", note.getDescription());
-            reportParams.put("nativeTotal", note.getNativeTotal());
-
-            JasperPrint jasperPrint = JasperFillManager.fillReport(report, reportParams, beanCollectionDataSource);
-            byte[] pdfByteArray = JasperExportManager.exportReportToPdf(jasperPrint);
-
-            InputStream stream = new ByteArrayInputStream(pdfByteArray);
-            pdfFile = new DefaultStreamedContent(stream, "application/pdf", "deliveryNote.pdf");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public StreamedContent getPdfFile() {
-        return pdfFile;
-    }
-
 
     /*************************************************************************
      * Section: MBeans
@@ -211,7 +234,7 @@ public class DeliveryNoteAdminBean
             if (!overloadPartsOfDelivery.isEmpty()) {
                 StringBuilder message = new StringBuilder();
                 for (Entry<MakeOrderItem, BigDecimal> entry : overloadPartsOfDelivery.entrySet()) {
-                    if(entry.getKey().getMaterial() != null)
+                    if (entry.getKey().getMaterial() != null)
                         message.append(entry.getKey().getMaterial().getLabel());
                     message.append("[");
                     message.append(entry.getKey().getExternalProductName());
--- sem/biz1/sem-makebiz/src/main/java/com/bee32/sem/makebiz/web/MakeOrderAdminBean.java
+++ sem/biz1/sem-makebiz/src/main/java/com/bee32/sem/makebiz/web/MakeOrderAdminBean.java
@@ -1,24 +1,31 @@
 package com.bee32.sem.makebiz.web;
 
-import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.faces.context.FacesContext;
+import javax.free.UnexpectedException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+
+import net.sf.jasperreports.engine.JRDataSource;
+import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JRExporterParameter;
 import net.sf.jasperreports.engine.JasperCompileManager;
-import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.JasperRunManager;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import net.sf.jasperreports.engine.export.JRCsvExporter;
 import net.sf.jasperreports.engine.export.JRXlsExporterParameter;
 
 import org.apache.commons.lang.StringUtils;
-import org.primefaces.model.DefaultStreamedContent;
-import org.primefaces.model.StreamedContent;
 
 import com.bee32.plover.criteria.hibernate.Equals;
 import com.bee32.plover.orm.annotation.ForEntity;
@@ -37,8 +44,8 @@ public class MakeOrderAdminBean
         extends ScrollEntityViewBean {
 
     private static final long serialVersionUID = 1L;
-
-    StreamedContent exportFile;
+    private static final Mime PDF = Mimes.application_pdf;
+    private static final Mime CSV = Mimes.text_csv;
 
     public MakeOrderAdminBean() {
         super(MakeOrder.class, MakeOrderDto.class, MakeOrderDto.ITEM_ATTRIBUTES);
@@ -47,112 +54,121 @@ public class MakeOrderAdminBean
     public void setChanceToApply(ChanceDto chance) {
         MakeOrderDto makeOrder = getOpenedObject();
 
-//        MakeOrder _checkSameChanceOrder = DATA(MakeOrder.class).getFirst(
-//                new Equals("chance.id", chance.getId()));
-//        if (_checkSameChanceOrder != null && !_checkSameChanceOrder.getId().equals(makeOrder.getId())) {
-//            uiLogger.error("此机会已经对应的定单!");
-//            return;
-//        }
+        MakeOrder _checkSameChanceOrder = DATA(MakeOrder.class).getFirst(new Equals("chance.id", chance.getId()));
+        if (_checkSameChanceOrder != null && !_checkSameChanceOrder.getId().equals(makeOrder.getId())) {
+            uiLogger.error("此机会已经对应的定单!");
+            return;
+        }
 
         BEAN(MakebizService.class).chanceApplyToMakeOrder(chance, makeOrder);
     }
 
     public void exportToPdf() {
         MakeOrderDto makeOrder = this.getOpenedObject();
-        JRBeanCollectionDataSource beanCollectionDataSource=new JRBeanCollectionDataSource(makeOrder.getItems());
+        JRBeanCollectionDataSource datasource = new JRBeanCollectionDataSource(makeOrder.getItems());
 
-        ClassLoader ccl = getClass().getClassLoader(); //Thread.currentThread().getContextClassLoader();
-        InputStream reportStream = ccl.getResourceAsStream("resources/3/15/6/3/order/report1.jrxml");
+        Map<String, Object> reportParams = new HashMap<String, Object>();
+        reportParams.put("id", makeOrder.getId());
+        reportParams.put("createDate", makeOrder.getCreatedDate());
+        reportParams.put("owner", makeOrder.getOwnerDisplayName());
+        reportParams.put("label", makeOrder.getLabel());
+        reportParams.put("customer", makeOrder.getCustomer().getDisplayName());
+        reportParams.put("description", makeOrder.getDescription());
 
-        try {
-            JasperReport report = JasperCompileManager.compileReport(reportStream);
-            Map<String, Object> reportParams = new HashMap<String, Object>();
-            reportParams.put("id", makeOrder.getId());
-            reportParams.put("createDate", makeOrder.getCreatedDate());
-            reportParams.put("owner", makeOrder.getOwnerDisplayName());
-            reportParams.put("label", makeOrder.getLabel());
-            reportParams.put("customer", makeOrder.getCustomer().getDisplayName());
-            reportParams.put("description", makeOrder.getDescription());
-
-            JasperPrint jasperPrint = JasperFillManager.fillReport(report, reportParams, beanCollectionDataSource);
-            byte[] pdfByteArray = JasperExportManager.exportReportToPdf(jasperPrint);
-
-            InputStream stream = new ByteArrayInputStream(pdfByteArray);
-            exportFile = new DefaultStreamedContent(stream, "application/pdf", "order.pdf");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        InputStream report = getClass().getClassLoader().getResourceAsStream("resources/3/15/6/3/order/report1.jrxml");
+        doExport(report, reportParams, datasource, "order.pdf", PDF);
     }
 
     public void exportToPdfOuter() {
         MakeOrderDto makeOrder = this.getOpenedObject();
-        JRBeanCollectionDataSource beanCollectionDataSource=new JRBeanCollectionDataSource(makeOrder.getItems());
+        JRBeanCollectionDataSource datasource = new JRBeanCollectionDataSource(makeOrder.getItems());
+        InputStream report = getClass().getClassLoader().getResourceAsStream("resources/3/15/6/3/order/report2.jrxml");
 
-        ClassLoader ccl = getClass().getClassLoader(); //Thread.currentThread().getContextClassLoader();
-        InputStream reportStream = ccl.getResourceAsStream("resources/3/15/6/3/order/report2.jrxml");
+        Map<String, Object> params = new HashMap<String, Object>();
+        params.put("id", makeOrder.getId());
+        params.put("createDate", makeOrder.getCreatedDate());
+        params.put("owner", makeOrder.getOwnerDisplayName());
+        params.put("label", makeOrder.getLabel());
+        params.put("customer", makeOrder.getCustomer().getDisplayName());
+        params.put("description", makeOrder.getDescription());
 
-        try {
-            JasperReport report = JasperCompileManager.compileReport(reportStream);
-            Map<String, Object> reportParams = new HashMap<String, Object>();
-            reportParams.put("id", makeOrder.getId());
-            reportParams.put("createDate", makeOrder.getCreatedDate());
-            reportParams.put("owner", makeOrder.getOwnerDisplayName());
-            reportParams.put("label", makeOrder.getLabel());
-            reportParams.put("customer", makeOrder.getCustomer().getDisplayName());
-            reportParams.put("description", makeOrder.getDescription());
+        doExport(report, params, datasource, "order.pdf", PDF);
 
-            JasperPrint jasperPrint = JasperFillManager.fillReport(report, reportParams, beanCollectionDataSource);
-            byte[] pdfByteArray = JasperExportManager.exportReportToPdf(jasperPrint);
-
-            InputStream stream = new ByteArrayInputStream(pdfByteArray);
-            exportFile = new DefaultStreamedContent(stream, "application/pdf", "order.pdf");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 
     public void exportToCsv() {
         MakeOrderDto makeOrder = this.getOpenedObject();
-        JRBeanCollectionDataSource beanCollectionDataSource=new JRBeanCollectionDataSource(makeOrder.getItems());
+        JRBeanCollectionDataSource datasource = new JRBeanCollectionDataSource(makeOrder.getItems());
+        InputStream report = getClass().getClassLoader().getResourceAsStream("resources/3/15/6/3/order/report1.jrxml");
 
-        ClassLoader ccl = getClass().getClassLoader(); //Thread.currentThread().getContextClassLoader();
-        InputStream reportStream = ccl.getResourceAsStream("resources/3/15/6/3/order/report1.jrxml");
+        Map<String, Object> params = new HashMap<String, Object>();
+        params.put("id", makeOrder.getId());
+        params.put("createDate", makeOrder.getCreatedDate());
+        params.put("owner", makeOrder.getOwnerDisplayName());
+        params.put("label", makeOrder.getLabel());
+        params.put("customer", makeOrder.getCustomer().getDisplayName());
+        params.put("description", makeOrder.getDescription());
 
-        try {
-            JasperReport report = JasperCompileManager.compileReport(reportStream);
-            Map<String, Object> reportParams = new HashMap<String, Object>();
-            reportParams.put("id", makeOrder.getId());
-            reportParams.put("createDate", makeOrder.getCreatedDate());
-            reportParams.put("owner", makeOrder.getOwnerDisplayName());
-            reportParams.put("label", makeOrder.getLabel());
-            reportParams.put("customer", makeOrder.getCustomer().getDisplayName());
-            reportParams.put("description", makeOrder.getDescription());
-
-            JasperPrint jasperPrint = JasperFillManager.fillReport(report, reportParams, beanCollectionDataSource);
-
-
-            JRCsvExporter exporter = new JRCsvExporter();
-            ByteArrayOutputStream csvReport = new ByteArrayOutputStream();
-
-            exporter.setParameter(JRExporterParameter.JASPER_PRINT,jasperPrint);
-            exporter.setParameter(JRExporterParameter.CHARACTER_ENCODING, "GBK");
-            exporter.setParameter(JRExporterParameter.OUTPUT_STREAM,csvReport);
-            exporter.setParameter(JRXlsExporterParameter.IS_REMOVE_EMPTY_SPACE_BETWEEN_ROWS,Boolean.TRUE);
-            exporter.setParameter(JRXlsExporterParameter.IS_ONE_PAGE_PER_SHEET,Boolean.FALSE);
-            exporter.setParameter(JRXlsExporterParameter.IS_WHITE_PAGE_BACKGROUND,Boolean.FALSE);
-            exporter.exportReport();
-
-            byte[] xlsByteArray = csvReport.toByteArray();
-
-            InputStream stream = new ByteArrayInputStream(xlsByteArray);
-            exportFile = new DefaultStreamedContent(stream, "application/csv", "order.csv");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        doExport(report, params, datasource, "order.csv", CSV);
     }
 
-    public StreamedContent getExportFile() {
-        return exportFile;
+    void doExport(InputStream template, Map<String, Object> params, JRDataSource datasource, String fileName,
+            Mime contentType)
+            throws IOException {
+        FacesContext context = FacesContext.getCurrentInstance();
+
+        byte[] outputByteArray = null;
+        JasperReport report;
+        ServletOutputStream outputStream;
+        HttpServletResponse response;
+
+        response = (HttpServletResponse) context.getExternalContext().getResponse();
+
+        /**
+         * @see RFC 5987 2.3
+         */
+        String encodedFilename;
+        try {
+            encodedFilename = URLEncoder.encode(fileName, "utf-8");
+            System.out.println(encodedFilename);
+        } catch (UnsupportedEncodingException e) {
+            throw new UnexpectedException(e);
+        }
+
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType(contentType.getContentType());
+        response.setHeader("Content-Disposition",
+                "attachment; filename*=UTF-8''" + encodedFilename + "." + contentType.getPreferredExtension());
+
+        try {
+            report = JasperCompileManager.compileReport(template);
+
+            if (PDF.equals(contentType)) {
+                outputByteArray = JasperRunManager.runReportToPdf(report, params, datasource);
+            }
+            if (CSV.equals(contentType)) {
+                JasperPrint print = JasperFillManager.fillReport(report, params, datasource);
+                JRCsvExporter exporter = new JRCsvExporter();
+                ByteArrayOutputStream csvReport = new ByteArrayOutputStream();
+
+                exporter.setParameter(JRExporterParameter.JASPER_PRINT, print);
+                exporter.setParameter(JRExporterParameter.CHARACTER_ENCODING, "GBK");
+                exporter.setParameter(JRExporterParameter.OUTPUT_STREAM, csvReport);
+                exporter.setParameter(JRXlsExporterParameter.IS_REMOVE_EMPTY_SPACE_BETWEEN_ROWS, true);
+                exporter.setParameter(JRXlsExporterParameter.IS_ONE_PAGE_PER_SHEET, false);
+                exporter.setParameter(JRXlsExporterParameter.IS_WHITE_PAGE_BACKGROUND, false);
+                exporter.exportReport();
+
+                outputByteArray = csvReport.toByteArray();
+            }
+
+            outputStream = response.getOutputStream();
+            outputStream.write(outputByteArray);
+            outputStream.flush();
+        } catch (JRException e) {
+            uiLogger.error("无法生成报表", e);
+        }
+        context.responseComplete();
     }
 
     /*************************************************************************
