0a334636c0b7f9047ceea41aa3e12ccb05e3f20e
Lenik <xjl@99jsj.com>
2013-01-25 16:05:24 +0800

    sem-workflow: Deprecated. See sem-process.

---
+++ sem/process/sem-workflow/src/main/java/com/bee32/sem/workflow/ProcessAction.java
@@ -0,0 +1,17 @@
+package com.bee32.sem.workflow;
+
+import com.bee32.plover.ox1.color.UIEntityAuto;
+
+/**
+ * BPMN: Biznis process model notation.
+ *
+ * BPEL: Biznis process execution language.
+ */
+public class ProcessAction
+        extends UIEntityAuto<Long> {
+
+    private static final long serialVersionUID = 1L;
+
+    String message;
+
+}
