e395ac96a7eb4190f51f79c5a4309ee158c739c4
Lenik <xjl@99jsj.com>
2013-01-25 15:58:18 +0800

    sem-mail: Removed unused Spring MVC Controllers.

--- sem/base/sem-mail/src/main/java/com/bee32/sem/mail/dao/MailDao.java
+++
@@ -1,9 +0,0 @@
-package com.bee32.sem.mail.dao;
-
-import com.bee32.plover.orm.entity.EntityDao;
-import com.bee32.sem.mail.entity.Mail;
-
-public class MailDao
-        extends EntityDao<Mail, Long> {
-
-}
