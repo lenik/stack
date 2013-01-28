81b5fbac43476ae9dd5c6b27dcf30fdf8eba36ca
jack <superjwl@gmail.com>
2013-01-28 15:52:03 +0800

    机会阶段只保留 INIT,并把INIT的文字 从“初始化”改为“初步信息采集”

--- sem/biz1/sem-chance/src/main/java/com/bee32/sem/chance/entity/ChanceStages.java
+++ sem/biz1/sem-chance/src/main/java/com/bee32/sem/chance/entity/ChanceStages.java
@@ -5,12 +5,6 @@ import com.bee32.plover.orm.sample.StandardSamples;
 public class ChanceStages
         extends StandardSamples {
 
-    public final ChanceStage INIT = new ChanceStage(10, "INIT", "初始化");
-    public final ChanceStage LAUNCHED = new ChanceStage(20, "LAUN", "初步沟通");
-    public final ChanceStage MEETING = new ChanceStage(30, "MEET", "交涉中");
-    public final ChanceStage QUOTED = new ChanceStage(40, "QUOT", "已经报价");
-    public final ChanceStage PAYMENT = new ChanceStage(50, "PAYM", "合同付款洽谈");
-    public final ChanceStage DONE = new ChanceStage(100, "DONE", "合同签订");
-    public final ChanceStage TERMINATED = new ChanceStage(110, "TERM", "终止");
+    public final ChanceStage INIT = new ChanceStage(10, "INIT", "初步信息采集");
 
 }
