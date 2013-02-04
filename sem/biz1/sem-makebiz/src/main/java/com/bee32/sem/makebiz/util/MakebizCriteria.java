9ff4190220e4d1310cfacecb9721c2a32862846b
ztf <ztf@bee32.com>
2013-02-04 17:01:40 +0800

    新建凭证时，会计科目不是末级不能保存。

--- sem/biz1/sem-makebiz/src/main/java/com/bee32/sem/makebiz/util/MakebizCriteria.java
+++ sem/biz1/sem-makebiz/src/main/java/com/bee32/sem/makebiz/util/MakebizCriteria.java
@@ -1,5 +1,7 @@
 package com.bee32.sem.makebiz.util;
 
+import java.util.Date;
+
 import org.hibernate.criterion.CriteriaSpecification;
 import org.hibernate.criterion.MatchMode;
 
@@ -14,16 +16,27 @@ public class MakebizCriteria
         extends CriteriaSpec {
 
     @LeftHand(MakeOrder.class)
-    public static ICriteriaElement customerLike(String pattern){
-        return compose(
-                alias("customer", "customer"),//
-                likeIgnoreCase("customer.label", pattern,
-                        MatchMode.ANYWHERE));
+    public static ICriteriaElement customerLike(String pattern) {
+        return compose(alias("customer", "customer"),//
+                likeIgnoreCase("customer.label", pattern, MatchMode.ANYWHERE));
     }
 
     @LeftHand(DeliveryNote.class)
     public static ICriteriaElement correspondingTicket(Long ticketId) {
         return compose(alias("ticket", "ticket", CriteriaSpecification.LEFT_JOIN), //
                 new Equals("ticket.id", ticketId));
+    }
+
+    public static ICriteriaElement existingMaterialCheck(String label, String modelSpec) {
+        return and(equals("label", label), equals("modelSpec", modelSpec));
+    }
+
+    public static ICriteriaElement existingPartCheck(String label, String modelSpec) {
+        return compose(alias("target", "material"), equals("material.label", label),
+                equals("material.modelSpec", modelSpec));
+    }
+
+    public static ICriteriaElement dateRangeRestriction(String propertyName, Date start, Date end) {
+        return and(greaterOrEquals(propertyName, start), lessThan(propertyName, end));
     }
 }
