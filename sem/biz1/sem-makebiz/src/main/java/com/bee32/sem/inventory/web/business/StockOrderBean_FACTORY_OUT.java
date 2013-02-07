e2d36e5d496ec803eb8131e617477fe4ec4628af
jack <superjwl@gmail.com>
2013-02-07 17:01:13 +0800

    生产出库继续中

--- sem/biz1/sem-makebiz/src/main/java/com/bee32/sem/inventory/web/business/StockOrderBean_FACTORY_OUT.java
+++ sem/biz1/sem-makebiz/src/main/java/com/bee32/sem/inventory/web/business/StockOrderBean_FACTORY_OUT.java
@@ -2,21 +2,17 @@ package com.bee32.sem.inventory.web.business;
 
 import com.bee32.plover.orm.annotation.ForEntity;
 import com.bee32.plover.orm.annotation.TypeParameter;
+import com.bee32.sem.inventory.dto.StockOrderDto;
 import com.bee32.sem.inventory.entity.StockOrder;
 import com.bee32.sem.inventory.entity.StockOrderSubject;
 import com.bee32.sem.inventory.util.StockJobStepping;
 import com.bee32.sem.makebiz.dto.MakeStepItemDto;
-import com.bee32.sem.makebiz.entity.DeliveryNote;
-import com.bee32.sem.makebiz.entity.MakeOrder;
-import com.bee32.sem.misc.UnmarshalMap;
 
 @ForEntity(value = StockOrder.class, parameters = @TypeParameter(name = "_subject", value = "TKFO"))
 public class StockOrderBean_FACTORY_OUT
         extends AbstractStockOrderBean {
 
     private static final long serialVersionUID = 1L;
-
-    MakeStepItemDto setpItem;
 
     public StockOrderBean_FACTORY_OUT() {
         this.subject = StockOrderSubject.FACTORY_OUT;
@@ -27,17 +23,15 @@ public class StockOrderBean_FACTORY_OUT
         return false;
     }
 
-    public void setMakeStepItem(MakeStepItemDto stepItem) {
-        this.getJob().setOpenedObject(stepItem);
-    }
+    public void setStepItem(MakeStepItemDto stepItem) {
+        StockOrderDto stockOrder = this.getOpenedObject();
+        stockOrder.setJob(stepItem);
 
-    @Override
-    protected boolean preUpdate(UnmarshalMap uMap)
-            throws Exception {
-        for (StockOrder _order : uMap.<StockOrder> entitySet()) {
-            //_order.setJob(this.getJob().getOpenedObject());
+        //如果生产数据已经有对应的出库单，则进行提示
+        stepItem = reload(stepItem, MakeStepItemDto.ORDERS);
+        if (stepItem.getStockOrders().size()>0) {
+            uiLogger.info("此生产数据已经有对应的生产出库单，请注意！");
         }
-        return true;
     }
 
 
