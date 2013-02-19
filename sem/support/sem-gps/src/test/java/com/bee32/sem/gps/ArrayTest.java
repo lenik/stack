7bb58ea603003bb4a4cbebbabdf9396a40430f11
ztf <ztf@bee32.com>
2013-02-19 10:54:39 +0800

    gps tracking: get started.

---
+++ sem/support/sem-gps/src/test/java/com/bee32/sem/gps/ArrayTest.java
@@ -0,0 +1,6 @@
+package com.bee32.sem.gps;
+
+
+public class ArrayTest {
+
+}
---
+++ sem/support/sem-gps/src/test/java/com/bee32/sem/gps/BundlePathTest.java
@@ -0,0 +1,12 @@
+package com.bee32.sem.gps;
+
+import org.junit.Assert;
+
+public class BundlePathTest
+        extends Assert {
+
+    public static void main(String[] args)
+            throws ClassNotFoundException {
+
+    }
+}
