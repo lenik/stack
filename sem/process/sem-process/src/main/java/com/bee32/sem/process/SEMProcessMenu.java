bc2a270ec4fb41f2fa3a8f15fb869aab61a9d20d
Lenik <xjl@99jsj.com>
2013-02-20 16:36:43 +0800

    NLS updated.

--- sem/process/sem-process/src/main/java/com/bee32/sem/process/SEMProcessMenu.java
+++ sem/process/sem-process/src/main/java/com/bee32/sem/process/SEMProcessMenu.java
@@ -11,12 +11,20 @@ public class SEMProcessMenu
     static Location __ = WEB_APP.join(SEMProcessModule.PREFIX + "/");
     SEMFrameMenu _frame_ = require(SEMFrameMenu.class);
 
+    public MenuNode WORKFLOW = menu(_frame_.START, 30, "WORKFLOW");
+    MenuNode inbox = entry(WORKFLOW, 10, "inbox", __.join("task/inbox"));
+    MenuNode my = entry(WORKFLOW, 20, "my", __.join("task/my"));
+    MenuNode queued = entry(WORKFLOW, 30, "queued", __.join("task/queued"));
+    MenuNode archived = entry(WORKFLOW, 40, "archived", __.join("task/archived"));
+    // MenuNode __1 = _separator_(WORKFLOW, 100);
+    // MenuNode processModel= entry(WORKFLOW, 110, "processModel", __.join("process/model"));
+
     public MenuNode CORE = menu(_frame_.PROCESS, 10, "CORE");
     public MenuNode VERIFY_POLICY = menu(CORE, 10, "VERIFY_POLICY");
 
-    MenuNode preference = entry(CORE, 20, "preference", __.join("pref/index-rich.jsf"));
-    MenuNode list = entry(VERIFY_POLICY, 1, "list", __.join("v1/index-rich.jsf"));
-    MenuNode level = entry(VERIFY_POLICY, 2, "level", __.join("v1x/index-rich.jsf"));
+    MenuNode preference = entry(CORE, 20, "preference", __.join("pref"));
+    MenuNode list = entry(VERIFY_POLICY, 1, "list", __.join("v1"));
+    MenuNode level = entry(VERIFY_POLICY, 2, "level", __.join("v1x"));
     // MenuNode p2next = entry(verifyPolicy, 3, "p2next", __.join("p2next/index.do"));
 
 }
