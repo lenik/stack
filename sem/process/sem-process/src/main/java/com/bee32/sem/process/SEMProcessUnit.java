42b7154fb39830eae2c306476a5bd34ecad8ccbc
Lenik <xjl@99jsj.com>
2013-05-28 18:08:17 +0800

    sem-process: Added entity state unit.

--- sem/process/sem-process/src/main/java/com/bee32/sem/process/SEMProcessUnit.java
+++ sem/process/sem-process/src/main/java/com/bee32/sem/process/SEMProcessUnit.java
@@ -3,6 +3,8 @@ package com.bee32.sem.process;
 import com.bee32.plover.orm.unit.ImportUnit;
 import com.bee32.plover.orm.unit.PersistenceUnit;
 import com.bee32.sem.event.SEMEventUnit;
+import com.bee32.sem.process.state.EntityStateDef;
+import com.bee32.sem.process.state.EntityStateOp;
 import com.bee32.sem.process.verify.builtin.PassStep;
 import com.bee32.sem.process.verify.builtin.PassToNextPolicy;
 import com.bee32.sem.process.verify.builtin.SingleVerifierLevel;
@@ -16,6 +18,9 @@ public class SEMProcessUnit
 
     @Override
     protected void preamble() {
+        add(EntityStateDef.class);
+        add(EntityStateOp.class);
+
         add(VerifyPolicyPref.class);
         add(SingleVerifierPolicy.class);
         add(SingleVerifierRankedPolicy.class, SingleVerifierLevel.class);
