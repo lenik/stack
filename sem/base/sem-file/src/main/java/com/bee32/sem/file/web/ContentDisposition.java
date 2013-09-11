a6ebd0c22c8d44bfc5f9be196601f4061f9e6c8c
Lenik <xjl@99jsj.com>
2013-09-11 18:05:01 +0800

    sem-file: Add filename to inline content, too.

--- sem/base/sem-file/src/main/java/com/bee32/sem/file/web/ContentDisposition.java
+++ sem/base/sem-file/src/main/java/com/bee32/sem/file/web/ContentDisposition.java
@@ -8,29 +8,28 @@ public class ContentDisposition {
     public static String format(String filename, boolean attachment, boolean inline) {
         String contentDisposition = null;
 
-        if (attachment) {
+        if (attachment)
             contentDisposition = "attachment";
-            if (filename != null) {
-                /**
-                 * @see RFC 5987 2.3
-                 */
-                String encodedFilename;
-                try {
-                    encodedFilename = URLEncoder.encode(filename, "utf-8");
-                } catch (UnsupportedEncodingException e) {
-                    throw new RuntimeException(e.getMessage(), e);
-                }
-
-                // This is for IE8:
-                // contentDisposition += "; filename=" + encodedFilename;
-
-                // This is for Firefox and rest.
-                contentDisposition += "; filename*=UTF-8''" + encodedFilename;
-            }
-        }
-
-        if (inline)
+        else if (inline)
             contentDisposition = "inline";
+
+        if (filename != null) {
+            /**
+             * @see RFC 5987 2.3
+             */
+            String encodedFilename;
+            try {
+                encodedFilename = URLEncoder.encode(filename, "utf-8");
+            } catch (UnsupportedEncodingException e) {
+                throw new RuntimeException(e.getMessage(), e);
+            }
+
+            // This is for IE8:
+            // contentDisposition += "; filename=" + encodedFilename;
+
+            // This is for Firefox and rest.
+            contentDisposition += "; filename*=UTF-8''" + encodedFilename;
+        }
 
         return contentDisposition;
     }
