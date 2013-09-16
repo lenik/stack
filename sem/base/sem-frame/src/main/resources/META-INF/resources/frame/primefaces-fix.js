4e36c08fe4d10d9100e949d60c9eac278ccc63a7
Lenik <xjl@99jsj.com>
2013-09-16 15:47:50 +0800

    sem-frame: Removed p:tab auto refresh from primefaces-fix. p:dataTable works fine in the latest p:tab.

--- sem/base/sem-frame/src/main/resources/META-INF/resources/frame/primefaces-fix.js
+++ sem/base/sem-frame/src/main/resources/META-INF/resources/frame/primefaces-fix.js
@@ -27,27 +27,4 @@
         });
     };
 
-    var _showTab = PrimeFaces.widget.TabView.prototype.show;
-    PrimeFaces.widget.TabView.prototype.show = function(newPanel) {
-        _showTab.apply(this, arguments);
-
-        var datatable = $(newPanel).find(".ui-datatable");
-        if (datatable.length == 0)
-            return;
-        datatable = datatable[0];
-
-        var form = $(datatable).parents("form");
-        if (form.length == 0)
-            return;
-        form = form[0];
-
-        PrimeFaces.ajax.AjaxRequest({
-            formId : form.id,
-            source : this.id,
-            process : "@all",
-            update : datatable.id
-        // there may >1 datatables.
-        });
-    };
-
 })(jQuery);
