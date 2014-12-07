a9ff8d8241b3022f7fa2ba498f454a019b16c29e
Lenik <xjl@99jsj.com>
2014-12-07 21:03:14 +0800

    sem cleanup.

--- sem/column-valcnt-gensql.sql
+++
@@ -1,7 +0,0 @@
-SELECT 
-  concat('select count(distinct "', column_name, '") "CNT_', column_name, '" ',
-    'from "', table_name, '"; ')
-  FROM 
-    information_schema.columns
-    where table_schema='public';
-
--- sem/table0-list.sql
+++
@@ -1,7 +0,0 @@
-vacuum;
-
-SELECT relname FROM pg_catalog.pg_class WHERE relpages = 0 
-AND NOT relisshared AND NOT relhassubclass AND relkind = 'r' 
-AND relnamespace <> 
-    (SELECT oid FROM pg_namespace WHERE nspname = 'pg_catalog'); 
-
